5e963e1fe267470a25ee6ecd645f689019c0fa05
burton <burton@spinn3r.com>
2018-06-15 17:39:11 -0700

    cancel zoom for now

--- web/dist/electron-bundle.js
+++ web/dist/electron-bundle.js
@@ -33097,6 +33097,8 @@ var HTMLViewer = function (_Viewer) {
 
             $(document).ready(function () {
 
+                this._captureBrowserZoom();
+
                 this.loadContentIFrame();
 
                 new IFrameWatcher(this.content, function () {
@@ -33110,6 +33112,30 @@ var HTMLViewer = function (_Viewer) {
                     this.startHandlingZoom();
                 }.bind(this)).start();
             }.bind(this));
+        }
+    }, {
+        key: "_captureBrowserZoom",
+        value: function _captureBrowserZoom() {
+
+            $(document).keydown(function (event) {
+                if (event.ctrlKey === true && (event.which === '61' || event.which === '107' || event.which === '173' || event.which === '109' || event.which === '187' || event.which === '189')) {
+
+                    console.log("Browser zoom detected. Preventing.");
+                    event.preventDefault();
+                }
+                // 107 Num Key  +
+                // 109 Num Key  -
+                // 173 Min Key  hyphen/underscor Hey
+                // 61 Plus key  +/= key
+            });
+
+            $(window).bind('mousewheel DOMMouseScroll', function (event) {
+                if (event.ctrlKey === true) {
+
+                    console.log("Browser zoom detected. Preventing.");
+                    event.preventDefault();
+                }
+            });
         }
     }, {
         key: "startHandlingZoom",
--- web/js/viewer/html/HTMLViewer.js
+++ web/js/viewer/html/HTMLViewer.js
@@ -21,6 +21,8 @@ class HTMLViewer extends Viewer {
 
         $(document).ready( function() {
 
+            this._captureBrowserZoom();
+
             this.loadContentIFrame();
 
             new IFrameWatcher(this.content, function () {
@@ -37,6 +39,36 @@ class HTMLViewer extends Viewer {
 
         }.bind(this));
 
+    }
+
+    _captureBrowserZoom() {
+
+        $(document).keydown(function(event) {
+            if (event.ctrlKey===true && (event.which === '61' ||
+                                         event.which === '107' ||
+                                         event.which === '173' ||
+                                         event.which === '109'  ||
+                                         event.which === '187'  ||
+                                         event.which === '189'  ) ) {
+
+                console.log("Browser zoom detected. Preventing.");
+                event.preventDefault();
+
+            }
+            // 107 Num Key  +
+            // 109 Num Key  -
+            // 173 Min Key  hyphen/underscor Hey
+            // 61 Plus key  +/= key
+        });
+
+        $(window).bind('mousewheel DOMMouseScroll', function (event) {
+            if (event.ctrlKey === true) {
+
+                console.log("Browser zoom detected. Preventing.");
+                event.preventDefault();
+
+            }
+        });
     }
 
     startHandlingZoom() {
