9b7f01526fc1b5ef76605a6c99efb9ebcfffa25a
burton <burton@spinn3r.com>
2018-06-22 22:12:23 -0700

    answering the requests works...

--- web/js/backend/proxyserver/ProxyServerTest.js
+++ web/js/backend/proxyserver/ProxyServerTest.js
@@ -1,15 +1,19 @@
 const http = require('http');
 const fs = require('fs');
 const assert = require('assert');
+const url = require('url');
 
 const {CacheRegistry} = require('./CacheRegistry');
 const {ProxyServer} = require('./ProxyServer');
 const {ProxyServerConfig} = require('./ProxyServerConfig');
 const {Hashcodes} = require('../../Hashcodes');
 const {assertJSON} = require("../../test/Assertions");
+const {Http} = require("../../util/Http");
 
+var HttpProxyAgent = require('http-proxy-agent');
+var HttpsProxyAgent = require('https-proxy-agent');
 
-describe('Webserver', function() {
+describe('ProxyServer', function() {
 
     describe('create', function() {
 
@@ -23,6 +27,31 @@ describe('Webserver', function() {
             proxyServer.stop();
 
         });
+
+        it("Proxy HTTP requests", async function () {
+
+            let proxyServerConfig = new ProxyServerConfig(8090);
+            let cacheRegistry = new CacheRegistry(proxyServerConfig);
+
+            let proxyServer = new ProxyServer(proxyServerConfig, cacheRegistry);
+            proxyServer.start();
+
+            // https://nodejs.org/api/http.html#http_http_request_options_callback
+
+            let options = url.parse("http://example.com");
+            options.method = "GET";
+            options.agent = new HttpProxyAgent("http://localhost:8090");
+
+            console.log("options: ", options);
+
+            let content = await Http.fetchContent(options);
+
+            assert.equal(content.toString(), "hello world");
+
+            proxyServer.stop();
+
+        });
+
 
     });
 
---
+++ web/js/util/Http.js
@@ -0,0 +1,47 @@
+const http = require("http");
+
+class Http {
+
+    /**
+     * Perform an HTTP test and get the response.
+     *
+     * https://nodejs.org/api/http.html#http_http_request_options_callback
+     *
+     * @param options
+     */
+    static async fetchContent(options) {
+
+        return new Promise(function (resolve, reject) {
+
+            http.get(options, function (response) {
+
+                if(response.statusCode !== 200) {
+                    reject(new Error("Wrong status code: " + response.statusCode));
+                }
+
+                // reject if we don't have the proper response
+
+                let data = [];
+
+                response.on('data', function(chunk) {
+                    data.push(chunk);
+                });
+
+                response.on('end', function() {
+                    //at this point data is an array of Buffers
+                    //so Buffer.concat() can make us a new Buffer
+                    //of all of them together
+                    let buffer = Buffer.concat(data);
+                    resolve(buffer);
+
+                });
+
+            })
+
+        });
+
+    }
+
+}
+
+module.exports.Http = Http;
