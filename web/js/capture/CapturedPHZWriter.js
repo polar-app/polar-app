fd0f01095997ceb38f47d2a175b93d6d428c6706
burton <burton@spinn3r.com>
2018-06-28 12:10:49 -0700

    ok.. we can convert the captured JSON now...

--- web/js/capture/CapturedPHZWriter.js
+++ web/js/capture/CapturedPHZWriter.js
@@ -1,17 +1,56 @@
 /**
  * Writes out a PHZ archive from the given captured JSON data.
  */
+const {Resource} = require("../phz/Resource");
+const {ResourceFactory} = require("../phz/ResourceFactory");
 const {PHZWriter} = require("../phz/PHZWriter");
+const {Objects} = require("../util/Objects");
+
 
 class CapturedPHZWriter {
-
 
     constructor(path) {
         this.path = path;
     }
 
-    write(captured) {
-        new PHZWriter()
+    /**
+     * Convert it to the PHZ file at the given path.
+     *
+     * @param captured
+     * @return {Promise<void>}
+     */
+    async convert(captured) {
+
+        let phzWriter = new PHZWriter(this.path);
+
+        // convert the captured to metadata...
+        let metadata = CapturedPHZWriter.toMetadata(captured);
+
+        // now work with each resource
+
+        for (let idx = 0; idx < captured.capturedDocuments.length; idx++) {
+
+            let capturedDocument = captured.capturedDocuments[idx];
+
+            let resource = ResourceFactory.create(capturedDocument.url, "text/html");
+            resource.title = capturedDocument.title;
+
+            await phzWriter.writeResource(resource, capturedDocument.content, capturedDocument.url);
+
+        }
+
+        await phzWriter.writeMetadata(metadata);
+
+        await phzWriter.close();
+
+    }
+
+    static toMetadata(captured) {
+        let metadata = Objects.duplicate(captured);
+        delete metadata.content;
+        delete metadata.capturedDocuments;
+        return metadata;
+
     }
 
 }
--- web/js/capture/CapturedPHZWriterTest.js
+++ web/js/capture/CapturedPHZWriterTest.js
@@ -1,7 +1,7 @@
 const assert = require('assert');
 const url = require('url');
 const fs = require('fs');
-const {CapturePHZWriter} = require("./CapturedPHZWriter");
+const {CapturedPHZWriter} = require("./CapturedPHZWriter");
 const {Files} = require("../util/Files");
 const {assertJSON} = require("../test/Assertions");
 const {Time} = require("../util/Time");
@@ -10,6 +10,12 @@ require("../test/TestingTime").freeze();
 
 describe('CapturedPHZWriter', function() {
 
+    it("write out captured JSON", async function () {
+
+        let capturedPHZWriter = new CapturedPHZWriter("/tmp/captured.phz");
+        await capturedPHZWriter.convert(CAPTURED);
+
+    });
 
 });
 
