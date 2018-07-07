8ae1873aa04c55d791388be3b3cf78203a766707
burton <burton@spinn3r.com>
2018-07-07 07:42:27 -0700

    fixed issue with edge restriction again

--- sandbox/drag-and-resize-pagemark/entry.js
+++ sandbox/drag-and-resize-pagemark/entry.js
@@ -170,8 +170,16 @@ function init(selector) {
         .draggable({
             restrict: {
                 restriction: "parent",
-                // elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
+                outer: 'parent',
+
+                elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
             },
+
+            restrictEdges: {
+                outer: 'parent',
+                // outer: computeRestriction,
+            },
+
         })
         .resizable({
 
