435444c816c49528269cd082e989429a43aaa4f9
burton <burton@spinn3r.com>
2018-07-07 11:49:54 -0700

    ... cleanup

--- sandbox/drag-and-resize-pagemark/entry.old.js
+++ sandbox/drag-and-resize-pagemark/entry.old.js
@@ -69,108 +69,6 @@ function calculateIntersectedPagemarks(x, y, element) {
 
 }
 
-function computeRestriction(x,y, interactionEvent) {
-
-    let element = interactionEvent.element;
-
-    if(! element) {
-        throw new Error("No element");
-    }
-
-    let parentElement = element.parentElement;
-
-    if(! parentElement) {
-        throw new Error("No parentElement");
-    }
-
-    let elementRect = element.getBoundingClientRect();
-    let parentRect = parentElement.getBoundingClientRect();
-
-    // TODO: this needs to be PAGE local.. not the entire document.
-    let pagemarks = Array.from(document.querySelectorAll(".pagemark"))
-                            .filter( current => current !== element);
-
-    let intersectedPagemarks =
-        pagemarks.filter(current => Rects.intersect(current.getBoundingClientRect(), elementRect));
-
-    let intersectedPagemarkRects =
-        intersectedPagemarks.map(current => current.getBoundingClientRect());
-
-    if(intersectedPagemarks.length > 0) {
-
-        console.log("FIXME: we intersect with N pagemarks: " + intersectedPagemarks.length);
-
-        let restrictedRect = computeBoundingRect(parentRect, {x: 10, y: 10}, intersectedPagemarkRects);
-
-        console.log("FIXME: returning restrictedRect: " + JSON.stringify(restrictedRect, null, "  "));
-
-        return restrictedRect;
-        //
-        // //return {left: 0, top: 0, right: 0, bottom: 0, width: 0, height: 0};
-        // let testRect = Objects.duplicate(parentRect);
-        // testRect.right = testRect.left;
-        // testRect.bottom = testRect.top;
-        // testRect.height = 0;
-        // testRect.width = 0;
-        // return testRect;
-
-    } else {
-        return parentRect;
-
-    }
-
-
-    //
-    // // TODO make this testable by immediately converting to rects and then
-    // // working with them directly.
-    //
-    // // FIXME: this actually DOES work to implement the restriction properly...
-    // // rect.height = rect.height - 100;
-    // // rect.bottom = rect.bottom - 100;
-    //
-    // // TODO: this needs to be PAGE local.. not the entire document.
-    // let pagemarks = document.querySelectorAll(".pagemark")
-    //                         .filter( current => current !== element);
-    //
-    // let intersectedPagemarks =
-    //     pagemarks.filter(current => Rects.intersect(current.getBoundingClientRect(), elementRect));
-    //
-    // console.log("FIXME: we intersect with N pagemarks: " + intersectedPagemarks.length);
-    //
-    // // find pagemarks that I intersect with..
-    //
-    //
-    // // if we have too many elements at the point then we should just return the
-    // // current element's bounding rect to prevent it from growing.
-    // let elementsFromPoint = document.elementsFromPoint(x,y);
-    //
-    // let filteredElements = elementsFromPoint.filter( current => current.matches(".pagemark"))
-    //
-    // // FIXME: this isn't going to work because what happens when we're on the
-    // // OTHER side of a rect!!!  plus we can SWALLOW a rect and expand past it!
-    // //
-    // // fuck.. this is actually a difficult problem.
-    //
-    // // FIXME: what if we just compute the union for box and them compute the
-    // // bounding box base on the union of the virtual rects its interacting
-    // // with.
-    //
-    // if(filteredElements.length === 0) {
-    //     console.log("Using default parent rect");
-    //     return rect;
-    // } else if (filteredElements.length === 1){
-    //     console.log("Using custom bounded rect");
-    //
-    //     return {top: 0, left: 0, width: 0, height: 0, bottom: 0, right: 0};
-    //
-    //     //return rect;
-    // } else {
-    //     log.error("Too many filtered elements found: ", filteredElements);
-    //     throw new Error("Too many filtered elements found: " + filteredElements.length)
-    // }
-
-}
-
 function updateTargetText(target) {
 
     target.textContent = JSON.stringify(Rects.fromElementStyle(target), null, "  ");
@@ -178,13 +76,6 @@ function updateTargetText(target) {
 }
 
 function init(selector) {
-
-    // FIXME: redesign this:
-    //
-    // - the restrictions should just do simple parent restrictions
-    // - all the code should be done within the moiving logic.
-
-
 
     interact(selector)
         .draggable({
@@ -245,10 +136,7 @@ function init(selector) {
             console.log("=====================")
             console.log("dragstart: event: ", event);
 
-            event.interaction.myTimestamp = new Date().toISOString();
-
             let target = event.target;
-
             event.interaction.startRect = Rects.fromElementStyle(target);
 
         })
@@ -264,66 +152,26 @@ function init(selector) {
 
             console.log("=====================")
             console.log("dragmove: event: ", event);
-            console.log("dragmove: event.interaction.myTimestamp: ", event.interaction.myTimestamp);
-            // console.log("dragmove: event.target: ", event.target);
-            // console.log("dragmove: event.restrict: ", event.restrict);
-            //
-            // // FIXME: ok.. event.dx and event.dy CAN NOT be used because it is
-            // // from the LAST position that dragmove was called...
-            // console.log(`dragmove: event.dx: ${event.dx} and event.dy: ${event.dy}`);
-            // console.log(`dragmove: event.x0: ${event.x0} and event.y0: ${event.y0}`);
-
-            console.log(`dragmove: event.clientX: ${event.clientX} and event.clientY: ${event.clientY}`);
-            console.log(`dragmove: event.clientX0: ${event.clientX0} and event.clientY0: ${event.clientY0}`);
+            console.log("dragmove: event.target: ", event.target);
+            //console.log(`dragmove: event.interaction.startCoords.page: ` + JSON.stringify(event.interaction.startCoords.page) );
 
             let target = event.target;
 
-            // we have to use the target rect because it MAY be stable and then
-            // we have to compute the delta vs the current position.
-            let targetRect = target.getBoundingClientRect();
-
-            // let deltaX = event.clientX - event.clientX0;
-            // let deltaY = event.clientY - event.clientY0;
-
-            // let deltaX = event.clientX - targetRect.left;
-            // let deltaY = event.clientY - targetRect.top;
-            //
-
+            // the change from the original position
             let delta = {
                 x: event.pageX - event.interaction.startCoords.page.x,
                 y: event.pageY - event.interaction.startCoords.page.y
-            }
+            };
 
             console.log(`dragmove: delta.x: ${delta.x} and delta.y: ${delta.y}`);
-
-            //console.log(`dragmove: event.interaction.startCoords.page: ` + JSON.stringify(event.interaction.startCoords.page) );
-
             console.log(`dragmove: testDelta: ` + JSON.stringify(delta));
 
-            //
-            // // FIXME: ahah! here is the bug... the deltaX grows vs the original position
-            // // but I'm continuing to update it!!!
-            //
             let x = event.interaction.startRect.left + delta.x;
             let y = event.interaction.startRect.top + delta.y;
 
             console.log("dragmove: x: ${x} and y: ${y}");
-            //
-            // // translate the element
-            // // target.style.webkitTransform =
-            // //     target.style.transform =
-            // //         'translate(' + x + 'px, ' + y + 'px)';
-            //
-            // target.style.left = `${x}px`;
-            // target.style.top = `${y}px`;
-            //
-            // // update the position attributes
-            // target.setAttribute('data-x', x);
-            // target.setAttribute('data-y', y);
-
 
             let intersectedPagemarks = calculateIntersectedPagemarks(x, y, event.currentTarget);
-
 
             if(intersectedPagemarks.intersected.length === 0) {
 
