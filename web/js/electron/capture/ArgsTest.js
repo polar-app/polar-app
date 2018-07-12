c8e66212e0d317fa0f142e5ea42cfad79fe3add3
burton <burton@spinn3r.com>
2018-07-11 20:36:29 -0700

    Fixed tests..

--- web/js/electron/capture/ArgsTest.js
+++ web/js/electron/capture/ArgsTest.js
@@ -1,5 +1,6 @@
 const assert = require('assert');
 const {Args} = require('./Args');
+const {assertJSON} = require("../../test/Assertions");
 
 describe('Args', function() {
 
@@ -7,17 +8,20 @@ describe('Args', function() {
 
         it("no args", function () {
 
-            assert.deepEqual(Args.parse([]), {
-                quit: true,
-                browser: 'MOBILE_GALAXY_S8_WITH_CHROME_61_WIDTH_750'
-            });
+            assertJSON(Args.parse([]), {
+                    "quit": true,
+                    "browser": "MOBILE_GALAXY_S8_WITH_CHROME_61_WIDTH_750",
+                    "profile": "headless"
+                }
+            );
 
         });
 
         it("change browser", function () {
-            assert.deepEqual(Args.parse(["--browser=TEST_BROWSER"]), {
-                quit: true,
-                browser: 'TEST_BROWSER'
+            assertJSON(Args.parse(["--browser=TEST_BROWSER"]), {
+                "browser": "TEST_BROWSER",
+                "quit": true,
+                "profile": "headless"
             });
         });
 
