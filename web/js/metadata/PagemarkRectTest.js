b543faf121d0e36f28d6b85e9223738c41044d8d
burton <burton@spinn3r.com>
2018-07-12 08:51:45 -0700

    ok.. more tests looking good.

--- web/js/metadata/PagemarkRectTest.js
+++ web/js/metadata/PagemarkRectTest.js
@@ -68,7 +68,7 @@ describe('PagemarkRect', function() {
         });
 
 
-        it("50%", function () {
+        it("50% top", function () {
 
             let pagemarkRect = new PagemarkRect({
                 top: 0,
@@ -86,6 +86,57 @@ describe('PagemarkRect', function() {
                 "bottom": 0.5,
                 "width": 1,
                 "height": 0.5
+            };
+
+            assertJSON(pagemarkRect.toFractionalRect(), expected);
+
+
+        });
+
+
+        it("50% left", function () {
+
+            let pagemarkRect = new PagemarkRect({
+                top: 0,
+                left: 0,
+                width: 50,
+                height: 100
+            });
+
+            assertJSON(pagemarkRect.toPercentage(), 50);
+
+            let expected = {
+                "left": 0,
+                "top": 0,
+                "right": 0.5,
+                "bottom": 1.0,
+                "width": 0.5,
+                "height": 1.0
+            };
+
+            assertJSON(pagemarkRect.toFractionalRect(), expected);
+
+
+        });
+
+        it("50% right", function () {
+
+            let pagemarkRect = new PagemarkRect({
+                top: 0,
+                left: 50,
+                width: 50,
+                height: 100
+            });
+
+            assertJSON(pagemarkRect.toPercentage(), 50);
+
+            let expected = {
+                "left": 0.5,
+                "top": 0,
+                "right": 1.0,
+                "bottom": 1.0,
+                "width": 0.5,
+                "height": 1.0
             };
 
             assertJSON(pagemarkRect.toFractionalRect(), expected);
