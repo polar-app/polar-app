aec0ae86918168d8fcbd2d91609616535714f910
burton <burton@spinn3r.com>
2018-07-15 13:35:28 -0700

    the Create Pagemark menu option works BUT it's viewport based...

--- web/dist/chrome-bundle.js
+++ web/dist/chrome-bundle.js
@@ -84978,7 +84978,13 @@ module.exports.VersionedObject = VersionedObject;
   !*** ./web/js/pagemarks/controller/PagemarkController.js ***!
   \***********************************************************/
 /*! no static exports found */
-/***/ (function(module, exports) {
+/***/ (function(module, exports, __webpack_require__) {
+
+const log = __webpack_require__(/*! ../../logger/Logger */ "./web/js/logger/Logger.js").create();
+const { DocFormatFactory } = __webpack_require__(/*! ../../docformat/DocFormatFactory */ "./web/js/docformat/DocFormatFactory.js");
+const { Rects } = __webpack_require__(/*! ../../Rects */ "./web/js/Rects.js");
+const { Pagemarks } = __webpack_require__(/*! ../../metadata/Pagemarks */ "./web/js/metadata/Pagemarks.js");
+const { PagemarkRects } = __webpack_require__(/*! ../../metadata/PagemarkRects */ "./web/js/metadata/PagemarkRects.js");
 
 class PagemarkController {
 
@@ -84988,15 +84994,62 @@ class PagemarkController {
      */
     constructor(model) {
         this.model = model;
+        this.docFormat = DocFormatFactory.getInstance();
     }
 
     start() {
 
-        window.addEventListener("message", this.onMessageReceived, false);
+        window.addEventListener("message", event => this.onMessageReceived(event), false);
     }
 
     onMessageReceived(event) {
-        console.log("Got message: " + event);
+
+        if (event.data && event.data.type === "create-pagemark") {
+            this.onCreatePagemark(event.data);
+        }
+    }
+
+    onCreatePagemark(data) {
+
+        // convert the point on the page to a pagemark and then save it into
+        // the model/docMeta... the view will do the rest.
+        console.log("Creating pagemark.");
+
+        // FIXME: The point is based on the viewport I think.
+
+        let elements = document.elementsFromPoint(data.point.x, data.point.y);
+
+        elements = elements.filter(element => element.matches(".page"));
+
+        if (elements.length === 1) {
+
+            let pageElement = elements[0];
+
+            log.info("Creating pagemark on pageElement: ", pageElement);
+
+            let pageNum = this.docFormat.getPageNumFromPageElement(pageElement);
+
+            let boxRect = Rects.createFromBasicRect({
+                left: data.point.x,
+                top: data.point.y,
+                width: 30,
+                height: 30
+            });
+
+            let containerRect = Rects.createFromOffset(pageElement);
+
+            let pagemarkRect = PagemarkRects.createFromPositionedRect(boxRect, containerRect);
+
+            let pagemark = Pagemarks.create({ rect: pagemarkRect });
+
+            this.model.docMeta.getPageMeta(pageNum).pagemarks[pagemark.id] = pagemark;
+
+            log.info("Using pagemarkRect: ", pagemarkRect);
+
+            // update the DocMeta with a pagemark on this page..
+        } else {
+            log.warn("No .page element clicked.");
+        }
     }
 
 }
--- web/js/pagemarks/controller/PagemarkController.js
+++ web/js/pagemarks/controller/PagemarkController.js
@@ -1,3 +1,9 @@
+const log = require("../../logger/Logger").create();
+const {DocFormatFactory} = require("../../docformat/DocFormatFactory");
+const {Rects} = require("../../Rects");
+const {Pagemarks} = require("../../metadata/Pagemarks");
+const {PagemarkRects} = require("../../metadata/PagemarkRects");
+
 class PagemarkController {
 
     /**
@@ -6,16 +12,66 @@ class PagemarkController {
      */
     constructor(model) {
         this.model = model;
+        this.docFormat = DocFormatFactory.getInstance();
     }
 
     start() {
 
-        window.addEventListener("message", this.onMessageReceived, false);
+        window.addEventListener("message", event => this.onMessageReceived(event), false);
 
     }
 
     onMessageReceived(event) {
-        console.log("Got message: " + event);
+
+        if(event.data && event.data.type === "create-pagemark") {
+            this.onCreatePagemark(event.data);
+        }
+
+    }
+
+    onCreatePagemark(data) {
+
+        // convert the point on the page to a pagemark and then save it into
+        // the model/docMeta... the view will do the rest.
+        console.log("Creating pagemark.");
+
+        // FIXME: The point is based on the viewport I think.
+
+        let elements = document.elementsFromPoint(data.point.x, data.point.y);
+
+        elements = elements.filter(element => element.matches(".page"));
+
+        if(elements.length === 1) {
+
+            let pageElement = elements[0];
+
+            log.info("Creating pagemark on pageElement: ", pageElement);
+
+            let pageNum = this.docFormat.getPageNumFromPageElement(pageElement);
+
+            let boxRect = Rects.createFromBasicRect({
+                left: data.point.x,
+                top: data.point.y,
+                width: 30,
+                height: 30
+            });
+
+            let containerRect = Rects.createFromOffset(pageElement);
+
+            let pagemarkRect = PagemarkRects.createFromPositionedRect(boxRect, containerRect);
+
+            let pagemark = Pagemarks.create({rect: pagemarkRect});
+
+            this.model.docMeta.getPageMeta(pageNum).pagemarks[pagemark.id] = pagemark;
+
+            log.info("Using pagemarkRect: ", pagemarkRect);
+
+            // update the DocMeta with a pagemark on this page..
+
+        } else {
+            log.warn("No .page element clicked.");
+        }
+
     }
 
 }
