7fa48190816c1401ed1031771e747a33f0f456a6
burton <burton@spinn3r.com>
2018-07-27 08:10:46 -0700

    deleting .js files that have been migrated to .ts

--- web/js/capture/renderer/TimeoutEvent.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class TimeoutEvent {
-    constructor(opts) {
-        Object.assign(this, opts);
-    }
-}
-exports.TimeoutEvent = TimeoutEvent;
-//# sourceMappingURL=TimeoutEvent.js.map
