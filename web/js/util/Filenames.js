d5b0f410088b37f1a40db72d6c315fe470c93993
burton <burton@spinn3r.com>
2018-08-25 09:09:02 -0700

    deleted orphan js

--- web/js/util/Filenames.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class Filenames {
-    static sanitize(filename) {
-        return filename.replace(/[^a-z0-9_]/gi, '_');
-    }
-}
-exports.Filenames = Filenames;
-//# sourceMappingURL=Filenames.js.map
