dd3295812af12d1ae4a9826773769482008ebec3
burton <burton@spinn3r.com>
2018-08-30 08:05:56 -0700

    removed orphan js

--- web/js/components/Component.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class Component {
-}
-exports.Component = Component;
-//# sourceMappingURL=Component.js.map
--- web/js/view/View.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class View {
-    constructor(model) {
-        this.model = model;
-    }
-}
-exports.View = View;
-//# sourceMappingURL=View.js.map
