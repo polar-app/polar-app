13ff7a66865f52d7ee67bf8d2bcf4861c96777aa
burton <burton@spinn3r.com>
2018-09-01 17:47:27 -0700

    the crash reporter is enabled for backtrace.io now...

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import {app} from 'electron';
+import {app, crashReporter} from 'electron';
 import {Logger} from './web/js/logger/Logger';
 import {MainApp} from './web/js/apps/main/MainApp';
 import {Cmdline} from './web/js/electron/Cmdline';
@@ -31,5 +31,12 @@ app.on('ready', async () => {
         log.info("Opening file given on the command line: " + fileArg);
         await mainAppController.handleLoadDoc(fileArg);
     }
+
+    crashReporter.start({
+        productName: "polar",
+        companyName: "polar",
+        submitURL: "https://polar.sp.backtrace.io:8443/post?format=minidump&token=70d81628b1f662962cc7afc25bc4fd0449677693809737577067ed72a78f6848",
+        uploadToServer: true
+    });
 
 });
