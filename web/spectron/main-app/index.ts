fc4f40083f441940c7223f98986c8789c595d7f0
burton <burton@spinn3r.com>
2018-09-05 15:06:18 -0700

    make sure the main browser launches.

--- web/spectron/main-app/index.ts
+++ web/spectron/main-app/index.ts
@@ -4,6 +4,10 @@ import {MemoryDatastore} from '../../js/datastore/MemoryDatastore';
 import {Logging} from '../../js/logger/Logging';
 import {MainApp} from '../../js/apps/main/MainApp';
 import BrowserWindow = Electron.BrowserWindow;
+import {BrowserWindowRegistry} from '../../js/electron/framework/BrowserWindowRegistry';
+import {wait} from 'dom-testing-library';
+import {assert} from 'chai';
+import waitForExpect from 'wait-for-expect';
 
 async function createWindow(): Promise<BrowserWindow> {
 
@@ -21,6 +25,18 @@ async function createWindow(): Promise<BrowserWindow> {
 }
 
 SpectronMain2.create({windowFactory: createWindow}).run(async state => {
+
+    await waitForExpect(() => {
+        let windows = BrowserWindowRegistry.tagged({name: 'app', value: 'repository'});
+        assert.ok(windows.length === 1);
+
+    });
+
+    // TODO: make sure the flashcard app is ready and running in the background
+
+    // TODO: switch to the main repository app and double click on a document.
+    // Might need to do this in the renderer context though as part of another
+    // test.
 
     await state.testResultWriter.write(true);
 
