7ba1866bf90a6822b245152f2a82be742b011278
burton <burton@spinn3r.com>
2018-09-15 06:35:20 -0700

    ...

--- screenshots/gen-dropshadow.sh
+++ screenshots/gen-dropshadow.sh
@@ -6,4 +6,4 @@ input=${1}
 output=${2}
 
 # try the equivalent of -shadow 100x5+0+0 where 100 is darkest and 5 is distance. The +0+0 means not to offset in any direction.
-convert ${input} -bordercolor transparent -border 15 \( +clone -background black -shadow 85x15+0+10 \) +swap -background transparent -layers merge +repage ${output}
+convert ${input} -bordercolor transparent -border 0 \( +clone -background black -shadow 85x15+0+10 \) +swap -background transparent -layers merge +repage ${output}
