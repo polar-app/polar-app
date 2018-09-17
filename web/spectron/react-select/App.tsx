4e2a3cc965b726baa760ba8b5a231136a14d8b9f
burton <burton@spinn3r.com>
2018-09-16 22:20:13 -0700

    ok.. I have a basic react-select working and this thing rocks.

---
+++ web/spectron/react-select/App.tsx
@@ -0,0 +1,41 @@
+import * as React from 'react';
+import Select from 'react-select';
+
+
+const options = [
+    { value: 'chocolate', label: 'Chocolate' },
+    { value: 'strawberry', label: 'Strawberry' },
+    { value: 'vanilla', label: 'Vanilla' }
+]
+class App<P> extends React.Component<{}, IAppState> {
+
+
+    constructor(props: P, context: any) {
+        super(props, context);
+
+    }
+
+    public render() {
+        return (
+
+            <div id="myapp">
+                hello world
+                <Select
+                    isMulti
+                    className="basic-multi-select"
+                    classNamePrefix="select"
+                    isClearable={true}
+                    options={options} />
+
+            </div>
+
+        );
+    }
+}
+
+export default App;
+
+interface IAppState {
+
+
+}
