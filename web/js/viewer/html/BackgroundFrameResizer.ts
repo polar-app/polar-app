1aa3c114aafb599d7e3208fd573c378cf3e20b5e
burton <burton@spinn3r.com>
2018-09-18 08:52:01 -0700

    removed FIXME and finished refactoring this code. Now using the FrameResizer.

--- web/js/viewer/html/BackgroundFrameResizer.ts
+++ web/js/viewer/html/BackgroundFrameResizer.ts
@@ -1,6 +1,8 @@
 import {Optional} from '../../util/ts/Optional';
 import {Styles} from '../../util/Styles';
 import {Logger} from '../../logger/Logger';
+import {Preconditions} from '../../Preconditions';
+import {FrameResizer} from './FrameResizer';
 
 const log = Logger.create();
 
@@ -23,33 +25,16 @@ export class BackgroundFrameResizer {
     // how long between polling should we wait to expand the size.
     private readonly timeoutInterval = 250;
 
-    private height: number | undefined;
-
     private resizes: number = 0;
 
-    // TODO:
-    //
-    // this may be a better way to do the resize animation frames.
-    //
-    // https://stackoverflow.com/questions/1835219/is-there-an-event-that-fires-on-changes-to-scrollheight-or-scrollwidth-in-jquery
+    private frameResizer: FrameResizer;
 
     constructor(parent: HTMLElement, iframe: HTMLIFrameElement) {
 
-        if(!parent) {
-            throw new Error("No parent");
-        }
+        this.parent = Preconditions.assertPresent(parent);
+        this.iframe = Preconditions.assertPresent(iframe);
 
-        if(!iframe) {
-            throw new Error("No iframe");
-        }
-
-        this.parent = parent;
-        this.iframe = iframe;
-
-        // the current height
-        this.height = undefined;
-
-        // FIXME: use FrameResizer
+        this.frameResizer = new FrameResizer(parent, iframe);
 
     }
 
@@ -59,7 +44,7 @@ export class BackgroundFrameResizer {
 
     private resizeParentInBackground() {
 
-        if(this.resizes > MAX_RESIZES) {
+        if (this.resizes > MAX_RESIZES) {
             log.info("Hit MAX_RESIZES: " + MAX_RESIZES);
             this.doBackgroundResize(true);
             return;
@@ -67,67 +52,22 @@ export class BackgroundFrameResizer {
             this.doBackgroundResize(false);
         }
 
-        setTimeout(this.resizeParentInBackground.bind(this), this.timeoutInterval);
+        setTimeout(() => this.resizeParentInBackground(), this.timeoutInterval);
 
     }
 
     /**
      * Perform the resize now.
      *
-     * @param final True when this is the final resize before terminating.  This
+     * @param force True when this is the final resize before terminating.  This
      * way, if we're doing any sort of caching or throttling of resize, we can
      * just force it one last time.
      */
-    private doBackgroundResize(final: boolean) {
+    private doBackgroundResize(force: boolean) {
 
         ++this.resizes;
 
-        // TODO: accidental horizontal overflow...
-        //
-        // - I can see if the CSS is done rendering.. there might still be CSS
-        //   transitions and other issues.
-        //
-        // - I could see if the CSS is loaded, and that no more images or other
-        //   resources have changed and then fix the page to that dimension.
-        //
-        // - I could back off significantly in duration if only a small percentage
-        //   of the page height has changed.  For example, if we're less than 1%
-        //   I can just wait until the final rendering.  We are often only off
-        //   by a few px.
-        //
-
-        const contentDocument = this.iframe.contentDocument;
-
-        if (! contentDocument) {
-            return;
-        }
-
-        if (! contentDocument.body) {
-            return;
-        }
-
-        const height = Styles.parsePX(Optional.of(this.iframe.style.height)
-                                        .filter( (current: any) => current !== null)
-                                        .filter( current => current !== "")
-                                        .getOrElse("0px"));
-
-        const newHeight = contentDocument.body.scrollHeight;
-
-        const delta = Math.abs(newHeight - height);
-
-        // delta as a percentage of total height.
-        const deltaPerc = 100 * (delta / height);
-
-        if (! final && deltaPerc < 5) {
-            return;
-        }
-
-        // we basically keep polling.
-        if (height !== newHeight) {
-            // log.info(`Setting new height to: ${newHeight} vs previous ${this.iframe.style.height}`);
-            this.iframe.style.height = `${newHeight}px`;
-            this.height = newHeight;
-        }
+        this.frameResizer.resize(force);
 
     }
 
