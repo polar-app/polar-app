e2932d36aa24d609158f92df2aaa9763ec8d9eaf
burton <burton@spinn3r.com>
2018-09-19 10:41:19 -0700

    removed orphan javascript

--- web/js/proxies/MutationListener.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class MutationListener {
-}
-exports.MutationListener = MutationListener;
-//# sourceMappingURL=MutationListener.js.map
--- web/js/proxies/TraceListener.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class TraceListener {
-}
-exports.TraceListener = TraceListener;
-//# sourceMappingURL=TraceListener.js.map
