5ed52faa563a4e43c0e1f1963ae061d4f51f9ca4
burton <burton@spinn3r.com>
2018-10-11 19:36:24 -0700

    have a text input button now which we can use to chagne the title of a document...

--- web/spectron/confirm-button/App.tsx
+++ web/spectron/confirm-button/App.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react';
 import {ConfirmButton} from '../../js/ui/confirm/ConfirmButton';
+import {TextInputButton} from '../../js/ui/text_input_button/TextInputButton';
+import {Dropdown, DropdownItem, DropdownMenu, DropdownToggle} from 'reactstrap';
 
 class App<P> extends React.Component<{}, IAppState> {
 
@@ -24,6 +26,64 @@ class App<P> extends React.Component<{}, IAppState> {
                                onConfirm={() => console.log('confirml')}>
                     <i className="fa fa-remove doc-button doc-button-active" />
                 </ConfirmButton>
+
+                <TextInputButton id="set-title"
+                                 title="Enter new title"
+                                 onComplete={(title) => console.log(title)}>
+
+                    Change title
+
+                </TextInputButton>
+
+
+                {/*<div className="dropdown">*/}
+                    {/*<button className="btn btn-secondary dropdown-toggle"*/}
+                            {/*type="button" id="dropdownMenuButton"*/}
+                            {/*data-toggle="dropdown" aria-haspopup="true"*/}
+                            {/*aria-expanded="false">*/}
+                        {/*Dropdown button*/}
+                    {/*</button>*/}
+
+                    {/*<div className="dropdown-menu"*/}
+                         {/*aria-labelledby="dropdownMenuButton">*/}
+                        {/*<a className="dropdown-item" href="#">Action</a>*/}
+                        {/*<a className="dropdown-item" href="#">Another action</a>*/}
+                        {/*<a className="dropdown-item" href="#">Something else*/}
+                            {/*here</a>*/}
+                    {/*</div>*/}
+                {/*</div>*/}
+                {/*<div className="btn-group">*/}
+                    {/*<button type="button" className="btn btn-danger">Action*/}
+                    {/*</button>*/}
+                    {/*<button type="button"*/}
+                            {/*className="btn btn-danger dropdown-toggle dropdown-toggle-split"*/}
+                            {/*data-toggle="dropdown" aria-haspopup="true"*/}
+                            {/*aria-expanded="false">*/}
+                        {/*<span className="sr-only">Toggle Dropdown</span>*/}
+                    {/*</button>*/}
+                    {/*<div className="dropdown-menu">*/}
+                        {/*<a className="dropdown-item" href="#">Action</a>*/}
+                        {/*<a className="dropdown-item" href="#">Another action</a>*/}
+                        {/*<a className="dropdown-item" href="#">Something else*/}
+                            {/*here</a>*/}
+                        {/*<div className="dropdown-divider"></div>*/}
+                        {/*<a className="dropdown-item" href="#">Separated link</a>*/}
+                    {/*</div>*/}
+                {/*</div>*/}
+
+
+                {/*<Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggle}>*/}
+                    {/*<DropdownToggle caret>*/}
+                        {/*Dropdown*/}
+                    {/*</DropdownToggle>*/}
+                    {/*<DropdownMenu>*/}
+                        {/*<DropdownItem header>Header</DropdownItem>*/}
+                        {/*<DropdownItem disabled>Action</DropdownItem>*/}
+                        {/*<DropdownItem>Another Action</DropdownItem>*/}
+                        {/*<DropdownItem divider />*/}
+                        {/*<DropdownItem>Another Action</DropdownItem>*/}
+                    {/*</DropdownMenu>*/}
+                {/*</Dropdown>*/}
 
             </div>
         );
