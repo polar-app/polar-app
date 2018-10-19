b70dc8f812060c955488baa7fefbdc17575fada7
burton <burton@spinn3r.com>
2018-10-19 07:10:26 -0700

    Lightbox popover so that I can pull out the lightbox functionality.

---
+++ web/js/ui/lightbox_popover/LightboxPopover.tsx
@@ -0,0 +1,57 @@
+/* eslint react/no-multi-comp: 0, react/prop-types: 0 */
+import React from 'react';
+import {Button, Form, FormGroup, Input, Label, Popover, PopoverBody} from 'reactstrap';
+import Popper from 'popper.js';
+import {Blackout} from '../../../../apps/repository/js/Blackout';
+
+/**
+ * Popover that functions like a normal popover but uses a lightbox to highlight
+ * the popover being selected.
+ */
+export class LightboxPopover extends React.Component<IProps, IState> {
+
+    private value: string = '';
+
+    constructor(props: IProps) {
+        super(props);
+
+    }
+
+    public componentWillReceiveProps(nextProps: Readonly<IProps>, nextContext: any): void {
+        if (this.props.open !== nextProps.open) {
+            Blackout.toggle(nextProps.open);
+        }
+    }
+
+    public render() {
+
+        return (
+
+            <Popover placement={this.props.placement || 'bottom'}
+                     isOpen={this.props.open}
+                     target={this.props.target}>
+
+                <PopoverBody>
+
+                    {this.props.children}
+
+                </PopoverBody>
+
+            </Popover>
+
+
+        );
+    }
+
+}
+
+interface IProps {
+    target: string;
+    open: boolean;
+    placement?: Popper.Placement;
+}
+
+interface IState {
+
+}
+
