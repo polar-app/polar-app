dd33418aa01a835aa969e808919f31ac545566d2
burton <burton@spinn3r.com>
2018-10-23 16:37:23 -0700

    ...

--- web/js/electron/capture/ArgsTest.js
+++ web/js/electron/capture/ArgsTest.js
@@ -1,32 +1,25 @@
-const assert = require('assert');
-const {Args} = require('./Args');
-const {assertJSON} = require("../../test/Assertions");
-
-describe('Args', function() {
-
-    describe('parse', function() {
-
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const Assertions_1 = require("../../test/Assertions");
+const Args_1 = require("./Args");
+describe('Args', function () {
+    describe('parse', function () {
         it("no args", function () {
-
-            assertJSON(Args.parse([]), {
-                    "quit": true,
-                    "browser": "DEFAULT",
-                    "profile": "WEBVIEW",
-                    "amp": true
-                }
-            );
-
+            Assertions_1.assertJSON(Args_1.Args.parse([]), {
+                "quit": true,
+                "browser": "DEFAULT",
+                "profile": "WEBVIEW",
+                "amp": true
+            });
         });
-
         it("change browser", function () {
-            assertJSON(Args.parse(["--browser=TEST_BROWSER"]), {
+            Assertions_1.assertJSON(Args_1.Args.parse(["--browser=TEST_BROWSER"]), {
                 "browser": "TEST_BROWSER",
                 "quit": true,
                 "profile": "WEBVIEW",
                 "amp": true
             });
         });
-
     });
-
 });
+//# sourceMappingURL=ArgsTest.js.map
