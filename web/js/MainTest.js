a235e7e91390027ad8bffe1ca80a36432d7058bb
burton <burton@spinn3r.com>
2018-10-23 09:30:04 -0700

    migrated code to typescript.

--- web/js/MainTest.js
+++ web/js/MainTest.js
@@ -1,41 +1,21 @@
-// all our unit tests...
-var chai = require("chai");
-var chaiDiff = require("chai-diff");
-
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const chai_1 = require("chai");
+const Assertions_1 = require("./test/Assertions");
 const utils = require("./utils");
-const {computeRangeBuffer} = require("./utils");
-const {DocMeta} = require("./metadata/DocMeta");
-const {DocInfo} = require("./metadata/DocInfo");
-const {PageInfo} = require("./metadata/PageInfo");
-const {PageMeta} = require("./metadata/PageMeta");
-const {DocMetas} = require("./metadata/DocMetas");
-const {MetadataSerializer} = require("./metadata/MetadataSerializer");
-const {Text} = require("./metadata/Text");
-const {Pagemark} = require("./metadata/Pagemark");
-const {Note} = require("./metadata/Note");
-const {TextHighlightRows} = require("./highlights/text/controller/TextHighlightRows.js");
-const {View} = require("./view/View");
-const {WebView} = require("./view/WebView");
-const {SyntheticClock} = require("./time/SyntheticClock");
-const {MemoryDatastore} = require("./datastore/MemoryDatastore");
-const {Model} = require("./model/Model.js");
-const {assertJSON} = require("./test/Assertions")
-
-//import * as utils from "./utils.js";
-
-var assert = chai.assert;
-var expect = chai.expect;
-
-chai.config.truncateThreshold = 0;
-chai.use(chaiDiff);
-
-// stable reference date for all tests.
+const { computeRangeBuffer } = require("./utils");
+const { TextHighlightRows } = require("./highlights/text/controller/TextHighlightRows");
 var date = new Date(Date.parse("2018-05-30T02:47:44.411Z"));
-
-describe('Test computeRectForRow for highlighting text...', function() {
-
+describe('Test computeRectForRow for highlighting text...', function () {
     it('Test with 1 entries', function () {
-
         var rectElements = [
             {
                 rect: {
@@ -49,15 +29,10 @@ describe('Test computeRectForRow for highlighting text...', function() {
                 element: null
             }
         ];
-
         var expected = { left: 0, top: 4, width: 721, height: 18, right: 721, bottom: 22 };
-
-        assert.deepEqual(TextHighlightRows.computeRectForRow(rectElements), expected);
-
+        chai_1.assert.deepEqual(TextHighlightRows.computeRectForRow(rectElements), expected);
     });
-
     it('Test with 2 entries', function () {
-
         var rects = [
             {
                 rect: {
@@ -81,28 +56,16 @@ describe('Test computeRectForRow for highlighting text...', function() {
                 },
                 element: null
             }
-
         ];
-
         var expected = { left: 0, top: 4, width: 800, height: 46, right: 800, bottom: 50 };
-
-        assert.deepEqual(TextHighlightRows.computeRectForRow(rects), expected);
-
+        chai_1.assert.deepEqual(TextHighlightRows.computeRectForRow(rects), expected);
     });
-
 });
-
-describe('Test computeRows for highlighting text...', function() {
-
+describe('Test computeRows for highlighting text...', function () {
     it('Test with no entries', function () {
-
-        assert.deepEqual([], []);
-
-
+        chai_1.assert.deepEqual([], []);
     });
-
     it('Test with 1 entries', function () {
-
         var rects = [
             {
                 rect: {
@@ -116,18 +79,12 @@ describe('Test computeRows for highlighting text...', function() {
                 element: null
             }
         ];
-
         var expected = [
-            [ { rect: {left: 0, top: 4, width: 721, height: 18, right: 721, bottom: 22 }, element: null} ]
+            [{ rect: { left: 0, top: 4, width: 721, height: 18, right: 721, bottom: 22 }, element: null }]
         ];
-
-        assert.deepEqual(TextHighlightRows.computeRows(rects), expected);
-
-
+        chai_1.assert.deepEqual(TextHighlightRows.computeRows(rects), expected);
     });
-
     it('Test with 2 entries and two rows', function () {
-
         var rectElements = [
             {
                 rect: {
@@ -152,29 +109,17 @@ describe('Test computeRows for highlighting text...', function() {
                 element: null
             }
         ];
-
         var expected = [
             [
-                { rect: { left: 0, top: 4, width: 200, height: 20, right: 200, bottom: 24}, element: null },
-                { rect: { left: 200, top: 4, width: 200, height: 20, right: 400, bottom: 24}, element: null }
+                { rect: { left: 0, top: 4, width: 200, height: 20, right: 200, bottom: 24 }, element: null },
+                { rect: { left: 200, top: 4, width: 200, height: 20, right: 400, bottom: 24 }, element: null }
             ]
         ];
-
-        assert.deepEqual(TextHighlightRows.computeRows(rectElements), expected);
-
+        chai_1.assert.deepEqual(TextHighlightRows.computeRows(rectElements), expected);
     });
-
-
-
-})
-
-
-describe('Testing for gaps in contiguous rects', function() {
-
+});
+describe('Testing for gaps in contiguous rects', function () {
     it('Test with no entries', function () {
-
-        // FIXME: break this out into a real test...
-
         var rects = [
             {
                 rect: {
@@ -219,7 +164,6 @@ describe('Testing for gaps in contiguous rects', function() {
                     "bottom": 22
                 },
                 element: null
-
             },
             {
                 rect: {
@@ -233,25 +177,17 @@ describe('Testing for gaps in contiguous rects', function() {
                 element: null
             }
         ];
-
-        assert.deepEqual(TextHighlightRows.computeContiguousRects([]), []);
-
+        chai_1.assert.deepEqual(TextHighlightRows.computeContiguousRects([]), []);
     });
-
-})
-
-describe('Testing computeContiguousRects.', function() {
-
+});
+describe('Testing computeContiguousRects.', function () {
     it('Test with no entries', function () {
-        assert.deepEqual(TextHighlightRows.computeContiguousRects([]), []);
+        chai_1.assert.deepEqual(TextHighlightRows.computeContiguousRects([]), []);
     });
-
     xit('Test with one entry', function () {
-
         let rects = [
-            { rect: {top: 10, left: 10, bottom: 50, right: 50}, element: null }
+            { rect: { top: 10, left: 10, bottom: 50, right: 50 }, element: null }
         ];
-
         let expected = [
             {
                 "rect": {
@@ -275,20 +211,14 @@ describe('Testing computeContiguousRects.', function() {
                 ]
             }
         ];
-
         let actual = TextHighlightRows.computeContiguousRects(rects);
-
-        assert.deepJSON(actual, expected);
-
+        Assertions_1.assertJSON(actual, expected);
     });
-
     xit('Test with two entries', function () {
-
         var boundingClientRects = [
-            {rect: {top: 10, left: 10, bottom: 20, right: 50}, element: null },
-            {rect: {top: 50, left: 10, bottom: 70, right: 50}, element: null }
+            { rect: { top: 10, left: 10, bottom: 20, right: 50 }, element: null },
+            { rect: { top: 50, left: 10, bottom: 70, right: 50 }, element: null }
         ];
-
         var expected = [
             {
                 "rect": {
@@ -333,251 +263,94 @@ describe('Testing computeContiguousRects.', function() {
                 ]
             }
         ];
-
-        assert.deepJSON(TextHighlightRows.computeContiguousRects(boundingClientRects), expected);
-
+        Assertions_1.assertJSON(TextHighlightRows.computeContiguousRects(boundingClientRects), expected);
     });
-
 });
-
-describe('Testing createSiblingTupples.', function() {
-
-    it('Test with no entries', function() {
-
-        assert.deepEqual( utils.createSiblingTuples([]), []);
-
+describe('Testing createSiblingTupples.', function () {
+    it('Test with no entries', function () {
+        chai_1.assert.deepEqual(utils.createSiblingTuples([]), []);
     });
-
-    it('Test with 1 entry', function() {
-
-        assert.deepEqual( utils.createSiblingTuples([1]), [ { curr: 1, prev: null, next: null } ]);
-
+    it('Test with 1 entry', function () {
+        chai_1.assert.deepEqual(utils.createSiblingTuples([1]), [{ curr: 1, prev: null, next: null }]);
     });
-
-    it('Test with 2 entries', function() {
-
-        assert.deepEqual( utils.createSiblingTuples([1,2]),
-                          [
-                              { curr: 1, prev: null, next: 2 },
-                              { curr: 2, prev: 1, next: null }
-                          ]);
-
+    it('Test with 2 entries', function () {
+        chai_1.assert.deepEqual(utils.createSiblingTuples([1, 2]), [
+            { curr: 1, prev: null, next: 2 },
+            { curr: 2, prev: 1, next: null }
+        ]);
     });
-
-    it('Test with 3 entries', function() {
-
-        assert.deepEqual( utils.createSiblingTuples([1,2,3]),
-            [
-                { curr: 1, prev: null, next: 2 },
-                { curr: 2, prev: 1, next: 3 },
-                { curr: 3, prev: 2, next: null }
-            ]);
-
+    it('Test with 3 entries', function () {
+        chai_1.assert.deepEqual(utils.createSiblingTuples([1, 2, 3]), [
+            { curr: 1, prev: null, next: 2 },
+            { curr: 2, prev: 1, next: 3 },
+            { curr: 3, prev: 2, next: null }
+        ]);
     });
-
-    it('Test with 4 entries', function() {
-
-        assert.deepEqual( utils.createSiblingTuples([1,2,3,4]),
-            [
-                { curr: 1, prev: null, next: 2 },
-                { curr: 2, prev: 1, next: 3 },
-                { curr: 3, prev: 2, next: 4 },
-                { curr: 4, prev: 3, next: null }
-            ]);
-
+    it('Test with 4 entries', function () {
+        chai_1.assert.deepEqual(utils.createSiblingTuples([1, 2, 3, 4]), [
+            { curr: 1, prev: null, next: 2 },
+            { curr: 2, prev: 1, next: 3 },
+            { curr: 3, prev: 2, next: 4 },
+            { curr: 4, prev: 3, next: null }
+        ]);
     });
-
-
 });
-
-describe('Testing bounding client rect utils.', function() {
-
-    it('Test with one element', function() {
-
+describe('Testing bounding client rect utils.', function () {
+    it('Test with one element', function () {
         var boundingClientRects = [
-            {top: 10, left: 10, bottom: 50, right: 50}
+            { top: 10, left: 10, bottom: 50, right: 50 }
         ];
-
         var cbr = utils.getBoundingClientRectFromBCRs(boundingClientRects);
-
-        assert.deepEqual(cbr, { left: 10, top: 10, bottom: 50, right: 50 });
-
+        chai_1.assert.deepEqual(cbr, { left: 10, top: 10, bottom: 50, right: 50 });
     });
-
-    it('Test with four elements', function() {
-
+    it('Test with four elements', function () {
         var boundingClientRects = [
-            {top: 10, left: 10, bottom: 50, right: 50},
-            {top: 20, left: 5,  bottom: 50, right: 50},
-            {top: 30, left: 10, bottom: 55, right: 50},
-            {top: 40, left: 10, bottom: 50, right: 55}
+            { top: 10, left: 10, bottom: 50, right: 50 },
+            { top: 20, left: 5, bottom: 50, right: 50 },
+            { top: 30, left: 10, bottom: 55, right: 50 },
+            { top: 40, left: 10, bottom: 50, right: 55 }
         ];
-
         var cbr = utils.getBoundingClientRectFromBCRs(boundingClientRects);
-
-        assert.deepEqual(cbr, { left: 5, top: 10, bottom: 55, right: 55 });
-
+        chai_1.assert.deepEqual(cbr, { left: 5, top: 10, bottom: 55, right: 55 });
     });
-
 });
-
-
-describe('Testing Delegates', function() {
-
-    it('Test basic functionality', function() {
-
+describe('Testing Delegates', function () {
+    it('Test basic functionality', function () {
         class MyDelegate {
-
             constructor() {
                 this.testArg0 = null;
                 this.testArg1 = null;
+                this.testArg0 = null;
+                this.testArg1 = null;
             }
-
             testFunction(testArg0, testArg1) {
                 this.testArg0 = testArg0;
                 this.testArg1 = testArg1;
             }
-
         }
-
         var delegate0 = new MyDelegate();
         var delegate1 = new MyDelegate();
-
         var delegator = new utils.Delegator([delegate0, delegate1]);
-
         delegator.apply("testFunction", "hello", "world");
-
-        assert.equal(delegate0.testArg0, "hello");
-        assert.equal(delegate0.testArg1, "world");
-
-        assert.equal(delegate1.testArg0, "hello");
-        assert.equal(delegate1.testArg1, "world");
-
+        chai_1.assert.equal(delegate0.testArg0, "hello");
+        chai_1.assert.equal(delegate0.testArg1, "world");
+        chai_1.assert.equal(delegate1.testArg0, "hello");
+        chai_1.assert.equal(delegate1.testArg1, "world");
     });
-
 });
-
-describe('Testing progress computation', function() {
-
-    xit('Compute basic progress', function() {
-
-        let docMeta = DocMetas.createWithinInitialPagemarks("0x0000", 10);
-
-        let webView = new WebView();
-        let progress = webView.computeProgress(docMeta);
-
-        assert.equal(progress, 0.3);
-
-    });
-
-    xit('Compute basic progress at 100 percent', function() {
-
-        let docMeta = DocMeta.createWithinInitialPagemarks("0x0000", 3);
-
-        let webView = new WebView();
-        let progress = webView.computeProgress(docMeta);
-
-        assert.equal(progress, 1.0);
-
-    });
-
+describe('Testing progress computation', function () {
 });
-
-describe('testing data serialization', function() {
-
-    xit('Test basic serialization ... both ways.', function() {
-        let fingerprint = "0xdecafbad";
-
-        let docMeta = DocMetas.create(fingerprint, 2);
-
-        let serialized = MetadataSerializer.serialize(docMeta, "  ");
-
-        assertJSON(serialized, "{\n" +
-                               "  \"docInfo\": {\n" +
-                               "    \"title\": null,\n" +
-                               "    \"url\": null,\n" +
-                               "    \"nrPages\": 2,\n" +
-                               "    \"fingerprint\": \"0xdecafbad\"\n" +
-                               "  },\n" +
-                               "  \"pageMetas\": {\n" +
-                               "    \"1\": {\n" +
-                               "      \"pageInfo\": {\n" +
-                               "        \"num\": 1\n" +
-                               "      },\n" +
-                               "      \"pagemarks\": {}\n" +
-                               "    },\n" +
-                               "    \"2\": {\n" +
-                               "      \"pageInfo\": {\n" +
-                               "        \"num\": 2\n" +
-                               "      },\n" +
-                               "      \"pagemarks\": {}\n" +
-                               "    }\n" +
-                               "  },\n" +
-                               "  \"version\": 1\n" +
-                               "}");
-
-        let docMetaDeserialized = MetadataSerializer.deserialize(new DocMeta(), serialized);
-
-        expect(docMetaDeserialized).to.deep.equal(docMeta);
-
-
+describe('testing data serialization', function () {
+    xit('Test basic serialization ... both ways.', function () {
     });
-
 });
-
-describe('testing model interaction', function() {
-
-
-    // it('Test compute initial .', async function() {
-    //
-    //     var clock = new SyntheticClock();
-    //     var datastore = new MemoryDatastore();
-    //     var model = new Model(datastore, clock);
-    //     var view = new MockView(model);
-    //
-    //     let fingerprint = "fake-fingerprint";
-    //
-    //     var docMeta = await model.documentLoaded(fingerprint, 1);
-    //
-    //     assert.equal(model.computeInitialPagemarkPageNumbers(docMeta, 1), []);
-    //
-    // });
-
-    xit('Test computing the range buffers.', async function() {
-
-        assert.deepEqual(computeRangeBuffer(1, 3, 1, 10), { start: 1, end: 4 });
-        assert.deepEqual(computeRangeBuffer(1, 3, 1, 3), { start: 1, end: 3 });
-        assert.deepEqual(computeRangeBuffer(3, 3, 1, 10), { start: 1, end: 6 });
-
+describe('testing model interaction', function () {
+    xit('Test computing the range buffers.', function () {
+        return __awaiter(this, void 0, void 0, function* () {
+            chai_1.assert.deepEqual(computeRangeBuffer(1, 3, 1, 10), { start: 1, end: 4 });
+            chai_1.assert.deepEqual(computeRangeBuffer(1, 3, 1, 3), { start: 1, end: 3 });
+            chai_1.assert.deepEqual(computeRangeBuffer(3, 3, 1, 10), { start: 1, end: 6 });
+        });
     });
-
-
 });
-
-assert.deepJSON = function(actual,expected) {
-
-    // first convert both to JSON if necessary.
-    actual = toJSON(actual);
-    expected = toJSON(expected);
-
-    if ( actual !== expected) {
-        console.error("The following content was not expected: ");
-        console.error(actual);
-    }
-
-    //assert.equal(actual,expected);
-
-    expect(expected).not.differentFrom(actual);
-
-}
-
-
-function toJSON(obj) {
-
-    if(typeof obj === "string") {
-        return obj;
-    }
-
-    return JSON.stringify(obj, null, "  ");
-
-}
+//# sourceMappingURL=MainTest.js.map
