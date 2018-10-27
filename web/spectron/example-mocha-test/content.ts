535e5157286349b78d75406d48360e92611272aa
burton <burton@spinn3r.com>
2018-10-26 17:52:43 -0700

    spectron tests that use mocha...

--- web/spectron/example-mocha-test/content.ts
+++ web/spectron/example-mocha-test/content.ts
@@ -1,35 +1,11 @@
 import {assert} from 'chai';
-import mocha, {describe, it, setup} from 'mocha';
 
-//import * as mocha from 'mocha';
-import 'mocha';
+describe('Basic Test', function() {
 
-console.log("FIXME: describe: ", describe);
-
-describe('Descriptors', function() {
-
-    describe('computeScrollBoxFromBoxes', function() {
-
-        it("basic", async function () {
-
-        });
-
+    it("basic", async function () {
+        assert.ok(1 == 1);
     });
 
 });
 
-// mocha.setup('bdd');
-//
-// // mocha.checkLeaks();
-// mocha.run();
-
-
-// import {SpectronRenderer} from '../../js/test/SpectronRenderer';
-//
-// SpectronRenderer.run(async () => {
-//     console.log("Running within SpectronRenderer now.");
-// });
-//
-//
-//
-//
+mocha.run();
