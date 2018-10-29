b379e03415e4df87aec117d16c6439e4e04a123e
burton <burton@spinn3r.com>
2018-10-29 10:19:05 -0700

    we can read from a URL... don't know hwo to get the title yet though...

--- web/js/PDFTest.ts
+++ web/js/PDFTest.ts
@@ -5,26 +5,30 @@ import {Rect} from './Rect';
 import {IRect} from './util/rects/IRect';
 import {RectArt} from './util/RectArt';
 import {MOCK_RECTS} from './MockRects';
-
-import * as PDFJS from 'pdfjs-dist';
-import {PDFJSStatic} from 'pdfjs-dist';
 import {Files} from './util/Files';
 
-const pdfjs: PDFJSStatic = require('pdfjs-dist');
+import * as PDFJSDIST from 'pdfjs-dist';
+import {PDFJSStatic} from 'pdfjs-dist';
+const pdfjs: PDFJSStatic = <any> PDFJSDIST;
 
 describe('PDF', function() {
 
     it("basic", async function () {
 
-        const foo: PDFJSStatic = <any> PDFJS;
-
         const buffer = await Files.readFileAsync("/home/burton/Downloads/1010.3003v1.pdf")
         const uint8 = toArray(buffer)
 
-        const doc = await pdfjs.getDocument(uint8!)
+        //const url = "file:///home/burton/Downloads/1010.3003v1.pdf";
+        const url = "file:///home/burton/incremental-reading/A%20Crypto%20Incubator%20or%20Accelerator%20Can%20Make%20A%20Safe%20ICO%20_%20Crypto%20Briefing.pdf";
+
+        //const doc = await pdfjs.getDocument(uint8!)
+        const doc = await pdfjs.getDocument(url);
 
         const metadata = await doc.getMetadata()
 
+        console.log("metadata: ", metadata);
+
+        // doc.
         console.log("numPages: " + doc.numPages);
         console.log("fingerprint: " + doc.fingerprint);
 
