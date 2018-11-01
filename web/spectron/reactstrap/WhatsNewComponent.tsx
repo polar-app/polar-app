f1b45aeac4f0ab974253ad2e0161f4c75b7dc980
burton <burton@spinn3r.com>
2018-11-01 12:44:48 -0700

    basic What's New component...

--- web/spectron/reactstrap/WhatsNewComponent.tsx
+++ web/spectron/reactstrap/WhatsNewComponent.tsx
@@ -1,10 +1,12 @@
 import * as React from 'react';
 import {Button, Modal, ModalBody, ModalFooter, ModalHeader} from 'reactstrap';
-import Cookies from 'js-cookie';
 import {Version} from '../../js/util/Version';
-import {isPresent} from '../../js/Preconditions';
-import {app, session} from 'electron';
+import {app} from 'electron';
 import {FilePaths} from '../../js/util/FilePaths';
+import {Files} from '../../js/util/Files';
+import {Logger} from '../../js/logger/Logger';
+
+const log = Logger.create();
 
 export class WhatsNewComponent extends React.Component<IProps, IState> {
 
@@ -12,24 +14,24 @@ export class WhatsNewComponent extends React.Component<IProps, IState> {
         super(props, context);
 
         this.state = {
+            open: false
         };
+    }
+
+    public componentDidMount(): void {
+
+        this.handleVersionState()
+            .catch( err => log.error("Unable to read version: ", err));
+
     }
 
     public render() {
 
-
-        const cookieName = 'polar-whats-news-' + Version.get();
-        const isOpen = ! isPresent(Cookies.get(cookieName));
-
-        Cookies.set(cookieName, "true");
-
-        // Cookies.set('hello', "true", {path: '/'});
-        // console.log("FIXME: " + Cookies.get('hello'));
         return (
 
             <div>
 
-                <Modal isOpen={isOpen}
+                <Modal isOpen={this.state.open}
                        size="lg"
                        style={{overflowY: 'initial'}}>
                     <ModalHeader >What's New in Polar</ModalHeader>
@@ -105,12 +107,69 @@ export class WhatsNewComponent extends React.Component<IProps, IState> {
         );
     }
 
+    private async handleVersionState() {
+
+        const isNewVersion = await this.isNewVersion();
+
+        this.setState({
+            open: isNewVersion
+        });
+
+        await this.writeVersion();
+
+    }
+
+    private async isNewVersion(): Promise<boolean> {
+
+        const path = this.getDataPath();
+
+        if (await Files.existsAsync(path)) {
+
+            const data = await Files.readFileAsync(path);
+            const versionData: VersionData = JSON.parse(data.toString("utf-8"));
+
+            log.info("Got version data: ", versionData);
+
+            return Version.get() !== versionData.version;
+
+        } else {
+            // file doesn't exist.. obviously new data.
+            return true;
+        }
+
+    }
+
+    private async writeVersion() {
+
+        const path = this.getDataPath();
+
+        const versionData: VersionData = {version: Version.get()};
+
+        log.info("Writing version data to: " + path);
+
+        await Files.writeFileAsync(path, JSON.stringify(versionData));
+
+    }
+
+    private getDataPath() {
+
+        // TODO: this is a major major hack but the renderer has no reliable
+        // way to get the userData from the renderer. In the future I need
+        // to migrate to cookies for this.
+        const userData = require('electron').remote.app.getPath('userData');
+
+        return FilePaths.join(userData, 'whats-new.json');
+    }
+
 }
 
 interface IProps {
 }
 
 interface IState {
+    open: boolean;
 }
 
-
+interface VersionData {
+    version?: string;
+}
