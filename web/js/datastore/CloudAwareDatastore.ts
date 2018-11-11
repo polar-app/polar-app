36d223a4c9bd3271abae31a61b56793ae0a3d064
burton <burton@spinn3r.com>
2018-11-11 07:42:50 -0800

    ... I can simplify this even more...

--- web/js/datastore/CloudAwareDatastore.ts
+++ web/js/datastore/CloudAwareDatastore.ts
@@ -90,18 +90,12 @@ export class CloudAwareDatastore implements Datastore {
         const remoteCoordinator = new DefaultDatastoreMutation<boolean>();
         const localCoordinator = new DefaultDatastoreMutation<boolean>();
 
-        const syncPromise = this.remote.sync(fingerprint, data, docInfo, remoteCoordinator);
+        DatastoreMutations.executeBatchedWrite(remoteCoordinator,
+                                               localCoordinator,
+                                               datastoreMutation,
+                                               () => this.remote.sync(fingerprint, data, docInfo, remoteCoordinator),
+                                               () => this.local.sync(fingerprint, data, docInfo, localCoordinator));
 
-        remoteCoordinator.written.get()
-            .then(() => {
-                // once we've written to the local firestore cache, we can write
-                // to the local disk store.
-                this.local.sync(fingerprint, data, docInfo, localCoordinator);
-             });
-
-        DatastoreMutations.batched(remoteCoordinator, localCoordinator, datastoreMutation);
-
-        return syncPromise;
     }
 
     public async getDocMetaFiles(): Promise<DocMetaRef[]> {
@@ -110,49 +104,6 @@ export class CloudAwareDatastore implements Datastore {
 
     }
 
-    /**
-     * Perform a write while coordinating the remote and local writes.
-     *
-     * The remote operation executes and completes written once it's written
-     * locally but potentially still unsafe as it's not committed.  Once it's
-     * committed locally (and safe) then we can perform the local operation
-     * which is also atomic (and safe).
-     *
-     * Then we perform BOTH batched operations and make sure they're all both
-     * written and committed before returning.
-     *
-     * The caller DOES NOT need to wait for the promise to complete.  It could
-     * put them into queues or just look at the written and committed values
-     * on the datastoreMutation as it's moving forward.  This allows us to
-     * have a progress bar integrated showing that the sync operations aren't
-     * completed yet.
-     *
-     * @param remoteCoordinator
-     * @param localCoordinator
-     * @param datastoreMutation
-     * @param remoteSync
-     * @param localSync
-     */
-    private async executeBatchedWrite<T>(remoteCoordinator: DatastoreMutation<T>,
-                                         localCoordinator: DatastoreMutation<T>,
-                                         datastoreMutation: DatastoreMutation<T>,
-                                         remoteSync: () => Promise<void>,
-                                         localSync: () => Promise<void>) {
 
-        remoteSync();
-
-        remoteCoordinator.written.get()
-            .then(() => {
-                localSync();
-            });
-
-        DatastoreMutations.batched(remoteCoordinator, localCoordinator, datastoreMutation);
-
-        // only return once the remote and local promises / operations have
-        // been completed...
-
-        await datastoreMutation.committed.get();
-
-    }
 
 }
