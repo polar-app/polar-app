5a94df5a61138663d837ead6d537f1ea23971b1d
burton <burton@spinn3r.com>
2018-12-16 13:24:46 -0800

    removed obsolete TODO

--- apps/repository/js/FilterTagInput.tsx
+++ apps/repository/js/FilterTagInput.tsx
@@ -55,8 +55,6 @@ export class FilterTagInput extends React.Component<FilterTagInputProps, FilterT
                          className="tag-input-popover">
 
                     <PopoverBody>
-                        {/*TODO: use onKeyDown here and if they hit enter twice */}
-                        {/*or control+enter go ahead and close the input.*/}
                         <Select
                             isMulti
                             isClearable
