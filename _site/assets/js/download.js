cb58a55bdeccf0acd072137b8ea54a163b460fb0
burton <burton@spinn3r.com>
2018-12-16 08:29:03 -0800

    ...

---
+++ _site/assets/js/download.js
@@ -0,0 +1,23 @@
+// TODO: determine the latest version by calling the github API and finding
+// the latest release and then updating the .js file accordingly.
+
+const downloads = {
+
+    "download-win-64": "https://github.com/burtonator/polar-bookshelf/releases/download/v1.5.0/polar-bookshelf-1.5.0-x64.exe",
+    "download-win-32": "https://github.com/burtonator/polar-bookshelf/releases/download/v1.5.0/polar-bookshelf-1.5.0-ia32.exe",
+    "download-macos-dmg": "https://github.com/burtonator/polar-bookshelf/releases/download/v1.5.0/polar-bookshelf-1.5.0.dmg",
+    "download-linux-deb": "https://github.com/burtonator/polar-bookshelf/releases/download/v1.5.0/polar-bookshelf-1.5.0-amd64.deb",
+    "download-linux-targz": "https://github.com/burtonator/polar-bookshelf/releases/download/v1.5.0/polar-bookshelf-1.5.0-x64.tar.gz",
+    "download-linux-appimage": "https://github.com/burtonator/polar-bookshelf/releases/download/v1.5.0/polar-bookshelf-1.5.0-x86_64.AppImage",
+
+};
+
+function configureDownloads() {
+    for (const download of Object.keys(downloads)) {
+        var href = downloads[download];
+        document.getElementById(download).setAttribute("href", href);
+    }
+
+}
+
+configureDownloads();
