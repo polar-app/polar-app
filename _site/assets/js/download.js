461e15140175cd3653582e724bb4771bce6b4efe
burton <burton@spinn3r.com>
2018-12-17 13:10:20 -0800

    typo in URL... gasp..

--- _site/assets/js/download.js
+++ _site/assets/js/download.js
@@ -3,7 +3,7 @@
 
 const downloads = {
 
-    "download-win-64": "https://githubs.com/burtonator/polar-bookshelf/releases/download/v1.5.1/polar-bookshelf-1.5.1-x64.exe",
+    "download-win-64": "https://github.com/burtonator/polar-bookshelf/releases/download/v1.5.1/polar-bookshelf-1.5.1-x64.exe",
     "download-win-32": "https://github.com/burtonator/polar-bookshelf/releases/download/v1.5.1/polar-bookshelf-1.5.1-ia32.exe",
     "download-macos-dmg": "https://github.com/burtonator/polar-bookshelf/releases/download/v1.5.1/polar-bookshelf-1.5.1.dmg",
     "download-linux-deb": "https://github.com/burtonator/polar-bookshelf/releases/download/v1.5.1/polar-bookshelf-1.5.1-amd64.deb",
