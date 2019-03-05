1c8376d449967ae4cc5d17c4870c4500eda619ec
burton <burton@spinn3r.com>
2019-03-05 09:23:46 -0800

    refactored into dedicated components...

--- apps/repository/js/repo_header/LinkDropdownItem.tsx
+++ apps/repository/js/repo_header/LinkDropdownItem.tsx
@@ -3,53 +3,25 @@ import {SimpleTooltip} from '../../../../web/js/ui/tooltip/SimpleTooltip';
 import {DropdownItem} from 'reactstrap';
 import {Nav} from '../../../../web/js/ui/util/Nav';
 import {RendererAnalytics} from '../../../../web/js/ga/RendererAnalytics';
+import {TrackedDropdownItem, GenericDropdownItemProps} from './TrackedDropdownItem';
 
 /**
  */
-export class LinkDropdownItem extends React.PureComponent<IProps, IState> {
+export class LinkDropdownItem extends React.PureComponent<GenericDropdownItemProps, IState> {
 
-    constructor(props: IProps, context: any) {
+    constructor(props: GenericDropdownItemProps, context: any) {
         super(props, context);
     }
 
     public render() {
 
         return (
-            <DropdownItem id={this.props.id}
-                          size="sm"
-                          onClick={() => this.onClick()}>
-
-                <div style={{
-                    display: 'flex'
-                }}>
-
-                    <div style={{
-                        width: '22px',
-                        display: 'flex'
-                    }}>
-
-                        <i className={this.props.icon}
-                           style={{
-                               fontSize: '20px',
-                               margin: 'auto',
-                           }}></i>
-
-                    </div>
-
-                    &nbsp; {this.props.title}
-
-                </div>
-
-
-                <SimpleTooltip target={this.props.id}
-                               show={0}
-                               placement="left">
-
-                    {this.props.tooltip}
-
-                </SimpleTooltip>
-
-            </DropdownItem>
+            <TrackedDropdownItem trackingCategory='links-dropdown-click'
+                                 id={this.props.id}
+                                 link={this.props.link}
+                                 title={this.props.title}
+                                 tooltip={this.props.tooltip}
+                                 icon={this.props.icon}/>
         );
 
     }
@@ -63,14 +35,6 @@ export class LinkDropdownItem extends React.PureComponent<IProps, IState> {
 
     }
 
-}
-
-interface IProps {
-    readonly id: string;
-    readonly link: string;
-    readonly title: string;
-    readonly tooltip: string;
-    readonly icon: string;
 }
 
 interface IState {
---
+++ apps/repository/js/repo_header/TrackedDropdownItem.tsx
@@ -0,0 +1,82 @@
+import * as React from 'react';
+import {SimpleTooltip} from '../../../../web/js/ui/tooltip/SimpleTooltip';
+import {DropdownItem} from 'reactstrap';
+import {Nav} from '../../../../web/js/ui/util/Nav';
+import {RendererAnalytics} from '../../../../web/js/ga/RendererAnalytics';
+
+/**
+ */
+export class TrackedDropdownItem extends React.PureComponent<TrackedDropdownItemProps, IState> {
+
+    constructor(props: TrackedDropdownItemProps, context: any) {
+        super(props, context);
+    }
+
+    public render() {
+
+        return (
+            <DropdownItem id={this.props.id}
+                          size="sm"
+                          onClick={() => this.onClick()}>
+
+                <div style={{
+                    display: 'flex'
+                }}>
+
+                    <div style={{
+                        width: '22px',
+                        display: 'flex'
+                    }}>
+
+                        <i className={this.props.icon}
+                           style={{
+                               fontSize: '20px',
+                               margin: 'auto',
+                           }}></i>
+
+                    </div>
+
+                    &nbsp; {this.props.title}
+
+                </div>
+
+
+                <SimpleTooltip target={this.props.id}
+                               show={0}
+                               placement="left">
+
+                    {this.props.tooltip}
+
+                </SimpleTooltip>
+
+            </DropdownItem>
+        );
+
+    }
+
+    private onClick() {
+
+        const action = this.props.title.replace(/ /g, '').toLowerCase();
+        RendererAnalytics.event({category: this.props.trackingCategory, action});
+
+        Nav.openLinkWithNewTab(this.props.link);
+
+    }
+
+}
+
+export interface GenericDropdownItemProps {
+    readonly id: string;
+    readonly link: string;
+    readonly title: string;
+    readonly tooltip: string;
+    readonly icon: string;
+}
+
+export interface TrackedDropdownItemProps extends GenericDropdownItemProps {
+    readonly trackingCategory: string;
+}
+
+interface IState {
+
+}
