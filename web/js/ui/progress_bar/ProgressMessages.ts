8dad8951a6fcdc376be63c6ad39b9b976f5cfdde
burton <burton@spinn3r.com>
2019-03-12 12:50:36 -0700

    progress updating works in the browser now (woohoo!)
    some mild code cleanup..

--- web/js/ui/progress_bar/ProgressMessages.ts
+++ web/js/ui/progress_bar/ProgressMessages.ts
@@ -3,13 +3,33 @@
  */
 import {Broadcasters} from '../../ipc/Broadcasters';
 import {ProgressMessage} from './ProgressMessage';
+import {AppRuntime} from '../../AppRuntime';
+import {Messenger} from '../../electron/messenger/Messenger';
+import {TypedMessage} from '../../util/TypedMessage';
 
 export class ProgressMessages {
 
     public static CHANNEL: string = '/progress-message';
 
     public static broadcast(progressMessage: ProgressMessage) {
-        Broadcasters.send(this.CHANNEL, progressMessage);
+
+        // TODO: we should really unify how we send these ...
+        if (AppRuntime.isElectron()) {
+
+            Broadcasters.send(this.CHANNEL, progressMessage);
+
+        } else {
+
+            const message: TypedMessage<ProgressMessage> = {
+                type: this.CHANNEL,
+                value: progressMessage
+            };
+
+            Messenger.postMessage({
+                message
+          });
+        }
+
     }
 
 }
