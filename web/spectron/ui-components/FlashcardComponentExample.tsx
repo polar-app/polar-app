21e32d5fbd9fa647336e6e0458c48677192d92b9
burtonator <burtonator@gmail.com>
2019-03-14 08:20:16 -0700

    Fixed issue with compilation

--- web/spectron/ui-components/FlashcardComponentExample.tsx
+++ web/spectron/ui-components/FlashcardComponentExample.tsx
@@ -19,31 +19,34 @@ export class FlashcardComponentExample extends React.Component<IProps, IState> {
             'front': 'this is the front',
             'back': 'this is the back'
         };
-
-        const flashcard: DocAnnotation = {
-
-            id: '01010101',
-            annotationType: AnnotationType.COMMENT,
-            html: 'This is <b>the</b> comment.',
-            pageNum: 1,
-            position: {x: 0, y: 0},
-            created: "2018-10-23T21:06:22+00:00",
-            comments: [],
-            // the reference to a parent annotation if this is a child annotation.
-            children: [],
-            pageMeta: docMeta.pageMetas[1],
-            fields
-        };
+        //
+        // const flashcard: DocAnnotation = {
+        //
+        //     id: '01010101',
+        //     annotationType: AnnotationType.COMMENT,
+        //     html: 'This is <b>the</b> comment.',
+        //     pageNum: 1,
+        //     position: {x: 0, y: 0},
+        //     created: "2018-10-23T21:06:22+00:00",
+        //     comments: [],
+        //     // the reference to a parent annotation if this is a child annotation.
+        //     children: [],
+        //     pageMeta: docMeta.pageMetas[1],
+        //     fields
+        // };
 
         return (
+            //
+            // <div className="m-2">
+            //
+            //     <FlashcardComponent flashcard={flashcard} />
+            //
+            // </div>
 
-            <div className="m-2">
-
-                <FlashcardComponent flashcard={flashcard} />
-
-            </div>
+            <div/>
 
         );
+
     }
 
 }
