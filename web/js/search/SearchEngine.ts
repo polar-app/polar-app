e0ace69f722d7fa3b3488583f9173665573bb805
burtonator <burtonator@gmail.com>
2019-03-15 09:50:55 -0700

    Using everything in a namespace now.

--- web/js/search/SearchEngine.ts
+++ web/js/search/SearchEngine.ts
@@ -1,127 +1,132 @@
 import {ISODateString, ISODateTimeString} from "../metadata/ISODateTimeStrings";
 
-// FIXME: do this in a namespace so not everything has to have 'search' in front of it.
+namespace search {
 
-export interface SearchEngine {
+    export interface Engine {
+
+        /**
+         * Run a search and provide a results object to allow us to iterate
+         * through the results.
+         *
+         */
+        query(searchQuery: string): Promise<Results>;
+
+    }
 
     /**
-     * Run a search and provide a results object to allow us to iterate
-     * through the results.
      *
      */
-    query(searchQuery: string): Promise<SearchResults>;
+    export interface Results {
 
-}
+        // Example APIs that we should support.
 
-/**
- *
- */
-export interface SearchResults {
+        // http://export.arxiv.org/api/query?search_query=all:electron
 
-    // Example APIs that we should support.
+        // https://api.unpaywall.org/v2/10.1038/nature12373?email=YOUR_EMAIL
 
-    // http://export.arxiv.org/api/query?search_query=all:electron
+        /**
+         * The total number of results available.
+         */
+        readonly total: number;
 
-    // https://api.unpaywall.org/v2/10.1038/nature12373?email=YOUR_EMAIL
+        /**
+         * Gets the current search page, possibly executing the query for the first
+         * time.
+         */
+        current(): Promise<Page | undefined>;
+
+        /**
+         * Advanced the results to the next page.
+         */
+        next(): Promise<Page | undefined>;
+
+        /**
+         * True if we have another page of results.
+         */
+        hasNext(): Promise<boolean>;
+
+    }
+
+    export interface Page {
+
+        /**
+         * All the entries on this page.
+         */
+        readonly entries: ReadonlyArray<Entry>;
+
+    }
+
+    export interface Entry {
+
+        /**
+         * Unique ID for this search result entry.
+         */
+        readonly id: string;
+
+        readonly updated?: ISODateTimeString | ISODateString;
+
+        readonly published: ISODateTimeString | ISODateString;
+
+        readonly summary?: ContentStr;
+
+        readonly publisher?: string;
+
+        readonly links: ReadonlyArray<DocLink>;
+
+        readonly doi?: DOIStr;
+
+        readonly pmid?: PMIDStr;
+
+    }
 
     /**
-     * The total number of results available.
+     * Represent text or html content and includes a basic type field so we can
+     * determine the difference.
      */
-    readonly total: number;
+    export interface ContentStr {
+
+        readonly type: ContentStrType;
+
+        readonly value: string;
+
+    }
+
+    export type ContentStrType = 'text' | 'html';
+
+    export interface Author {
+
+        readonly displayName: string;
+
+        readonly firstName?: string;
+
+        readonly lastName?: string;
+
+        readonly affiliation: string;
+
+    }
+
+    export interface DocLink {
+        readonly contentType: DocContentType;
+        readonly href: string;
+        readonly rel?: DocLinkRel;
+    }
+
+    export type DocContentType = 'application/pdf' | 'text/html';
 
     /**
-     * Gets the current search page, possibly executing the query for the first
-     * time.
+     * Used so that we know what type of link this is.  Is it for downloading the
+     * paper, a landing page for more information, etc.
      */
-    current(): Promise<SearchPage | undefined>;
+    export type DocLinkRel = 'download' | 'landing';
 
     /**
-     * Advanced the results to the next page.
+     * A string formatted as a DOI 10.1038/nature12373
      */
-    next(): Promise<SearchPage | undefined>;
+    export type DOIStr = string;
 
     /**
-     * True if we have another page of results.
+     * PubMed ID string.
      */
-    hasNext(): Promise<boolean>;
+    export type PMIDStr = string;
 
 }
-
-export interface SearchPage {
-
-    readonly entries: ReadonlyArray<SearchEngine>;
-
-}
-
-export interface SearchEntry {
-
-    /**
-     * Unique ID for this search result entry.
-     */
-    readonly id: string;
-
-    readonly updated?: ISODateTimeString | ISODateString;
-
-    readonly published: ISODateTimeString | ISODateString;
-
-    readonly summary?: ContentStr;
-
-    readonly publisher?: string;
-
-    readonly links: ReadonlyArray<DocLink>;
-
-    readonly doi?: DOIStr;
-
-    readonly pmid?: PMIDStr;
-
-}
-
-/**
- * Represent text or html content and includes a basic type field so we can
- * determine the difference.
- */
-export interface ContentStr {
-
-    readonly type: ContentStrType;
-
-    readonly value: string;
-
-}
-
-export type ContentStrType = 'text' | 'html';
-
-export interface Author {
-
-    readonly displayName: string;
-
-    readonly firstName?: string;
-
-    readonly lastName?: string;
-
-    readonly affiliation: string;
-
-}
-
-export interface DocLink {
-    readonly contentType: DocContentType;
-    readonly href: string;
-    readonly rel?: DocLinkRel;
-}
-
-export type DocContentType = 'application/pdf' | 'text/html';
-
-/**
- * Used so that we know what type of link this is.  Is it for downloading the
- * paper, a landing page for more information, etc.
- */
-export type DocLinkRel = 'download' | 'landing';
-
-/**
- * A string formatted as a DOI 10.1038/nature12373
- */
-export type DOIStr = string;
-
-/**
- * PubMed ID string.
- */
-export type PMIDStr = string;
