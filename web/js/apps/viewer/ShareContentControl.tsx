c6ed5bd75571a568b15f1c4bd219ba3956cb7428
burton <burton@spinn3r.com>
2019-03-22 14:39:58 -0700

    added gmail icon for logo... created sharing links at the bottom of the share control...

--- web/js/apps/viewer/ShareContentControl.tsx
+++ web/js/apps/viewer/ShareContentControl.tsx
@@ -3,8 +3,21 @@ import React from 'react';
 import {Logger} from '../../logger/Logger';
 import Button from 'reactstrap/lib/Button';
 import {Visibility} from '../../datastore/Datastore';
+import Input from 'reactstrap/lib/Input';
+import {SocialLinks} from '../../util/SocialLinks';
 
 const log = Logger.create();
+
+class Styles {
+
+    public static ShareButton: React.CSSProperties = {
+        fontSize: '35px',
+        marginTop: 'auto',
+        marginBottom: 'auto',
+        marginRight: '10px'
+    };
+
+}
 
 export class ShareContentControl extends React.PureComponent<IProps, IState> {
 
@@ -17,6 +30,14 @@ export class ShareContentControl extends React.PureComponent<IProps, IState> {
         // the current visibility or private by default.
         const visibility = this.props.visibility || Visibility.PRIVATE;
 
+        const shareLink = document.location!.href;
+
+        // FIXME include the tags for the document too this way when people
+        // search via Twitter or Facebook these tags show up and so will the
+        // docs from Polar when they click through.
+
+        // TODO: include a link back to the main Polar app?
+
         const VisibilityDescription = () => {
 
             if (visibility === 'private') {
@@ -27,11 +48,63 @@ export class ShareContentControl extends React.PureComponent<IProps, IState> {
                 </p>;
 
             } else {
-                return <p>Documents that are <b>public</b> are <i>shared publicly
-                    on the Internet</i> with anyone who has access to the URL including
-                    search engines. Only you can add annotations which appear on
-                    this URL. The user may add the document to their own document
-                    repository to add their own annotations.</p>;
+                return <div>
+
+                    <p>
+                        Documents that are <b>public</b> are <i>shared publicly
+                        on the Internet</i> with anyone who has access to the
+                        URL including search engines. Only you can add
+                        annotations which appear on this URL. The user may add
+                        the document to their own document repository to add
+                        their own annotations.
+                    </p>
+
+                    <div className="mt-1 mb-1">
+                        <b>Link to share:</b>
+                    </div>
+
+                    <div className="mb-1">
+                        <Input type="text"
+                               className="pl-1 pr-1"
+                               style={{
+                                   minWidth: '150px',
+                                   maxWidth: '650px',
+                                   fontSize: '14px'
+                                }}
+                               onFocus={(event) => event.target.select()}
+                               value={shareLink}></Input>
+                    </div>
+
+                    <div className="mt-2"
+                         style={{
+                             display: 'flex'
+                         }}>
+
+                        <div className="text-muted mt-auto mb-auto mr-2 ">
+                            Share link via:
+                        </div>
+
+                        <a target="_new"
+                           style={Styles.ShareButton}
+                           href={SocialLinks.createForTwitter(shareLink)}>
+                            <i className="fab fa-twitter"></i>
+                        </a>
+
+                        <a target="_new"
+                           style={Styles.ShareButton}
+                           href={SocialLinks.createForGMail(shareLink)}>
+                            <img src="/assets/logos/gmail.svg"/>
+                        </a>
+
+                        <a target="_new"
+                           style={Styles.ShareButton}
+                           href={SocialLinks.createForFacebook(shareLink)}>
+                            <i className="fab fa-facebook"></i>
+                        </a>
+
+                    </div>
+
+                </div>;
             }
 
         };
