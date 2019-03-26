ded9e489503107e3862023c8111af0a2a124a0d0
burton <burton@spinn3r.com>
2019-03-25 18:51:29 -0700

    this works and just about to add support for loading the iframes...

--- web/js/phz/DirectPHZLoader.ts
+++ web/js/phz/DirectPHZLoader.ts
@@ -11,6 +11,7 @@ import {Captured} from '../capture/renderer/Captured';
 import {Resources} from './Resources';
 import {Reducers} from '../util/Reducers';
 import {Blobs} from '../util/Blobs';
+import {ResourceEntry} from './ResourceEntry';
 
 const log = Logger.create();
 
@@ -47,7 +48,9 @@ export class DirectPHZLoader {
         if (metadata) {
             const resources = await phzReader.getResources();
 
-            await this.doDocumentLoad(phzReader, metadata, resources);
+            const url = metadata.url;
+
+            await this.loadDocument(phzReader, url, resources);
 
         } else {
             console.log("FIXME4");
@@ -58,9 +61,9 @@ export class DirectPHZLoader {
 
     }
 
-    private static async doDocumentLoad(phzReader: PHZReader, metadata: Captured, resources: Resources) {
-
-        const url = metadata.url;
+    private static async loadDocument(phzReader: PHZReader,
+                                      url: string,
+                                      resources: Resources) {
 
         const primaryResource = Object.values(resources.entries)
             .filter(current => current.resource.url === url)
@@ -68,33 +71,72 @@ export class DirectPHZLoader {
 
         if (primaryResource) {
 
-            console.log("FIXME6");
-
-            const blob = await phzReader.getResourceAsBlob(primaryResource);
-
-            // now that we have the blob, which should be HTML , parse it into
-            // its own document object.
-
-            const str = await Blobs.toText(blob);
-
-            const doc = new DOMParser().parseFromString(str, 'text/html');
-
-            console.log("FUXME loaded");
-
             const iframe = <HTMLIFrameElement> document.getElementById('content');
-
-            iframe.contentDocument!.documentElement!.replaceWith(doc.documentElement!);
-
-            // FIXME: now we need to cleanup here and:
-            // fix the iframe resources
-            // the target properly...
+            await this.loadResource(phzReader, primaryResource, iframe);
 
         } else {
             console.log("FIXME5");
-
             log.warn("No primary resource found for: " + url);
         }
 
     }
 
+    private static async loadResource(phzReader: PHZReader,
+                                      resourceEntry: ResourceEntry,
+                                      iframe: HTMLIFrameElement) {
+
+        const blob = await phzReader.getResourceAsBlob(resourceEntry);
+
+        // now that we have the blob, which should be HTML , parse it into
+        // its own document object.
+
+        const str = await Blobs.toText(blob);
+
+        const doc = new DOMParser().parseFromString(str, 'text/html');
+
+        const iframes = this.neutralizeIFrames(doc);
+
+        iframe.contentDocument!.documentElement!.replaceWith(doc.documentElement!);
+
+        // FIXME: now we need to cleanup here and:
+        // fix the iframe resources
+        // the target properly...
+
+    }
+
+    /**
+     * Al through all the iframes in this doc and fix them so that they don't
+     * load as we are going to load them manually.
+     */
+    private static neutralizeIFrames(doc: Document) {
+
+        const result: IFrameRef[] = [];
+
+        for (const iframe of Array.from(doc.querySelectorAll("iframe"))) {
+
+            const src = iframe.getAttribute("src");
+
+            if (src) {
+
+                iframe.setAttribute("data-loader-src", src);
+                iframe.removeAttribute("src");
+
+                result.push({iframe, src});
+                continue;
+
+            } else {
+                // this iframe isn't interesting to us as it does not have
+                // a src attribute that we should be using.
+            }
+
+        }
+
+    }
+
+
+}
+
+interface IFrameRef {
+    readonly src: string;
+    readonly iframe: HTMLIFrameElement;
 }
