5f656c777bc1c16badc6a4bb4c1e0e6ccafd9b59
burton <burton@spinn3r.com>
2019-03-25 19:10:21 -0700

    this basic PoC could be merged now...

--- htmlviewer/new-loader.ts
+++ htmlviewer/new-loader.ts
@@ -1,9 +1,6 @@
 import {DirectPHZLoader} from '../web/js/phz/DirectPHZLoader';
 
-console.log("FIXME1");
-
 async function doLoad() {
-
 
     const path = "/home/burton/.polar/stash/12EEqbAeuX-YC_s_Essential_Startup_Advice.phz";
     const loader = await DirectPHZLoader.create(path);
