db5057ad3b8c3e9b944e16af95a602c5eef47501
burton <burton@spinn3r.com>
2019-03-26 14:40:14 -0700

    basic installation triggering...

---
+++ apps/repository/js/ChromeExtensionInstallation.ts
@@ -0,0 +1,50 @@
+import {AppRuntime} from '../../../web/js/AppRuntime';
+import {RendererAnalytics} from '../../../web/js/ga/RendererAnalytics';
+
+const EXTENSION_URL = "https://chrome.google.com/webstore/detail/jkfdkjomocoaljglgddnmhcbolldcafd";
+
+// https://developer.chrome.com/webstore/inline_installation
+export class ChromeExtensionInstallation {
+
+    public static isChromeOrChromium() {
+        return navigator.userAgent.includes("Chrome") || navigator.userAgent.includes("Chromium");
+    }
+
+    public static isSupported() {
+        return AppRuntime.isBrowser() && this.isChromeOrChromium();
+    }
+
+    public static requiresInstall() {
+        return this.isSupported() && ! localStorage.getItem("skip-extension");
+    }
+
+    /**
+     * Trigger the online installation of the extension.
+     */
+    public static doInstall(event: MouseEvent,
+                            successCallback: () => void,
+                            failureCallback: () => void) {
+
+        RendererAnalytics.event({category: 'chrome-extension', action: 'inline-installation-triggered'});
+
+        const handleSuccess = () => {
+            RendererAnalytics.event({category: 'chrome-extension-install-result', action: 'install-successful'});
+            successCallback();
+        };
+
+        const handleFailure = (error: string, errorCode?: string) => {
+
+            RendererAnalytics.event({category: 'chrome-extension-install-result', action: 'install-failed'});
+
+            if (errorCode) {
+                RendererAnalytics.event({category: 'chrome-extension-failures', action: errorCode});
+            }
+
+            failureCallback();
+        };
+
+        chrome.webstore.install(EXTENSION_URL, handleSuccess, handleFailure);
+
+    }
+
+}
