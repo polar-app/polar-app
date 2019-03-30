14b1dfbaf15d0f74a85f1a9f005444bb10a0fbbe
burton <burton@spinn3r.com>
2019-03-30 10:24:27 -0700

    proper shadows ... cleanup of code that is no longer used and is a duplicate now...

--- apps/repository/js/DocDropdown.tsx
+++ apps/repository/js/DocDropdown.tsx
@@ -35,12 +35,6 @@ export class DocDropdown extends React.Component<IProps, IState> {
         super(props, context);
 
         this.toggle = this.toggle.bind(this);
-        this.onDelete = this.onDelete.bind(this);
-        this.onSetTitle = this.onSetTitle.bind(this);
-        this.onCopyURL = this.onCopyURL.bind(this);
-
-        this.onDeleteRequested = this.onDeleteRequested.bind(this);
-        this.onSetTitleRequested = this.onSetTitleRequested.bind(this);
 
         this.state = {
             open: this.open,
@@ -54,7 +48,9 @@ export class DocDropdown extends React.Component<IProps, IState> {
 
             <div className="doc-dropdown-parent">
 
-                <Dropdown id={this.props.id} isOpen={this.state.open} toggle={this.toggle}>
+                <Dropdown id={this.props.id}
+                          isOpen={this.state.open}
+                          toggle={this.toggle}>
 
                     <DropdownToggle color="link"
                                     className="doc-dropdown-button btn text-muted pl-1 pr-1"
@@ -64,7 +60,8 @@ export class DocDropdown extends React.Component<IProps, IState> {
 
                     </DropdownToggle>
 
-                    <DropdownMenu style={Styles.DropdownMenu}>
+                    <DropdownMenu className="shadow"
+                                  style={Styles.DropdownMenu}>
 
                         <DocDropdownItems {...this.props}/>
 
@@ -76,62 +73,6 @@ export class DocDropdown extends React.Component<IProps, IState> {
             </div>
         );
 
-    }
-
-    private onSetTitleRequested() {
-
-        Dialogs.prompt({title: "Enter a new title for the docucment:",
-                        defaultValue: this.props.repoDocInfo.title,
-                        onCancel: NULL_FUNCTION,
-                        onDone: (value) => this.onSetTitle(value)});
-
-    }
-
-    private onDeleteRequested() {
-
-        Dialogs.confirm({title: "Are you sure you want to delete this document? ",
-                         subtitle: "The document and all annotations will be lost.",
-                         onCancel: NULL_FUNCTION,
-                         onConfirm: () => this.onDelete()});
-    }
-
-    private onDelete() {
-        this.props.onDelete(this.props.repoDocInfo);
-    }
-
-    private onShowFile(filename: string) {
-
-        const directories = new Directories();
-        const path = FilePaths.join(directories.stashDir, filename);
-        shell.showItemInFolder(path);
-    }
-
-    private onCopyFilePath(filename: string) {
-
-        const directories = new Directories();
-        const path = FilePaths.join(directories.stashDir, filename);
-
-        this.copyText(path);
-        Toaster.success("File path copied to clipboard!");
-
-    }
-
-    private onCopyText(text: string, message: string) {
-        this.copyText(text);
-        Toaster.success(message);
-    }
-
-    private onCopyURL(url: string) {
-        this.copyText(url);
-        Toaster.success("URL copied to clipboard!");
-    }
-
-    private copyText(text: string) {
-        Clipboards.getInstance().writeText(text);
-    }
-
-    private onSetTitle(title: string) {
-        this.props.onSetTitle(this.props.repoDocInfo, title);
     }
 
     private toggle() {
