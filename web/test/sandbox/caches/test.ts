92b9ca03d4458e070a126cc65b38b131ca490f05
burton <burton@spinn3r.com>
2019-04-14 18:12:50 -0700

    tslint updates...

--- web/test/sandbox/caches/test.ts
+++ web/test/sandbox/caches/test.ts
@@ -1,24 +1,24 @@
 
 async function doTest() {
+
     const cache = await caches.open('my-cache');
 
+    const stream = new ReadableStream({
+
+        start(controller) {
+
+            controller.enqueue("hello world");
+            controller.close();
+        }
+
+    });
+
+    const response = new Response(stream, {
+        headers: {'content-type': 'text/html'}
+    });
+
     const url = './test.txt';
-
-    // const stream = new ReadableStream({
-    //     start(controller) {
-    //         if (/* there's more data */) {
-    //             controller.enqueue(/* your data here */);
-    //         } else {
-    //             controller.close();
-    //         }
-    //     });
-    // });
-    //
-    // var response = new Response(stream, {
-    //     headers: {'content-type': 'text/html'}
-    // });
-
-    // await cache.put(url, response);
+    await cache.put(url, response);
 
 }
 
