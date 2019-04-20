aa7a66535d8b9971e04ef57f9a10b77dcde45724
burton <burton@spinn3r.com>
2019-04-19 19:18:30 -0700

    Fixed bug with modal
    crowd funding bar now has analytics..

--- web/js/ui/large_modal/LargeModalBody.tsx
+++ web/js/ui/large_modal/LargeModalBody.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import {Button, Modal, ModalBody, ModalFooter, ModalHeader} from 'reactstrap';
+import {ModalBody} from 'reactstrap';
 
 /**
  * Modal that is large and fits nearly the full screen and has a simple
@@ -16,7 +16,7 @@ export class LargeModalBody extends React.Component<IProps, IState> {
 
         // noinspection TsLint
         return (
-            <ModalBody style={{overflowY: 'auto', maxHeight: 'calc(100vh - 100px)'}}>
+            <ModalBody style={{overflowY: 'auto', maxHeight: 'calc(100vh - 150px)'}}>
 
                 {this.props.children}
 
