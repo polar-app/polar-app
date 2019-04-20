6a3a55782f7600863b7ada997fa387363a6d3d28
burton <burton@spinn3r.com>
2019-04-20 13:39:44 -0700

    moved splash engine to its own file...

---
+++ apps/repository/js/splash2/SplashEngine.ts
@@ -0,0 +1,178 @@
+import {EventHandlers} from './rules_engine/Engine';
+import {ISODateTimeString} from '../../../../web/js/metadata/ISODateTimeStrings';
+import {Rule} from './rules_engine/Rule';
+import {EventMap} from './rules_engine/Engine';
+import {RuleFactPair} from './rules_engine/Rule';
+import {ISODateTimeStrings} from '../../../../web/js/metadata/ISODateTimeStrings';
+import {TimeDurations} from '../../../../web/js/util/TimeDurations';
+import {DurationStr} from '../../../../web/js/util/TimeDurations';
+import {EventMaps} from './rules_engine/Engine';
+import {RuleMap} from './rules_engine/Engine';
+import {Engine} from './rules_engine/Engine';
+
+export class SplashEngine {
+
+    private engine: Engine<UserFacts, SplashEventHandlers>;
+
+    constructor(facts: UserFacts, eventHandlers: SplashEventHandlers) {
+
+        const rules: RuleMap<UserFacts, SplashEventHandlers> = {
+            whatsNew: new WhatsNewRule(),
+            netPromoter: new NetPromoterRule()
+        };
+
+        this.engine = new Engine(facts, rules, eventHandlers);
+
+    }
+
+    public run() {
+        this.engine.run();
+    }
+
+}
+
+
+export interface SplashEventHandlers extends EventHandlers {
+    readonly onWhatsNew: () => void;
+    readonly onNetPromoter: () => void;
+}
+
+
+/**
+ * Stores a time for each of our event handlers to verify that events
+ * do not occur too often and are ordered properly.
+ */
+type SplashEventTimes = {
+    [id in keyof SplashEventHandlers]: ISODateTimeString | undefined
+};
+
+export interface UserFacts {
+
+    /**
+     * The time the datastore was created.
+     */
+    datastoreCreated: ISODateTimeString;
+
+    /**
+     * The currently running version.
+     */
+    version: string;
+
+    eventTimes: SplashEventTimes;
+
+}
+
+interface WhatsNewState {
+
+    version: string;
+
+}
+
+class WhatsNewRule implements Rule<UserFacts, SplashEventHandlers, WhatsNewState> {
+
+    public run(facts: Readonly<UserFacts>,
+               eventMap: EventMap<SplashEventHandlers>,
+               state?: Readonly<WhatsNewState>): RuleFactPair<UserFacts, WhatsNewState> {
+
+        const updated = state && state.version !== facts.version;
+
+        if (updated) {
+            eventMap.onWhatsNew.handler();
+        }
+
+        state = {version: facts.version};
+
+        return [facts, state];
+
+    }
+
+}
+
+interface NetPromoterState {
+
+}
+
+class NetPromoterRule implements Rule<UserFacts, SplashEventHandlers, NetPromoterState> {
+
+    public run(facts: Readonly<UserFacts>,
+               eventMap: EventMap<SplashEventHandlers>,
+               state?: Readonly<NetPromoterState>): RuleFactPair<UserFacts, NetPromoterState> {
+
+        if (! state) {
+            state = {};
+        }
+
+        const hasExistingAgedDatastore = () => {
+
+            // datastore should be created for at least 7 days.
+
+            if (facts.datastoreCreated) {
+
+                const since = ISODateTimeStrings.parse(facts.datastoreCreated);
+
+                if (TimeDurations.hasElapsed(since, '1w')) {
+                    return true;
+                }
+
+            }
+
+            return false;
+
+        };
+
+        const hasMinimumTimeSince = (epoch: ISODateTimeString | undefined,
+                                     duration: DurationStr,
+                                     defaultValue: boolean = true) => {
+
+            if (epoch) {
+
+                const since = ISODateTimeStrings.parse(epoch);
+                return TimeDurations.hasElapsed(since, duration);
+
+            } else {
+                // our epoch hasn't happened yet so it's ok to send out this message.
+                return defaultValue;
+            }
+
+        };
+
+        const hasMinimumTimeSinceLastEvent = () => {
+
+            const epoch = EventMaps.earliestExecution(eventMap);
+
+            return hasMinimumTimeSince(epoch, '15m');
+
+        };
+
+        const hasMinimumTimeSinceLastNPS = () => {
+            const epoch = eventMap.onNetPromoter.lastExecuted;
+            return hasMinimumTimeSince(epoch, '7d');
+        };
+
+        const canShow = () => {
+
+            if (! hasExistingAgedDatastore()) {
+                return false;
+            }
+
+            if (! hasMinimumTimeSinceLastEvent()) {
+                return false;
+            }
+
+            if (! hasMinimumTimeSinceLastNPS()) {
+                return false;
+            }
+
+            return true;
+
+        };
+
+        if (canShow()) {
+            eventMap.onNetPromoter.handler();
+        }
+
+        return [facts, state];
+
+    }
+
+}
--- apps/repository/js/splash2/rules_engine/EngineTest.ts
+++ apps/repository/js/splash2/rules_engine/EngineTest.ts
@@ -1,20 +1,22 @@
 import {ISODateTimeString} from '../../../../../web/js/metadata/ISODateTimeStrings';
+import {ISODateTimeStrings} from '../../../../../web/js/metadata/ISODateTimeStrings';
 import {assert} from 'chai';
 import {Rule} from './Rule';
 import {RuleFactPair} from './Rule';
 import {RuleMap} from './Engine';
 import {Engine} from './Engine';
-import {RuleOrder} from './Engine';
-import {NULL_FUNCTION} from '../../../../../web/js/util/Functions';
 import {EventMaps} from './Engine';
+import {EventMap} from './Engine';
+import {EventHandlers} from './Engine';
+import {NULL_FUNCTION} from '../../../../../web/js/util/Functions';
 import {isPresent} from '../../../../../web/js/Preconditions';
 import {TestingTime} from '../../../../../web/js/test/TestingTime';
 import {assertJSON} from '../../../../../web/js/test/Assertions';
-import {EventMap} from './Engine';
-import {EventHandlers} from './Engine';
 import {TimeDurations} from '../../../../../web/js/util/TimeDurations';
-import {ISODateTimeStrings} from '../../../../../web/js/metadata/ISODateTimeStrings';
 import {DurationStr} from '../../../../../web/js/util/TimeDurations';
+import {UserFacts} from '../SplashEngine';
+import {SplashEventHandlers} from '../SplashEngine';
+import {SplashEngine} from '../SplashEngine';
 
 describe('Engine', function() {
 
@@ -27,152 +29,6 @@ describe('Engine', function() {
     });
 
     it('basic', function() {
-
-
-        interface SplashEventHandlers extends EventHandlers {
-            readonly onWhatsNew: () => void;
-            readonly onNetPromoter: () => void;
-        }
-
-
-        /**
-         * Stores a time for each of our event handlers to verify that events
-         * do not occur too often and are ordered properly.
-         */
-        type SplashEventTimes = {
-            [id in keyof SplashEventHandlers]: ISODateTimeString | undefined
-        };
-
-        interface UserFacts {
-
-            /**
-             * The time the datastore was created.
-             */
-            datastoreCreated: ISODateTimeString;
-
-            /**
-             * The currently running version.
-             */
-            version: string;
-
-            eventTimes: SplashEventTimes;
-
-        }
-
-        interface WhatsNewState {
-
-            version: string;
-
-        }
-
-        class WhatsNewRule implements Rule<UserFacts, SplashEventHandlers, WhatsNewState> {
-
-            public run(facts: Readonly<UserFacts>,
-                       eventMap: EventMap<SplashEventHandlers>,
-                       state?: Readonly<WhatsNewState>): RuleFactPair<UserFacts, WhatsNewState> {
-
-                const updated = state && state.version !== facts.version;
-
-                if (updated) {
-                    eventMap.onWhatsNew.handler();
-                }
-
-                state = {version: facts.version};
-
-                return [facts, state];
-
-            }
-
-        }
-
-        interface NetPromoterState {
-
-        }
-
-        class NetPromoterRule implements Rule<UserFacts, SplashEventHandlers, NetPromoterState> {
-
-            public run(facts: Readonly<UserFacts>,
-                       eventMap: EventMap<SplashEventHandlers>,
-                       state?: Readonly<NetPromoterState>): RuleFactPair<UserFacts, NetPromoterState> {
-
-                if (! state) {
-                    state = {};
-                }
-
-                const hasExistingAgedDatastore = () => {
-
-                    // datastore should be created for at least 7 days.
-
-                    if (facts.datastoreCreated) {
-
-                        const since = ISODateTimeStrings.parse(facts.datastoreCreated);
-
-                        if (TimeDurations.hasElapsed(since, '1w')) {
-                            return true;
-                        }
-
-                    }
-
-                    return false;
-
-                };
-
-                const hasMinimumTimeSince = (epoch: ISODateTimeString | undefined,
-                                             duration: DurationStr,
-                                             defaultValue: boolean = true) => {
-
-                    if (epoch) {
-
-                        const since = ISODateTimeStrings.parse(epoch);
-                        return TimeDurations.hasElapsed(since, duration);
-
-                    } else {
-                        // our epoch hasn't happened yet so it's ok to send out this message.
-                        return defaultValue;
-                    }
-
-                };
-
-                const hasMinimumTimeSinceLastEvent = () => {
-
-                    const epoch = EventMaps.earliestExecution(eventMap);
-
-                    return hasMinimumTimeSince(epoch, '15m');
-
-                };
-
-                const hasMinimumTimeSinceLastNPS = () => {
-                    const epoch = eventMap.onNetPromoter.lastExecuted;
-                    return hasMinimumTimeSince(epoch, '7d');
-                };
-
-                const canShow = () => {
-
-                    if (! hasExistingAgedDatastore()) {
-                        return false;
-                    }
-
-                    if (! hasMinimumTimeSinceLastEvent()) {
-                        return false;
-                    }
-
-                    if (! hasMinimumTimeSinceLastNPS()) {
-                        return false;
-                    }
-
-                    return true;
-
-                };
-
-                if (canShow()) {
-                    eventMap.onNetPromoter.handler();
-                }
-
-                return [facts, state];
-
-            }
-
-        }
 
         const facts: UserFacts = {
 
@@ -191,18 +47,12 @@ describe('Engine', function() {
         let whatsNewCalled: number = 0;
         let netPromoterCalled: number = 0;
 
-        const rules: RuleMap<UserFacts, SplashEventHandlers> = {
-            whatsNew: new WhatsNewRule(),
-            netPromoter: new NetPromoterRule()
-        };
-
         const eventHandlers: SplashEventHandlers = {
             onWhatsNew: () => ++whatsNewCalled,
             onNetPromoter: () => ++netPromoterCalled,
         };
 
-        const engine: Engine<UserFacts, SplashEventHandlers>
-            = new Engine(facts, rules, eventHandlers);
+        const engine = new SplashEngine(facts, eventHandlers);
 
         engine.run();
 
