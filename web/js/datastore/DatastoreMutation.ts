b7ae940685b029b2e7974500d752564691726de1
burton <burton@spinn3r.com>
2019-05-05 08:09:46 -0700

    ...

--- web/js/datastore/DatastoreMutation.ts
+++ web/js/datastore/DatastoreMutation.ts
@@ -39,12 +39,9 @@ abstract class AbstractDatastoreMutation<T> implements DatastoreMutation<T> {
  */
 export class DefaultDatastoreMutation<T> extends AbstractDatastoreMutation<T> {
 
-    // TODO: refactor and rename class as DatastoreMutations
+    // TODO: refactor and rename class as DatastoreLatchPair
     constructor() {
         super();
-
-        console.error("FIXME: datastore mutation with id N was created here: " + this.id);
-
     }
 
     public readonly written = new Latch<T>();
