c35c5a14950ba22838603b20ada6619b3c26542f
burton <burton@spinn3r.com>
2019-05-06 09:13:44 -0700

    ...

---
+++ build-env.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+export FIREBASE_USER=getpolarized.test+test@gmail.com
+export FIREBASE_PASS=mk9z79vlquixvqd
+
+export GH_TOKEN=26722e632127b9548ce62024686d64c38e2b1994
+export CSC_IDENTITY_AUTO_DISCOVERY=true
+
+## used so that we run tests offscreen so that we don't pollute the UI...
+export SPECTRON_OFFSCREEN=true
