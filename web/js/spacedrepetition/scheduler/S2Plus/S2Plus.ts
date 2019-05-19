da2a7bcea9732ef43a966ba26c221616f48cd467
burton <burton@spinn3r.com>
2019-05-19 08:42:04 -0700

    basic s2 scheduler...

---
+++ web/js/spacedrepetition/scheduler/S2Plus/S2Plus.ts
@@ -0,0 +1,65 @@
+import {Dates} from './Dates';
+
+const GRADE_MIN = 0;
+const GRADE_MAX = 1;
+const GRADE_CUTOFF = 0.6;
+const DEFAULT_DIFFICULTY = 0.3;
+
+/**
+ * https://github.com/pensieve-srs/pensieve-srs
+ * http://www.blueraja.com/blog/477/a-better-spaced-repetition-learning-algorithm-sm2
+ */
+export class S2Plus {
+
+    public static clamp(value: number, min: number, max: number) {
+        return Math.min(Math.max(value, min), max);
+    }
+
+    public static calcRecallRate(reviewedAt: Date, interval: number, today = new Date()) {
+        const diff = Dates.diffDays(today, reviewedAt);
+        const recall = 2 ** (-diff / interval);
+        return Math.ceil(recall * 100) / 100;
+    }
+
+    public static calcPercentOverdue(reviewedAt: Date, interval: number, today = new Date()) {
+        const diff = Dates.diffDays(today, reviewedAt);
+        const calculated = diff / interval;
+        return Math.min(2, calculated);
+    }
+
+    public static calculate(reviewedAt: Date,
+                            prevDifficulty: number,
+                            prevInterval: number,
+                            performanceRating: number,
+                            today = new Date()) {
+
+        const percentOverdue = this.calcPercentOverdue(reviewedAt, prevInterval, today);
+
+        const difficultyDelta = percentOverdue * (1 / 17) * (8 - 9 * performanceRating);
+        const difficulty = this.clamp(prevDifficulty + difficultyDelta, 0, 1);
+
+        const difficultyWeight = 3 - 1.7 * difficulty;
+
+        let intervalDelta;
+        if (performanceRating < GRADE_CUTOFF) {
+            intervalDelta = Math.round(1 / difficultyWeight ** 2) || 1;
+        } else {
+            intervalDelta = 1 + Math.round((difficultyWeight - 1) * percentOverdue);
+        }
+
+        const interval = prevInterval * intervalDelta;
+
+        const nextReviewDate = Dates.addDays(today, interval);
+
+        return {
+            difficulty,
+            interval,
+            nextReviewDate,
+            reviewedAt: today,
+        };
+
+    }
+
+
+}
+
---
+++ web/js/spacedrepetition/scheduler/S2Plus/S2PlusTest.ts
@@ -0,0 +1,114 @@
+
+import {expect} from 'chai';
+import {oneWeekAgo} from './DateConstants';
+import {oneDayAgo} from './DateConstants';
+import {today} from './DateConstants';
+import {Dates} from './Dates';
+import {S2Plus} from './S2Plus';
+
+const testData = [
+    {
+        reviewedAt: oneWeekAgo,
+        interval: 7,
+        percentOverdue: 1,
+        recallRate: 0.5,
+    },
+    {
+        reviewedAt: oneWeekAgo,
+        interval: 14,
+        percentOverdue: 0.5,
+        recallRate: 0.71,
+    },
+    {
+        reviewedAt: oneWeekAgo,
+        interval: 1,
+        percentOverdue: 2,
+        recallRate: 0.01,
+    },
+    {
+        reviewedAt: oneDayAgo,
+        interval: 1,
+        percentOverdue: 1,
+        recallRate: 0.5,
+    },
+    {
+        reviewedAt: oneDayAgo,
+        interval: 4,
+        percentOverdue: 0.25,
+        recallRate: 0.85,
+    },
+    {
+        reviewedAt: today,
+        interval: 4,
+        percentOverdue: 0,
+        recallRate: 1,
+    },
+];
+
+const testDataCalculate = [
+    {
+        reviewedAt: Dates.subtractDays(today, 1),
+        performanceRating: 1,
+        difficulty: 0.5,
+        interval: 1,
+        nextDifficulty: 0.44,
+        nextInterval: 2,
+    },
+    {
+        reviewedAt: Dates.subtractDays(today, 10),
+        performanceRating: 1,
+        difficulty: 0.5,
+        interval: 5,
+        nextDifficulty: 0.38,
+        nextInterval: 20,
+    },
+    {
+        reviewedAt: Dates.subtractDays(today, 18),
+        performanceRating: 0,
+        difficulty: 0.3,
+        interval: 14,
+        nextDifficulty: 0.91,
+        nextInterval: 14,
+    },
+    {
+        reviewedAt: Dates.subtractDays(today, 200),
+        performanceRating: 1,
+        difficulty: 0.3,
+        interval: 100,
+        nextDifficulty: 0.18,
+        nextInterval: 400,
+    },
+];
+
+describe("calcRecallRate", () => {
+    it("it should return recall rate", () => {
+        testData.forEach(({ reviewedAt, interval, recallRate }, i) => {
+            const result = S2Plus.calcRecallRate(reviewedAt, interval);
+            expect(result).to.equal(recallRate);
+        });
+    });
+});
+
+describe("calcPercentOverdue", () => {
+    it("should return the percent overdue for an item reviewed in the past", () => {
+        testData.forEach(({ reviewedAt, interval, percentOverdue }) => {
+            const actual = S2Plus.calcPercentOverdue(reviewedAt, interval);
+            expect(actual).to.equal(percentOverdue);
+        });
+    });
+    it("should return a maximum value of 2", () => {
+        const actual = S2Plus.calcPercentOverdue(oneWeekAgo, 1);
+        expect(actual).to.equal(2);
+    });
+});
+describe("calculate", () => {
+    it("should calculate the next review data", () => {
+        testDataCalculate.forEach(data => {
+            const { reviewedAt, difficulty, interval, performanceRating } = data;
+            const result = S2Plus.calculate(reviewedAt, difficulty, interval, performanceRating, today);
+            expect(result.reviewedAt).to.equal(today);
+            expect(result.interval).to.equal(data.nextInterval);
+            expect(result.difficulty.toFixed(2)).to.equal(data.nextDifficulty.toString());
+        });
+    });
+});
