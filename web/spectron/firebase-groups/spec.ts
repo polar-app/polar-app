b8a06f26ca894967e38612d1aa394da7997916aa
burton <burton@getpolarized.io>
2019-07-09 19:33:48 -0700

    ...

--- web/spectron/firebase-groups/spec.ts
+++ web/spectron/firebase-groups/spec.ts
@@ -1,15 +1,18 @@
 import {Spectron} from '../../js/test/Spectron';
 import {SpectronSpec} from '../../js/test/SpectronSpec';
+import {FirebaseTesting} from "../../js/firebase/FirebaseTesting";
 
 describe('firebase-groups', function() {
 
     Spectron.setup(__dirname);
     this.timeout(240000);
 
+    it('valid users', async function() {
+        FirebaseTesting.validateUsers();
+    });
+
     it('basic tests', async function() {
-
         await SpectronSpec.create(this.app).waitFor(true);
-
     });
 
 });
