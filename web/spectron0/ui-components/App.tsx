2e77b6414ffe7b72417185b2ca719e7d0e36782b
burton <burton@getpolarized.io>
2019-07-25 17:11:10 -0700

    ...

--- web/spectron0/ui-components/App.tsx
+++ web/spectron0/ui-components/App.tsx
@@ -6,6 +6,10 @@ import {SimpleTab} from "../../js/ui/simple_tab/SimpleTab";
 import {Feedback} from "../../js/ui/feedback/Feedback";
 import {NULL_FUNCTION} from "../../js/util/Functions";
 import {UpgradeRequired} from "../../js/ui/account_upgrade/UpgradeRequired";
+import {MessageBox} from "../../js/ui/util/MessageBox";
+import {BlackoutBox} from "../../js/ui/util/BlackoutBox";
+import Button from "reactstrap/lib/Button";
+import {DropdownChevron} from "../../js/ui/util/DropdownChevron";
 
 const styles = {
     swatch: {
@@ -122,7 +126,71 @@ class App<P> extends React.Component<{}, IAppState> {
 
                 {/*<AccountUpgradeBarView plan='free' accountUsage={{storageInBytes: 5000000000}}/>*/}
 
-                <UpgradeRequired planRequired='gold'/>
+                <BlackoutBox>
+
+                    <MessageBox position='top'>
+
+                        <div className="text-center text-grey400 mb-2"
+                             style={{fontSize: '95px'}}>
+
+                            <i className="fas fa-smile"></i>
+
+                        </div>
+
+                        <div className="text-grey700 text-bold mb-3 text-center"
+                             style={{fontSize: '25px', fontWeight: 'bold'}}>
+
+                            It's time to upgrade!
+
+                        </div>
+
+                        <div style={{maxWidth: '400px'}} className="ml-auto mr-auto text-center">
+
+
+                            <p className="">
+                                You've reach the limits of your plan.
+                            </p>
+
+                            <p className="">
+                                You'll need to upgrade to premium to add this
+                                document.
+                            </p>
+
+                            <i className="fas fa-check text-success"></i> More storage for larger repositories. <br/>
+                            <i className="fas fa-check text-success"></i> Supports more devices for cross-device sync.<br/>
+                            <i className="fas fa-check text-success"></i> Helps fund future development of Polar.<br/>
+
+                        </div>
+
+                        <div className="text-center mt-4">
+
+                            <Button color="secondary"
+                                    outline
+                                    size="md"
+                                    onClick={NULL_FUNCTION}
+                                    className="">
+
+                                No Thanks
+
+                            </Button>
+
+                            <Button color="success"
+                                    size="md"
+                                    onClick={NULL_FUNCTION}
+                                    className="ml-1">
+
+                                Upgrade
+
+                            </Button>
+
+                        </div>
+
+
+                    </MessageBox>
+
+                </BlackoutBox>
+
+                {/*<UpgradeRequired planRequired='gold'/>*/}
 
                 {/*<Feedback category='net-promoter-score'*/}
                 {/*          title='How likely are you to recommend Polar?'*/}
