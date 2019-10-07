dea919642a1e92f324f77c8819a924918161771d
burton <burton@getpolarized.io>
2019-10-07 15:43:06 -0700

    ...

--- web/spectron0/ui-components/content.tsx
+++ web/spectron0/ui-components/content.tsx
@@ -1,10 +1,9 @@
 import {SpectronRenderer} from '../../js/test/SpectronRenderer';
 import * as ReactDOM from 'react-dom';
 import * as React from 'react';
-import App from './App';
+import {App} from './App';
 
 import html2canvas from 'html2canvas';
-
 
 SpectronRenderer.run(async () => {
 
