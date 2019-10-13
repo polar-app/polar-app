15d68d9e8273244162ca58ded05952c4d7e67a72
burton <burton@getpolarized.io>
2019-10-13 14:55:28 -0700

    ....

--- apps/preview/index.ts
+++ apps/preview/index.ts
@@ -1,63 +1,17 @@
-import PDFJS, {PDFPageProxy, DocumentInitParameters} from 'pdfjs-dist';
+import PDFJS, {PDFPageProxy, PDFSource} from 'pdfjs-dist';
 import {FilePaths} from "polar-shared/src/util/FilePaths";
 
 import {PDFSinglePageViewer, TextLayerMode} from 'pdfjs-dist/web/pdf_viewer';
 
 PDFJS.GlobalWorkerOptions.workerSrc = '../../node_modules/pdfjs-dist/build/pdf.worker.js';
-// (<any> PDFJS).cMapUrl = '../../node_modules/pdfjs-dist/cmaps/';
-// (<any> PDFJS).cMapPacked = true;
-
-console.log("FIXME: ", PDFJS);
-
-async function doLoad() {
-
-    const url = FilePaths.toURL("/home/burton/projects/polar-app/packages/polar-bookshelf/docs/examples/pdf/availability.pdf");
-
-    const doc = await PDFJS.getDocument(url).promise;
-
-    const page = await doc.getPage(1);
-
-    // const scale = 1;
-    // const viewport = page.getViewport({ scale: scale, });
-
-
-    const calculateScale = (page: PDFPageProxy, desiredWidth: number) => {
-        const viewport = page.getViewport({ scale: 1, });
-        console.log("FIXME: viewport width: " + viewport.width);
-
-        return desiredWidth / viewport.width;
-    };
-
-    const scale = calculateScale(page, 1024);
-    const viewport = page.getViewport({ scale});
-
-    const canvas = document.createElement('canvas');
-    const context = canvas.getContext('2d')!;
-
-    canvas.height = viewport.height;
-    canvas.width = viewport.width;
-
-    const renderContext = {
-        canvasContext: context,
-        viewport: viewport
-    };
-
-    page.render(renderContext);
-
-    document.body.appendChild(canvas);
-
-}
 
 async function doLoad2() {
-
-    // FIXME: this KIND of works and only renders a single page BUT what happens is that
-    // the fonts look like crap and scaled down they will probably look worse.
 
     // FIXME: accept a URL to render..
 
     const url = FilePaths.toURL("/home/burton/projects/polar-app/packages/polar-bookshelf/docs/examples/pdf/availability.pdf");
 
-    const init: DocumentInitParameters = {
+    const init: PDFSource = {
         url,
         cMapPacked: true,
         cMapUrl: '../../node_modules/pdfjs-dist/cmaps/'
@@ -78,36 +32,11 @@ async function doLoad2() {
 
     viewer.setDocument(doc);
 
-    // FIXME: compute the scale from the height or the width (whichever is larger)
-    const calculateScaleFromWidth = (page: PDFPageProxy, desiredWidth: number) => {
-        const viewport = page.getViewport({ scale: 1, });
-        return calculateScale(desiredWidth, viewport.width);
-    };
-
-    const calculateScaleFromHeight = (page: PDFPageProxy, desiredHeight: number) => {
-        const viewport = page.getViewport({ scale: 1, });
-        console.log("FIXME: viewport.height: " + viewport.height);
-
-        return calculateScale(desiredHeight, viewport.height);
-    };
-
     const calculateScale = (to: number, from: number) => {
         return to / from;
     };
 
-    const page = await doc.getPage(1);
-
-    // const width = document.body.clientWidth;
-    // const width = window.innerWidth;
-    // console.log("FIXME: width: " + width);
-    // const scale = calculateScaleFromWidth(page, width) - 1;
-
-    // const height = window.innerHeight;
-    // const height = ;
-    // console.log("FIXME: height: " + height);
     const scale = calculateScale(window.innerHeight, container.offsetHeight);
-    console.log("FIXME: scale: " + scale);
-
 
     viewer.currentScale = scale;
 
