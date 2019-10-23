4af06ae9f9e187f7d87b6831a93f004758112391
burton <burton@getpolarized.io>
2019-10-23 16:04:14 -0700

    add a new host that's allowed for authentication..

--- web/js/apps/main/AuthHosts.ts
+++ web/js/apps/main/AuthHosts.ts
@@ -7,6 +7,7 @@ export class AuthHosts {
 
         return [
             "accounts.google.com",
+            "accounts.youtube.com",
             "polar-32b0f.firebaseapp.com",
             "accountchooser.com",
             "www.accountchooser.com",
