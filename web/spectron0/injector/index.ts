b8aec188be5ef64a6d9dd31e2e9c35ff880411b8
burton <burton@getpolarized.io>
2019-10-24 08:10:42 -0700

    use the current dir so it can be found on windows..

--- web/spectron0/injector/index.ts
+++ web/spectron0/injector/index.ts
@@ -1,5 +1,4 @@
 import {SpectronMain2} from '../../js/test/SpectronMain2';
-import {Messenger} from '../../js/electron/messenger/Messenger';
 import {FilePaths} from 'polar-shared/src/util/FilePaths';
 import {Injector} from 'injector';
 
@@ -8,7 +7,9 @@ SpectronMain2.create().run(async state => {
     await state.window.loadURL(`file://${__dirname}/content.html`);
 
     const path = FilePaths.join(__dirname, "content.js");
-    Injector.inject(state.window, path);
+
+    Injector.inject(state.window, path)
+        .catch(err => console.error(err));
 
 });
 
