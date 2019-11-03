90a78296df21fd26b6d10f76dda7d267df5af851
burton <burton@getpolarized.io>
2019-11-03 08:15:53 -0800

    Fixed compilation issues...

--- apps/repository/js/annotation_repo/RepoAnnotationMetaView.tsx
+++ apps/repository/js/annotation_repo/RepoAnnotationMetaView.tsx
@@ -68,8 +68,8 @@ export class RepoAnnotationMetaView extends React.Component<IProps, IState> {
                         </div>
 
                         <div>
-                            <DocThumbnail docInfo={repoAnnotation.docInfo}
-                                          persistenceLayerProvider={() => this.props.persistenceLayerManager.get()}/>
+                            {/*<DocThumbnail docInfo={repoAnnotation.docInfo}*/}
+                            {/*              persistenceLayerProvider={() => this.props.persistenceLayerManager.get()}/>*/}
                         </div>
 
                     </div>
