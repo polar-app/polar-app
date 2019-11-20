dd090e11d94fd85fe72ab26aa327f9b83d7c505e
burton <burton@getpolarized.io>
2019-11-19 16:47:09 -0800

    ...

--- credentials.sh
+++ credentials.sh
@@ -1,3 +1,11 @@
 
 export GH_TOKEN=26722e632127b9548ce62024686d64c38e2b1994
 export CSC_IDENTITY_AUTO_DISCOVERY=true
+
+
+export APPLEID=burtonator2011@gmail.com
+
+
+if [ "${APPLEIDPASS}" = ""]; then
+  echo "ERROR: Must set APPLEIDPASS. Hint: pass is F*******3"
+fi
