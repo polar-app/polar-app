6295950ee676491d3a08639338ba59c00e4e30f8
burtonator <burtonator@gmail.com>
2019-11-30 17:18:16 -0800

    upgrade button should work...

--- apps/repository/js/repo_header/UpgradeAccountButton.tsx
+++ apps/repository/js/repo_header/UpgradeAccountButton.tsx
@@ -1,11 +1,14 @@
 import * as React from 'react';
 import {Button} from 'reactstrap';
 import {AccountProvider} from "../../../../web/js/accounts/AccountProvider";
+import {RendererAnalytics} from "../../../../web/js/ga/RendererAnalytics";
 
 export class UpgradeAccountButton extends React.PureComponent<IProps, IState> {
 
     constructor(props: IProps, context: any) {
         super(props, context);
+
+        this.onUpgrade = this.onUpgrade.bind(this);
     }
 
     public render() {
@@ -20,13 +23,22 @@ export class UpgradeAccountButton extends React.PureComponent<IProps, IState> {
         return (
             <Button color="light"
                     size="md"
+                    onClick={() => this.onUpgrade()}
                     className="border border-success">
+
+                {/*<i className="fas fa-certificate"/>*/}
+
                 Upgrade Account
+
             </Button>
         );
 
     }
 
+    private onUpgrade() {
+        RendererAnalytics.event({category: 'premium', action: 'upgrade-account-button'});
+        document.location.hash = "plans";
+    }
 
 }
 
