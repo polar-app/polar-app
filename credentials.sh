b0a25aff44bd3a126fbcd485cc9f5846f2897dc2
burtonator <burtonator@gmail.com>
2019-12-16 19:47:11 -0800

    ...

--- credentials.sh
+++ credentials.sh
@@ -3,6 +3,7 @@ export GH_TOKEN=26722e632127b9548ce62024686d64c38e2b1994
 export CSC_IDENTITY_AUTO_DISCOVERY=true
 
 export APPLEID=burtonator2011@gmail.com
+export APPLEIDPASS=nlrs-matd-xulq-hvur
 
 if [ "${TERM_PROGRAM}" = "Apple_Terminal" ] && [ "${APPLEIDPASS}" = "" ]; then
   echo "ERROR: Must set APPLEIDPASS. Hint: pass is F*******3"
