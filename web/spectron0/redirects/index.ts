e5faf5f1395facc2b1e8021d1dd65c693caa8333
burton <burton@getpolarized.io>
2019-12-19 10:56:45 -0800

    basic tests...

--- web/spectron0/redirects/index.ts
+++ web/spectron0/redirects/index.ts
@@ -9,5 +9,24 @@ app.on('ready', async () => {
     //
     // console.log("READY");
 
+    // const url = "https://github.com/burtonator/polar-bookshelf/releases/download/v1.80.3/latest-mac.yml";
+    // const url = "https://github.com/burtonator/polar-bookshelf/releases/download/v1.80.3/Polar-Bookshelf-1.80.3-mac.zip";
+    // const url = "https://github.com/burtonator/polar-bookshelf/releases/download/v1.80.3/Polar-Bookshelf-1.80.3.dmg";
+    const url = "https://github.com/burtonator/polar-bookshelf/releases/download/v1.80.3/Polar-Bookshelf-1.80.3.dmg.blockmap";
+
+    const request = net.request({url, redirect: 'follow'});
+    request.on('response', (response) => {
+        console.log(`STATUS: ${response.statusCode}`);
+        console.log(`HEADERS: ${JSON.stringify(response.headers)}`);
+        response.on('data', (chunk) => {
+            // console.log(`BODY: ${chunk}`);
+            console.log("got chunk");
+        })
+        response.on('end', () => {
+            console.log('No more data in response.');
+        });
+    });
+    request.end();
+
 });
 
