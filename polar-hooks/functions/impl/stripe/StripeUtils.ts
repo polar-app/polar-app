f32c3e4c4f41e19cbc75993718dec6e551948068
burton <burton@getpolarized.io>
2019-12-21 12:08:53 -0800

    changed to console.log instead of console.warn

--- polar-hooks/functions/impl/stripe/StripeUtils.ts
+++ polar-hooks/functions/impl/stripe/StripeUtils.ts
@@ -9,11 +9,11 @@ function getStripeKey() {
     }
 
     if (testMode || process.env.STRIPE_TEST === 'true') {
-        console.warn("Using STRIPE_TEST credentials");
+        console.log("Using STRIPE_TEST credentials");
         return "sk_test_4qvDsYhxF53NhLCzcGtwaJA100PQdzhkoG";
     }
 
-    console.warn("Using production stripe credentials");
+    console.log("Using production stripe credentials");
 
     return 'sk_live_6N0oXmsdPxKS6gbUhwHFLncD00Gv3ztN4A';
 
