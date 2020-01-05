b968c79692009237dcf4ef9b8c4f023fd7e1ef6b
burtonator <burtonator@gmail.com>
2020-01-04 19:05:00 -0800

    more padding for tablets...

--- apps/repository/js/repo_footer/RepoFooter.tsx
+++ apps/repository/js/repo_footer/RepoFooter.tsx
@@ -1,43 +1,42 @@
 import * as React from 'react';
 import {BottomNavButton} from "./BottomNavButton";
 import {DeviceRouter} from "../../../../web/js/ui/DeviceRouter";
+import {Devices} from "../../../../web/js/util/Devices";
 
-/**
- * Simple header for the repository which supports arbitrary children.
- */
-export class RepoFooter extends React.PureComponent<IProps> {
+export const RepoFooter = () => {
 
-    public render() {
+    const style: React.CSSProperties = {
+        width: '100%',
+        backgroundColor: 'var(--primary-background-color)',
+    };
 
-        const Delegate =
-            <footer className="border-top text-lg"
-                    style={{
-                        width: '100%',
-                        backgroundColor: 'var(--primary-background-color)',
-                    }}>
-
-                <div className=""
-                     style={{
-                         display: 'flex'
-                     }}>
-
-                    <BottomNavButton pathname="/annotations" icon="fas fa-home" replace={true}/>
-                    <BottomNavButton pathname="/stats" icon="fas fa-chart-line" replace={true}/>
-                    <BottomNavButton pathname="/settings" icon="fas fa-cog" replace={true}/>
-
-                </div>
-
-            </footer>;
-
-        return (
-
-            <DeviceRouter phone={Delegate} tablet={Delegate}/>
-
-        );
-
+    if (Devices.get() === 'tablet') {
+        // TODO: maybe do this via CSS
+        style.paddingTop = '0.5em';
+        style.paddingBottom = '0.5em';
     }
 
-}
+    const Delegate =
+        <footer className="border-top text-lg"
+                style={style}>
 
-interface IProps {
-}
+            <div className=""
+                 style={{
+                     display: 'flex'
+                 }}>
+
+                <BottomNavButton pathname="/annotations" icon="fas fa-home" replace={true}/>
+                <BottomNavButton pathname="/stats" icon="fas fa-chart-line" replace={true}/>
+                <BottomNavButton pathname="/settings" icon="fas fa-cog" replace={true}/>
+
+            </div>
+
+        </footer>;
+
+    return (
+
+        <DeviceRouter phone={Delegate} tablet={Delegate}/>
+
+    );
+
+};
