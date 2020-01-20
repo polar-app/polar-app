267e5725c961d062c30bdd489896ef5d72268c33
burtonator <burtonator@gmail.com>
2020-01-20 10:23:42 -0800

    improved logging when adding files...

--- polar-hooks/functions/impl/docs/DocAddFunctions.ts
+++ polar-hooks/functions/impl/docs/DocAddFunctions.ts
@@ -7,6 +7,8 @@ export const DocAddFunction = functions.https.onRequest(async (req, res) => {
 
     // TODO: accept a POST here with the data int the body with the proper mine type
 
+    console.log(`Handling url ${req.url} from originalURL: ${req.originalUrl}`);
+
     const parsedURL = AddURLs.parse(req.url);
     
     if (! parsedURL) {
@@ -15,10 +17,8 @@ export const DocAddFunction = functions.https.onRequest(async (req, res) => {
 
     const importedDoc = await DatastoreFetchImports.doFetch(parsedURL.target);
 
-
     console.log("Parsed URL as: " + JSON.stringify(parsedURL, null, "   "));
     console.log("Imported doc to: " + JSON.stringify(importedDoc, null, "   "));
-
 
     const {url} = importedDoc;
 
