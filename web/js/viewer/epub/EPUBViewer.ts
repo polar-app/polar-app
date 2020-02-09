0fd986f60bc5d74c87bac202eeb5adc5ce252c28
burtonator <burtonator@gmail.com>
2020-02-09 12:19:05 -0700

    getting closer now...

--- web/js/viewer/epub/EPUBViewer.ts
+++ web/js/viewer/epub/EPUBViewer.ts
@@ -2,6 +2,9 @@ import {Viewer} from "../Viewer";
 import {DocDetail} from "../../metadata/DocDetail";
 import {Model} from "../../model/Model";
 import ePub from 'epubjs';
+import { Logger } from "polar-shared/src/logger/Logger";
+
+const log = Logger.create();
 
 export class EPUBViewer extends Viewer {
 
@@ -20,6 +23,7 @@ export class EPUBViewer extends Viewer {
         console.log("Starting the epub viewer");
 
         const book = ePub("file:///Users/burton/Downloads/pg61335-images.epub");
+        // const book = ePub("file:///Users/burton/Downloads/package.opf");
 
         const pageElement = document.querySelector(".page")!;
 
@@ -27,7 +31,80 @@ export class EPUBViewer extends Viewer {
         // const rendition = book.renderTo(pageElement, { flow: "continuous", width: '100%', height: '100%'});
         const rendition = book.renderTo(pageElement, { flow: "scrolled-doc", width: '100%', height: '100%'});
 
-        const displayed = rendition.display();
+        const handle = async () => {
+            await rendition.display();
+            await rendition.next();
+
+            const metadata = await book.loaded.metadata;
+
+            console.log({metadata});
+
+            const navigation = await book.loaded.navigation;
+
+            console.log("toc: ", navigation.toc);
+
+            const pageList = await book.loaded.pageList;
+
+            console.log("pageList: ", pageList);
+
+        };
+
+        handle()
+            .catch(err => log.error(err));
+
+        //
+        // const next = document.getElementById("next")!;
+        // next.addEventListener("click", function(e) {
+        //     rendition.next();
+        //     e.preventDefault();
+        // }, false);
+        //
+        // const prev = document.getElementById("prev")!;
+        // prev.addEventListener("click", function(e) {
+        //     rendition.prev();
+        //     e.preventDefault();
+        // }, false);
+        //
+        // rendition.on("relocated", function(location: any) {
+        //     console.log(location);
+        // });
+
+        rendition.on("rendered", function(section: any) {
+            //
+            // const nextSection = section.next();
+            // const prevSection = section.prev();
+            //
+            // if (nextSection) {
+            //
+            //     const nextNav = book.navigation.get(nextSection.href);
+            //
+            //     if (nextNav) {
+            //         nextLabel = nextNav.label;
+            //     } else {
+            //         nextLabel = "next";
+            //     }
+            //
+            //     next.textContent = nextLabel + " »";
+            // } else {
+            //     next.textContent = "";
+            // }
+            //
+            // if (prevSection) {
+            //     prevNav = book.navigation.get(prevSection.href);
+            //
+            //     if(prevNav) {
+            //         prevLabel = prevNav.label;
+            //     } else {
+            //         prevLabel = "previous";
+            //     }
+            //
+            //     prev.textContent = "« " + prevLabel;
+            // } else {
+            //     prev.textContent = "";
+            // }
+
+        });
+
 
     }
 
