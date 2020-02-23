68ded6e6fc44998f8a6e854d8dde7034f557a0e5
burtonator <burtonator@gmail.com>
2020-02-22 18:20:01 -0700

    init the firebase backends... initialize the backend...

--- apps/repository/js/reviewer/FirestoreCollections.ts
+++ apps/repository/js/reviewer/FirestoreCollections.ts
@@ -3,6 +3,7 @@ import {SpacedReps} from "polar-firebase/src/firebase/om/SpacedReps";
 import {SpacedRepStats} from "polar-firebase/src/firebase/om/SpacedRepStats";
 import {FirestoreLike} from "polar-firebase/src/firebase/Collections";
 import {isPresent} from "polar-shared/src/Preconditions";
+import {DocPreviews} from "polar-firebase/src/firebase/om/DocPreviews";
 
 export class FirestoreCollections {
 
@@ -11,7 +12,7 @@ export class FirestoreCollections {
         // TODO: dependency injection would rock here.
         const firestore = await Firestore.getInstance();
 
-        for (const firestoreBacked of [SpacedReps, SpacedRepStats]) {
+        for (const firestoreBacked of [SpacedReps, SpacedRepStats, DocPreviews]) {
             if (isPresent(firestoreBacked.firestoreProvider)) {
                 // already configured
                 continue;
