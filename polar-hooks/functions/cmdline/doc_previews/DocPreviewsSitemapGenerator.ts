325b4a6359d0dfaecac1382b416d3e7acb3774af
burtonator <burtonator@gmail.com>
2020-02-23 12:26:18 -0700

    - TODO for collections so that we can just list if we wanted (in the future) - ability to write write the sitemap generator...

---
+++ polar-hooks/functions/cmdline/doc_previews/DocPreviewsSitemapGenerator.ts
@@ -0,0 +1,36 @@
+import {FirebaseAdmin} from "../../impl/util/FirebaseAdmin";
+import {DocPreview} from "polar-firebase/src/firebase/om/DocPreviews";
+import {DocPreviewURLs} from "polar-webapp-links/src/docs/DocPreviewURLs";
+
+export class DocPreviewsSitemapGenerator {
+
+    public static async generate() {
+
+        const app = FirebaseAdmin.app();
+
+        const firestore = app.firestore()
+
+        const snapshot = await firestore.collection('doc_preview')
+                                    .limit(1000)
+                                    .get();
+
+        const docPreviews = snapshot.docs.map(doc => doc.data() as DocPreview);
+
+        for (const docPreview of docPreviews) {
+
+            const url = DocPreviewURLs.create({
+                id: docPreview.urlHash,
+                category: docPreview.category,
+                title: docPreview.title
+            });
+
+            console.log(url);
+
+        }
+
+    }
+
+}
+
+DocPreviewsSitemapGenerator.generate()
+    .catch(err => console.error(err));
