afa3771dc26253e67d2570000e8991385174c95e
burtonator <burtonator@gmail.com>
2020-02-22 19:29:58 -0700

    - load the docPreview structure now...

--- apps/preview/index.ts
+++ apps/preview/index.ts
@@ -5,7 +5,8 @@ import {PDFSinglePageViewer} from 'pdfjs-dist/web/pdf_viewer';
 import {DocPreviewURLs} from "polar-webapp-links/src/docs/DocPreviewURLs";
 import {
     DocPreview,
-    DocPreviews
+    DocPreviews,
+    BaseDocPreview, DocPreviewCached
 } from "polar-firebase/src/firebase/om/DocPreviews";
 
 PDFJS.GlobalWorkerOptions.workerSrc = '../../node_modules/pdfjs-dist/build/pdf.worker.js';
@@ -26,30 +27,36 @@ function getURL(): string {
 
 }
 
-async function getDocPreview(): Promise<DocPreview> {
+async function getDocPreview(): Promise<DocPreviewCached> {
 
     const parsedURL = DocPreviewURLs.parse(document.location.href);
 
     if (parsedURL) {
 
-        const docPreview = await DocPreviews.get(parsedURL.hashcode);
+        const docPreview = await DocPreviews.get(parsedURL.id);
 
         if (! docPreview) {
-            throw new Error("No doc for: " + parsedURL.hashcode);
+            throw new Error("No doc for: " + parsedURL.id);
+        }
+
+        if (! docPreview.cached) {
+            throw new Error("Doc not cached: " + parsedURL.id);
         }
 
         return docPreview;
 
     }
 
-    const hashcode = '12345';
+    const urlHash = '01234';
+    const docHash = '56789';
     const datastoreURL = FilePaths.toURL("/Users/burton/projects/polar-app/packages/polar-bookshelf/docs/examples/pdf/availability.pdf");
 
     return {
+        urlHash,
+        docHash,
+        datastoreURL,
         cached: true,
-        fingerprint: hashcode,
-        hashcode,
-        datastoreURL
+        url: datastoreURL
     };
 
 }
@@ -59,13 +66,10 @@ interface DocMetadata {
     readonly description: string;
 }
 
-// FIXME: get the DocInfo from either the URL command line or firebase.
-
 async function doLoad2() {
 
-    // FIXME: accept a URL to render..
-
-    const url = getURL();
+    const docPreview = await getDocPreview();
+    const url = docPreview.datastoreURL;
 
     const init: DocumentInitParameters = {
         url,
