3abf2ef3882bb6aa93ecf7446c7ba9c0f8801654
burtonator <burtonator@gmail.com>
2020-02-22 18:05:27 -0700

    This should work including the full path through the system...

--- polar-hooks/functions/impl/docs/DocPreviewFunctions.ts
+++ polar-hooks/functions/impl/docs/DocPreviewFunctions.ts
@@ -3,6 +3,7 @@ import {AddURLs} from "polar-webapp-links/src/docs/AddURLs";
 import {DatastoreFetchImports} from "../datastore/DatastoreFetchImports";
 import {ExpressRequests} from "../util/ExpressRequests";
 import {DocPreviewURLs} from "polar-webapp-links/src/docs/DocPreviewURLs";
+import {DocPreviews} from "polar-firebase/src/firebase/om/DocPreviews";
 
 export const DocPreviewFunction = functions.https.onRequest(async (req, res) => {
 
@@ -18,14 +19,49 @@ export const DocPreviewFunction = functions.https.onRequest(async (req, res) => 
         throw new Error("Wrong URL");
     }
 
+    // this will return immediately due to the cache after the first
     const importedDoc = await DatastoreFetchImports.doFetch(parsedURL.target);
+
+    const docPreview = await DocPreviews.get(importedDoc.hashcode);
 
     console.log("Parsed URL as: " + JSON.stringify(parsedURL, null, "   "));
     console.log("Imported doc to: " + JSON.stringify(importedDoc, null, "   "));
 
-    const {url} = importedDoc;
+    const updateDocPreviewCache = async () => {
 
-    const redirectURL = DocPreviewURLs.create({hashcode: importedDoc.hashcode});
+        if (! docPreview) {
+            // this is a raw URL
+            return;
+        }
+
+        if (docPreview.cached) {
+            // already cached.
+            return;
+        }
+
+        console.log("Updating doc_preview cache");
+
+        await DocPreviews.set({
+            ...docPreview,
+            cached: true,
+            datastoreURL: importedDoc.docURL,
+        });
+
+    };
+
+    await updateDocPreviewCache();
+
+    const createRedirectURL = () => {
+
+        if (docPreview) {
+            return DocPreviewURLs.create(docPreview);
+        } else {
+            return DocPreviewURLs.create({hashcode: importedDoc.hashcode});
+        }
+
+    };
+
+    const redirectURL = createRedirectURL();
 
     console.log("Sending redirect to: " + redirectURL);
 
