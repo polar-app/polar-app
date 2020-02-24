aedd7a1d105aa807b88f25d70c622b96c6612178
burtonator <burtonator@gmail.com>
2020-02-24 11:24:33 -0700

    new SSR which should work now...

--- polar-hooks/functions/impl/impl/ssr/SSRFunction.ts
+++ polar-hooks/functions/impl/impl/ssr/SSRFunction.ts
@@ -1,15 +1,37 @@
 import * as functions from "firebase-functions";
 import express from 'express';
+import {Callback} from "polar-shared/src/util/Functions";
+import {Webapps} from "../../webapp/Webapps";
 
-const app = express();
+let app = express();
 
-console.log("SSR: Running with minimal SSR function");
+console.log("SSR: Running with production SSR function");
+
+const prerender =
+    require('prerender-node')
+        .set('prerenderToken', 'nHFtg5f01o0FJZXDtAlR')
+        .set('beforeRender', function(req: express.Request, done: Callback) {
+            console.log("SSR: beforeRender");
+            done();
+        })
+        .set('afterRender', function(err: Error | undefined, req: express.Request, done: Callback) {
+
+            if (err) {
+                console.log("SSR: afterRender: FAIL: ", err);
+            } else {
+                console.log("SSR: afterRender: SUCCESS");
+            }
+            done();
+        });
+
+console.log("Running with crawler user agents: ", prerender.crawlerUserAgents);
 
 app.use((req: express.Request, res: express.Response, next: express.NextFunction) => {
-    console.log("SSR: : handling basic request");
-    res.status(200).send('Hello World\n');
-    next();
+    console.log("SSR: handling prerender");
+    prerender(req, res, next);
 });
+
+app = Webapps.createWebapp(app);
 
 // https://expressjs.com/en/guide/error-handling.html
 // add a better error handler as it might be that Google isn't logging
