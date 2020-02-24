6bf8fbb06fe9a02cf5c5ccafd3b7cff8f62abf6c
burtonator <burtonator@gmail.com>
2020-02-23 20:50:05 -0700

    using new syntax to see if this is the bug

--- polar-hooks/functions/impl/impl/ssr/SSRFunction.ts
+++ polar-hooks/functions/impl/impl/ssr/SSRFunction.ts
@@ -1,15 +1,7 @@
 import {createWebapp} from "../../webapp/Webapp";
 import * as functions from "firebase-functions";
 
-const prerender = require('prerender-node');
-
-prerender.set('prerenderToken', 'nHFtg5f01o0FJZXDtAlR');
-prerender.crawlerUserAgents.push('googlebot');
-prerender.crawlerUserAgents.push('bingbot');
-prerender.crawlerUserAgents.push('yandex');
-prerender.crawlerUserAgents.push('twitterbot');
-
 const app = createWebapp();
-app.use(prerender);
+app.use(require('prerender-node').set('prerenderToken', 'nHFtg5f01o0FJZXDtAlR'));
 
 export const SSRFunction = functions.https.onRequest(app);
