7a750773afe009b2a9327e790a97e543d1f06c1a
burtonator <burtonator@gmail.com>
2020-02-27 23:07:58 -0700

    the basic file loading works now.

--- web/js/apps/main/file_loaders/DefaultFileLoader.ts
+++ web/js/apps/main/file_loaders/DefaultFileLoader.ts
@@ -2,6 +2,7 @@ import {FileLoader} from './FileLoader';
 import {CacheRegistry} from '../../../backend/proxyserver/CacheRegistry';
 import {PHZLoader} from './PHZLoader';
 import {PDFLoader} from './PDFLoader';
+import {EPUBLoader} from './EPUBLoader';
 import {LoadedFile} from './LoadedFile';
 import {FilePaths} from 'polar-shared/src/util/FilePaths';
 import {FileRegistry} from "polar-shared-webserver/src/webserver/FileRegistry";
@@ -16,12 +17,15 @@ export class DefaultFileLoader extends FileLoader {
 
     private readonly phzLoader: PHZLoader;
 
+    private readonly epubLoader: EPUBLoader;
+
     constructor(fileRegistry: FileRegistry, cacheRegistry: CacheRegistry) {
         super();
         this.fileRegistry = fileRegistry;
         this.cacheRegistry = cacheRegistry;
         this.pdfLoader = new PDFLoader(fileRegistry);
         this.phzLoader = new PHZLoader(cacheRegistry, fileRegistry);
+        this.epubLoader = new EPUBLoader(fileRegistry);
     }
 
     public async registerForLoad(path: string): Promise<LoadedFile> {
@@ -30,6 +34,8 @@ export class DefaultFileLoader extends FileLoader {
             return this.pdfLoader.registerForLoad(path);
         } else if (FilePaths.hasExtension(path, "phz")) {
             return this.phzLoader.registerForLoad(path);
+        } else if (FilePaths.hasExtension(path, "epub")) {
+            return this.epubLoader.registerForLoad(path);
         } else {
             throw new Error("Unable to handle file: " + path);
         }
