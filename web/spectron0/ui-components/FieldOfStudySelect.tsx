dd1a994b3e2b96fac1fe5a6524ec67aa9ea757ce
burtonator <burtonator@gmail.com>
2020-03-18 08:53:00 -0600

    Now have a field of study selector...

---
+++ web/spectron0/ui-components/FieldOfStudySelect.tsx
@@ -0,0 +1,93 @@
+import * as React from "react";
+import Select from "react-select";
+import {nullToUndefined} from "polar-shared/src/util/Nullable";
+
+export interface IOption<T> {
+    readonly value: T;
+    readonly label: string;
+}
+
+const fieldsOfStudy = [
+    "Agricultural and Biological Sciences",
+    "Arts and Humanities",
+    "Biochemistry, Genetics and Molecular Biology",
+    "Business, Management and Accounting",
+    "Chemical Engineering",
+    "Chemistry",
+    "Computer Science",
+    "Decision Sciences",
+    "Design",
+    "Earth and Planetary Sciences",
+    "Economics, Econometrics and Finance",
+    "Energy",
+    "Engineering",
+    "Environmental Science",
+    "Immunology and Microbiology",
+    "Linguistics",
+    "Materials Science",
+    "Mathematics",
+    "Medicine and Dentistry",
+    "Neuroscience",
+    "Nursing and Health Professions",
+    "Pharmacology, Toxicology and Pharmaceutical Science",
+    "Philosophy",
+    "Physics and Astronomy",
+    "Psychology",
+    "Social Sciences",
+    "Sports and Recreations",
+    "Veterinary Science and Veterinary Medicine",
+];
+
+export interface FieldOfStudy {
+    readonly id: string;
+    readonly name: string;
+}
+
+
+function toFieldOfStudy(name: string): FieldOfStudy {
+    const id = name.toLowerCase()
+                   .replace(/[ ,]/g, "-")
+                   .replace(/[-]+/g, '-');
+    return {id, name};
+}
+
+function toOption(fieldOfStudy: FieldOfStudy): IOption<FieldOfStudy> {
+    return {
+        value: fieldOfStudy,
+        label: fieldOfStudy.name
+    };
+}
+
+const options: ReadonlyArray<IOption<FieldOfStudy>>
+    = fieldsOfStudy.map(toFieldOfStudy)
+                   .map(toOption);
+
+
+
+interface IProps {
+    /**
+     *
+     */
+    readonly onSelect: (option: IOption<FieldOfStudy> | undefined) => void;
+}
+
+
+export const FieldOfStudySelect = (props: IProps) => {
+
+    type RawOption = IOption<FieldOfStudy> | null;
+
+    return (
+        <Select
+            isClearable
+            autoFocus
+            placeholder="Search for a field of study..."
+            options={options}
+            onChange={(option => props.onSelect(nullToUndefined(option as RawOption)))}
+            // onKeyDown={event => props.onKeyDown(event)}
+            // onChange={(selectedOptions) => props.handleChange(selectedOptions as TagOption[])}
+            // value={props.pendingTagOptions}
+            // defaultValue={props.pendingTagOptions}
+        />
+    );
+};
+
--- web/spectron0/ui-components/SchoolSelectDemo.tsx
+++ web/spectron0/ui-components/SchoolSelectDemo.tsx
@@ -1,83 +1,19 @@
-import AsyncSelect from 'react-select/async';
 import * as React from "react";
-import {Universities, University} from "polar-shared/src/util/Universities";
-import {arrayStream} from "polar-shared/src/util/ArrayStreams";
-import {isPresent} from "polar-shared/src/Preconditions";
-
-const LIMIT = 25;
-
-interface IOption<T> {
-    readonly value: T;
-    readonly label: string;
-}
-
-const universities
-    = arrayStream(Universities.get())
-        .sort(((a, b) => a.name.localeCompare(b.name)))
-        .collect();
-
-function toOption(university: University): IOption<University> {
-    return {
-        value: university,
-        label: university.name
-    };
-}
-
-const options: ReadonlyArray<IOption<University>>
-    = arrayStream(universities)
-        .map(toOption)
-        .collect();
-
-type OptionsCallback<T> = (options: ReadonlyArray<IOption<T>>) => void;
-
-function loadOptions(inputValue: string, callback: OptionsCallback<University>) {
-    callback(Loader.filter(inputValue));
-}
-
-class Loader {
-
-    public static defaultOptions() {
-
-        // no real query string so just return the top items.
-        return arrayStream(options)
-            .head(LIMIT)
-            .collect();
-
-    }
-
-    public static filter(inputString: string) {
-
-        if (! inputString || inputString.trim() === '') {
-            this.defaultOptions();
-        }
-
-        const predicate = (option: IOption<University>) => {
-
-            return isPresent(option.label) &&
-                option.label.toLowerCase().indexOf(inputString.toLowerCase()) !== -1;
-
-        };
-
-        return arrayStream(options)
-            .filter(predicate)
-            .head(LIMIT)
-            .collect();
-    }
-
-}
+import {UniversitySelect} from "./UniversitySelect";
+import {FieldOfStudySelect} from "./FieldOfStudySelect";
 
 export const SchoolSelectDemo = () => (
-    <AsyncSelect
-        isClearable
-        autoFocus
-        cacheOptions
-        placeholder="Select a university..."
-        defaultOptions={Loader.defaultOptions()}
-        loadOptions={loadOptions}
-        // onKeyDown={event => props.onKeyDown(event)}
-        // onChange={(selectedOptions) => props.handleChange(selectedOptions as TagOption[])}
-        // value={props.pendingTagOptions}
-        // defaultValue={props.pendingTagOptions}
-    />
+
+    <div>
+
+        <div className="m-1">
+            <FieldOfStudySelect onSelect={selected => console.log({selected})}/>
+        </div>
+
+        <div className="m-1">
+            <UniversitySelect onSelect={selected => console.log({selected})}/>
+        </div>
+
+    </div>
 );
 
