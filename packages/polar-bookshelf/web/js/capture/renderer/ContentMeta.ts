b4cca203dad0ea2e09964c101cf5d2cac53a064e
Kevin Burton <burtonator@gmail.com>
2020-04-02 21:27:16 +0000

    Imported polar-app as a subtree.

--- packages/polar-bookshelf/web/js/capture/renderer/ContentMeta.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Represents metadata about a HTML page including the title, description,
- * image, etc.
- */
-export interface ContentMeta {
-    title?: string;
-    description?: string;
-}
