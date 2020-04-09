7606a0a931c3895590ae2876fd7712404b726813
burtonator <burtonator@gmail.com>
2020-04-08 18:51:45 -0600

    the flashcards and comments display now...

--- web/js/annotation_sidebar/DocAnnotationLoader.ts
+++ web/js/annotation_sidebar/DocAnnotationLoader.ts
@@ -7,6 +7,8 @@ import {IAreaHighlight} from "polar-shared/src/metadata/IAreaHighlight";
 import {IPageMeta} from "polar-shared/src/metadata/IPageMeta";
 import {ArrayStreams} from "polar-shared/src/util/ArrayStreams";
 import {ITextHighlight} from "polar-shared/src/metadata/ITextHighlight";
+import {IComment} from "polar-shared/src/metadata/IComment";
+import {IFlashcard} from "polar-shared/src/metadata/IFlashcard";
 
 /**
  * Handles updating the DocAnnotationIndex when we receive new updates to DocMeta.
@@ -35,10 +37,20 @@ export class DocAnnotationLoader {
             return DocAnnotations.createFromTextHighlight(docMeta, annotation, pageMeta);
         };
 
+        const createCommentConverter = (pageMeta: IPageMeta) => (annotation: IComment) => {
+            return DocAnnotations.createFromComment(docMeta, annotation, pageMeta);
+        };
+
+        const createFlashcardConverter = (pageMeta: IPageMeta) => (annotation: IFlashcard) => {
+            return DocAnnotations.createFromFlashcard(docMeta, annotation, pageMeta);
+        };
+
         for (const pageMeta of Object.values(docMeta.pageMetas)) {
 
             const areaHighlightConverter = createAreaHighlightConverter(pageMeta);
             const textHighlightConverter = createTextHighlightConverter(pageMeta);
+            const commentConverter = createCommentConverter(pageMeta);
+            const flashcardConverter = createFlashcardConverter(pageMeta);
 
             ArrayStreams.ofMapValues(pageMeta.areaHighlights)
                         .map(current => areaHighlightConverter(current))
@@ -46,6 +58,14 @@ export class DocAnnotationLoader {
 
             ArrayStreams.ofMapValues(pageMeta.textHighlights)
                 .map(current => textHighlightConverter(current))
+                .transferTo(values => docAnnotationIndex.put(...values));
+
+            ArrayStreams.ofMapValues(pageMeta.comments)
+                .map(current => commentConverter(current))
+                .transferTo(values => docAnnotationIndex.put(...values));
+
+            ArrayStreams.ofMapValues(pageMeta.flashcards)
+                .map(current => flashcardConverter(current))
                 .transferTo(values => docAnnotationIndex.put(...values));
 
         }
