e79c585f107faf869c14a485bd37ef43f98b8cbb
burtonator <burtonator@gmail.com>
2020-04-22 10:40:02 -0600

    Fixed color button..

--- web/js/ui/colors/ColorButton.tsx
+++ web/js/ui/colors/ColorButton.tsx
@@ -1,66 +1,26 @@
 import * as React from 'react';
 import {NULL_FUNCTION} from 'polar-shared/src/util/Functions';
+import {createStyles, makeStyles, Theme} from "@material-ui/core/styles";
 
-export class ColorButton extends React.PureComponent<IProps, IState> {
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
 
-    constructor(props: IProps, context: any) {
-        super(props, context);
+        selected: {
+            border: `2px solid ${theme.palette.info.dark}`
+        },
 
-    }
+        notSelected: {
+            border: '2px solid rgba(0, 0, 0, 0.1)'
+        }
 
-    public render() {
+    })
+);
 
-        const {props} = this;
 
-        const createBackgroundColor = () => {
-
-            switch (props.color) {
-
-                case 'yellow':
-                    return 'rgba(255,255,0)';
-                case 'red':
-                    return 'rgba(255,0,0)';
-                case 'green':
-                    return 'rgba(0,255,0)';
-                default:
-                    return props.color;
-            }
-
-        };
-
-        const backgroundColor = createBackgroundColor();
-
-        const onSelected = props.onSelected || NULL_FUNCTION;
-
-        const size = props.size || '30px';
-
-        // FIXME: this isn't picking / highlighting the item as being active
-        const border = this.props.selected ?
-            '1pt solid var(--primary)' :
-            '1pt solid rgba(0, 0, 0, 0.1)';
-
-        return <div className="ml-1 mr-1"
-                    style={{
-                        display: 'flex',
-                    }}>
-            <button id={props.id}
-                    className={"p-0"}
-                    title=""
-                    aria-label=""
-                    onClick={() => onSelected(props.color)}
-                    style={{
-                        backgroundColor,
-                        border,
-                        width: size,
-                        height: size
-                    }}>
-
-            </button>
-        </div>;
-
-    }
-
-}
+/**
+ * RGB color in the a CSS color.
+ */
+export type RGBColor = string;
 
 interface IProps {
     readonly color: RGBColor;
@@ -70,10 +30,51 @@ interface IProps {
     readonly onSelected?: (color: string) => void;
 }
 
-interface IState {
+export const ColorButton = (props: IProps) => {
+
+    const classes = useStyles();
+
+    const createBackgroundColor = () => {
+
+        switch (props.color) {
+
+            case 'yellow':
+                return 'rgba(255,255,0)';
+            case 'red':
+                return 'rgba(255,0,0)';
+            case 'green':
+                return 'rgba(0,255,0)';
+            default:
+                return props.color;
+        }
+
+    };
+
+    const backgroundColor = createBackgroundColor();
+
+    const onSelected = props.onSelected || NULL_FUNCTION;
+
+    const size = props.size || '30px';
+
+    const buttonClassName = props.selected ? classes.selected : classes.notSelected;
+
+    return <div className="ml-1 mr-1"
+                style={{
+                    display: 'flex',
+                }}>
+        <button id={props.id}
+                className={buttonClassName}
+                title=""
+                aria-label=""
+                onClick={() => onSelected(props.color)}
+                style={{
+                    backgroundColor,
+                    width: size,
+                    height: size
+                }}>
+
+        </button>
+    </div>;
+
 }
 
-/**
- * RGB color in the a CSS color.
- */
-export type RGBColor = string;
