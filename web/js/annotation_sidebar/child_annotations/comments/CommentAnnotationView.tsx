fc5c9e45d1550e62324fd88594554b8da236a510
burtonator <burtonator@gmail.com>
2020-04-26 12:09:10 -0600

    cleanup to functional component...

--- web/js/annotation_sidebar/child_annotations/comments/CommentAnnotationView.tsx
+++ web/js/annotation_sidebar/child_annotations/comments/CommentAnnotationView.tsx
@@ -11,86 +11,6 @@ import {Tag} from 'polar-shared/src/tags/Tags';
 
 const log = Logger.create();
 
-export class CommentAnnotationView extends React.Component<IProps, IState> {
-
-    constructor(props: IProps, context: any) {
-        super(props, context);
-
-        this.onDelete = this.onDelete.bind(this);
-        this.state = {};
-
-    }
-
-    public render() {
-        const { comment } = this.props;
-
-        const key = 'comment-' + comment.id;
-
-        return (
-
-            <div className="ml-1">
-
-                <div key={key} className="comment muted-color-root">
-
-                    <div className="p-1" onDoubleClick={() => this.props.onEdit()}>
-
-                        {/*TODO: based on the state determine if we should be*/}
-                        {/*editing or just displaying the comment*/}
-
-                        <span dangerouslySetInnerHTML={{__html: comment.html!}}>
-
-                        </span>
-
-                    </div>
-
-                    <div style={{
-                             display: 'flex',
-                             alignItems: 'center',
-                         }}
-                         className="p-1">
-
-                        <DocAuthor author={comment.author}/>
-
-                        <div className="text-muted">
-                            <DocAnnotationMoment created={comment.created}/>
-                        </div>
-
-                        <div style={{
-                                 flexGrow: 1,
-                                 display: 'flex',
-                                 alignItems: 'center',
-                                 justifyContent: 'flex-end'
-                             }}>
-
-                            <AnnotationTagInputButton tagsProvider={this.props.tagsProvider}
-                                                      annotation={this.props.comment}/>
-
-                            <NullCollapse open={! comment.immutable}>
-                                {this.props.editButton}
-                            </NullCollapse>
-
-                            <CommentDropdown id={'comment-dropdown-' + comment.id}
-                                             disabled={comment.immutable}
-                                             comment={comment}
-                                             onDelete={() => this.onDelete(comment)}/>
-
-                        </div>
-
-                    </div>
-
-                </div>
-
-            </div>
-        );
-
-    }
-
-    private onDelete(comment: DocAnnotation) {
-        log.info("Comment deleted: ", comment);
-        delete comment.pageMeta.comments[comment.id];
-    }
-
-}
 interface IProps {
     readonly tagsProvider: () => ReadonlyArray<Tag>;
     readonly doc: Doc;
@@ -99,8 +19,75 @@ interface IProps {
     readonly onEdit: () => void;
 }
 
-interface IState {
+export const CommentAnnotationView = (props: IProps) => {
+
+    const { comment } = props;
+
+    const handleDelete = () => {
+        log.info("Comment deleted: ", comment);
+        delete comment.pageMeta.comments[comment.id];
+    }
+
+    const key = 'comment-' + comment.id;
+
+    return (
+
+        <div className="ml-1">
+
+            <div key={key} className="comment muted-color-root">
+
+                <div className="p-1" onDoubleClick={() => props.onEdit()}>
+
+                    {/*TODO: based on the state determine if we should be*/}
+                    {/*editing or just displaying the comment*/}
+
+                    <span dangerouslySetInnerHTML={{__html: comment.html!}}>
+
+                    </span>
+
+                </div>
+
+                <div style={{
+                         display: 'flex',
+                         alignItems: 'center',
+                     }}
+                     className="p-1">
+
+                    <DocAuthor author={comment.author}/>
+
+                    <div className="text-muted">
+                        <DocAnnotationMoment created={comment.created}/>
+                    </div>
+
+                    <div style={{
+                             flexGrow: 1,
+                             display: 'flex',
+                             alignItems: 'center',
+                             justifyContent: 'flex-end'
+                         }}>
+
+                        <AnnotationTagInputButton tagsProvider={props.tagsProvider}
+                                                  annotation={props.comment}/>
+
+                        <NullCollapse open={! comment.immutable}>
+                            {props.editButton}
+                        </NullCollapse>
+
+                        <CommentDropdown id={'comment-dropdown-' + comment.id}
+                                         disabled={comment.immutable}
+                                         comment={comment}
+                                         onDelete={() => handleDelete()}/>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+    );
 
 }
+
 
 
