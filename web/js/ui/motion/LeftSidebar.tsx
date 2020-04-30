e395e72df4e0a11c12634794146c2e4299a6b92e
burtonator <burtonator@gmail.com>
2020-04-30 15:08:59 -0600

    cleanup memo...

--- web/js/ui/motion/LeftSidebar.tsx
+++ web/js/ui/motion/LeftSidebar.tsx
@@ -1,14 +1,6 @@
 import * as React from "react";
 import {Sidebar} from "./Sidebar";
 
-export const LeftSidebar = (props: IProps) => (
-    <Sidebar {...props}
-             style={{
-                 left: 0
-             }}
-             borderBarClassName="border-right"
-             buttonBarClassName="text-right"/>
-);
 
 interface IProps {
     readonly width?: number;
@@ -17,3 +9,12 @@ interface IProps {
     readonly children: React.ReactElement;
     readonly onClose: () => void;
 }
+
+export const LeftSidebar = React.memo((props: IProps) => (
+    <Sidebar {...props}
+             style={{
+                 left: 0
+             }}
+             borderBarClassName="border-right"
+             buttonBarClassName="text-right"/>
+));
