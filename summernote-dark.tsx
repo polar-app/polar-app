0436a462954984a0e43c40d19f36711b6976b791
burtonator <burtonator@gmail.com>
2020-05-05 13:21:21 -0600

    ...

--- summernote-dark.tsx
+++ summernote-dark.tsx
@@ -1,54 +1,55 @@
+const foo = {
 ".note-frame *": {
-    color:#000;
+    color: "#000",
 },
 "kbd": {
-    color:#fff;
-    background-color:#000;
+    color: "#fff",
+    backgroundColor: "#000",
 },
 ".note-btn": {
-    color:#333;
-    background-color:#fff;
-    border-color:#fff;
+    color: "#333",
+    backgroundColor: "#fff",
+    borderColor: "#fff",
 },
 ".note-btn:focus,.note-btn.focus": {
-    color:#333;
-    background-color:#ebebeb;
-    border-color:#fff
+    color: "#333",
+    backgroundColor: "#ebebeb",
+    borderColor:#fff
 },
 ".note-btn:hover": {
-    color:#333;
-    background-color:#ebebeb;
-    border-color:#fff
+    color: "#333",
+    backgroundColor: "#ebebeb",
+    borderColor:#fff
 },
 ".note-btn.disabled:focus,.note-btn[disabled]:focus,fieldset[disabled] .note-btn:focus,.note-btn.disabled.focus,.note-btn[disabled].focus,fieldset[disabled] .note-btn.focus": {
-    background-color:#fff;
-    border-color:#fff
+    backgroundColor: "#fff",
+    borderColor:#fff
 },
 ".note-btn:hover,.note-btn:focus,.note-btn.focus": {
-    color:#333;
-    background-color:#ebebeb;
+    color: "#333",
+    backgroundColor: "#ebebeb",
 },
 ".note-btn-primary": {
-    color:#fff;
+    color: "#fff",
 },
 ".note-btn-primary:hover,.note-btn-primary:focus,.note-btn-primary.focus": {
-    color:#fff;
-    background-color:#fa6362;
+    color: "#fff",
+    backgroundColor: "#fa6362",
 },
 ".close": {
-    color:#000;
+    color: "#000",
 },
 ".note-dropdown-item:hover": {
-    background-color:#ebebeb
+    backgroundColor:#ebebeb
 },
 "a.note-dropdown-item,a.note-dropdown-item:hover": {
-    color:#000;
+    color: "#000",
 },
 ".note-modal-footer a": {
-    color:#337ab7;
+    color: "#337ab7",
 },
 ".note-modal-footer a:hover,.note-modal-footer a:focus": {
-    color:#23527c;
+    color: "#23527c",
 },
 ".note-modal-footer .note-btn": {
 },
@@ -71,89 +72,89 @@
     color:#eee
 },
 ".note-tooltip.bottom .note-tooltip-arrow": {
-    border-bottom-color:#000;
+    border-bottom-color: "#000",
 },
 ".note-tooltip.top .note-tooltip-arrow": {
-    border-top-color:#000;
+    border-top-color: "#000",
 },
 ".note-tooltip.right .note-tooltip-arrow": {
-    border-right-color:#000;
+    border-right-color: "#000",
 },
 ".note-tooltip.left .note-tooltip-arrow": {
-    border-left-color:#000;
+    border-left-color: "#000",
 },
 ".note-tooltip-arrow": {
-    border-color:transparent;
+    borderColor: "transparent",
 },
 ".note-tooltip-content": {
-    color:#fff;
-    background-color:#000
+    color: "#fff",
+    backgroundColor:#000
 },
 ".note-popover.bottom .note-popover-arrow": {
-    border-bottom-color:#999;
-    border-bottom-color:rgba(0,0,0,0.25);
+    border-bottom-color: "#999",
+    border-bottom-color: "rgba(0,0,0,0.25)",
 },
 ".note-popover.bottom .note-popover-arrow::after": {
-    border-bottom-color:#fff;
+    border-bottom-color: "#fff",
 },
 ".note-popover.top .note-popover-arrow": {
-    border-top-color:#999;
-    border-top-color:rgba(0,0,0,0.25);
+    border-top-color: "#999",
+    border-top-color: "rgba(0,0,0,0.25)",
 },
 ".note-popover.top .note-popover-arrow::after": {
-    border-top-color:#fff;
+    border-top-color: "#fff",
 },
 ".note-popover.right .note-popover-arrow": {
-    border-right-color:#999;
-    border-right-color:rgba(0,0,0,0.25);
+    border-right-color: "#999",
+    border-right-color: "rgba(0,0,0,0.25)",
 },
 ".note-popover.right .note-popover-arrow::after": {
-    border-right-color:#fff;
+    border-right-color: "#fff",
 },
 ".note-popover.left .note-popover-arrow": {
-    border-left-color:#999;
-    border-left-color:rgba(0,0,0,0.25);
+    border-left-color: "#999",
+    border-left-color: "rgba(0,0,0,0.25)",
 },
 ".note-popover.left .note-popover-arrow::after": {
-    border-left-color:#fff;
+    border-left-color: "#fff",
 },
 ".note-popover-arrow::after": {
-    border-color:transparent;
+    borderColor: "transparent",
 },
 ".note-popover-content": {
-    color:#000;
-    background-color:#fff
+    color: "#000",
+    backgroundColor:#fff
 },
 ".note-editor .note-dropzone": {
-    color:#87cefa;
-    background-color:#fff;
+    color: "#87cefa",
+    backgroundColor: "#fff",
 },
 ".note-editor .note-dropzone.hover": {
     color:#098ddf
 },
 ".note-editor .note-editing-area .note-editable a": {
-    color:#337ab7;
-    background-color:inherit
+    color: "#337ab7",
+    backgroundColor:inherit
 },
 ".note-editor .note-editing-area .note-editable a:hover,.note-editor .note-editing-area .note-editable a:focus": {
-    color:#23527c;
+    color: "#23527c",
 },
 ".note-editor.note-frame .note-editing-area .note-editable": {
-    color:#000;
-    background-color:#fff
+    color: "#000",
+    backgroundColor:#fff
 },
 ".note-editor.note-frame .note-editing-area .note-editable[contenteditable="false"]": {
-    background-color:#e5e5e5
+    backgroundColor:#e5e5e5
 },
 ".note-editor.note-frame .note-editing-area .note-codable": {
-    color:#ccc;
-    background-color:#222;
+    color: "#ccc",
+    backgroundColor: "#222",
 },
 ".note-editor.note-frame.fullscreen .note-editable": {
-    background-color:#fff
+    backgroundColor:#fff
 },
 ".note-editor.note-frame .note-status-output": {
-    color:#000;
+    color: "#000",
 },
 ".note-editor.note-frame .note-status-output .text-muted": {
     color:#777
@@ -174,54 +175,55 @@
     color:#a94442
 },
 ".note-editor.note-frame .note-status-output .alert": {
-    color:#000;
-    background-color:#f5f5f5;
+    color: "#000",
+    backgroundColor: "#f5f5f5",
 },
 ".note-editor.note-frame .note-status-output .alert-success": {
-    color:#3c763d!important;
-    background-color:#dff0d8!important
+    color: "#3c763d!important",
+    backgroundColor:#dff0d8!important
 },
 ".note-editor.note-frame .note-status-output .alert-info": {
-    color:#31708f!important;
-    background-color:#d9edf7!important
+    color: "#31708f!important",
+    backgroundColor:#d9edf7!important
 },
 ".note-editor.note-frame .note-status-output .alert-warning": {
-    color:#8a6d3b!important;
-    background-color:#fcf8e3!important
+    color: "#8a6d3b!important",
+    backgroundColor:#fcf8e3!important
 },
 ".note-editor.note-frame .note-status-output .alert-danger": {
-    color:#a94442!important;
-    background-color:#f2dede!important
+    color: "#a94442!important",
+    backgroundColor:#f2dede!important
 },
 ".note-editor.note-frame .note-statusbar": {
-    background-color:#f5f5f5;
+    backgroundColor: "#f5f5f5",
 },
 ".note-popover .note-popover-content,.note-toolbar": {
-    background-color:#fff
+    backgroundColor:#fff
 },
 ".note-popover .note-popover-content .note-color .note-dropdown-menu .note-palette .note-color-reset,.note-toolbar .note-color .note-dropdown-menu .note-palette .note-color-reset": {
-    background-color:#fff;
+    backgroundColor: "#fff",
 },
 ".note-popover .note-popover-content .note-dropdown-menu.note-check .note-dropdown-item i,.note-toolbar .note-dropdown-menu.note-check .note-dropdown-item i": {
-    color:deepskyblue;
+    color: "deepskyblue",
 },
 ".note-modal .note-modal-body .help-list-item:hover": {
-    background-color:#e0e0e0
+    backgroundColor:#e0e0e0
 },
 ".note-placeholder": {
-    color: gray;
+    color: " gray",
 },
 ".note-handle .note-control-selection .note-control-selection-bg": {
-    background-color:#000;
+    backgroundColor: "#000",
 },
 ".note-handle .note-control-selection .note-control-sizing": {
-    background-color:#fff;
+    backgroundColor: "#fff",
 },
 ".note-handle .note-control-selection .note-control-selection-info": {
-    color:#fff;
-    background-color:#000;
+    color: "#fff",
+    backgroundColor: "#000",
 },
 ".note-hint-popover .note-popover-content .note-hint-group .note-hint-item.active,.note-hint-popover .note-popover-content .note-hint-group .note-hint-item:hover": {
-    color:#fff;
-    background-color:#428bca;
+    color: "#fff",
+    backgroundColor: "#428bca",
 },
+}
