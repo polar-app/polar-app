16eb4a94f06e47320d4ecad348a7bef4f62bae91
burtonator <burtonator@gmail.com>
2020-05-19 09:13:14 -0600

    clean this up so it uses mutation listeners...

--- apps/pdf/src/annotations/AnnotationHooks.tsx
+++ apps/pdf/src/annotations/AnnotationHooks.tsx
@@ -4,8 +4,6 @@ import {
     useComponentWillUnmount
 } from "../../../../web/js/hooks/lifecycle";
 import {Debouncers} from "polar-shared/src/util/Debouncers";
-import {SnapshotUnsubscriber} from "../../../../web/js/firebase/SnapshotSubscribers";
-
 
 /**
  * Unsubscribes to the action created by the subscriber.
@@ -17,7 +15,7 @@ export type Unsubscriber = () => void;
  */
 export type Subscriber = () => Unsubscriber
 
-function useSubscription(subscriber: Subscriber) {
+function useSubscriber(subscriber: Subscriber) {
     
     const unsubscriberRef = React.useRef<Unsubscriber | undefined>(undefined);
 
@@ -36,6 +34,75 @@ function useSubscription(subscriber: Subscriber) {
     })
 
 }
+
+function createScrollSubscriber(delegate: () => void): Subscriber {
+
+    return () => {
+
+        const viewerContainer = document.getElementById('viewerContainer');
+
+        function handleScroll() {
+            delegate();
+        }
+
+        viewerContainer!.addEventListener('scroll', handleScroll);
+
+        return () => {
+            viewerContainer!.removeEventListener('scroll', handleScroll);
+        }
+
+    }
+
+}
+
+function createResizeSubscriber(delegate: () => void): Subscriber {
+
+    return () => {
+
+        function handleResize() {
+            delegate();
+        }
+
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+
+        }
+    }
+
+}
+
+
+
+function createMutationObserverSubscriber(delegate: () => void): Subscriber {
+
+    return () => {
+
+        const viewerContainer = document.getElementById('viewerContainer')!;
+
+        const observer = new MutationObserver((mutations) => {
+            for (const mutation of mutations) {
+                if (mutation.type === "attributes") {
+                    delegate();
+                }
+            }
+
+        });
+
+        observer.observe(viewerContainer, {
+            // only monitor attributes.
+            attributes: true
+        });
+
+        return () => {
+            observer.disconnect();
+        }
+
+    }
+
+}
+
 
 
 export function useAnnotationContainer(page: number) {
@@ -74,80 +141,14 @@ export function useAnnotationContainer(page: number) {
 
     useComponentDidMount(() => {
 
+        // call the delegate directly to force a draw when the component mounts
         doUpdateDelegate();
 
-        function registerScrollListener(): Unsubscriber {
-
-            const viewerContainer = document.getElementById('viewerContainer');
-
-            function handleScroll() {
-                doUpdate()
-            }
-
-            viewerContainer!.addEventListener('scroll', handleScroll);
-
-            return () => {
-                viewerContainer!.removeEventListener('scroll', handleScroll);
-            }
-
-        }
-
-        function registerResizeListener(): Unsubscriber {
-
-            function handleResize() {
-                doUpdate();
-            }
-
-            window.addEventListener('resize', handleResize);
-
-            return () => {
-                window.removeEventListener('resize', handleResize);
-            }
-
-        }
-
-        function registerMutationObserver(): SnapshotUnsubscriber {
-
-            // FIXME: how do we unregister these listeners? if an annotaiton
-            // vanishes these aren't going to be cleaned up I think...
-
-            // FIXME: there are also TOO MANY listeners already.
-
-            // FIXME: I need componentWillUnmount too..
-
-            const viewerContainer = document.getElementById('viewerContainer')!;
-
-            const observer = new MutationObserver((mutations) => {
-                for (const mutation of mutations) {
-                    if (mutation.type === "attributes") {
-                        doUpdate();
-                    }
-                }
-
-            });
-
-            observer.observe(viewerContainer, {
-                // only monitor attributes.
-                attributes: true
-            });
-
-            return () => {
-                observer.disconnect();
-            }
-
-        }
-
-        // FIXME: mutation listener here would solve the problem... I think we
-        // have to go with this even though it's nasty...
-
-        // FIXME: should I use effect?
-        // FIXME: remove event listeners on unmount...
-
-        registerScrollListener();
-        registerResizeListener();
-        registerMutationObserver();
-
     });
+
+    useSubscriber(createScrollSubscriber(doUpdate));
+    useSubscriber(createResizeSubscriber(doUpdate));
+    useSubscriber(createMutationObserverSubscriber(doUpdate));
 
     return container;
 
