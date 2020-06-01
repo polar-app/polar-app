33dbffe637128cb518be79efe5ec7795fd1e389f
Kevin Burton <burtonator@gmail.com>
2020-06-01 02:52:18 +0000

    Imported polar-app as a subtree.

--- packages/polar-bookshelf/web/js/annotations/ipc/electron/README.md
+++
@@ -1,1 +0,0 @@
-Electron implementation of annotation creation.  Uses Electron IPC.
--- packages/polar-bookshelf/web/test/sandbox/jquery-ui/index.ts
+++
@@ -1,5 +0,0 @@
-import {SpectronMain} from '../../../js/test/SpectronMain';
-
-SpectronMain.run(async (state) => {
-    state.window.loadFile(__dirname + '/app.html');
-});
