d0e8d12e273f13c4f4d22f4d4fbb9569cfff669a
Kevin Burton <burtonator@gmail.com>
2020-06-02 13:18:31 +0000

    Imported polar-app as a subtree.

--- packages/polar-bookshelf/web/js/capture/renderer/CapturePreload.ts
+++
@@ -1,5 +0,0 @@
-import {ContentCapture} from "polar-content-capture/src/capture/ContentCapture";
-import {Preconditions} from 'polar-shared/src/Preconditions';
-
-Preconditions.assertPresent(ContentCapture);
-
