483ea36e037bc7339d2c071cba70da1c8f7248f5
burtonator <burtonator@gmail.com>
2020-06-02 15:56:22 -0600

    ...

--- apps/pdf/src/renderers/epub/EPUBViewerContainer.tsx
+++ apps/pdf/src/renderers/epub/EPUBViewerContainer.tsx
@@ -2,6 +2,24 @@ import React from 'react';
 
 export const EPUBViewerContainer = React.memo(() => {
 
-    return null;
+    return (
 
+        <main id="viewerContainer"
+              style={{
+                  position: 'absolute',
+                  overflow: 'auto',
+                  top: '0',
+                  width: '100%',
+                  height: '100%'
+              }}
+              itemProp="mainContentOfPage">
+
+            <div>
+                <div id="viewer" className="epubViewer">
+                    <div className="page"/>
+                </div>
+            </div>
+
+        </main>
+    );
 })
