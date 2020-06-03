adeebc264abef2abaeb5a8fe528c5e4cb2c70992
burtonator <burtonator@gmail.com>
2020-06-03 09:24:12 -0600

    update of epubjs code...

--- apps/doc/src/renderers/epub/EPUBDocument.tsx
+++ apps/doc/src/renderers/epub/EPUBDocument.tsx
@@ -3,9 +3,14 @@ import {useComponentDidMount} from "../../../../../web/js/hooks/lifecycle";
 import ePub from "epubjs";
 import {URLStr} from "polar-shared/src/util/Strings";
 import useTheme from "@material-ui/core/styles/useTheme";
+import Section from "epubjs/types/section";
 
 interface IProps {
     readonly docURL: URLStr;
+}
+
+interface ExtendedSpine {
+    readonly items: ReadonlyArray<Section>;
 }
 
 export const EPUBDocument = React.memo((props: IProps) => {
@@ -28,14 +33,30 @@ export const EPUBDocument = React.memo((props: IProps) => {
         rendition.themes.override('background-color', theme.palette.background.default);
         rendition.themes.override('color', theme.palette.text.primary);
 
+        console.log("FIXME: contents, ", rendition.getContents())
+        console.log("FIXME: views, ", rendition.views())
+
         await rendition.display();
         await rendition.next();
 
+        console.log("FIXME: currentLocation: ", rendition.currentLocation())
+
         const metadata = await book.loaded.metadata;
+
+
+        const spine = (await book.loaded.spine) as any as ExtendedSpine;
+
+        for (const spineItem of spine.items) {
+            console.log("FIXME: spineItem: ", spineItem);
+        }
+
+        // FIXME: build a navigator object from the spine now...
 
         console.log({metadata});
 
         const navigation = await book.loaded.navigation;
+
+        console.log("landmarks: ", navigation.landmarks);
 
         console.log("toc: ", navigation.toc);
 
