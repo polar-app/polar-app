bf1f6ac8e405b16d788d1c8674662cd0a81ab493
burtonator <burtonator@gmail.com>
2020-06-16 14:07:03 -0600

    chrome extension works a lot better

--- web/spectron0/material-ui/SaveToPolarDemo.tsx
+++ web/spectron0/material-ui/SaveToPolarDemo.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import {createStyles, makeStyles, Theme} from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
+import LinearProgress from '@material-ui/core/LinearProgress';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -42,6 +41,8 @@ export const SaveToPolarDemo = () => {
 
 
             </AppBar>
+
+            <LinearProgress/>
 
             <div style={{display: 'flex'}}>
 
