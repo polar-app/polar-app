a6b57b1f396d73e57f1c4325f1a12fb7858e3b57
Kevin Burton <burtonator@gmail.com>
2020-06-17 02:15:16 +0000

    Imported polar-app-public as a subtree.

---
+++ packages/polar-app-public/polar-web-extension/src/ui/capture/CaptureApp.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import * as ReactDOM from 'react-dom';
+import {ReadabilityCapture} from "../../ReadabilityCapture";
+import {CaptureRoot} from "./CaptureRoot";
+import {MUIAppRoot} from 'polar-bookshelf/web/js/mui/MUIAppRoot';
+import ICapturedContent = ReadabilityCapture.ICapturedContent;
+
+const CaptureContentContext = React.createContext<ICapturedContent>(null!);
+
+export function useCaptureContentContext() {
+    return React.useContext(CaptureContentContext);
+}
+
+export class CaptureApp {
+
+    public static start(content: ICapturedContent) {
+
+        ReactDOM.render(
+            <MUIAppRoot>
+                <CaptureContentContext.Provider value={content}>
+                    <CaptureRoot/>
+                </CaptureContentContext.Provider>
+            </MUIAppRoot>
+            ,
+            document.body
+        );
+
+    }
+
+}
