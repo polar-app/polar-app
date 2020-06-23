a287d216055302d225f36f8ebb513156d903433e
Kevin Burton <burtonator@gmail.com>
2020-06-23 13:47:46 +0000

    Imported polar-app as a subtree.

--- packages/polar-bookshelf/web/spectron2/ui-notifier/app.ts
+++
@@ -1,6 +0,0 @@
-import {SpectronRenderer} from '../../js/test/SpectronRenderer';
-import {WebContentsNotifiers} from '../../js/electron/web_contents_notifier/WebContentsNotifiers';
-
-SpectronRenderer.run(async () => {
-    WebContentsNotifiers.dispatchEvent('hello', 'world');
-});
