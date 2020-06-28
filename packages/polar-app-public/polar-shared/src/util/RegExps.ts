1c55c5af8122077945afa285d4871c85df5b6ba6
Kevin Burton <burtonator@gmail.com>
2020-06-28 17:13:08 +0000

    Imported polar-app as a subtree.

--- packages/polar-app-public/polar-shared/src/util/RegExps.ts
+++
@@ -1,9 +0,0 @@
-export class RegExps {
-
-    public static escape(str: string): string {
-        return str.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'); // $& means the whole matched string
-    }
-
-}
-
-export type RegexStr = string;
--- packages/polar-bookshelf/web/js/ipc/pipes/README.md
+++
@@ -1,4 +0,0 @@
-Supports a channel API that can be used with IPC for establishing new
-bidirectional channel without any race conditions.
-
-Neither channel moves forward until both ends of the channel are available.
