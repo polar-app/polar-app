12e23c3aed9d5a0deced153be2bf474009862d7b
burtonator <burtonator@gmail.com>
2020-07-01 16:43:44 -0600

    electron-notarize upgrade and using new app specifci password...

--- credentials.sh
+++ credentials.sh
@@ -7,7 +7,9 @@ export CSC_IDENTITY_AUTO_DISCOVERY=true
 
 export APPLEID=burtonator2011@gmail.com
 # export APPLEIDPASS=nlrs-matd-xulq-hvur
-export APPLEIDPASS=fzuy-vfgn-syyj-dvlb
+# export APPLEIDPASS=fzuy-vfgn-syyj-dvlb
+export APPLEIDPASS=cfop-pksv-kbwa-wumi
+#
 
 if [ "${TERM_PROGRAM}" = "Apple_Terminal" ] && [ "${APPLEIDPASS}" = "" ]; then
   echo "ERROR: Must set APPLEIDPASS. Hint: pass is F*******3"
