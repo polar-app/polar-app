f9b5da390cd0c185710e35bd775afae2b88345a6
burtonator <burtonator@gmail.com>
2020-07-08 21:37:01 -0600

    cleanup of old code

--- web/js/apps/repository/importers/DocMetadata.ts
+++ web/js/apps/repository/importers/DocMetadata.ts
@@ -1,18 +1,18 @@
-import {DocFormatName} from "../../../docformat/DocFormat";
 import {PDFMetadata} from "polar-pdf/src/pdf/PDFMetadata";
 import {EPUBMetadata} from "polar-epub/src/EPUBMetadata";
+import {FileType} from "../../main/file_loaders/FileType";
 
 export namespace DocMetadata {
 
-    export async function getMetadata(docPath: string, type: DocFormatName) {
+    export async function getMetadata(docPath: string, fileType: FileType) {
 
-        if (type === 'pdf') {
+        if (fileType === 'pdf') {
             return await PDFMetadata.getMetadata(docPath);
-        } else if (type === 'epub') {
+        } else if (fileType === 'epub') {
             return await EPUBMetadata.getMetadata(docPath);
         }
 
-        throw new Error("Invalid type: " + type);
+        throw new Error("Invalid type: " + fileType);
 
     }
 
