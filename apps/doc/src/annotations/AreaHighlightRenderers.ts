a0294ea1a4f8c2915282d257f3c9c4ff67a21c07
burtonator <burtonator@gmail.com>
2020-07-08 21:08:44 -0600

    cleanup

--- apps/doc/src/annotations/AreaHighlightRenderers.ts
+++ apps/doc/src/annotations/AreaHighlightRenderers.ts
@@ -1,16 +1,12 @@
 import {ILTRect} from "polar-shared/src/util/rects/ILTRect";
 import {AnnotationRects} from "../../../../web/js/metadata/AnnotationRects";
-import {Rects} from "../../../../web/js/Rects";
 import {AreaHighlights} from "../../../../web/js/metadata/AreaHighlights";
 import {IAreaHighlight} from "polar-shared/src/metadata/IAreaHighlight";
-import {IPageMeta} from "polar-shared/src/metadata/IPageMeta";
-import {IDocMeta} from "polar-shared/src/metadata/IDocMeta";
 import {IPoint} from "../../../../web/js/Point";
 import {IDocScale} from "../DocViewerStore";
 import {Screenshots} from "../../../../web/js/screenshots/Screenshots";
 import {ICapturedScreenshot} from "../../../../web/js/screenshots/Screenshot";
 import {Position} from "polar-shared/src/metadata/IBaseHighlight";
-import {computePageDimensions} from "./AnnotationHooks";
 import {FileType} from "../../../../web/js/apps/main/file_loaders/FileType";
 
 export namespace AreaHighlightRenderers {
@@ -90,23 +86,3 @@ export namespace AreaHighlightRenderers {
 
 }
 
-
-function createAreaHighlight(rect: ILTRect,
-                             pageNum: number) {
-
-    const pageDimensions = computePageDimensions(pageNum)
-
-    const annotationRect = AnnotationRects.createFromPositionedRect(Rects.createFromBasicRect(rect),
-                                                                    pageDimensions);
-
-    return AreaHighlights.create({rect: annotationRect});
-
-
-}
-
-export interface IWriteAreaHighlightOpts {
-    readonly docMeta: IDocMeta;
-    readonly pageMeta: IPageMeta;
-    readonly rect: ILTRect;
-    readonly areaHighlight: IAreaHighlight;
-}
