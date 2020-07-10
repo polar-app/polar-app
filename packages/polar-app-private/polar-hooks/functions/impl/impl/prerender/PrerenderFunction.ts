3f5a86427a5996d88cfadfee4509d6118eeb88e7
Kevin Burton <burtonator@gmail.com>
2020-07-10 18:36:00 +0000

    Imported polar-app as a subtree.

--- packages/polar-app-private/polar-hooks/functions/impl/impl/prerender/PrerenderFunction.ts
+++
@@ -1,7 +0,0 @@
-// import * as functions from "firebase-functions";
-//
-// process.env.ALLOWED_DOMAINS = "app.getpolarized.io,getpolarized.io";
-//
-// const prerender = require('prerender');
-// const server = prerender();
-// export const PrerenderFunction = functions.https.onRequest(server);
