c4d71d16c1500e5e4bf4fac4aca8d8a80a0196d5
burtonator <burtonator@gmail.com>
2020-07-18 16:13:28 -0600

    ...

--- apps/dev2/HitHighlighter.tsx
+++ apps/dev2/HitHighlighter.tsx
@@ -4,6 +4,8 @@ import {
     useResizeEventListener,
     useScrollEventListener
 } from "../../web/js/react/WindowHooks";
+import {ILTRect} from "polar-shared/src/util/rects/ILTRect";
+import {Rects} from "../../web/js/Rects";
 
 interface IPosition {
     readonly top: number;
@@ -38,16 +40,79 @@ function createPosition(highlight: IHighlightNode): IPosition {
 
 }
 
+/**
+ * Debouncer that use requestAnimationFrame
+ */
+function withAnimationFrame(delegate: () => void): () => void {
+
+    let pending: boolean = false;
+
+    function handleDelegate() {
+
+        try {
+            delegate();
+        } finally {
+            pending = false;
+        }
+
+    }
+
+    return () => {
+
+        if (pending) {
+            return;
+        }
+
+        window.requestAnimationFrame(handleDelegate);
+
+        pending = true;
+
+    };
+
+}
+
+function intersects(parent: ILTRect, child: ILTRect): boolean {
+
+    return Rects.intersect(Rects.createFromBasicRect(parent), Rects.createFromBasicRect(child));
+
+}
+
+function intersectWithWindow(position: IPosition) {
+
+    const parent: ILTRect = {
+        top: 0,
+        left: 0,
+        width: window.innerWidth,
+        height: window.innerHeight
+    };
+
+    return intersects(parent, position);
+
+}
+
 export const HitHighlighter = memoForwardRef((props: IProps) => {
+
+    // FIXME: this WORKS but for some reason there is some lag which is mildly
+    // distracting
 
     const [position, setPosition] = React.useState<IPosition>(createPosition(props))
 
-    const redrawCallback = React.useCallback(() => {
+    const redrawCallback = React.useCallback(withAnimationFrame(() => {
         setPosition(createPosition(props));
-    }, []);
+    }), []);
 
     useScrollEventListener(redrawCallback);
     useResizeEventListener(redrawCallback);
+
+    // FIXME: if the position is actually offscreen, don't draw it to save
+    // some CPU..
+
+    // FIXME: could we improve performance by computing the position relative
+    // to the document, not the client rect...
+
+    if (! intersectWithWindow(position)) {
+        return null;
+    }
 
     return (
         <div style={{
