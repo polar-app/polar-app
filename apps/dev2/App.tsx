39436440ad775066f6776aa4af40b49c86dcbd64
burtonator <burtonator@gmail.com>
2020-07-22 17:16:46 -0600

    ...

--- apps/dev2/App.tsx
+++ apps/dev2/App.tsx
@@ -1,39 +1,18 @@
 import React from 'react';
-import {Content} from "./Content";
-import {SearchBar} from "./SearchBar";
-import { DOMHighlights } from './DOMHighlights';
-import { DOMTextHit } from 'polar-dom-text-search/src/DOMTextHit';
-import {DOMTextSearch} from "polar-dom-text-search/src/DOMTextSearch";
 
 export const App = () => {
 
-    const [hits, setHits] = React.useState<ReadonlyArray<DOMTextHit>>([]);
+    const [element, setElement] = React.useState<HTMLElement | null>(null);
 
-    function handleSearch(text: string) {
-
-        if (text.trim() === '') {
-            setHits([]);
-            return;
-        }
-
-        // use the new search frame work to see if we can find the text on
-        // the page...
-        const index = DOMTextSearch.createIndex(document, document.getElementById('content')!);
-
-        const hits = index.search(text, 0, {caseInsensitive: true});
-        setHits(hits);
-
-        // TextHighlighter.createTextHighlight()
-
-        console.log("Found N hits: " + hits.length, hits);
-
-    }
+    console.log('render')
 
     return (
-        <div style={{padding: '15px'}}>
-            <SearchBar onSearch={handleSearch}/>
-            <Content/>
-            <DOMHighlights hits={hits}/>
+        <div ref={(ref) => {
+            console.log('got ref');
+            setElement(ref);
+        }}>
+            test
         </div>
     );
+
 }
