d629c7b807ddab3ccd33fa3a6baa63bba6254b6d
burtonator <burtonator@gmail.com>
2020-08-15 19:35:29 -0600

    ...

--- apps/doc/src/renderers/epub/contextmenu/EPUBContextMenuRoot.tsx
+++ apps/doc/src/renderers/epub/contextmenu/EPUBContextMenuRoot.tsx
@@ -2,13 +2,22 @@ import {deepMemo} from "../../../../../../web/js/react/ReactUtils";
 import React from "react";
 import {createQuerySelector} from "../../../../../dev2/QuerySelector";
 import {EPUBContextMenuFinderContext} from "./EPUBContextMenuFinderContext";
+import {useDocViewerElementsContext} from "../../DocViewerElementsContext";
 
 const EPUBIFrameQuerySelector = createQuerySelector<HTMLIFrameElement>();
 
 export const EPUBContextMenuRoot = deepMemo(() => {
 
+    const docViewerElementsContext = useDocViewerElementsContext();
+
+    function selector() {
+        const docViewerElement = docViewerElementsContext.getDocViewerElement();
+        return docViewerElement.querySelector('iframe');
+    }
+
     return (
         <EPUBIFrameQuerySelector component={EPUBContextMenuFinderContext}
-                                 selector={() => document.querySelector('iframe')}/>
+                                 selector={selector}/>
     );
+
 });
