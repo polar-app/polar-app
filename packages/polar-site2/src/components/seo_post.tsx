34fe71c50e1ba4f318359c6e2ea034137770c650
Kevin Burton <burtonator@gmail.com>
2020-09-11 23:32:42 +0000

    Imported polar-site2 as a subtree.

---
+++ packages/polar-site2/src/components/seo_post.tsx
@@ -0,0 +1,103 @@
+/**
+ * SEO component that queries for data with
+ *  Gatsby's useStaticQuery React hook
+ *
+ * See: https://www.gatsbyjs.org/docs/use-static-query/
+ */
+
+import React from "react";
+import PropTypes from "prop-types";
+// import { PageProps } from "gatsby"
+import { Helmet } from "react-helmet";
+import { useStaticQuery, graphql } from "gatsby";
+
+type Data = {
+  site: {
+    siteMetadata: {
+      title: string;
+      description: string;
+      social: {
+        twitter: string;
+      };
+    };
+  };
+};
+
+const SEO = ({ description, lang, meta, title, image }) => {
+  const { site }: Data = useStaticQuery(
+    graphql`
+      query {
+        site {
+          siteMetadata {
+            title
+            description
+            social {
+              twitter
+            }
+          }
+        }
+      }
+    `
+  );
+
+  const metaDescription = description || site.siteMetadata.description;
+
+  return (
+    <Helmet
+      htmlAttributes={{
+        lang,
+      }}
+      title={title}
+      titleTemplate={`%s | ${site.siteMetadata.title}`}
+      meta={[
+        {
+          name: `description`,
+          content: metaDescription,
+        },
+        {
+          property: `og:description`,
+          content: metaDescription,
+        },
+        {
+          property: `og:type`,
+          content: `website`,
+        },
+        {
+          name: `twitter:card`,
+          content: `summary_large_image`,
+        },
+        {
+          name: `twitter:creator`,
+          content: site.siteMetadata.social.twitter,
+        },
+        {
+          name: `twitter:title`,
+          content: title,
+        },
+        {
+          name: `twitter:description`,
+          content: metaDescription,
+        },
+        {
+          name: `twitter:image`,
+          content: image,
+        },
+      ].concat(meta)}
+    />
+  );
+};
+
+SEO.defaultProps = {
+  lang: `en`,
+  meta: [],
+  description: ``,
+};
+
+SEO.propTypes = {
+  description: PropTypes.string,
+  lang: PropTypes.string,
+  meta: PropTypes.arrayOf(PropTypes.object),
+  title: PropTypes.string.isRequired,
+};
+
+export default SEO;
