b9934d15777b231108a3f384b1b5cf26e3962d4e
burtonator <burtonator@gmail.com>
2020-09-21 11:23:01 -0600

    cleanup and refactor of code...

--- web/js/apps/repository/upload/AddFileDropzone.tsx
+++ web/js/apps/repository/upload/AddFileDropzone.tsx
@@ -10,62 +10,10 @@ import {
 import {AddFileDropzoneDialog2} from "./AddFileDropzoneDialog2";
 import {useLogger} from "../../../mui/MUILogger";
 import {AddFileHooks} from "./AddFileHooks";
-import {IWebkitFileSystem} from "./IWebkitFileSystem";
-import {FileSystemDirectoryReaders} from "./FileSystemDirectoryReaders";
-import {FileSystemEntries} from "./FileSystemEntries";
 import {Uploads} from "./Uploads";
 import {UploadFilters} from "./UploadFilters";
+import {FileSystemEntries} from "./FileSystemEntries";
 import useAddFileImporter = AddFileHooks.useAddFileImporter;
-import IWebkitFileSystemFileEntry = IWebkitFileSystem.IWebkitFileSystemFileEntry;
-import IWebkitFileSystemEntry = IWebkitFileSystem.IWebkitFileSystemEntry;
-
-async function recurseDataTransferItems(items: ReadonlyArray<DataTransferItem>): Promise<ReadonlyArray<IWebkitFileSystemFileEntry>> {
-    return recurseFileSystemEntries(items.map(item => item.webkitGetAsEntry()));
-}
-
-async function recurseFileSystemEntries(entries: ReadonlyArray<IWebkitFileSystemEntry>): Promise<ReadonlyArray<IWebkitFileSystemFileEntry>> {
-
-    const result: IWebkitFileSystemFileEntry[] = [];
-
-    for (const entry of entries) {
-
-        if (entry.isFile) {
-            result.push(entry);
-        } else if (FileSystemEntries.isDirectory(entry)) {
-
-            const reader = entry.createReader();
-            const asyncReader = FileSystemDirectoryReaders.toAsync(reader);
-
-            while (true) {
-
-                const dirEntries = await asyncReader.readEntries();
-
-                if (dirEntries.length === 0) {
-
-                    // https://stackoverflow.com/questions/3590058/does-html5-allow-drag-drop-upload-of-folders-or-a-folder-tree
-
-                    // To actually get all the files, we'll need to call
-                    // readEntries repeatedly (for each directory we encounter)
-                    // until it returns an empty array. If we don't, we will
-                    // miss some files/sub-directories in a directory e.g. in
-                    // Chrome, readEntries will only return at most 100 entries
-                    // at a time.
-                    break;
-
-                }
-
-                const recursedEntries = await recurseFileSystemEntries(dirEntries);
-                result.push(...recursedEntries);
-
-            }
-
-        }
-
-    }
-
-    return result;
-
-}
 
 function useDropHandler() {
 
@@ -98,7 +46,7 @@ function useDropHandler() {
                 const items = Array.from(event.dataTransfer.items)
                                    .filter(UploadFilters.filterByDocumentType);
 
-                const files = await recurseDataTransferItems(items);
+                const files = await FileSystemEntries.recurseDataTransferItems(items);
                 const uploads = await Uploads.fromFileSystemEntries(files);
                 addFileImporter(uploads);
 
