26d030d342d15a0e638aaf62ec0dd1829658d578
Kevin Burton <burtonator@gmail.com>
2020-10-06 13:38:00 -0600

    ...

--- apps/repository/js/premium/PremiumScreen.tsx
+++ apps/repository/js/premium/PremiumScreen.tsx
@@ -3,7 +3,6 @@ import {FixedNav, FixedNavBody} from '../FixedNav';
 import {RepoFooter} from "../repo_footer/RepoFooter";
 import {PremiumStoreProvider} from './PremiumStore';
 import {PremiumContent2} from "./PremiumContent2";
-import { PremiumContent } from './PremiumContent';
 
 export const PremiumScreen = () => {
 
