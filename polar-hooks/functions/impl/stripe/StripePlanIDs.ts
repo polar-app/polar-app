640663f4f59a5c66c5a96417d29eef853e4ee6b4
Kevin Burton <burtonator@gmail.com>
2020-10-08 12:59:10 -0600

    nw stripe plan IDs...

--- polar-hooks/functions/impl/stripe/StripePlanIDs.ts
+++ polar-hooks/functions/impl/stripe/StripePlanIDs.ts
@@ -2,39 +2,35 @@ import {Billing} from 'polar-accounts/src/Billing';
 import {Plans} from "polar-accounts/src/Plans";
 
 export enum StripePlanID {
-
     FREE = "plan_free",
     GOLD = "plan_gold",
     SILVER = "plan_silver",
     BRONZE = "plan_bronze"
-
 }
 
 export enum StripeYearPlanID {
-
     FREE = "plan_free_year",
     GOLD = "plan_gold_year",
     SILVER = "plan_silver_year",
     BRONZE = "plan_bronze_year"
-
 }
 
 export enum StripePlanV2ID {
-    FREE = "price_1HZQdbJvJ2rsXwXz3bOYr735",
-    PLUS = "price_1HZ3lvJvJ2rsXwXzdK2V5XnM",
-    PRO = "price_1HZ3mBJvJ2rsXwXzR4AN6Ub8",
+    FREE = "price_1Ha2GwJvJ2rsXwXzNDIxQ70K",
+    PLUS = "price_1Ha27jJvJ2rsXwXzY5Yq42aD",
+    PRO = "price_1Ha28iJvJ2rsXwXzlPCQiD0h",
 }
 
 export enum StripeYearPlanV2ID {
-    FREE = "price_1HZQdbJvJ2rsXwXz3bOYr735",
-    PLUS = "price_1HZ3mfJvJ2rsXwXzBh0uKg3y",
-    PRO = "price_1HZ3n9JvJ2rsXwXzg8iEHuLg",
+    FREE = "price_1Ha2GwJvJ2rsXwXzNDIxQ70K",
+    PLUS = "price_1Ha29PJvJ2rsXwXzEeM0YrOm",
+    PRO = "price_1Ha2F8JvJ2rsXwXzt9zPYMCe",
 }
 
 export enum Stripe4YearPlanV2ID {
-    FREE = "price_1HZQdbJvJ2rsXwXz3bOYr735",
-    PLUS = "price_1HZ3nPJvJ2rsXwXzN3IlrHtn",
-    PRO = "price_1HZ3ncJvJ2rsXwXzIMze3OBe",
+    FREE = "price_1Ha2GwJvJ2rsXwXzNDIxQ70K",
+    PLUS = "price_1Ha2G0JvJ2rsXwXzNe7aumpf",
+    PRO = "price_1Ha2GbJvJ2rsXwXzVp5bHmnC",
 }
 
 export class StripePlanIDs {
