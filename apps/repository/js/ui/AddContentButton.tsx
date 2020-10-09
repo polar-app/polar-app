8c1db7dc0eec7156fc1ed1fb1e4ac88648714766
Kevin Burton <burtonator@gmail.com>
2020-10-09 12:04:09 -0600

    add content button tooltip

--- apps/repository/js/ui/AddContentButton.tsx
+++ apps/repository/js/ui/AddContentButton.tsx
@@ -3,6 +3,7 @@ import Button from '@material-ui/core/Button';
 import AddIcon from '@material-ui/icons/Add';
 import {AddContentFab} from "./AddContentFab";
 import {useHistory} from 'react-router-dom';
+import {MUITooltip} from "../../../../web/js/mui/MUITooltip";
 
 export namespace AddContent {
 
@@ -31,17 +32,19 @@ export namespace AddContent {
         const doAdd = useAddFileDropzone();
 
         return (
-            <Button id="add-content-dropdown"
-                    variant="contained"
-                    color="primary"
-                    startIcon={<AddIcon/>}
-                    onClick={doAdd}
-                    style={{
-                        minWidth: '285px'
-                    }}
-                    size="large">
-                Add Document
-            </Button>
+            <MUITooltip title="Upload and add PDFs and EPUBs to your repository">
+                <Button id="add-content-dropdown"
+                        variant="contained"
+                        color="primary"
+                        startIcon={<AddIcon/>}
+                        onClick={doAdd}
+                        style={{
+                            minWidth: '285px'
+                        }}
+                        size="large">
+                    Add Document
+                </Button>
+            </MUITooltip>
         );
     };
 
