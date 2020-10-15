36faf59fd279e378db091fedbd5ef62c3c5b9562
Kevin Burton <burtonator@gmail.com>
2020-10-14 18:40:52 -0600

    small fixes and updated to latest amplitude lib...

--- gatsby-config.js
+++ gatsby-config.js
@@ -68,7 +68,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: "UA-122721184-3",
       },
     },
     `gatsby-plugin-sitemap`,
