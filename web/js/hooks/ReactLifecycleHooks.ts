2733698b62ae3d17b798a6be4beb13aabf1386ac
Kevin Burton <burtonator@gmail.com>
2020-10-16 15:36:50 -0600

    cleanup...

--- web/js/hooks/ReactLifecycleHooks.ts
+++ web/js/hooks/ReactLifecycleHooks.ts
@@ -1,6 +1,8 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+
 import {useEffect} from "react";
 
-export function useComponentDidMount<T>(delegate: () => void) {
+export function useComponentDidMount(delegate: () => void) {
     // https://dev.to/trentyang/replace-lifecycle-with-hooks-in-react-3d4n
 
     // will only execute the first time.
