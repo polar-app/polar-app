7bc83185bd92a7d3ec0465a3823c100815059853
Kevin Burton <burtonator@gmail.com>
2020-10-18 15:16:38 -0600

    now a functional component

--- web/js/annotation_sidebar/child_annotations/CancelButton.tsx
+++ web/js/annotation_sidebar/child_annotations/CancelButton.tsx
@@ -1,39 +1,16 @@
 import * as React from 'react';
 import Button from '@material-ui/core/Button';
 
-/**
- * TODO: refactor as functional component
- */
-export class CancelButton extends React.PureComponent<IProps, IState> {
-
-    constructor(props: IProps, context: any) {
-        super(props, context);
-    }
-
-    public render() {
-
-        return (
-
-            <Button onClick={() => this.onClick()}>
-
-                Cancel
-
-            </Button>
-
-        );
-
-    }
-
-    private onClick() {
-        this.props.onClick();
-    }
-
-}
-
 interface IProps {
     readonly onClick: () => void;
 }
 
-interface IState {
+export const CancelButton = React.memo((props: IProps) => {
 
-}
+    return (
+        <Button onClick={props.onClick}>
+            Cancel
+        </Button>
+    );
+
+});
