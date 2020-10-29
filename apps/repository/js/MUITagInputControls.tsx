8b2e32a1c5c16d32f2dbac5224564904628908e0
Kevin Burton <burtonator@gmail.com>
2020-10-29 16:28:25 -0600

    input trim tags before creating them...

--- apps/repository/js/MUITagInputControls.tsx
+++ apps/repository/js/MUITagInputControls.tsx
@@ -1,10 +1,11 @@
+import * as React from 'react';
 import {Tag} from "polar-shared/src/tags/Tags";
-import * as React from "react";
 import {ValueAutocompleteOption} from "../../../web/js/mui/autocomplete/MUICreatableAutocomplete";
 
 export namespace MUITagInputControls {
 
     export function toAutocompleteOption(tag: Tag): ValueAutocompleteOption<Tag> {
+
         return {
             id: tag.id,
             label: tag.label,
@@ -13,9 +14,15 @@ export namespace MUITagInputControls {
                 label: tag.label,
             }
         };
-    };
+
+    }
 
     export function createOption(input: string): ValueAutocompleteOption<Tag> {
+
+        // trim the input so that if the user enters a space its is trimmed as tags that
+        // end with strings don't make any sense.
+        input = input.trim();
+
         return {
             id: input,
             label: input,
@@ -24,5 +31,7 @@ export namespace MUITagInputControls {
                 label: input,
             }
         };
+
     }
+
 }
