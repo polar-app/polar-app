63cf3c56658f1ecfb5c6bba603f85c33c4259500
Kevin Burton <burtonator@gmail.com>
2020-10-30 14:11:45 -0600

    - Fixed bug with LineTest.ts - new package for the backnd api which has types for the frontend too

--- web/js/util/LineTest.ts
+++ web/js/util/LineTest.ts
@@ -6,7 +6,7 @@ describe('Line', function() {
 
     it("length", function() {
 
-        const line = new Line(10, 20);
+        const line = new Line(10, 20, 'x');
         assert.equal(line.length, 10);
 
         const expected = {
