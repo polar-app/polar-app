6674d7f356afa8ff75d2f435012878e4e6d13133
Kevin Burton <burtonator@gmail.com>
2020-10-31 11:44:38 -0600

    ...

--- apps/repository/js/doc_repo/doc_metadata_editor/TextProperty.tsx
+++ apps/repository/js/doc_repo/doc_metadata_editor/TextProperty.tsx
@@ -4,6 +4,8 @@ import {DocInfoProperty} from "./DocMetadataEditor";
 import {IDocInfo} from "polar-shared/src/metadata/IDocInfo";
 import {Strings} from "polar-shared/src/util/Strings";
 import TextareaAutosize from "@material-ui/core/TextareaAutosize";
+import FormHelperText from "@material-ui/core/FormHelperText";
+import InputLabel from "@material-ui/core/InputLabel";
 
 interface IProps extends DocInfoProperty {
     readonly className?: string;
@@ -18,12 +20,26 @@ export const TextProperty = deepMemo((props: IProps) => {
     const label = props.label || Strings.upperFirst(props.name);
 
     return (
-        <TextareaAutosize className={props.className}
-                          style={props.style}
-                          rowsMin={3}
-                          rowsMax={10}
-                          required={! props.optional}
-                          defaultValue={props.value || ''}
-                          onChange={event => props.onChange(event.target.value)}/>
+        <div className={props.className}
+             style={{
+                 ...props.style,
+             }}>
+
+            <InputLabel shrink>{label}</InputLabel>
+
+            <div style={{display: 'flex'}}>
+                <TextareaAutosize className={props.className}
+                              style={{flexGrow: 1}}
+                              rowsMin={5}
+                              required={! props.optional}
+                              defaultValue={props.value || ''}
+                              onChange={event => props.onChange(event.target.value)}/>
+            </div>
+            {props.description !== undefined && (
+                <div>
+                    <FormHelperText>{props.description}</FormHelperText>
+                </div>
+            )}
+        </div>
     );
 });
