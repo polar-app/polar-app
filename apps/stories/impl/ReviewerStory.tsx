c7dc857bb086de7b294d57deb958f0359fd1a7be
Kevin Burton <burtonator@gmail.com>
2020-11-01 10:09:44 -0700

    ... the sides don't flip for cloze deletions.

--- apps/stories/impl/ReviewerStory.tsx
+++ apps/stories/impl/ReviewerStory.tsx
@@ -6,17 +6,17 @@ import {
 import {Refs} from "polar-shared/src/metadata/Refs";
 import {ISODateTimeStrings} from "polar-shared/src/metadata/ISODateTimeStrings";
 import {TasksCalculator} from "polar-spaced-repetition/src/spaced_repetition/scheduler/S2Plus/TasksCalculator";
-import {Preconditions} from "polar-shared/src/Preconditions";
 import {BrowserRouter, Switch} from "react-router-dom";
 import {FlashcardTaskAction} from "../../repository/js/reviewer/cards/FlashcardTaskAction";
 import {MockDocMetas} from "../../../web/js/metadata/DocMetas";
 import {Flashcards} from "../../../web/js/metadata/Flashcards";
 import {DocAnnotations} from "../../../web/js/annotation_sidebar/DocAnnotations";
 import {FlashcardTaskActions} from "../../repository/js/reviewer/cards/FlashcardTaskActions";
-import {RatingCallback} from "../../repository/js/reviewer/ReviewerStore";
-import {FinishedCallback, Reviewer3, SuspendedCallback} from "../../repository/js/reviewer/Reviewer3";
+import {Reviewer3} from "../../repository/js/reviewer/Reviewer3";
 import {ReactRouters} from "../../../web/js/react/router/ReactRouters";
 import {ReviewerDialog2} from "../../repository/js/reviewer/ReviewerDialog2";
+import Button from '@material-ui/core/Button';
+import {HTMLStr} from "polar-shared/src/util/Strings";
 //
 // const createFlashcardTaskReps = async () => {
 //
@@ -39,47 +39,51 @@ const createFlashcardTaskReps = (): ReadonlyArray<TaskRep<FlashcardTaskAction>> 
     const pageMeta = Object.values(docMeta.pageMetas)[0];
     const ref = Refs.create('1234', 'text-highlight');
 
-    const createFrontAndBackAction = () => {
+    const createFrontAndBackAction = (front: string, back: string) => {
 
-        const flashcard = Flashcards.createFrontBack('What is the capital of California? ', 'Sacramento', ref);
+        const flashcard = Flashcards.createFrontBack(front, back, ref);
         const docAnnotation = DocAnnotations.createFromFlashcard(docMeta, flashcard, pageMeta);
         const flashcardTaskActions = FlashcardTaskActions.create(flashcard, docAnnotation);
         return flashcardTaskActions[0];
     };
 
-    const createClozeAction = () => {
-        const flashcard = Flashcards.createCloze('The capital of california is {{c1::Sacramento}}.', ref);
+    const createClozeAction = (text: HTMLStr) => {
+        const flashcard = Flashcards.createCloze(text, ref);
         const docAnnotation = DocAnnotations.createFromFlashcard(docMeta, flashcard, pageMeta);
         const flashcardTaskActions = FlashcardTaskActions.create(flashcard, docAnnotation);
         return flashcardTaskActions[0];
     };
-
-    const clozeAction = createClozeAction();
-
-    Preconditions.assertPresent(clozeAction, 'clozeAction');
 
     const tasks: ReadonlyArray<Task<FlashcardTaskAction>> = [
         {
             id: "10102",
-            action: clozeAction,
+            action: createClozeAction('The capital of california is {{c1::Sacramento}}.'),
             created: ISODateTimeStrings.create(),
             color: 'red',
             mode: 'flashcard'
         },
         {
             id: "10102",
-            action: createFrontAndBackAction(),
+            action: createFrontAndBackAction('What is the capital of California? ', 'Sacramento'),
             created: ISODateTimeStrings.create(),
             color: 'red',
             mode: 'flashcard'
         },
-        // {
-        //     id: "10103",
-        //     action: createFrontAndBackAction(),
-        //     created: ISODateTimeStrings.create(),
-        //     color: 'red',
-        //     mode: 'flashcard'
-        // }
+        {
+            id: "10103",
+            action: createFrontAndBackAction('Who let the dogs out?', 'woof, woof, woof, woof.'),
+            created: ISODateTimeStrings.create(),
+            color: 'red',
+            mode: 'flashcard'
+        },
+        {
+            id: "10103",
+            action: createFrontAndBackAction('Who is your daddy and what does he do?', "It's not a tumor!"),
+            created: ISODateTimeStrings.create(),
+            color: 'red',
+            mode: 'flashcard'
+        }
+
     ];
 
     return tasks.map(task => TasksCalculator.createInitialLearningState(task));
@@ -88,35 +92,48 @@ const createFlashcardTaskReps = (): ReadonlyArray<TaskRep<FlashcardTaskAction>> 
 
 const taskReps = createFlashcardTaskReps();
 
-const doRating: RatingCallback<FlashcardTaskAction> = async (taskRep, rating) => {
-    console.log("onRating: ", {taskRep, rating});
-};
-
-const doSuspended: SuspendedCallback<FlashcardTaskAction> = async (taskRep) => {
-    console.log("onSuspended: ", {taskRep});
-};
-
-const doFinished: FinishedCallback = async (cancelled) => {
-    console.log("onFinished: ", {cancelled});
-};
-
 export const ReviewerStory = () => {
 
-    const [open, setOpen] = React.useState(true);
+    const [open, setOpen] = React.useState(false);
+
+    const doRating = React.useCallback(async (taskRep, rating) => {
+        console.log("onRating: ", {taskRep, rating});
+    }, []);
+
+    const doSuspended = React.useCallback(async (taskRep) => {
+        console.log("onSuspended: ", {taskRep});
+    }, []);
+
+    const doFinished = React.useCallback(async (cancelled) => {
+        console.log("onFinished: ", {cancelled});
+        setOpen(false);
+    }, []);
 
     return (
 
         <BrowserRouter key="browser-router">
             <Switch location={ReactRouters.createLocationWithPathAndHash()}>
-                <ReviewerDialog2 open={open}
-                                 onClose={() => setOpen(false)}>
 
-                    <Reviewer3 taskReps={taskReps}
-                               doRating={doRating}
-                               doSuspended={doSuspended}
-                               doFinished={doFinished}/>
+                <>
+                    {open && (
+                        <ReviewerDialog2 open={open}
+                                     onClose={() => setOpen(false)}>
 
-                </ReviewerDialog2>
+                            <Reviewer3 taskReps={taskReps}
+                                       doRating={doRating}
+                                       doSuspended={doSuspended}
+                                       doFinished={doFinished}/>
+
+                        </ReviewerDialog2>)}
+
+                    <Button variant="contained"
+                            color="primary"
+                            size="large"
+                            onClick={() => setOpen(true)}>
+                        Start Review
+                    </Button>
+                </>
+
             </Switch>
         </BrowserRouter>
 
