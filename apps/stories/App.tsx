5720f498b6b2231c7ddc99493fe0bc4ed8333f00
Kevin Burton <burtonator@gmail.com>
2020-11-01 18:33:13 -0700

    card layout...

--- apps/stories/App.tsx
+++ apps/stories/App.tsx
@@ -14,6 +14,7 @@ import {CKEditor5Story} from "./impl/CKEditor5Story";
 import {NotesStory} from "./impl/NotesStory";
 import {EPUBThumbnailerStory} from "./impl/EPUBThumbnailerStory";
 import {ReviewerStory} from "./impl/ReviewerStory";
+import {DocCardStory} from "./impl/DocCardStory";
 
 interface IStory {
     readonly name: string;
@@ -59,6 +60,10 @@ const stories = createStoryIndex([
     {
         name: "Reviewer",
         component: <ReviewerStory/>
+    },
+    {
+        name: "Doc Cards",
+        component: <DocCardStory/>
     }
 
 ]);
