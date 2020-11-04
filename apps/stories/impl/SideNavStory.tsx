ea725a215fc03de8f9657442536e914fd01b9c15
Kevin Burton <burtonator@gmail.com>
2020-11-04 12:40:12 -0700

    ...

--- apps/stories/impl/SideNavStory.tsx
+++ apps/stories/impl/SideNavStory.tsx
@@ -1,11 +1,61 @@
 import * as React from 'react';
 import {SideNav} from "../../../web/js/sidenav/SideNav";
-import {SideNavStoreProvider, useSideNavCallbacks} from "../../../web/js/sidenav/SideNavStore";
+import {SideNavStoreProvider, TabDescriptor, useSideNavCallbacks} from "../../../web/js/sidenav/SideNavStore";
 import Button from '@material-ui/core/Button';
 import {DocCardImages} from "./doc_cards/DocCardImages";
 import {SideNavContent} from "../../../web/js/sidenav/SideNavContent";
 
 let seq = 0;
+
+const createWebCard = (id: number): TabDescriptor => {
+    return {
+
+        id,
+        url: `http://example.com/${id}`,
+        title: 'this is the title',
+        icon: (
+            <img src={DocCardImages.WEB_CARD_IMAGE_URL} alt='foo'/>
+        ),
+        content: (
+            <div>this is the content for tab {id}</div>
+        ),
+        image: {
+            url: DocCardImages.WEB_CARD_IMAGE_URL,
+            width: 200,
+            height: 200
+        }
+    };
+}
+
+
+const createPDFCard = (id: number): TabDescriptor => {
+    return {
+
+        id,
+        url: `http://example.com/${id}`,
+        title: 'this is the title',
+        icon: (
+            <img src={DocCardImages.PDF_CARD_IMAGE_URL} alt='foo'/>
+        ),
+        content: (
+            <div>this is the content for tab {id}</div>
+        ),
+        image: {
+            url: DocCardImages.PDF_CARD_IMAGE_URL,
+            width: 200,
+            height: 200
+        }
+    };
+}
+
+const createCard = (id: number) => {
+
+    if (id % 2 === 0) {
+        return createPDFCard(id);
+    } else {
+        return createWebCard(id)
+    }
+}
 
 export const Main = () => {
 
@@ -13,26 +63,8 @@ export const Main = () => {
 
     const doAddTab = React.useCallback(() => {
 
-        seq = seq + 1;
-
-        const id = seq;
-
-        addTab({
-            id,
-            url: `http://example.com/${id}`,
-            title: 'this is the title',
-            icon: (
-                <img src={DocCardImages.WEB_CARD_IMAGE_URL} alt='foo'/>
-            ),
-            content: (
-                <div>this is the content for tab {id}</div>
-            ),
-            image: {
-                url: DocCardImages.WEB_CARD_IMAGE_URL,
-                width: 200,
-                height: 200
-            }
-        });
+        const id = seq++;
+        addTab(createCard(id));
 
     }, [addTab]);
 
