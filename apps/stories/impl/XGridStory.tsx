dfa5e7dd7e7046e67ff559949d3998067322fcf9
Kevin Burton <burtonator@gmail.com>
2020-11-09 19:18:55 -0700

    removed xgrid test.

--- apps/stories/impl/XGridStory.tsx
+++ apps/stories/impl/XGridStory.tsx
@@ -1,20 +1,21 @@
 import * as React from 'react';
-import { XGrid } from '@material-ui/x-grid';
-import { useDemoData } from '@material-ui/x-grid-data-generator';
+// import { XGrid } from '@material-ui/x-grid';
+// import { useDemoData } from '@material-ui/x-grid-data-generator';
 
 export default function XGridStory() {
-    const { data } = useDemoData({
-        dataSet: 'Commodity',
-        rowLength: 1000,
-    });
-
-    return (
-        <XGrid
-            {...data}
-            loading={data.rows.length === 0}
-            rowHeight={38}
-            hideFooter={true}
-            checkboxSelection
-        />
-    );
+    // const { data } = useDemoData({
+    //     dataSet: 'Commodity',
+    //     rowLength: 1000,
+    // });
+    //
+    // return (
+    // //     <XGrid
+    //         {...data}
+    //         loading={data.rows.length === 0}
+    //         rowHeight={38}
+    //         hideFooter={true}
+    //         checkboxSelection
+    //     />
+    // );
+    return null;
 }
