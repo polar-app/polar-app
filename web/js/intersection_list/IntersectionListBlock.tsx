d075ad8c1521837ddd788fb2828fa979e423f1b8
Kevin Burton <burtonator@gmail.com>
2020-11-10 08:51:26 -0700

    attempting to get this to work with pluggable strategies.

--- web/js/intersection_list/IntersectionListBlock.tsx
+++ web/js/intersection_list/IntersectionListBlock.tsx
@@ -23,42 +23,86 @@ interface IProps<V extends ListValue> {
 
 }
 
+interface IViewState {
+    readonly ref: (node?: Element | null | undefined) => void;
+    readonly inView: boolean;
+}
+
+interface IntersectionObserverViewStateOpts {
+    readonly root: HTMLElement;
+
+}
+
+function useIntersectionObserverViewState(opts: IntersectionObserverViewStateOpts): IViewState {
+
+    const {root} = opts;
+
+    const [inView, setUseInView] = React.useState(false);
+
+    const observation = useInView({
+        threshold: 0,
+        trackVisibility: true,
+        delay: 100,
+        root
+    });
+
+    if (observation.inView !== inView) {
+        // only fire when inView changes.
+        setUseInView(observation.inView);
+    }
+
+    return {
+        ref: observation.ref,
+        inView
+    };
+
+}
+
+export function useIntersectionObserverUsingCalculationViewState(opts: IntersectionObserverViewStateOpts) {
+
+    const {ref, inView, entry} = useInView({
+        threshold: 0,
+        trackVisibility: true,
+        delay: 100,
+        root: opts.root
+    });
+
+    //
+    // function computeInView() {
+    //
+    //     const buffer = window.outerHeight * 0.5;
+    //
+    //     if (! entry) {
+    //         return false;
+    //     }
+    //
+    //     const line = new Line(0 - buffer, window.outerHeight + buffer, 'y');
+    //
+    //     if (line.containsPoint(entry.boundingClientRect.top)) {
+    //         console.log(`FIXME top: ${entry.boundingClientRect.top} [${line.start}, ${line.end}]`);
+    //         return true;
+    //     }
+    //
+    //     if (line.containsPoint(entry.boundingClientRect.bottom)) {
+    //         console.log(`FIXME bottom: ${entry.boundingClientRect.bottom} [${line.start}, ${line.end}]`);
+    //         return true;
+    //     }
+    //
+    //     return true;
+    //
+    // }
+    //
+    // const inView = computeInView();
+}
+
+
 export const IntersectionListBlock = typedMemo(function<V extends ListValue>(props: IProps<V>) {
 
     const BlockComponent = props.blockComponent;
 
-    const {ref, entry} = useInView({
-        threshold: 0,
-        root: props.root
-    });
-
-    function computeInView() {
-
-        const buffer = window.outerHeight * 0.5;
-
-        if (! entry) {
-            return false;
-        }
-
-        const line = new Line(0 - buffer, window.outerHeight + buffer, 'y');
-
-        if (line.containsPoint(entry.boundingClientRect.top)) {
-            return true;
-        }
-
-        if (line.containsPoint(entry.boundingClientRect.bottom)) {
-            return true;
-        }
-
-        return true;
-
-    }
-
-    const inView = computeInView();
+    const {ref, inView} = useIntersectionObserverViewState(props);
 
     const indexBase = props.blockIndex * props.blockSize;
-
-    // FIXME: we can use just the
 
     return (
         <BlockComponent innerRef={ref} values={props.values}>
