3c80f8c8d485793c4e6919bce36ba482094c1b05
Kevin Burton <burtonator@gmail.com>
2020-11-11 15:16:21 -0700

    removal of .jsx

--- web/js/ui/svg_icons/PolarSVGIcon.jsx
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.PolarSVGIcon = void 0;
-var React = require("react");
-var icon_svg_1 = require("polar-assets/src/assets/logo/icon.svg");
-// TODO: if we embed how do we specify the width and height
-exports.PolarSVGIcon = React.memo(function (props) { return (<img src={icon_svg_1.default} width={props.width} height={props.height} className={props.className} alt="Polar Logo"/>); });
