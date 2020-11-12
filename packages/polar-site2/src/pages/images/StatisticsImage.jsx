6179f9e01a9806ee1149fb2424c242a641290f36
Kevin Burton <burtonator@gmail.com>
2020-11-12 18:12:38 +0000

    Imported polar-site2 as a subtree.

---
+++ packages/polar-site2/src/pages/images/StatisticsImage.jsx
@@ -0,0 +1,11 @@
+"use strict";
+var __makeTemplateObject = (this && this.__makeTemplateObject) || function (cooked, raw) {
+    if (Object.defineProperty) { Object.defineProperty(cooked, "raw", { value: raw }); } else { cooked.raw = raw; }
+    return cooked;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var React = require("react");
+var gatsby_1 = require("gatsby");
+var gatsby_image_1 = require("gatsby-image");
+exports.default = (function (props) { return (<gatsby_1.StaticQuery query={gatsby_1.graphql(templateObject_1 || (templateObject_1 = __makeTemplateObject(["\n  query {\n    file(relativePath: {eq: \"screenshots/2020-10-statistics.png\"}) {\n      childImageSharp {\n        fluid(maxWidth: 1280) {\n            ...GatsbyImageSharpFluid_withWebp\n        }\n      }\n    }\n  }\n        "], ["\n  query {\n    file(relativePath: {eq: \"screenshots/2020-10-statistics.png\"}) {\n      childImageSharp {\n        fluid(maxWidth: 1280) {\n            ...GatsbyImageSharpFluid_withWebp\n        }\n      }\n    }\n  }\n        "])))} render={function (data) { return (<gatsby_image_1.default fluid={data.file.childImageSharp.fluid} style={props.style} className={props.className} alt={props.alt}/>); }}/>); });
+var templateObject_1;
