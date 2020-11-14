06aba7d7d0a85b659715a6245836c337ea183ce7
Kevin Burton <burtonator@gmail.com>
2020-11-14 11:33:27 -0700

    basic wrapper works now

--- App.tsx
+++ App.tsx
@@ -1,13 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import WebView from "react-native-webview";
+// const UserAgent = require('react-native-user-agent')
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    return (
+        <WebView source={{ uri: "https://beta.getpolarized.io/" }}
+                 // source={{
+                 //   uri: "https://www.whatismybrowser.com/detect/what-is-my-user-agent",
+                 // }}
+                 userAgent={getUserAgent()}
+                 style={{ width: "100%", height: "100%", marginTop: 20 }}/>
   );
 }
 
@@ -19,3 +23,16 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+
+//google blocks oath via WebView, this hides the fact that
+// the app runs in web view.
+function getUserAgent() {
+  //
+  // let userAgent = UserAgent.getUserAgent();
+  // userAgent = userAgent.replace("wv", "");
+  //
+  // return userAgent;
+
+  return "Mozilla/5.0 (Linux; Android 9; Android SDK built for x86_64 Build/PSR1.180720.075) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/69.0.3497.100 Mobile Safari/537.36";
+}
