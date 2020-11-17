3b91a8fba1ec2dc5ca52434bbfe2fb4afd024443
Kevin Burton <burtonator@gmail.com>
2020-11-17 13:42:23 -0700

    .... coment out so we can push.

--- web/js/annotation_sidebar/annotations/AnnotationView2.tsx
+++ web/js/annotation_sidebar/annotations/AnnotationView2.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import {IDocAnnotationRef} from '../DocAnnotation';
 import {isPresent} from 'polar-shared/src/Preconditions';
 import {AnnotationType} from 'polar-shared/src/metadata/AnnotationType';
-import {MUIHoverController} from "../../mui/context/MUIHoverContext";
+// import {MUIHoverController} from "../../mui/context/MUIHoverContext";
 import {AreaHighlightAnnotationView2} from "./AreaHighlightAnnotationView2";
 import {TextHighlightAnnotationView2} from './TextHighlightAnnotationView2';
 import {ViewOrEditFlashcard2} from "../child_annotations/flashcards/ViewOrEditFlashcard2";
@@ -10,6 +10,9 @@ import {ViewOrEditComment2} from '../child_annotations/comments/ViewOrEditCommen
 import {AnnotationInputView} from "../AnnotationInputView";
 import {ChildAnnotationSection2} from "../child_annotations/ChildAnnotationSection2";
 import {deepMemo} from "../../react/ReactUtils";
+// import {createContextMenu} from "../../../../apps/repository/js/doc_repo/MUIContextMenu2";
+// import {IDocViewerContextMenuOrigin} from "../../../../apps/doc/src/DocViewerMenu";
+// import {MUIDocDropdownMenuItems} from "../../../../apps/repository/js/doc_repo/MUIDocDropdownMenuItems";
 
 interface IProps {
     readonly annotation: IDocAnnotationRef;
@@ -47,6 +50,9 @@ const AnnotationTypeComponent = deepMemo((props: IProps) => {
     }
 
 });
+
+// export const [AnnotationContextMenu, useAnnotationContextMenu]
+//     = createContextMenu<IDocViewerContextMenuOrigin>(MUIDocDropdownMenuItems);
 
 /**
  * A generic wrapper that determines which sub-component to render.
