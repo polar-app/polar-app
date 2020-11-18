5b5b427e0aa7d4020256081979b4535ab6879bb1
Kevin Burton <burtonator@gmail.com>
2020-11-17 19:00:32 -0700

    bacic custom CSS for CKEditor5.tsx

--- apps/stories/impl/ckeditor5/CKEditor5.tsx
+++ apps/stories/impl/ckeditor5/CKEditor5.tsx
@@ -2,6 +2,8 @@ import CKEditor from "@ckeditor/ckeditor5-react";
 import BalloonEditor from "@ckeditor/ckeditor5-build-balloon";
 import React from "react";
 import {deepMemo} from "../../../../web/js/react/ReactUtils";
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import {CKEditor5GlobalCss} from "./CKEditor5GlobalCss";
 
 interface IProps {
     readonly content: string;
@@ -10,24 +12,27 @@ interface IProps {
 
 export const CKEditor5 = deepMemo((props: IProps) => {
     return (
-        <CKEditor
-            editor={ BalloonEditor }
-            data={props.content}
-            onInit={ (editor: any) => {
-                // You can store the "editor" and use when it is needed.
-                console.log( 'Editor is ready to use!', editor );
-            } }
-            onChange={ ( event: any, editor: any ) => {
-                const data = editor.getData();
-                console.log( { event, editor, data } );
-                props.onChange(data);
-            } }
-            onBlur={ ( event: any, editor: any ) => {
-                console.log( 'Blur.', editor );
-            } }
-            onFocus={ ( event: any, editor: any ) => {
-                console.log( 'Focus.', editor );
-            } }
-        />
+        <>
+            <CKEditor5GlobalCss/>
+            <CKEditor
+                editor={ ClassicEditor }
+                data={props.content}
+                onInit={ (editor: any) => {
+                    // You can store the "editor" and use when it is needed.
+                    console.log( 'Editor is ready to use!', editor );
+                } }
+                onChange={ ( event: any, editor: any ) => {
+                    const data = editor.getData();
+                    console.log( { event, editor, data } );
+                    props.onChange(data);
+                } }
+                onBlur={ ( event: any, editor: any ) => {
+                    console.log( 'Blur.', editor );
+                } }
+                onFocus={ ( event: any, editor: any ) => {
+                    console.log( 'Focus.', editor );
+                } }
+            />
+        </>
     );
 });
