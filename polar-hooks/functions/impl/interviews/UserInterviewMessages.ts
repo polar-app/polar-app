7a1e1ddfb840fb1874a43159c4425c5ebc20b229
Kevin Burton <burtonator@gmail.com>
2020-11-26 15:33:38 -0700

    some new (rough) user interview code...

--- polar-hooks/functions/impl/interviews/UserInterviewMessages.ts
+++ polar-hooks/functions/impl/interviews/UserInterviewMessages.ts
@@ -74,22 +74,13 @@ I need a favor! Could I interview you about how you use Polar?
 
 It won't take more than 15 minutes.
 
-This feedback *really* helps me prioritize important features.
+This feedback *really* helps us prioritize important features.
 
 My calendar link is here:
 
 ${this.messageOpts.from.calendarLink}
 
-... and you can just pick a time directly and send you a confirmation!  I picked a 
-30 minute window in case it runs over but it's usually about 15 minutes.
-
-If you're too busy don't worry don't worry about it.  
-
-I created a survey you can use here:
-
-https://kevinburton1.typeform.com/to/TkmP8x 
-
-It's not the same but either way we I appreciate the feedback!
+... and you can just pick a time directly and send you a confirmation!  
 
 Thanks!
 
