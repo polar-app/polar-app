5665206d37e2e4fb3212bbbd442a39801a64e8de
Kevin Burton <burtonator@gmail.com>
2020-11-28 01:26:24 +0000

    Imported polar-bookshelf as a subtree.

---
+++ packages/polar-bookshelf/apps/doc/src/renderers/DocRenderer.tsx
@@ -0,0 +1,118 @@
+import {PDFViewerContainer} from "./pdf/PDFViewerContainer";
+import {PDFDocument} from "./pdf/PDFDocument";
+import * as React from "react";
+import {useDocViewerStore} from "../DocViewerStore";
+import {URLStr} from "polar-shared/src/util/Strings";
+import isEqual from "react-fast-compare";
+import {EPUBDocument} from "./epub/EPUBDocument";
+import {EPUBViewerContainer} from "./epub/EPUBViewerContainer";
+import {FileTypes} from "../../../../web/js/apps/main/file_loaders/FileTypes";
+import {IDocMeta} from "polar-shared/src/metadata/IDocMeta";
+import {FileType} from "../../../../web/js/apps/main/file_loaders/FileType";
+import {EPUBFinderProvider} from "./epub/EPUBFinderStore";
+import {isPresent} from "polar-shared/src/Preconditions";
+import { EPUBDocumentStoreProvider } from "./epub/EPUBDocumentStore";
+
+interface ILoadedProps {
+    readonly docURL: URLStr;
+    readonly docMeta: IDocMeta;
+    readonly children: React.ReactNode;
+}
+
+const PDFDocumentRenderer = (props: ILoadedProps) => {
+    return (
+        <>
+            <PDFViewerContainer/>
+
+            <PDFDocument {...props}/>
+
+        </>
+    );
+}
+
+const EPUBDocumentRenderer = (props: ILoadedProps) => {
+    return (
+        <EPUBDocumentStoreProvider>
+            <EPUBFinderProvider>
+                <>
+                    <EPUBViewerContainer>
+                        <EPUBDocument {...props}/>
+                    </EPUBViewerContainer>
+
+                </>
+            </EPUBFinderProvider>
+        </EPUBDocumentStoreProvider>
+    );
+}
+
+interface IDocViewerContext {
+    readonly fileType: FileType;
+    readonly docID: string;
+}
+
+export const DocViewerContext = React.createContext<IDocViewerContext>(null!);
+
+/**
+ * Provided so we can determine which type of doc type we should load (epub,
+ * pdf, etc)
+ */
+export function useDocViewerContext(): IDocViewerContext {
+    const result = React.useContext(DocViewerContext);
+
+    if (! isPresent(result)) {
+        throw new Error("DocViewerContext not defined.");
+    }
+
+    return result;
+}
+
+interface DocRendererDelegateProps {
+    readonly docURL: string,
+    readonly docMeta: IDocMeta;
+    readonly fileType: FileType;
+    readonly children: React.ReactNode;
+}
+
+const DocRendererDelegate = React.memo((props: DocRendererDelegateProps) => {
+
+    switch (props.fileType) {
+
+        case "pdf":
+            return (
+                <PDFDocumentRenderer docURL={props.docURL} docMeta={props.docMeta}>
+                    {props.children}
+                </PDFDocumentRenderer>
+            );
+        case "epub":
+            return (
+                <EPUBDocumentRenderer docURL={props.docURL} docMeta={props.docMeta}>
+                    {props.children}
+                </EPUBDocumentRenderer>
+            );
+        default:
+            return null;
+    }
+
+});
+
+interface IProps {
+    readonly children: React.ReactNode;
+}
+
+export const DocRenderer = React.memo((props: IProps) => {
+
+    const {docURL, docMeta} = useDocViewerStore(['docURL', 'docMeta']);
+
+    if (! docURL || ! docMeta) {
+        return null;
+    }
+
+    const fileType = FileTypes.create(docURL);
+
+    return (
+        <DocRendererDelegate docURL={docURL} docMeta={docMeta} fileType={fileType}>
+            {props.children}
+        </DocRendererDelegate>
+    );
+
+}, isEqual);
