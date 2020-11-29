f2cca51e0dfb0f4bf47081b74f173bfc1b7dcf18
Kevin Burton <burtonator@gmail.com>
2020-11-28 19:26:22 -0700

    ...

--- apps/repository/js/logs/LogsScreen.tsx
+++ apps/repository/js/logs/LogsScreen.tsx
@@ -4,16 +4,21 @@ import {CopyLogsToClipboardButton} from './CopyLogsToClipboardButton';
 import {ClearLogsButton} from './ClearLogsButton';
 import {DefaultPageLayout} from "../page_layout/DefaultPageLayout";
 import {MUIButtonBar} from "../../../../web/js/mui/MUIButtonBar";
+import Box from '@material-ui/core/Box';
 
 export const LogsScreen = () => (
 
     <DefaultPageLayout>
         <>
-            <MUIButtonBar>
-                <CopyLogsToClipboardButton/>
-                <ClearLogsButton/>
 
-            </MUIButtonBar>
+            <Box mb={1} mt={1}>
+
+                <MUIButtonBar>
+                    <CopyLogsToClipboardButton/>
+                    <ClearLogsButton/>
+                </MUIButtonBar>
+
+            </Box>
 
             <div>
                 <LogsContent/>
