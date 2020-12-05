8f9d4b2c05454ccda9b97ef7512cad4089ae989e
Kevin Burton <burtonator@gmail.com>
2020-12-05 00:06:42 +0000

    Imported polar-bookshelf as a subtree.

---
+++ packages/polar-bookshelf/web/js/datastore/sharing/db/UserRequest.ts
@@ -0,0 +1,21 @@
+/**
+ * A request payload targeting a specific user. We include the idToken so that we
+ * can decode the token and determine the user that executed the request.
+ *
+ * Separating the value from the idToken means we avoid accidentally copying
+ * the idToken in rest (...) arguments but also avoid conflating object usage.
+ */
+export interface UserRequest<R> {
+
+    /**
+     * The token to authenticate the user.
+     */
+    readonly idToken: IDTokenStr;
+
+    /**
+     * The actual request object.
+     */
+    readonly request: R;
+}
+
+export type IDTokenStr = string;
