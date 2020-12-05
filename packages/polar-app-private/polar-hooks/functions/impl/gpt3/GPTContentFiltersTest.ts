76e83009ba3daeae7626c38e4466928578605115
Kevin Burton <burtonator@gmail.com>
2020-12-05 00:39:21 +0000

    Imported polar-app-private as a subtree.

---
+++ packages/polar-app-private/polar-hooks/functions/impl/gpt3/GPTContentFiltersTest.ts
@@ -0,0 +1,14 @@
+import {assert} from 'chai';
+import {GPTContentFilters} from "./GPTContentFilters";
+
+xdescribe('GPTContentFilters', function() {
+
+    it("basic", async function() {
+
+        const response = await GPTContentFilters.exec(['hello world'])
+
+        assert.equal(response, "safe");
+
+    });
+
+});
