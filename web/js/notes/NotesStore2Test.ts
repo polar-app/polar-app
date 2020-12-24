881099e7b270a6ed6700c257376fb17c4f21df05
Kevin Burton <burtonator@gmail.com>
2020-12-24 10:31:13 -0700

    JSON works now...

--- web/js/notes/NotesStore2Test.ts
+++ web/js/notes/NotesStore2Test.ts
@@ -1,8 +1,18 @@
 import {MockNotes} from "../../../apps/stories/impl/MockNotes";
 import {NotesStore} from "./NotesStore2";
 import {assertJSON} from "../test/Assertions";
+import {Arrays} from "polar-shared/src/util/Arrays";
+import {TestingTime} from "polar-shared/src/test/TestingTime";
 
 describe('NotesStore2', function() {
+
+    beforeEach(() => {
+        TestingTime.freeze()
+    });
+
+    afterEach(() => {
+        TestingTime.unfreeze();
+    });
 
     it("basic", async function() {
 
@@ -30,6 +40,16 @@ describe('NotesStore2', function() {
             "Canada",
             "Germany"
         ]);
+
+        assertJSON(Arrays.first(Object.values(store.index)), {
+            "_content": "World War II (WWII or WW2), also known as the Second World War, was a global war that lasted from 1939 to 1945. It involved the vast majority of the world's countries—including all the great powers—forming two opposing military alliances: the Allies and the Axis.",
+            "_created": "2012-03-02T11:38:49.321Z",
+            "_id": "100",
+            "_items": [],
+            "_links": [],
+            "_type": "item",
+            "_updated": "2012-03-02T11:38:49.321Z",
+        });
 
     });
 
