9f2107ffdf48965de45b0838527404f8b65513df
Kevin Burton <burtonator@gmail.com>
2021-01-18 21:47:29 +0000

    Imported polar-app-public as a subtree.

---
+++ packages/polar-app-public/polar-snapshot-cache/src/ICacheKeyCalculator.ts
@@ -0,0 +1,19 @@
+import { IDocumentSnapshot } from "./store/IDocumentSnapshot";
+import {IDocumentReference} from "./store/IDocumentReference";
+import {IDocumentChange} from "./store/IDocumentChange";
+
+export interface ICacheKeyCalculator {
+
+    /**
+     * Given a document snapshot,  Usually we use the doc ID for this snapshot.
+     */
+    readonly computeForDoc: (collectionName: string, documentSnapshot: IDocumentSnapshot | IDocumentReference | IDocumentChange) => string;
+
+    /**
+     * Compute the cache key for an entire snapshot.  We need to use a unique
+     * snapshot key for each type of snapshot query so that they aren't
+     * intermingled.
+     */
+    readonly computeForQuery: (collectionName: string) => string;
+
+}
