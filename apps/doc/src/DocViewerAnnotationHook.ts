a38811228cca8c8d4bee027e733273713318fdde
Kevin Burton <burtonator@gmail.com>
2021-01-24 16:15:54 -0700

    only process URL jumps for the current URL...

--- apps/doc/src/DocViewerAnnotationHook.ts
+++ apps/doc/src/DocViewerAnnotationHook.ts
@@ -3,6 +3,8 @@ import {useDocViewerCallbacks} from "./DocViewerStore";
 import {AnnotationLinks} from "../../../web/js/annotation_sidebar/AnnotationLinks";
 import {ILocation} from "../../../web/js/react/router/ReactRouters";
 import React from 'react';
+import {useDocViewerContext} from "./renderers/DocRenderer";
+import {DocViewerAppURLs} from "./DocViewerAppURLs";
 
 export type DocViewerJumpCause = 'init' | 'history';
 
@@ -10,21 +12,27 @@ export function useDocViewerJumpToPageLoader(): (location: ILocation, cause: Doc
 
     const {onPageJump} = useDocViewerCallbacks();
     const prevLocationRef = React.useRef<ILocation | undefined>();
+    const currentDocumentLocationPredicate = useCurrentDocumentLocationPredicate();
 
     return (location, cause) => {
 
         try {
 
-            if (prevLocationRef.current?.hash === location.hash) {
-                return false;
-            }
+            if (currentDocumentLocationPredicate(location)) {
 
-            const annotationLink = AnnotationLinks.parse(location.hash);
+                if (prevLocationRef.current?.hash === location.hash) {
+                    return false;
+                }
 
-            if (annotationLink?.page) {
-                console.log(`Jumping to page ${annotationLink.page} due to '${cause}'`);
-                onPageJump(annotationLink.page);
-                return true;
+                const annotationLink = AnnotationLinks.parse(location.hash);
+
+                if (annotationLink?.page) {
+                    console.log(`Jumping to page ${annotationLink.page} due to '${cause}'`);
+                    onPageJump(annotationLink.page);
+                    return true;
+                }
+
+
             }
 
             return false;
@@ -37,6 +45,24 @@ export function useDocViewerJumpToPageLoader(): (location: ILocation, cause: Doc
 
 }
 
+
+/**
+ * Only return true if the URL we're on is for the current doc.  For anything without sidenav
+ * this will just return true.
+ */
+function useCurrentDocumentLocationPredicate() {
+
+    const {docID} = useDocViewerContext();
+
+    return React.useCallback((location: ILocation): boolean => {
+
+        const parsedURL = DocViewerAppURLs.parse(`${location.pathname}${location.search}${location.hash}`);
+
+        return docID === parsedURL?.id;
+
+    }, [docID]);
+
+}
 
 export function useDocViewerPageJumpListener() {
     const location = useLocation();
