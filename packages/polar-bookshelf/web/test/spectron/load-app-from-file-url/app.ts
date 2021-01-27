1f3b9d56aefc183901d205128c21666caa0b6d9d
Kevin Burton <burtonator@gmail.com>
2021-01-27 00:42:17 +0000

    Imported polar-app as a subtree.

--- packages/polar-bookshelf/web/test/spectron/load-app-from-file-url/app.ts
+++
@@ -1,6 +0,0 @@
-import {SpectronRenderer} from '../../../js/test/SpectronRenderer';
-
-SpectronRenderer.run(async (state) => {
-    console.log("Running within SpectronRenderer now.");
-    state.testResultWriter.write(true);
-});
