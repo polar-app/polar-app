74a4ae9dedb6b320b1b2122273346e71e7eaa17e
Kevin Burton <burtonator@gmail.com>
2021-01-29 05:34:02 +0000

    Imported polar-app-public as a subtree.

---
+++ packages/polar-app-public/polar-snapshot-cache/src/ICachedDoc.ts
@@ -0,0 +1,15 @@
+import {TDocumentData} from "./store/TDocumentData";
+
+export interface ICachedDoc {
+
+    readonly id: string;
+
+    /**
+     * false if this value is cached as a negative entry.  This can be used to
+     * listen to snapshot values which don't exist.
+     */
+    readonly exists: boolean;
+
+    readonly data: TDocumentData | undefined;
+
+}
