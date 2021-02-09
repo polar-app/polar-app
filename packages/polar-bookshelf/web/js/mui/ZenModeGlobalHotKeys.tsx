358bf2bb04b7482d27bcd058f7ccdf0e7a3d164f
Kevin Burton <burtonator@gmail.com>
2021-02-09 00:25:08 +0000

    Imported polar-bookshelf as a subtree.

---
+++ packages/polar-bookshelf/web/js/mui/ZenModeGlobalHotKeys.tsx
@@ -0,0 +1,36 @@
+import * as React from 'react';
+import MenuIcon from '@material-ui/icons/Menu';
+import {GlobalKeyboardShortcuts, keyMapWithGroup} from '../keyboard_shortcuts/GlobalKeyboardShortcuts';
+import { useZenModeCallbacks } from './ZenModeStore';
+
+const globalKeyMap = keyMapWithGroup({
+    group: "Zen Mode",
+    groupPriority: -1,
+    keyMap: {
+
+        TOGGLE_ZEN_MODE: {
+            icon: <MenuIcon/>,
+            name: "Toggle Zen Mode",
+            description: "Toggle 'zen' mode by hiding extra navigation items like toolbars, side navigation, etc.",
+            sequences: ['command+shift+z', 'ctrl+shift+z']
+        },
+
+    }
+});
+
+export const ZenModeGlobalHotKeys = React.memo(function ZenModeGlobalHotKeys() {
+
+    const {toggleZenMode} = useZenModeCallbacks();
+
+    const globalKeyHandlers = {
+        TOGGLE_ZEN_MODE: toggleZenMode,
+    };
+    return (
+        <GlobalKeyboardShortcuts
+            keyMap={globalKeyMap}
+            handlerMap={globalKeyHandlers}/>
+    );
+
+});
+
+
