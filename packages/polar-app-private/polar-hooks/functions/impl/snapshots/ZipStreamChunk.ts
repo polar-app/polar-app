61d1e3a86aef44d78b9b356a7782d55ed1a47a9d
Kevin Burton <burtonator@gmail.com>
2021-02-13 17:12:08 +0000

    Imported polar-app as a subtree.

--- packages/polar-app-private/polar-hooks/functions/impl/snapshots/ZipStreamChunk.ts
+++
@@ -1,7 +0,0 @@
-import {Readable} from "stream";
-import archiver from "archiver";
-
-export type ZipStreamChunk = {
-    source: string | Readable | Buffer
-    data?: archiver.EntryData | archiver.ZipEntryData | undefined
-}
