fbd0854a57355808ae7eaaccfa28ecdd2e2b07dd
Kevin Burton <burtonator@gmail.com>
2021-02-14 03:59:18 +0000

    Imported polar-bookshelf as a subtree.

---
+++ packages/polar-bookshelf/web/spectron.disabled/drawer/content.ts
@@ -0,0 +1,9 @@
+import {SpectronRenderer} from '../../js/test/SpectronRenderer';
+
+SpectronRenderer.run(async (state) => {
+    console.log("Running within SpectronRenderer now.");
+    // you can also update the result in the renderer
+    // state.testResultWriter.write(true);
+});
+
+
