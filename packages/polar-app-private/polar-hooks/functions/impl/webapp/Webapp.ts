4d790035bc6117fa3e349f3e2b15c5b065fe6c3d
Kevin Burton <burtonator@gmail.com>
2021-02-14 02:20:23 +0000

    Imported polar-app as a subtree.

--- packages/polar-app-private/polar-hooks/functions/impl/webapp/Webapp.ts
+++
@@ -1,4 +0,0 @@
-import * as functions from 'firebase-functions';
-import {Webapps} from "./Webapps";
-
-export const Webapp = functions.https.onRequest(Webapps.createWebapp());
--- packages/polar-bookshelf/web/spectron0/ui-components/KeyDownDemo.tsx
+++
@@ -1,7 +0,0 @@
-import * as React from 'react';
-
-export const KeyDownDemo = () => (
-    <div onKeyDown={() => console.log("got key down")}>
-        <input type="text"/>
-    </div>
-);
