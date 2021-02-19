1429d4456cddcc26a5044edd4cf10cc7d343cd9e
Kevin Burton <burtonator@gmail.com>
2021-02-19 10:51:28 -0700

    Fixed bug with test...

--- web/js/notes/MarkdownContentEscaperTest.ts
+++ web/js/notes/MarkdownContentEscaperTest.ts
@@ -8,7 +8,7 @@ describe('MarkdownContentEscaper', function() {
         const input = "This is some **bold** text and this is a wiki link [[Hello World]]"
 
         const escaped = MarkdownContentEscaper.escape(input);
-        assert.equal(escaped, "<div>This is some <strong>bold</strong> text and this is a wiki link <a href=\"#Hello World\">Hello World</a></div>");
+        assert.equal(escaped, "This is some <b>bold</b> text and this is a wiki link <a href=\"#Hello World\">Hello World</a>");
 
         const unescaped = MarkdownContentEscaper.unescape(escaped);
         assert.equal(unescaped, input);
