64c4cc9e331d1d7baafcf4e54f5849f6cda0c54f
Kevin Burton <burtonator@gmail.com>
2021-02-20 20:40:12 +0000

    Imported polar-bookshelf as a subtree.

---
+++ packages/polar-bookshelf/apps/stories/impl/MinimalContentEditableStory.tsx
@@ -0,0 +1,30 @@
+import * as React from 'react';
+import {NULL_FUNCTION} from "polar-shared/src/util/Functions";
+import {CKEditor5BalloonEditor} from './ckeditor5/CKEditor5BalloonEditor';
+import {CKEditor5GlobalCss} from "./ckeditor5/CKEditor5GlobalCss";
+import { Numbers } from 'polar-shared/src/util/Numbers';
+import {MinimalContentEditable} from "../../../web/js/notes/textarea/MinimalContentEditable";
+
+export const MinimalContentEditableStory = () => {
+
+    const handleKeyDown = React.useCallback((event: React.KeyboardEvent) => {
+
+        if (event.key === 'Enter') {
+            event.preventDefault();
+            event.stopPropagation();
+        }
+
+    }, []);
+
+    return (
+        <div>
+            <MinimalContentEditable id="1234"
+                                    parent={undefined}
+                                    content="this is the content"
+                                    onKeyDown={handleKeyDown}
+                                    onChange={NULL_FUNCTION}/>
+
+        </div>
+    );
+
+}
