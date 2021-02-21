c24dd885503b0f8bc370bf9eaa3c5e17c90bd1b1
Kevin Burton <burtonator@gmail.com>
2021-02-21 10:12:39 -0700

    ...

--- web/js/mui/CircularProgressWithLabel.tsx
+++ web/js/mui/CircularProgressWithLabel.tsx
@@ -9,21 +9,26 @@ interface IProps extends CircularProgressProps {
 }
 
 export const CircularProgressWithLabel = deepMemo((props: IProps) => {
+
+    const rounded = Math.round(props.value);
+    const text = `${rounded}%`;
+
     return (
         <Box position="relative" display="inline-flex">
             <CircularProgress variant="static" {...props} />
-            <Box
-                top={0}
-                left={0}
-                bottom={0}
-                right={0}
-                position="absolute"
-                display="flex"
-                alignItems="center"
-                justifyContent="center">
-                <Typography variant="caption" component="div" color="textSecondary">{`${Math.round(
-                    props.value,
-                )}%`}</Typography>
+            <Box top={0}
+                 left={0}
+                 bottom={0}
+                 right={0}
+                 position="absolute"
+                 display="flex"
+                 alignItems="center"
+                 justifyContent="center">
+
+                 <Typography variant="caption" component="div" color="textSecondary">
+                     {text}
+                 </Typography>
+
             </Box>
         </Box>
     );
