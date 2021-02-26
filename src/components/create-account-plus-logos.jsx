2d1bc0e763ceff0091e9766d5a0fcd8f145be55b
Kevin Burton <burtonator@gmail.com>
2021-02-25 22:36:02 -0700

    Fixed create account section...

--- src/components/create-account-plus-logos.jsx
+++ src/components/create-account-plus-logos.jsx
@@ -3,32 +3,39 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var React = require("react");
 var core_1 = require("@material-ui/core");
 var CreateAccountButton_1 = require("./CreateAccountButton");
+var createStyles_1 = require("@material-ui/styles/createStyles");
 var ImgLogoBar = require("../../content/assets/logos/avaliable-logo-bar.png");
-var useStyles = core_1.makeStyles({
-    logoBar: {},
-    logoFrame: {
-        display: "flex",
-        justifyContent: "center",
-        flexDirection: "column",
-        alignItems: "center",
-        alignContent: "center",
-        maxWidth: '1200px',
-        marginLeft: 'auto',
-        marginRight: 'auto',
-        paddingLeft: "3%",
-        paddingRight: "3%",
-        height: '50vh'
-    },
-    background: {},
-    h1: {
-        fontSize: '3rem',
-        textAlign: "center",
-    },
-    h2: {
-        fontSize: '2rem',
-        textAlign: "center",
-        marginBottom: "20px",
-    }
+var useStyles = core_1.makeStyles(function (theme) {
+    return createStyles_1.default({
+        logoBar: {
+            maxWidth: '600px',
+            color: theme.palette.text.hint
+        },
+        logoFrame: {
+            display: "flex",
+            justifyContent: "center",
+            flexDirection: "column",
+            alignItems: "center",
+            alignContent: "center",
+            maxWidth: '1200px',
+            marginLeft: 'auto',
+            marginRight: 'auto',
+            paddingLeft: "3%",
+            paddingRight: "3%",
+            height: '40vh',
+            marginBottom: '85px'
+        },
+        background: {},
+        h1: {
+            fontSize: '3rem',
+            textAlign: "center",
+        },
+        h2: {
+            fontSize: '2rem',
+            textAlign: "center",
+            marginBottom: "20px",
+        }
+    });
 });
 var AccountWLogos = function (_a) {
     var transparent = _a.transparent;
@@ -43,12 +50,12 @@ var AccountWLogos = function (_a) {
         <CreateAccountButton_1.CreateAccountButton />
 
         
-        
-        
-        
-        
-        
-        
+          <h2 className={classes.h2}>
+            Available On
+          </h2>
+          <core_1.Box className={classes.logoBar}>
+            <img src={ImgLogoBar} style={{ marginBottom: "3%" }}/>
+          </core_1.Box>
 
         
       </core_1.Box>
--- src/components/create-account-plus-logos.tsx
+++ src/components/create-account-plus-logos.tsx
@@ -1,42 +1,47 @@
 import * as React from "react"
 import {Box, makeStyles} from "@material-ui/core";
 import {CreateAccountButton} from "./CreateAccountButton";
+import createStyles from "@material-ui/styles/createStyles";
 const ImgLogoBar = require("../../content/assets/logos/avaliable-logo-bar.png");
 
-const useStyles = makeStyles({
-  logoBar: {
-  },
+const useStyles = makeStyles((theme) =>
+  createStyles({
+    logoBar: {
+      maxWidth: '600px',
+      color: theme.palette.text.hint
+    },
 
-  logoFrame: {
-    display: "flex",
-    justifyContent: "center",
-    flexDirection: "column",
-    alignItems: "center",
-    alignContent: "center",
+    logoFrame: {
+      display: "flex",
+      justifyContent: "center",
+      flexDirection: "column",
+      alignItems: "center",
+      alignContent: "center",
 
-    maxWidth: '1200px',
-    marginLeft: 'auto',
-    marginRight: 'auto',
-    paddingLeft: "3%",
-    paddingRight: "3%",
-    height: '50vh'
-  },
+      maxWidth: '1200px',
+      marginLeft: 'auto',
+      marginRight: 'auto',
+      paddingLeft: "3%",
+      paddingRight: "3%",
+      height: '40vh',
+      marginBottom: '85px'
+    },
 
-  background: {
-  },
+    background: {
+    },
 
-  h1: {
-    fontSize: '3rem',
-    textAlign: "center",
-  },
+    h1: {
+      fontSize: '3rem',
+      textAlign: "center",
+    },
 
-  h2: {
-    fontSize: '2rem',
-    textAlign: "center",
-    marginBottom: "20px",
-  }
+    h2: {
+      fontSize: '2rem',
+      textAlign: "center",
+      marginBottom: "20px",
+    }
 
-});
+}));
 
 const AccountWLogos = ({ transparent }) => {
   const classes = useStyles();
@@ -51,12 +56,12 @@ const AccountWLogos = ({ transparent }) => {
         <CreateAccountButton/>
 
         {/*<div style={{marginTop: '2em'}}>*/}
-        {/*  <h2 className={classes.h2}>*/}
-        {/*    Available On*/}
-        {/*  </h2>*/}
-        {/*  <Box className={classes.logoBar}>*/}
-        {/*    <img src={ImgLogoBar} style={{ marginBottom: "3%" }} />*/}
-        {/*  </Box>*/}
+          <h2 className={classes.h2}>
+            Available On
+          </h2>
+          <Box className={classes.logoBar}>
+            <img src={ImgLogoBar} style={{ marginBottom: "3%" }} />
+          </Box>
 
         {/*</div>*/}
       </Box>
