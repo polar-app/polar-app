f605d7c1c64dbed0c05e49b5e610af0285645f05
Kevin Burton <burtonator@gmail.com>
2021-03-15 17:38:23 +0000

    Imported polar-bookshelf as a subtree.

---
+++ packages/polar-bookshelf/web/js/notes/NoteFormatHooks.tsx
@@ -0,0 +1,44 @@
+
+// FIXME: we need mapping that our markdown system uses for all of these elements.
+
+export function useNoteFormatHandlers(onUpdated: () => void) {
+
+    // FIXME we need one that's a clear formatting which removes bold, italics, link, etc.
+
+    function doExecCommand(command: string) {
+        document.execCommand(command);
+        onUpdated();
+    }
+
+    function onBold() {
+        doExecCommand('bold');
+    }
+
+    function onItalic() {
+        doExecCommand('italic');
+    }
+
+    function onQuote() {
+        // doSelectionWrap('blockquote');
+    }
+
+    function onUnderline() {
+        doExecCommand('underline')
+    }
+
+    function onStrikethrough() {
+        // FIXME: this command resets the range selection...
+        doExecCommand('strikeThrough')
+    }
+
+    function onSubscript() {
+        doExecCommand('subscript')
+    }
+
+    function onSuperscript() {
+        doExecCommand('superscript')
+    }
+
+    return {onBold, onItalic, onQuote, onUnderline, onStrikethrough, onSubscript, onSuperscript}
+
+}
