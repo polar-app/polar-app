55d42ea1757422810a953b401e116f032c8e3db8
WinterCore <hogobbl@gmail.com>
2021-03-17 21:50:50 +0200

    Align the top left logo with the toolbar

--- web/js/sidenav/SideNav.tsx
+++ web/js/sidenav/SideNav.tsx
@@ -45,14 +45,16 @@ const useStyles = makeStyles((theme) =>
         logo: {
             display: 'flex',
             cursor: 'pointer',
-            marginTop: '8px',
+            height: '55px',
+            flex: '0 0 55px',
+            alignItems: 'center',
             "& *": {
                 marginLeft: 'auto',
                 marginRight: 'auto',
             },
         },
         dividerTop: {
-            marginTop: '4px',
+            marginTop: 0,
             marginBottom: '5px',
             margin: theme.spacing(1),
             height: '1px'
