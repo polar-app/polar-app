72f6192be7174b5f6ebf4a7023ca134de02e2226
Kevin Burton <burtonator@gmail.com>
2021-03-17 21:29:08 +0000

    Imported polar-app as a subtree.

--- packages/polar-bookshelf/apps/repository/js/login/SignInScreen.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import {Authenticator} from "./Authenticator";
-
-export const SignInScreen = React.memo(() => {
-
-    return (
-        <Authenticator mode='sign-in'/>
-    );
-});
