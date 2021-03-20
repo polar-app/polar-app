9cafc8ae89380bb7df4588ed44b2f2a3aa4d008c
Kevin Burton <burtonator@gmail.com>
2021-03-20 16:46:37 +0000

    Imported polar-app as a subtree.

--- packages/polar-bookshelf/docs/notes/MAC_APP_STORE.md
+++
@@ -1,1 +0,0 @@
-https://github.com/electron/electron/blob/master/docs/tutorial/mac-app-store-submission-guide.md
--- packages/polar-bookshelf/web/spectron0/material-ui/TableDemo.tsx
+++
@@ -1,9 +0,0 @@
-import Container from "@material-ui/core/Container";
-import {MuiThemeProvider} from "@material-ui/core/styles";
-import * as React from "react";
-
-export const TableDemo = () => (
-    <Container style={{display: 'flex', flexGrow: 1}}>
-
-    </Container>
-);
