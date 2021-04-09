9fd5f49c0a96c3b92a4de643190cb2d2043ff75e
Kevin Burton <burtonator@gmail.com>
2021-04-09 02:00:30 +0000

    Imported polar-app as a subtree.

--- packages/polar-app-public/polar-shared/src/util/ImageType.ts
+++
@@ -1,1 +0,0 @@
-export type ImageType = 'image/png' | 'image/jpeg' | 'image/webp' | 'image/gif' | 'image/svg+xml' | 'image/avif' | 'image/apng';
--- packages/polar-bookshelf/apps/stories/impl/ExportDefaultComponent.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export default function ExportDefaultComponent() {
-    return (
-        <div>hello world</div>
-    )
-}
