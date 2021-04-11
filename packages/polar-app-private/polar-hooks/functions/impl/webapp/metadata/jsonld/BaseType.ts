fb210efc614bfdac167a2913f47d0dfbaafc91c2
Kevin Burton <burtonator@gmail.com>
2021-04-11 04:19:25 +0000

    Imported polar-app-private as a subtree.

---
+++ packages/polar-app-private/polar-hooks/functions/impl/webapp/metadata/jsonld/BaseType.ts
@@ -0,0 +1,15 @@
+
+export interface BaseTypeInit {
+    /**
+     * An ID for an object. which is optional of course.
+     */
+    readonly "@id"?: string;
+
+}
+
+export interface BaseType {
+
+    readonly "@type": string;
+
+
+}
