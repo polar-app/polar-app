b7765ace1fb898a3de3b6950f2ecd36d8b1edb51
Kevin Burton <burtonator@gmail.com>
2021-04-12 18:21:43 +0000

    Imported polar-site2 as a subtree.

---
+++ packages/polar-site2/src/pages/images/DocumentViewerImage.tsx
@@ -0,0 +1,34 @@
+import * as React from "react"
+import {graphql, StaticQuery} from "gatsby"
+import Img from "gatsby-image"
+
+interface IProps {
+    readonly style?: React.CSSProperties;
+    readonly className?: string;
+    readonly alt?: string;
+}
+
+export default (props: IProps) => (
+    <StaticQuery
+        query={graphql`
+  query {
+    file(relativePath: {eq: "screenshots/2020-10-13-document-viewer.png"}) {
+      childImageSharp {
+        fluid(maxWidth: 1280) {
+            ...GatsbyImageSharpFluid_withWebp
+        }
+      }
+    }
+  }
+        `}
+        render={data => (
+            <Img fluid={data.file.childImageSharp.fluid}
+                 style={props.style}
+                 className={props.className}
+                 alt={props.alt}
+                 />
+        )}
+    />
+
+);
+
