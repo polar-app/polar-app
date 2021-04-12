9a499370a02eb34f45a602778839311295964084
Kevin Burton <burtonator@gmail.com>
2021-04-12 04:19:17 +0000

    Imported polar-app as a subtree.

--- packages/polar-app-private/polar-hooks/functions/cmdline/user_interviews/stats.ts
+++
@@ -1,9 +0,0 @@
-import {Stats} from "../../impl/user_interviews/Stats";
-
-async function exec() {
-
-    await Stats.exec()
-}
-
-exec()
-    .catch(err => console.error(err));
--- packages/polar-bookshelf/web/test/sandbox/contextmenu-style/README.md
+++
@@ -1,1 +0,0 @@
-https://codepen.io/anon/pen/pKPxpW
