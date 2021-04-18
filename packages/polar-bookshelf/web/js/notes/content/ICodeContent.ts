bb8c1e21b3dfe5bf1bc4f24990e4529484bab9e1
Kevin Burton <burtonator@gmail.com>
2021-04-18 00:09:18 +0000

    Imported polar-app as a subtree.

--- packages/polar-bookshelf/web/js/notes/content/ICodeContent.ts
+++
@@ -1,7 +0,0 @@
-export type CodeStr = string;
-
-export interface ICodeContent {
-    readonly type: 'code';
-    readonly lang: 'typescript' | 'javascript' | 'markdown' | 'java' | string;
-    readonly value: CodeStr;
-}
