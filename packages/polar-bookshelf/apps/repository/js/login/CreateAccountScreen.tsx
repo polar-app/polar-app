86862f0ed52655eef233eb5a8ae62eea7831be59
Kevin Burton <burtonator@gmail.com>
2021-04-19 21:31:21 +0000

    Imported polar-app as a subtree.

--- packages/polar-bookshelf/apps/repository/js/login/CreateAccountScreen.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import {Authenticator} from "./Authenticator";
-
-export const CreateAccountScreen = React.memo(function CreateAccountScreen() {
-
-    return (
-        <Authenticator mode='create-account'/>
-    );
-});
