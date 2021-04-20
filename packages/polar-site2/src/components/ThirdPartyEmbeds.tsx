22a1f58a7779c65b6b5df965c15466a2a3082984
Kevin Burton <burtonator@gmail.com>
2021-04-20 23:01:44 +0000

    Imported polar-site2 as a subtree.

---
+++ packages/polar-site2/src/components/ThirdPartyEmbeds.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+export const ThirdPartyEmbeds = () => {
+    return (
+        <>
+            <script type="text/javascript" src="https://a.omappapi.com/app/js/api.min.js" data-account="103414" data-user="92156" async></script>
+        </>
+    )
+}
