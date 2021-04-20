e6bdac842ca5edf1fb5975691488a4bc2597fdcd
Kevin Burton <burtonator@gmail.com>
2021-04-20 22:58:26 +0000

    Imported polar-app as a subtree.

--- packages/polar-bookshelf/apps/repository/js/doc_repo/buttons/MUIDocArchiveButton.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ArchiveIcon from "@material-ui/icons/Archive";
-import { ToggleButtonProps, StandardToggleButton } from "./StandardToggleButton";
-
-export const MUIDocArchiveButton = React.memo((props: ToggleButtonProps) => (
-    <StandardToggleButton tooltip="Archive" {...props}>
-        <ArchiveIcon/>
-    </StandardToggleButton>
-));
--- packages/polar-bookshelf/scripts/test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ "$OSTYPE" = 'msys' ]; then
-  mocha-parallel-tests --timeout 60000 --exit --max-parallel=1 'web\js\**\*Test.js' 'apps\**\*Test.js'
-else
-  mocha-parallel-tests --timeout 60000 --exit --max-parallel=1 'web/js/**/*Test.js' 'apps/**/*Test.js'
-fi
-
-
