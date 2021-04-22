6aee1a93bbcf601b1231857ceff343b8da9b2d3c
Kevin Burton <burtonator@gmail.com>
2021-04-22 18:25:47 +0000

    Imported polar-app as a subtree.

--- packages/polar-app-public/polar-snapshot-cache/src/store/IQueryDocumentSnapshot.ts
+++
@@ -1,8 +0,0 @@
-import {TDocumentData} from "./TDocumentData";
-import {IDocumentSnapshot} from "./IDocumentSnapshot";
-
-export interface IQueryDocumentSnapshot extends IDocumentSnapshot {
-
-    readonly data: () => TDocumentData;
-
-}
--- packages/polar-bookshelf/web/js/notes/content/IBaseBlockContent.ts
+++
@@ -1,5 +0,0 @@
-import {IBlockContent} from "../store/BlocksStore";
-
-export interface IBaseBlockContent {
-    update: (content: IBlockContent) => void;
-}
