a6cebf076d286ba2bd2c1f765802c740df240893
Kevin Burton <burtonator@gmail.com>
2021-04-23 23:53:44 +0000

    Imported polar-app as a subtree.

--- packages/polar-app-private/polar-hooks/functions/impl/util/ExpressRequests.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-
-export class ExpressRequests {
-
-    public static toFullURL(req: express.Request) {
-        return req.protocol + '://' + req.get('host') + req.originalUrl;
-    }
-
-}
