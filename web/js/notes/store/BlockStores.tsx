9b87b74e4a3ca8c9a9de08f7d014d06413f5a4f3
Kevin Burton <burtonator@gmail.com>
2021-04-25 12:02:54 -0500

    - filterPresent in ArrayStreams.ts - new assertion for null - computeMutationBlocks

--- web/js/notes/store/BlockStores.tsx
+++ web/js/notes/store/BlockStores.tsx
@@ -67,6 +67,9 @@ export namespace BlockStores {
 
     export function applyUndoMutations(blocksStore: BlocksStore, mutations: ReadonlyArray<IBlocksStoreMutation>) {
 
+        // TODO: we might not need to mutate something if it hasn't actually
+        // changed and we can look at 'updated' for this
+
         const handleUpdated = (mutation: IBlocksStoreMutation) => {
 
             // updated means we need to restore it to the older version.
@@ -171,6 +174,66 @@ export namespace BlockStores {
             ...added.map(current => toMutation(current, 'added')),
             ...removed.map(current => toMutation(current, 'removed'))
         ];
+
+    }
+
+    /**
+     * For a given ID, compute all the blocks that could be involved in a
+     * mutation including the immediate parents and all the children, the
+     * identifiers, themselves, and all the descendants.
+     */
+    export function computeMutationBlocks(blocksStore: BlocksStore, identifiers: ReadonlyArray<BlockIDStr>) {
+
+        const computeChildren = (identifiers: ReadonlyArray<BlockIDStr>) => {
+
+            const computeChildrenForBlock = (id: BlockIDStr): ReadonlyArray<BlockIDStr> => {
+
+                const items = blocksStore.getBlock(id)?.items || [];
+
+                const descendants = arrayStream(items)
+                    .map(current => computeChildrenForBlock(current))
+                    .flatMap(current => current)
+                    .collect();
+
+                return [
+                    ...items,
+                    ...descendants
+                ];
+
+            }
+
+            return arrayStream(identifiers)
+                    .map(current => computeChildrenForBlock(current))
+                    .flatMap(current => current)
+                    .unique()
+                    .collect();
+        }
+
+        const children = computeChildren(identifiers);
+
+        const computeParents = (identifiers: ReadonlyArray<BlockIDStr>): ReadonlyArray<BlockIDStr> => {
+
+            const getParent = (id: BlockIDStr): BlockIDStr | undefined => {
+                return blocksStore.getBlock(id)?.parent;
+            }
+
+            return arrayStream(identifiers)
+                .map(current => getParent(current))
+                .filterPresent()
+                .collect();
+
+        }
+
+        const primaryBlockIdentifiers
+            = arrayStream([...identifiers, ...children])
+                .unique()
+                .collect();
+
+        const parents = computeParents(primaryBlockIdentifiers);
+
+        return arrayStream([...identifiers, ...children, ...parents])
+                .unique()
+                .collect();
 
     }
 
