c993f809f9db889dd38e375d1c914f73a1b70d38
Kevin Burton <burtonator@gmail.com>
2021-04-29 02:45:40 +0000

    Imported polar-app as a subtree.

--- packages/polar-bookshelf/web/js/annotation_sidebar/annotations/AnnotationDivider.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-import {deepMemo} from "../../react/ReactUtils";
-import Divider from "@material-ui/core/Divider";
-
-export const AnnotationDivider = deepMemo(() => (
-    <div style={{padding: '10px'}}>
-        <Divider/>
-    </div>
-));
