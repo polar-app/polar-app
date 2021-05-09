1e3bf915f720f3cc4aaa1f8eca330469c5247b3c
Kevin Burton <burtonator@gmail.com>
2021-05-09 18:42:56 +0000

    Imported polar-app as a subtree.

--- packages/polar-bookshelf/web/js/datastore/firebase/CloudFunctions.ts
+++
@@ -1,8 +0,0 @@
-export class CloudFunctions {
-
-    public static createEndpoint() {
-        const project = process.env.POLAR_TEST_PROJECT || "polar-32b0f";
-        return `https://us-central1-${project}.cloudfunctions.net`;
-    }
-
-}
