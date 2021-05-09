1682a4ffd344922f60bbd047abc51659cc1c8763
Kevin Burton <burtonator@gmail.com>
2021-05-09 22:28:08 +0000

    Imported polar-app as a subtree.

--- packages/polar-app-public/polar-web-extension/src/PopupScriptMessages.ts
+++
@@ -1,9 +0,0 @@
-import {ActiveTabs} from "./chrome/ActiveTabs";
-
-export namespace PopupScriptMessages {
-
-    export async function sendStartCapture() {
-        await ActiveTabs.sendMessage({type: "start-capture"})
-    }
-
-}
