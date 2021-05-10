f84292d4ece2307e51b4a23a7b3a1e26befd8588
Kevin Burton <burtonator@gmail.com>
2021-05-10 19:15:07 +0000

    Imported polar-app as a subtree.

--- packages/polar-app-public/polar-test/src/test/Diffs.ts
+++
@@ -1,9 +0,0 @@
-import {createPatch} from 'diff';
-
-export class Diffs {
-
-    public static compute(before: string, after: string): string {
-        return createPatch('patch.txt', before, after, undefined, undefined, {ignoreWhitespace: true});
-    }
-
-}
