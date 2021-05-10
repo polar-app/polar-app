9fb286cb0e8a96a673c356e6c1098466d966a7d8
WinterCore <hogobbl@gmail.com>
2021-05-10 22:27:47 +0200

    Change the area highlight toggle icon

--- apps/repository/js/doc_repo/buttons/MUIDocAreaHighlightModeToggle.tsx
+++ apps/repository/js/doc_repo/buttons/MUIDocAreaHighlightModeToggle.tsx
@@ -1,9 +1,9 @@
 import React from "react";
-import Crop54Icon from '@material-ui/icons/Crop54';
+import PhotoSizeSelectLargeIcon from '@material-ui/icons/PhotoSizeSelectLarge';
 import { ToggleButtonProps, StandardToggleButton } from "./StandardToggleButton";
 
 export const MUIDocAreaHighlightModeToggle = React.memo((props: ToggleButtonProps) => (
     <StandardToggleButton tooltip="Area highlight mode" {...props}>
-        <Crop54Icon/>
+        <PhotoSizeSelectLargeIcon/>
     </StandardToggleButton>
 ));
