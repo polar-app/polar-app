db61c65b145a3562c18c3a6055fa28928d190596
Kevin Burton <burtonator@gmail.com>
2021-05-17 11:46:52 -0600

    Test that we can login via karma...

--- web/js/firebase/FirebaseTesting.ts
+++ web/js/firebase/FirebaseTesting.ts
@@ -1,3 +1,13 @@
+
+export const FIREBASE_USER="getpolarized.test+test@gmail.com";
+export const FIREBASE_PASS="mk9z79vlquixvqd";
+
+export const FIREBASE_USER1="getpolarized.test+test1@gmail.com";
+export const FIREBASE_PASS1="mk9z79vlquixvqd";
+
+export const FIREBASE_USER2="getpolarized.test+test2@gmail.com";
+export const FIREBASE_PASS2="mk9z79vlquixvqd";
+
 export class FirebaseTesting {
 
     public static validateUsers() {
--- web/js/notes/persistence/BlocksPersistenceKarma.ts
+++ web/js/notes/persistence/BlocksPersistenceKarma.ts
@@ -1,15 +1,23 @@
 import {Firestore} from "../../firebase/Firestore";
 import {assert} from "chai";
+import {Firebase} from "../../firebase/Firebase";
+import {FIREBASE_PASS, FIREBASE_USER} from "../../firebase/FirebaseTesting";
 
 describe("BlocksPersistence", () => {
 
-    it("should say 'Hello world!'", async () => {
-        console.log("hello world");
+    it("basic", async () => {
+
+        const app = Firebase.init();
+
+        const auth = app.auth();
+
+        await auth.signInWithEmailAndPassword(FIREBASE_USER, FIREBASE_PASS);
 
         const firestore = await Firestore.getInstance();
 
         console.log(firestore);
         assert.equal(true, true);
+
     });
 });
 
