01658e13634a09abe343cc0bbeede12b1470cade
Mohamed Adel <ov3rcontrol@hotmail.com>
2021-05-28 18:44:58 +0200

    add graph vis with types

--- packages/polar-bookshelf/apps/stories/impl/index.d.ts
+++ packages/polar-bookshelf/apps/stories/impl/index.d.ts
@@ -1,9 +1,18 @@
 import '@antv/g6';
+import { GraphOptions, IAbstractGraph, IG6GraphEvent, IGraph } from '@antv/g6';
 
 declare module '@antv/g6' {
-    interface GraphOptions {
-        // container: string | HTMLElement | null;
-
+    type IGraphOptions =  Omit<GraphOptions,"container"> & {
+        container: string | HTMLElement | null;
+    }
+    type prefix = "node" | "edge"
+    type eventType = "mouseenter" | "mouseleave"
+    type GraphOnEvent = `${prefix}:${eventType}`
+     class Graph extends AbstractGraph implements IGraph {
+            constructor(cfg: IGraphOptions)
+    }
+    abstract class AbstractGraph extends EventEmitter implements IAbstractGraph {
+        on<T = IG6GraphEvent>(eventName: GraphOnEvent, callback: (e: T) => void, once?: boolean): this;
     }
 
 }
