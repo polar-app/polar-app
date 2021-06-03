750a953b2d1ac55c15047fb8acac0ff74edbba23
Kevin Burton <burtonator@gmail.com>
2021-06-03 07:36:51 -0600

    Rework to support block expands...

---
+++ packages/polar-bookshelf/web/js/notes/persistence/BlockExpandSnapshots.ts
@@ -0,0 +1,55 @@
+import {useFirestore} from "../../../../apps/repository/js/FirestoreProvider";
+import React from "react";
+import {IBlock, UIDStr} from "../store/IBlock";
+import {IQuerySnapshot} from "polar-snapshot-cache/src/store/IQuerySnapshot";
+import {MockBlocks} from "../../../../apps/stories/impl/MockBlocks";
+import {IDocumentChange} from "polar-snapshot-cache/src/store/IDocumentChange";
+import {createMockSnapshot, IGenericSnapshot} from "./IGenericSnapshot";
+import {IGenericDocumentChange} from "./IGenericDocumentChange";
+import {useGenericFirestoreSnapshots} from "./GenericFirestoreSnapshots";
+import {BlockIDStr} from "../store/BlocksStore";
+import {IDStr} from "polar-shared/src/util/Strings";
+
+const IS_NODE = typeof window === 'undefined';
+
+export type IBlocksPersistenceSnapshot = IGenericSnapshot<IBlock>;
+
+/**
+ * This is just a hook that will be re-called from within the UI...
+ */
+export type BlocksPersistenceSnapshotsHook = () => IBlocksPersistenceSnapshot;
+
+export interface IBlockExpand {
+
+    /**
+     * "The ID of this record which is just the UID.
+     */
+    readonly id: UIDStr;
+
+    readonly uid: UIDStr;
+
+    /**
+     * The blocks that are currently expanded.
+     */
+    readonly expanded: ReadonlyArray<BlockIDStr>;
+}
+
+export function useFirestoreBlocksExpandSnapshots(listener: (snapshot: IGenericSnapshot<IBlockExpand>) => void) {
+
+    const {user} = useFirestore();
+
+    return useGenericFirestoreSnapshots('block_expand', ['uid', '==', user?.uid], listener);
+
+
+}
+
+export function useBlockExpandSnapshots(listener: (snapshot: IGenericSnapshot<IBlockExpand>) => void) {
+
+    if (IS_NODE) {
+        listener(createMockSnapshot([]));
+    }
+
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    useFirestoreBlocksExpandSnapshots(listener);
+
+}
--- packages/polar-bookshelf/web/js/notes/persistence/BlocksPersistenceSnapshots.ts
+++ packages/polar-bookshelf/web/js/notes/persistence/BlocksPersistenceSnapshots.ts
@@ -1,11 +1,7 @@
 import {useFirestore} from "../../../../apps/repository/js/FirestoreProvider";
-import React from "react";
 import {IBlock} from "../store/IBlock";
-import {IQuerySnapshot} from "polar-snapshot-cache/src/store/IQuerySnapshot";
 import {MockBlocks} from "../../../../apps/stories/impl/MockBlocks";
-import {IDocumentChange} from "polar-snapshot-cache/src/store/IDocumentChange";
-import {IGenericSnapshot} from "./IGenericSnapshot";
-import {IGenericDocumentChange} from "./IGenericDocumentChange";
+import {createMockSnapshot, IGenericSnapshot} from "./IGenericSnapshot";
 import {useGenericFirestoreSnapshots} from "./GenericFirestoreSnapshots";
 
 const IS_NODE = typeof window === 'undefined';
@@ -17,57 +13,18 @@ export type IBlocksPersistenceSnapshot = IGenericSnapshot<IBlock>;
  */
 export type BlocksPersistenceSnapshotsHook = () => IBlocksPersistenceSnapshot;
 
-/**
- * Use blocks to create mock snapshots where everything is 'added'
- */
-export function createMockBlocksPersistenceSnapshot(blocks: ReadonlyArray<IBlock>): IBlocksPersistenceSnapshot {
-
-    const convertBlockToDocChange = (block: IBlock): IGenericDocumentChange<IBlock> => {
-
-        return {
-            id: block.id,
-            type: 'added',
-            data: block
-        }
-    }
-
-    return {
-        empty: blocks.length === 0,
-        metadata: {
-            hasPendingWrites: false,
-            fromCache: true
-        },
-        docChanges: blocks.map(current => convertBlockToDocChange(current))
-    };
-
-}
-
-export function createEmptyBlocksPersistenceSnapshot(): IBlocksPersistenceSnapshot {
-
-    return {
-        empty: true,
-        metadata: {
-            hasPendingWrites: false,
-            fromCache: true
-        },
-        docChanges: []
-    }
-
-}
-
 export function useFirestoreBlocksPersistenceSnapshots(listener: (snapshot: IBlocksPersistenceSnapshot) => void) {
 
     const {user} = useFirestore();
 
     return useGenericFirestoreSnapshots('block', ['nspace', 'in', [user?.uid]], listener);
 
-
 }
 
 export function useBlocksPersistenceSnapshots(listener: (snapshot: IBlocksPersistenceSnapshot) => void) {
 
     if (IS_NODE) {
-        listener(createMockBlocksPersistenceSnapshot(MockBlocks.create()));
+        listener(createMockSnapshot(MockBlocks.create()));
     }
 
     // eslint-disable-next-line react-hooks/rules-of-hooks
