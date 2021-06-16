e8c1004e2495e9c5e2d8f18dda57544cab5ffcd3
WinterCore <hogobbl@gmail.com>
2021-06-17 01:12:15 +0200

    Fix whitespace in imports

--- packages/polar-bookshelf/web/js/notes/NoteRoot.tsx
+++ packages/polar-bookshelf/web/js/notes/NoteRoot.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import {MUIBrowserLinkStyle} from "../mui/MUIBrowserLinkStyle";
 import {NotesInbound} from "./NotesInbound";
-import { Block } from "./Block";
-import { NoteStyle } from "./NoteStyle";
-import { BlockIDStr } from "./store/BlocksStore";
-import { useBlocksStore } from "./store/BlocksStore";
-import { observer } from "mobx-react-lite"
+import {Block} from "./Block";
+import {NoteStyle} from "./NoteStyle";
+import {BlockIDStr} from "./store/BlocksStore";
+import {useBlocksStore} from "./store/BlocksStore";
+import {observer} from "mobx-react-lite"
 import {NoteSelectionHandler} from "./NoteSelectionHandler";
 import {ActionMenuPopup} from "../mui/action_menu/ActionMenuPopup";
-import { ActionMenuStoreProvider } from "../mui/action_menu/ActionStore";
+import {ActionMenuStoreProvider} from "../mui/action_menu/ActionStore";
 import LinearProgress from "@material-ui/core/LinearProgress";
 import {NotesToolbar} from "./NotesToolbar";
 import {Block as BlockClass} from "./store/Block";
