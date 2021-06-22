caacd4257c5df07231d0ffe38cd6c7aec3001135
Dzhuneyt <dzhuneyt@dzhuneyt.com>
2021-06-23 00:06:52 +0300

    refactor: Make steps towards a customized iOS build with a real /ios folder, so that we can inject the static frontend somewhere within that folder and the Expo build can pick it up and serve it

--- packages/polar-mobile-app2/App.tsx
+++ packages/polar-mobile-app2/App.tsx
@@ -1,32 +1,29 @@
 import {StatusBar} from 'expo-status-bar';
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {Alert, Button, SafeAreaView, StyleSheet, Text, View} from 'react-native';
 import WebView from "react-native-webview";
 // const UserAgent = require('react-native-user-agent')
 
 export default function App() {
-    return (
-        <WebView source={{uri: "https://app.getpolarized.io/"}}
-                 userAgent={getUserAgent()}
-                 style={styles.container}/>
-    );
+  return (
+      <SafeAreaView>
+        <Button
+            title={'Buy me'}
+            onPress={() => Alert.alert('Simple Button pressed')}
+        />
+      </SafeAreaView>
+  )
+      ;
 }
-
-const styles = StyleSheet.create({
-    container: {
-        width: "100%", height: "100%", marginTop: 20,
-    },
-});
-
 
 //google blocks oath via WebView, this hides the fact that
 // the app runs in web view.
 function getUserAgent() {
-    //
-    // let userAgent = UserAgent.getUserAgent();
-    // userAgent = userAgent.replace("wv", "");
-    //
-    // return userAgent;
+  //
+  // let userAgent = UserAgent.getUserAgent();
+  // userAgent = userAgent.replace("wv", "");
+  //
+  // return userAgent;
 
-    return "Mozilla/5.0 (Linux; Android 9; Android SDK built for x86_64 Build/PSR1.180720.075) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/69.0.3497.100 Mobile Safari/537.36";
+  return "Mozilla/5.0 (Linux; Android 9; Android SDK built for x86_64 Build/PSR1.180720.075) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/69.0.3497.100 Mobile Safari/537.36";
 }
