b0a205d0685503180b005ac8375f8ddde0f49683
Dzhuneyt <dzhuneyt@dzhuneyt.com>
2021-06-28 18:43:26 +0300

    chore: Add React native version of the app

---
+++ packages/polar-mobile-app3/App.tsx
@@ -0,0 +1,44 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import React from 'react';
+import {SafeAreaView, StyleSheet, View} from 'react-native';
+import {InAppLiteServer} from './InAppLiteServer';
+import {IapTest} from './IapTest';
+
+const App = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <View
+        style={{
+          flex: 1,
+          flexDirection: 'column',
+        }}>
+        <View
+          style={{flex: 3, alignSelf: 'stretch', flexGrow: 1, flexShrink: 0}}>
+          <InAppLiteServer />
+        </View>
+        <View style={{flex: 1, flexBasis: 'auto', flexGrow: 1, flexShrink: 0}}>
+          <IapTest />
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+};
+export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  text: {
+    color: '#922f2f',
+  },
+});
---
+++ packages/polar-mobile-app3/InAppLiteServer.tsx
@@ -0,0 +1,65 @@
+import React, {Component} from 'react';
+import RNFS from 'react-native-fs';
+import {StyleSheet, Text} from 'react-native';
+import WebView from 'react-native-webview';
+// @ts-ignore
+import StaticServer from 'react-native-static-server';
+
+export class InAppLiteServer extends Component<
+  any,
+  {
+    isRunning: boolean;
+    url: string;
+  }
+> {
+  private readonly server: StaticServer;
+
+  constructor(props: any) {
+    super(props);
+    let path = RNFS.MainBundlePath + '/static';
+    this.server = new StaticServer(8080, path);
+    this.state = {
+      isRunning: false,
+      url: '',
+    };
+  }
+
+  componentDidMount() {
+    // Start the server
+    this.server.start().then((url: string) => {
+      console.log('Serving at URL', url);
+      this.setState({
+        isRunning: true,
+        url,
+      });
+    });
+  }
+
+  componentWillUnmount() {
+    if (this.server && this.server.isRunning()) {
+      this.server.stop();
+    }
+  }
+
+  render() {
+    if (!this.state.isRunning) {
+      return <Text style={styles.text}>Server not running yet</Text>;
+    }
+    return (
+      <WebView
+        originWhitelist={['*']}
+        source={{
+          // uri: 'https://app.getpolarized.io',
+          uri: this.state.url,
+        }}
+        style={{marginTop: 20}}
+      />
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  text: {
+    color: '#922f2f',
+  },
+});
