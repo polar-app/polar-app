dab13d28432d83ae0383d6bd25af5bb9e0fa4971
Dzhuneyt <dzhuneyt@dzhuneyt.com>
2021-06-29 22:41:14 +0300

    feat: Add listeners for new purchase within the React Native app

--- packages/polar-mobile-app3/IapTest.tsx
+++ packages/polar-mobile-app3/IapTest.tsx
@@ -1,12 +1,36 @@
 import * as RNIap from 'react-native-iap';
-import {Platform, Text, FlatList, StyleSheet, View} from 'react-native';
-import {Product} from 'react-native-iap';
+import {Platform, Text, FlatList, StyleSheet, View, EmitterSubscription} from 'react-native';
+import {
+    InAppPurchase,
+    Product,
+    ProductPurchase, PurchaseError,
+    purchaseErrorListener,
+    purchaseUpdatedListener,
+    SubscriptionPurchase
+} from 'react-native-iap';
 import React, {Component} from 'react';
+
+class PolarBackendService {
+    static validateReceiptOnServer(transactionReceipt: string) {
+        console.log(transactionReceipt);
+        return new Promise(resolve => {
+            resolve(true);
+        });
+    }
+}
+
+function isSuccess(serverResponse: unknown) {
+    return true;
+}
 
 export class IapTest extends Component<any,
     {
         products: Product[];
     }> {
+
+    purchaseUpdateSubscription: EmitterSubscription | null = null
+    purchaseErrorSubscription: EmitterSubscription | null = null
+
     componentDidMount() {
         const productIds = Platform.select({
             ios: ['plan_plus'],
@@ -22,6 +46,38 @@ export class IapTest extends Component<any,
                 console.warn(reason.code);
                 console.warn(reason.message);
             });
+
+        this.purchaseUpdateSubscription = purchaseUpdatedListener((purchase: InAppPurchase | SubscriptionPurchase | ProductPurchase) => {
+            console.log('purchaseUpdatedListener', purchase);
+            const receipt = purchase.transactionReceipt;
+            console.log('receipt', receipt);
+            if (receipt) {
+                PolarBackendService.validateReceiptOnServer(purchase.transactionReceipt)
+                    .then(async (deliveryResult) => {
+                        if (isSuccess(deliveryResult)) {
+                            // Tell the store that you have delivered what has been paid for.
+                            // Failure to do this will result in the purchase being refunded on Android and
+                            // the purchase event will reappear on every relaunch of the app until you succeed
+                            // in doing the below. It will also be impossible for the user to purchase consumables
+                            // again until you do this.
+
+                            try {
+                                await RNIap.finishTransaction(purchase, true);
+                                // If not consumable
+                                await RNIap.finishTransaction(purchase, false);
+                            } catch (e) {
+                                console.error(e);
+                            }
+                        } else {
+                            // Retry / conclude the purchase is fraudulent, etc...
+                        }
+                    });
+            }
+        });
+
+        this.purchaseErrorSubscription = purchaseErrorListener((error: PurchaseError) => {
+            console.warn('purchaseErrorListener', error);
+        });
     }
 
     render() {
