3c5a0b79fb75c03c400116b4d500e7d948c37b1e
Kevin Burton <burtonator@gmail.com>
2021-06-30 10:50:32 -0600

    more cleanup...

--- packages/polar-app-private/polar-hooks/functions/impl/groups/db/ProfileOwners.ts
+++ packages/polar-app-private/polar-hooks/functions/impl/groups/db/ProfileOwners.ts
@@ -1,10 +1,10 @@
 import {Firestore} from '../../util/Firestore';
-import {Collections} from './Collections';
 import {Preconditions} from "polar-shared/src/Preconditions";
 import {Dictionaries} from "polar-shared/src/util/Dictionaries";
 import {IWriteBatch} from "polar-firestore-like/src/IWriteBatch";
 import {EmailStr, HandleStr, ProfileIDStr, UserIDStr } from 'polar-firebase/src/firebase/om/Profiles';
 import {IDocumentReference} from "polar-firestore-like/src/IDocumentReference";
+import {Collections} from "polar-firestore-like/src/Collections";
 
 /**
  * Allow the backend to lookup a profile ID via UID.
@@ -28,11 +28,13 @@ export class ProfileOwners {
     }
 
     public static async getByProfileID(profileID: ProfileIDStr): Promise<ProfileOwner | undefined> {
-        return await Collections.getByFieldValue(this.COLLECTION, 'profileID', profileID);
+        const firestore = Firestore.getInstance();
+        return await Collections.getByFieldValue(firestore, this.COLLECTION, 'profileID', profileID);
     }
 
     public static async getByEmail(email: EmailStr): Promise<ProfileOwner | undefined> {
-        return await Collections.getByFieldValue(this.COLLECTION, 'email', email);
+        const firestore = Firestore.getInstance();
+        return await Collections.getByFieldValue(firestore, this.COLLECTION, 'email', email);
     }
 
     public static set(batch: IWriteBatch<unknown>, uid: UserIDStr, profileOwner: ProfileOwner) {
@@ -42,7 +44,8 @@ export class ProfileOwners {
     }
 
     public static async delete(batch: IWriteBatch<unknown>, id: UserIDStr) {
-        await Collections.deleteByID(batch, this.COLLECTION, async () => [{id}] );
+        const firestore = Firestore.getInstance();
+        await Collections.deleteByID(firestore, this.COLLECTION, batch, async () => [{id}] );
     }
 
 }
