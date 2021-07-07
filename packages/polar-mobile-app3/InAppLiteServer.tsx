8775f4de52cffdd58340cc74415e4d942d5c8caa
Dzhuneyt <dzhuneyt@dzhuneyt.com>
2021-07-08 00:26:17 +0300

    feat: Save progress on Apps. Introduce command for bundling Polar Bookshelf directly within the React Native app: "OUTPUT_PATH=$(pwd)/../../packages/polar-mobile-app3/static/polar WEBPACK_BUNDLE=repository npx webpack build" (from within /packages/polar-bookshelf)

--- packages/polar-mobile-app3/InAppLiteServer.tsx
+++ packages/polar-mobile-app3/InAppLiteServer.tsx
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import RNFS from 'react-native-fs';
-import {StyleSheet, Text} from 'react-native';
+import {Button, StyleSheet, Text, View} from 'react-native';
 import WebView from 'react-native-webview';
 // @ts-ignore
 import StaticServer from 'react-native-static-server';
@@ -16,11 +16,11 @@ export class InAppLiteServer extends Component<Props, {
 
 }> {
     private readonly server: StaticServer;
-    webview: any = null;
+    webview: WebView<{}> | undefined = undefined;
 
     constructor(props: any) {
         super(props);
-        let path = RNFS.MainBundlePath + '/static';
+        let path = RNFS.MainBundlePath + '/static/polar';
         this.server = new StaticServer(9000, path);
         this.state = {
             isRunning: false,
@@ -52,38 +52,55 @@ export class InAppLiteServer extends Component<Props, {
         const runFirst = `
             // Allow the React app to read this and do stuff based on the fact, e.g. show or hide UI elements
             window.isNativeApp = true;
+            window.onerror = function(message, sourcefile, lineno, colno, error) {
+              alert("Message: " + message + " - Source: " + sourcefile + " Line: " + lineno + ":" + colno);
+              return true;
+            };
             true; // note: this is required, or you'll sometimes get silent failures
         `;
         return (
-            <WebView
-                ref={(ref) => (this.webview = ref)}
-                originWhitelist={['*']}
-                cacheEnabled={false}
-                javaScriptEnabled={true}
-                scrollEnabled={true}
-                thirdPartyCookiesEnabled={true}
-                source={{
-                    // uri: 'https://app.getpolarized.io',
-                    uri: this.state.url,
-                }}
-                style={{marginTop: 20}}
-                injectedJavaScriptBeforeContentLoaded={runFirst}
-                onMessage={(event) => {
-                    const payload: {
-                        action: string,
-                        data: {
-                            plan: string,
-                        },
-                    } = JSON.parse(event.nativeEvent.data);
+            <View style={{height: "100%"}}>
+                <Button title={"Reload the WebView"} onPress={() => {
+                    alert('Reloading');
+                    this.webview?.reload();
+                }
+                }/>
+                <WebView
+                    nativeConfig={{props: {webContentsDebuggingEnabled: true}}}
+                    ref={(ref) => {
+                        // @ts-ignore
+                        this.webview = ref;
+                    }
+                    }
+                    originWhitelist={['*']}
+                    cacheEnabled={false}
+                    javaScriptEnabled={true}
+                    scrollEnabled={true}
+                    thirdPartyCookiesEnabled={true}
+                    source={{
+                        // uri: 'https://app.getpolarized.io',
+                        uri: this.state.url,
+                    }}
+                    style={{marginTop: 20}}
+                    injectedJavaScriptBeforeContentLoaded={runFirst}
+                    onMessage={(event) => {
+                        const payload: {
+                            action: string,
+                            data: {
+                                plan: string,
+                            },
+                        } = JSON.parse(event.nativeEvent.data);
 
-                    this.props.onBuy(payload.data.plan);
+                        this.props.onBuy(payload.data.plan);
 
-                    // @ts-ignore
-                    alert('Initiating purchase of plan: ' + payload.data.plan);
-                    console.log(payload);
+                        // @ts-ignore
+                        alert('Initiating purchase of plan: ' + payload.data.plan);
+                        console.log(payload);
 
-                }}
-            />
+                    }}
+                />
+            </View>
+
         );
     }
 }
