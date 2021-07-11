6185c9ec410581ba4673f7d8941ad2c0bd76740f
Kevin Burton <burtonator@gmail.com>
2021-07-11 13:59:20 -0600

    hydrate function...

--- packages/polar-bookshelf/web/js/ssr/SSRClient.tsx
+++ packages/polar-bookshelf/web/js/ssr/SSRClient.tsx
@@ -1,3 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import {HelloServerSideRender} from "./HelloServerSideRender";
+
 export namespace SSRClient {
+
+    export function resume() {
+        // TODO: we need some sort of path matching system.
+        ReactDOM.hydrate(<HelloServerSideRender/>, document.getElementById('root'));
+    }
 
 }
