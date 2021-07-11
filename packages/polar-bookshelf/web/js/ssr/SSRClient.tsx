74e06b76555707104fd4f8ae0654b5cd1a2ccd1e
Kevin Burton <burtonator@gmail.com>
2021-07-11 14:00:26 -0600

    hydrate function...

--- packages/polar-bookshelf/web/js/ssr/SSRClient.tsx
+++ packages/polar-bookshelf/web/js/ssr/SSRClient.tsx
@@ -6,6 +6,10 @@ import {HelloServerSideRender} from "./HelloServerSideRender";
 export namespace SSRClient {
 
     export function resume() {
+
+        // FIXME: only attempt ot hydrate if there is no __session cookie
+        // and our paths match up.
+
         // TODO: we need some sort of path matching system.
         ReactDOM.hydrate(<HelloServerSideRender/>, document.getElementById('root'));
     }
