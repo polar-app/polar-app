ed264d033208f58a688ac0728883da7caf759d2a
Kevin Burton <burtonator@gmail.com>
2021-07-12 09:22:04 -0500

    SSRClient has hydrate now...

--- packages/polar-bookshelf/web/js/ssr/SSRClient.tsx
+++ packages/polar-bookshelf/web/js/ssr/SSRClient.tsx
@@ -2,16 +2,34 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import {HelloServerSideRender} from "./HelloServerSideRender";
+import Cookies from "js-cookie";
 
 export namespace SSRClient {
 
     export function resume() {
 
-        // FIXME: only attempt ot hydrate if there is no __session cookie
-        // and our paths match up.
+        // TODO
+        //  - our paths match up to /notes and that we have a SSR component that represents this path
 
-        // TODO: we need some sort of path matching system.
-        ReactDOM.hydrate(<HelloServerSideRender/>, document.getElementById('root'));
+        function hasPublicAnonUser(): boolean {
+            return Cookies.get('__session') === undefined;
+        }
+
+        function hasSSRMeta(): boolean {
+
+            // look for <meta name="ssr" content="true" />
+
+            return document.querySelector('meta[meta=ssr][content=true]') !== null;
+
+        }
+
+        if (hasPublicAnonUser() && hasSSRMeta()) {
+
+            // TODO: we need some sort of path matching system.
+            ReactDOM.hydrate(<HelloServerSideRender/>, document.getElementById('root'));
+
+        }
+
     }
 
 }
