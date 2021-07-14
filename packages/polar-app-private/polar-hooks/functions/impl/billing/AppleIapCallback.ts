1e1511878142c4d3256ca44286aeb33965de33d0
Dzhuneyt <dzhuneyt@dzhuneyt.com>
2021-07-14 21:41:31 +0300

    feat: Try to find the latest active receipt inside AppleIapCallback.ts cloud function

--- packages/polar-app-private/polar-hooks/functions/impl/billing/AppleIapCallback.ts
+++ packages/polar-app-private/polar-hooks/functions/impl/billing/AppleIapCallback.ts
@@ -3,7 +3,7 @@ import {Lazy} from "../util/Lazy";
 import {FirebaseAdmin} from "polar-firebase-admin/src/FirebaseAdmin";
 import {default as fetch} from "node-fetch";
 import {Accounts} from "../stripe/Accounts";
-import {Billing, V2PlanLevel} from "polar-accounts/src/Billing";
+import {Billing} from "polar-accounts/src/Billing";
 
 const firebaseProvider = Lazy.create(() => FirebaseAdmin.app());
 
@@ -29,6 +29,7 @@ interface AppleVerifyReceiptResponse {
     latest_receipt_info: {
         product_id: "plan_plus" | "plan_pro",
         transaction_id: string,
+        original_transaction_id: string,
         purchase_date_ms: string,
         expire_date_ms: string,
         in_app_ownership_type: "PURCHASED",
@@ -43,6 +44,10 @@ export async function verifyReceipt(receiptData: string): Promise<AppleVerifyRec
         body: JSON.stringify({
             password: '7d8689e01af245d0bd2030ba908ecc06', // Taken from Apple Appstoreconnect -> In App Purchases page
             "receipt-data": receiptData,
+            // We are interested in only the latest renewal attempt
+            // In other words, this forces the response field "latest_receipt_info" to contain
+            // only a single element instead of the full history of renewal attempts by Apple
+            "exclude-old-transactions": true,
         }),
         headers: {'Content-Type': 'application/json'},
     });
@@ -56,9 +61,11 @@ export const AppleIapCallback = ExpressFunctions.createHookAsync('AppleIapCallba
     // console.log("Handling request: ", typeof request, JSON.stringify(request, null, 2));
 
     if (!request.email) {
-        ExpressFunctions.sendError(res, Error('Failed to validate the receipt with Apple'));
+        ExpressFunctions.sendError(res, Error('Invalid "email" provided'));
         return;
     }
+
+    const email = request.email;
 
     const result = await verifyReceipt(request.receipt);
 
@@ -69,13 +76,14 @@ export const AppleIapCallback = ExpressFunctions.createHookAsync('AppleIapCallba
 
     // console.log("Response from Apple verifyReceipt(): ", JSON.stringify(result, null, 2));
 
-    // Sort the receipts based on "expire_date_ms" descending so newest is at the top
-    result.latest_receipt_info.sort((a, b) => parseInt(b.expire_date_ms) - parseInt(a.expire_date_ms));
-
     console.log('latest_receipt_info->sorted', result.latest_receipt_info);
 
     // Get latest receipt
-    const latestActiveReceipt = result.latest_receipt_info.find(receipt => parseInt(receipt.expire_date_ms) > new Date().getTime());
+    const latestActiveReceipt = result.latest_receipt_info.find(receipt => {
+        console.log('Evaluating if receipt with Transaction ID ' + receipt.transaction_id + ' is still valid');
+        console.log(receipt);
+        return parseInt(receipt.expire_date_ms) > new Date().getTime();
+    });
 
     console.log('latestActiveReceipt', latestActiveReceipt);
 
@@ -84,15 +92,18 @@ export const AppleIapCallback = ExpressFunctions.createHookAsync('AppleIapCallba
         return;
     }
 
-    const email = request.email;
-    const customerId = '999999'; // @TODO this is Stripe customer, work around this because this callback is from Apple not Stripe
+    const customer: Accounts.Customer = {
+        type: "apple_iap",
+        customerID: latestActiveReceipt.original_transaction_id,
+    }; // @TODO this is Stripe customer, work around this because this callback is from Apple not Stripe
+
     const plan: Billing.Plan = {
         ver: "v2",
-        level: latestActiveReceipt.product_id.replace('plan_', '') as V2PlanLevel, // remove the "plan_" prefix
+        level: latestActiveReceipt.product_id.replace('plan_', '') as Billing.V2PlanLevel, // remove the "plan_" prefix
     };
     const interval = 'month';
 
-    await Accounts.changePlanViaEmail(email, customerId, plan, interval);
+    await Accounts.changePlanViaEmail(email, customer, plan, interval);
 
     const response: Response = {
         code: 'ok',
