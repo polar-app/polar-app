7d7d79891a99d4ea8b1d2f1a6fd89c60d5a35277
Dzhuneyt <dzhuneyt@dzhuneyt.com>
2021-08-05 19:11:25 +0300

    feat(polar_bookshelf): Don't trigger the Apple IAP procedure if the app loaded within an Android version of the app

--- packages/polar-bookshelf/apps/repository/js/premium/actions/DefaultChangePlanContextProvider.tsx
+++ packages/polar-bookshelf/apps/repository/js/premium/actions/DefaultChangePlanContextProvider.tsx
@@ -77,7 +77,9 @@ function usePurchaseOrChangePlanAction() {
                 // This boolean flag is injected through the native app's WebView
                 // and is used to tell the React code (like the one here) that Polar Bookshelf
                 // is being viewed through the native app's WebView
-                if ((window as any).isNativeApp) {
+                const isMobileApp = (window as any).isNativeApp;
+                const isIOS = (window as any).mobileOS === 'ios';
+                if (isMobileApp && isIOS) {
                     const email = userInfoContext?.userInfo?.email;
                     if (!email) {
                         alert(`Can not change to plan ${plan.level} without a valid email`);
