a9fdf331210848688cf87a53314f7439520cadae
Kevin Burton <burtonator@gmail.com>
2021-08-15 20:14:21 -0600

    ...

--- packages/polar-app-public/polar-answers/src/AnswerIndexer.ts
+++ packages/polar-app-public/polar-answers/src/AnswerIndexer.ts
@@ -1,5 +1,6 @@
 import {URLStr} from "polar-shared/src/util/Strings";
 import {PDFText} from "polar-pdf/src/pdf/PDFText";
+import {SentenceShingler} from "./SentenceShingler";
 
 export namespace AnswerIndexer {
 
@@ -10,14 +11,19 @@ export namespace AnswerIndexer {
     export async function doIndex(opts: IndexOpts) {
 
 
-        await PDFText.getText(opts.url, pdfTextContent => {
+        await PDFText.getText(opts.url, async pdfTextContent => {
+
             const {extract, pageNum} = pdfTextContent;
 
             const content = extract.map(current => current.map(word => word.str).join(" ")).join("\n");
 
             // now build the sentence shingles over this...
 
+            const shingles = await SentenceShingler.computeShinglesFromContent(content);
 
+            // FIXME: now for each shingle, persist it out to ES
+
+            // FIXME: tests to make sure shingles are computed properly.
 
         });
 
