f83e63295db99de31393dce18a0d2a62fd9b6c1f
Kevin Burton <burtonator@gmail.com>
2021-08-17 14:45:51 -0600

    Test for loading with the indexer..

---
+++ packages/polar-app-public/polar-answers/src/AnswerIndexerTest.ts
@@ -0,0 +1,28 @@
+import { FilePaths } from "polar-shared/src/util/FilePaths";
+import {FirebaseAdmin} from "polar-firebase-admin/src/FirebaseAdmin";
+import {AnswerIndexer} from "./AnswerIndexer";
+
+xdescribe("AnswerIndexer", async function() {
+
+    it("basic", async function() {
+
+        const url = FilePaths.toURL("/Users/burton/projects/polar-app/packages/polar-bookshelf/docs/examples/pdf/bigtable.pdf");
+
+        const app = FirebaseAdmin.app()
+
+        const auth = app.auth();
+        const user = await auth.getUserByEmail('burton@inputneuron.io')
+
+        if (! user) {
+            throw new Error("No user");
+        }
+
+        await AnswerIndexer.doIndex({
+            docID: '1234',
+            uid: user.uid,
+            url
+        })
+
+    });
+
+})
