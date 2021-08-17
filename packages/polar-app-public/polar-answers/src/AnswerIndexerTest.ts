8de97749ef6fc9ab29ae925f230c83156e001540
Kevin Burton <burtonator@gmail.com>
2021-08-17 14:55:56 -0600

    OK... the indexer works now...

--- packages/polar-app-public/polar-answers/src/AnswerIndexerTest.ts
+++ packages/polar-app-public/polar-answers/src/AnswerIndexerTest.ts
@@ -4,6 +4,8 @@ import {AnswerIndexer} from "./AnswerIndexer";
 
 xdescribe("AnswerIndexer", async function() {
 
+    this.timeout(300000);
+
     it("basic", async function() {
 
         const url = FilePaths.toURL("/Users/burton/projects/polar-app/packages/polar-bookshelf/docs/examples/pdf/bigtable.pdf");
