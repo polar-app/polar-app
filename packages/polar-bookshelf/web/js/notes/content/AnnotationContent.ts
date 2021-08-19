242cd09528f4c5896b50ff0ce47387063cfe4226
WinterCore <hogobbl@gmail.com>
2021-08-19 07:59:40 +0200

    Refactor annotation content classes

--- packages/polar-bookshelf/web/js/notes/content/AnnotationContent.ts
+++ packages/polar-bookshelf/web/js/notes/content/AnnotationContent.ts
@@ -1,24 +1,39 @@
-import {computed, makeObservable, observable} from "mobx";
-import {IAnnotationContentType, IAreaHighlightAnnotationContent, ICommentAnnotationContent, IFlashcardAnnotationContent, ITextHighlightAnnotationContent} from "polar-blocks/src/blocks/content/IAnnotationContent";
+import cloneDeep from "lodash/cloneDeep";
+import {computed, makeObservable, observable, toJS} from "mobx";
+import {IAnnotationContent, IAnnotationContentBase, IAnnotationContentType, IAreaHighlightAnnotationContent, ICommentAnnotationContent, IFlashcardAnnotationContent, ITextHighlightAnnotationContent} from "polar-blocks/src/blocks/content/IAnnotationContent";
 import {IBaseBlockContent} from "polar-blocks/src/blocks/content/IBaseBlockContent";
 import {IBlockContent} from "polar-blocks/src/blocks/IBlock";
-import {IAreaHighlight} from "polar-shared/src/metadata/IAreaHighlight";
-import {IComment} from "polar-shared/src/metadata/IComment";
-import {IFlashcard} from "polar-shared/src/metadata/IFlashcard";
-import {ITextHighlight} from "polar-shared/src/metadata/ITextHighlight";
 import {DeviceIDStr} from "polar-shared/src/util/DeviceIDManager";
 import {IDStr} from "polar-shared/src/util/Strings";
 
-// TODO: find a way to refactor these 4
+function cleanse<T extends { [key: string]: any }>(obj: T) {
+    Object.keys(obj).forEach(function(key) {
+        // Get this value and its type
+        var value = obj[key];
+        var type = typeof value;
+        if (type === "object") {
+            // Recurse...
+            cleanse(value);
+            // ...and remove if now "empty" (NOTE: insert your definition of "empty" here)
+            if (!Object.keys(value).length) {
+                delete obj[key]
+            }
+        }
+        else if (type === "undefined") {
+            // Undefined, remove it
+            delete obj[key];
+        }
+    });
+}
 
-export class TextHighlightAnnotationContent implements ITextHighlightAnnotationContent, IBaseBlockContent {
-    @observable private readonly _type: IAnnotationContentType.TEXT_HIGHLIGHT;
+abstract class AnnotationContentBase<T extends IAnnotationContent> implements IAnnotationContentBase<T['type'], T['value']>, IBaseBlockContent {
+    @observable private readonly _type: T['type'];
     @observable private _mutator: DeviceIDStr;
     @observable private _docID: IDStr;
     @observable private _pageNum: number;
-    @observable private _value: ITextHighlight;
+    @observable private _value: T['value'];
 
-    constructor(opts: ITextHighlightAnnotationContent) {
+    constructor(opts: T) {
 
         this._type = opts.type;
         this._mutator = opts.mutator || '';
@@ -66,214 +81,30 @@ export class TextHighlightAnnotationContent implements ITextHighlightAnnotationC
 
     }
 
-    public toJSON(): ITextHighlightAnnotationContent {
+    private valueToJSON(value: T['value']): T['value'] {
+        const serialized = JSON.parse(JSON.stringify(toJS(value)));
+        cleanse(serialized);
+        return { ...serialized };
+    }
+
+    public toJSON(): T {
         return {
             type: this._type,
             docID: this._docID,
             pageNum: this._pageNum,
-            value: this._value,
+            value: this.valueToJSON(this._value),
             mutator: this._mutator,
-        };
+        } as T;
     }
 }
 
-export class AreaHighlightAnnotationContent implements IAreaHighlightAnnotationContent, IBaseBlockContent {
-    @observable private readonly _type: IAnnotationContentType.AREA_HIGHLIGHT;
-    @observable private _mutator: DeviceIDStr;
-    @observable private _docID: IDStr;
-    @observable private _pageNum: number;
-    @observable private _value: IAreaHighlight;
+export class TextHighlightAnnotationContent extends AnnotationContentBase<ITextHighlightAnnotationContent> {}
 
-    constructor(opts: IAreaHighlightAnnotationContent) {
+export class AreaHighlightAnnotationContent extends AnnotationContentBase<IAreaHighlightAnnotationContent> {}
 
-        this._type = opts.type;
-        this._mutator = opts.mutator || '';
-        this._docID = opts.docID;
-        this._pageNum = opts.pageNum;
-        this._value = opts.value;
+export class CommentAnnotationContent extends AnnotationContentBase<ICommentAnnotationContent> {}
 
-        makeObservable(this);
-
-    }
-
-    @computed get type() {
-        return this._type;
-    }
-
-    @computed get mutator() {
-        return this._mutator;
-    }
-
-    @computed get docID() {
-        return this._docID;
-    }
-
-    @computed get pageNum() {
-        return this._pageNum;
-    }
-
-    @computed get value() {
-        return this._value;
-    }
-
-    public setMutator(mutator: DeviceIDStr) {
-        this._mutator = mutator;
-    }
-
-    public update(content: IBlockContent) {
-        if (content.type === this._type) {
-            this._pageNum = content.pageNum;
-            this._docID = content.docID;
-            this._value = content.value;
-            this._mutator = content.mutator || '';
-        } else {
-            throw new Error("Invalid type: " +  content.type)
-        }
-
-    }
-
-    public toJSON(): IAreaHighlightAnnotationContent {
-        return {
-            type: this._type,
-            docID: this._docID,
-            pageNum: this._pageNum,
-            value: this._value,
-            mutator: this._mutator,
-        };
-    }
-}
-
-export class CommentAnnotationContent implements ICommentAnnotationContent, IBaseBlockContent {
-    @observable private readonly _type: IAnnotationContentType.COMMENT;
-    @observable private _mutator: DeviceIDStr;
-    @observable private _docID: IDStr;
-    @observable private _pageNum: number;
-    @observable private _value: IComment;
-
-    constructor(opts: ICommentAnnotationContent) {
-
-        this._type = opts.type;
-        this._mutator = opts.mutator || '';
-        this._docID = opts.docID;
-        this._pageNum = opts.pageNum;
-        this._value = opts.value;
-
-        makeObservable(this);
-
-    }
-
-    @computed get type() {
-        return this._type;
-    }
-
-    @computed get mutator() {
-        return this._mutator;
-    }
-
-    @computed get docID() {
-        return this._docID;
-    }
-
-    @computed get pageNum() {
-        return this._pageNum;
-    }
-
-    @computed get value() {
-        return this._value;
-    }
-
-    public setMutator(mutator: DeviceIDStr) {
-        this._mutator = mutator;
-    }
-
-    public update(content: IBlockContent) {
-        if (content.type === this._type) {
-            this._pageNum = content.pageNum;
-            this._docID = content.docID;
-            this._value = content.value;
-            this._mutator = content.mutator || '';
-        } else {
-            throw new Error("Invalid type: " +  content.type)
-        }
-
-    }
-
-    public toJSON(): ICommentAnnotationContent {
-        return {
-            type: this._type,
-            docID: this._docID,
-            pageNum: this._pageNum,
-            value: this._value,
-            mutator: this._mutator,
-        };
-    }
-}
-
-export class FlashcardAnnotationContent implements IFlashcardAnnotationContent, IBaseBlockContent {
-    @observable private readonly _type: IAnnotationContentType.FLASHCARD;
-    @observable private _mutator: DeviceIDStr;
-    @observable private _docID: IDStr;
-    @observable private _pageNum: number;
-    @observable private _value: IFlashcard;
-
-    constructor(opts: IFlashcardAnnotationContent) {
-
-        this._type = opts.type;
-        this._mutator = opts.mutator || '';
-        this._docID = opts.docID;
-        this._pageNum = opts.pageNum;
-        this._value = opts.value;
-
-        makeObservable(this);
-
-    }
-
-    @computed get type() {
-        return this._type;
-    }
-
-    @computed get mutator() {
-        return this._mutator;
-    }
-
-    @computed get docID() {
-        return this._docID;
-    }
-
-    @computed get pageNum() {
-        return this._pageNum;
-    }
-
-    @computed get value() {
-        return this._value;
-    }
-
-    public setMutator(mutator: DeviceIDStr) {
-        this._mutator = mutator;
-    }
-
-    public update(content: IBlockContent) {
-        if (content.type === this._type) {
-            this._pageNum = content.pageNum;
-            this._docID = content.docID;
-            this._value = content.value;
-            this._mutator = content.mutator || '';
-        } else {
-            throw new Error("Invalid type: " +  content.type)
-        }
-
-    }
-
-    public toJSON(): IFlashcardAnnotationContent {
-        return {
-            type: this._type,
-            docID: this._docID,
-            pageNum: this._pageNum,
-            value: this._value,
-            mutator: this._mutator,
-        };
-    }
-}
+export class FlashcardAnnotationContent extends AnnotationContentBase<IFlashcardAnnotationContent> {}
 
 
 export type AnnotationContent = TextHighlightAnnotationContent
