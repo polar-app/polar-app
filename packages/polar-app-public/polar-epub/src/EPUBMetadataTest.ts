de7dc6266fd1b7bcc8aa012be1e444e457ec15e3
Kevin Burton <burtonator@gmail.com>
2021-08-23 11:40:36 -0600

    this works but now has a totally different issue with node-fetch.

--- packages/polar-app-public/polar-epub/src/EPUBMetadataTest.ts
+++ packages/polar-app-public/polar-epub/src/EPUBMetadataTest.ts
@@ -4,7 +4,14 @@ import {FilePaths} from "polar-shared/src/util/FilePaths";
 describe('EPUBMetadata', function() {
 
     it("basic", async function() {
-        await EPUBMetadata.getMetadata(FilePaths.resolve(__dirname, '../test/'));
+
+        const path = FilePaths.resolve(__dirname, '../alice.epub');
+        const url = FilePaths.toURL(path);
+
+        console.log("path: ", path);
+        console.log("url: ", url);
+        await EPUBMetadata.getMetadata(url);
+
     });
 
 });
