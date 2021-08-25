1a0845e9885b0bd76f07c859fcf2290091ff616a
Dzhuneyt <dzhuneyt@dzhuneyt.com>
2021-08-26 01:05:54 +0300

    chore: Restore EPUBDocs.ts to its "master" branch version and revert some experiments by Kevin on it

--- packages/polar-app-public/polar-epub/src/EPUBDocs.ts
+++ packages/polar-app-public/polar-epub/src/EPUBDocs.ts
@@ -2,9 +2,6 @@ import {URLStr} from "polar-shared/src/util/Strings";
 import { URLs } from "polar-shared/src/util/URLs";
 import {Fetches} from "polar-shared/src/util/Fetch";
 import ePub from "epubjs";
-import { Files } from "polar-shared/src/util/Files";
-import {Buffers} from "polar-shared/src/util/Buffers";
-import {JSDOM} from "jsdom";
 
 export namespace EPUBDocs {
 
@@ -14,39 +11,20 @@ export namespace EPUBDocs {
 
     export async function getDocument(opts: Opts) {
 
-        const dom = new JSDOM(``, {url: 'https://www.example.com'});
-        global.document = dom.window.document;
-        (global as any).window = dom.window;
-        (global as any).localStorage = dom.window.localStorage;
-        (global as any).navigator = dom.window.navigator;
-        (global as any).performance = dom.window.performance;
-        (global as any).XMLHttpRequest = dom.window.XMLHttpRequest;
-        (global as any).window.requestAnimationFrame = (delegate: () => void) => setTimeout(() => delegate(), 1);
+        const docURL = await URLs.toURL(opts.url);
 
-        // async function toArrayBuffer() {
+        async function toArrayBuffer() {
+            // we have to convert the URL to an ArrayBuffer otherwise epub
+            // gets confused and tries to load this incorrectly with epubs.
+            //
+            // TODO: we should have a progress listener here ...
 
-        //     if (URLs.isURL(opts.url)) {
-        //
-        //         const docURL = await URLs.toURL(opts.url);
-        //
-        //         // we have to convert the URL to an ArrayBuffer otherwise epub
-        //         // gets confused and tries to load this incorrectly with epubs.
-        //         //
-        //         // TODO: we should have a progress listener here ...
-        //
-        //         const response = await Fetches.fetch(docURL)
-        //         return await response.arrayBuffer();
-        //
-        //     } else {
-        //         const buff = await Files.readFileAsync(opts.url);
-        //         return Buffers.toArrayBuffer(buff);
-        //     }
-        //
-        // }
-        //
-        // const arrayBuffer = await toArrayBuffer();
-        return ePub(opts.url);
+            const response = await Fetches.fetch(docURL)
+            return await response.arrayBuffer();
+        }
 
+        const arrayBuffer = await toArrayBuffer();
+        return ePub(arrayBuffer);
     }
 
 }
