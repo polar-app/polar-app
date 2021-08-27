0cb62f3ed8a1a891bed1da3245b91879d5122d63
Kevin Burton <burtonator@gmail.com>
2021-08-27 17:43:47 -0600

    OK... re-indexed everything and things look good.

--- packages/polar-app-public/polar-answers/src/AnswerExecutorTest.ts
+++ packages/polar-app-public/polar-answers/src/AnswerExecutorTest.ts
@@ -266,7 +266,8 @@ xdescribe("AnswerExecutor", async function() {
             await assertQuestionAndAnswer("Contrast the mountains on Mars and Venus with those on Earth and the Moon.", [
                 "The mountains on Mars and Venus are much higher than those on Earth and the Moon.",
                 "On Mars, the mountains are volcanoes, produced by repeated eruptions of lava from the same vents. On Earth, the mountains are the result of compression and uplift of the surface. On the Moon and Mercury, the major mountains are ejecta thrown up by the large basin-forming impacts that took place billions of years ago.",
-                "The mountains on Mars and Venus are higher than those on Earth and the Moon."
+                "The mountains on Mars and Venus are higher than those on Earth and the Moon.",
+                "The mountains on Mars and Venus are the result of compression and uplift of the surface.  On Earth, this crustal compression results from collisions of one continental plate with another."
             ]);
 
         })
