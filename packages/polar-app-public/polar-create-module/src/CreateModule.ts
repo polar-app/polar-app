d08d0a0213823b696aa3f190231425ceebeff2e6
Kevin Burton <burtonator@gmail.com>
2021-09-06 08:50:29 -0600

    misc changes and upgrades to 107...

--- packages/polar-app-public/polar-create-module/src/CreateModule.ts
+++ packages/polar-app-public/polar-create-module/src/CreateModule.ts
@@ -1,5 +1,7 @@
 import fs from 'fs';
 import * as readline from 'readline';
+import {ESLint} from "./configs/ESLint";
+import {TSConfig} from "./configs/TSConfig";
 
 // & Interfaces
 interface PackageJson {
@@ -9,7 +11,8 @@ interface PackageJson {
     author: string;
     license: string;
     scripts?: Scripts;
-    devdependencies?: Record<string, unknown>;
+    devDependencies?: Record<string, unknown>;
+    dependencies?: Record<string, unknown>;
 }
 interface Scripts {
     test?: string;
@@ -45,7 +48,13 @@ async function getUserInput(property: string): Promise<string> {
             terminal.close();
             resolve(answer);
         });
-    }); 
+    });
+}
+
+export function createJSONDataFile(obj: any) {
+
+    return `// THIS FILE IS AUTO-GENERATED, DO NOT EDIT\n\n` + JSON.stringify(obj, null, "  ");
+
 }
 
 /**
@@ -53,23 +62,31 @@ async function getUserInput(property: string): Promise<string> {
  * @returns void
  */
 async function updateScripts(): Promise<void> {
-    // ~ Read and parse Package.json
-    const data = await fs.promises.readFile('package.json');
-    const content = JSON.parse(data.toString('utf-8'));
 
-    // ~ Update Scripts
-    content.scripts.test = "if [ -z $(find . -name '**Test.js') ]; then echo 'No Tests Found!!'; else yarn run mocha; fi;";
-    content.scripts.mocha = "mocha --timeout 20000 --exit **/**/*Test.js"
-    content.scripts.eslint = "eslint -c ./.eslintrc.json .";
-    content.scripts.eslintfix = "eslint -c ./.eslintrc.json . --fix";
-    content.scripts.compile = "tsc";
+    async function updatePackageJSON() {
 
-    // ~ Update Package.Json File
-    await fs.promises.writeFile('package.json', JSON.stringify(content, null, 2));
+        // ~ Read and parse Package.json
+        const data = await fs.promises.readFile('package.json');
+        const content = JSON.parse(data.toString('utf-8'));
+
+        // ~ Update Scripts
+        content.scripts.test = "if [ -z $(find src -name '**Test.js') ]; then echo 'No tests'; else yarn run mocha; fi;";
+        content.scripts.mocha = "mocha --timeout 20000 --exit **/**/*Test.js"
+        content.scripts.eslint = "eslint -c ./.eslintrc.json .";
+        content.scripts.eslintfix = "eslint -c ./.eslintrc.json . --fix";
+        content.scripts.compile = "tsc";
+
+        // ~ Update Package.Json File
+        await fs.promises.writeFile('package.json', JSON.stringify(content, null, 2));
+
+    }
+
+    await updatePackageJSON();
 
     // ~ copy over other files
-    await fs.promises.copyFile('/polar-app/packages/polar-app-public/polar-create-module/.eslintrc.json', './.eslintrc.json');
-    await fs.promises.copyFile('/polar-app/packages/polar-app-public/polar-create-module/tsconfig.json', './tsconfig.json');
+    await fs.promises.writeFile('.eslintrc.json', createJSONDataFile(ESLint.createV0()));
+    await fs.promises.writeFile('tsconfig.json', createJSONDataFile(TSConfig.createV0()));
+
 }
 
 /**
@@ -77,15 +94,18 @@ async function updateScripts(): Promise<void> {
  * @returns Promise<void>
  */
  async function createNewModule(): Promise<void> {
+
     // ~ Get user input
     const packageName: string = await getUserInput("Package Name: ");
     const packageDescription: string = await getUserInput("Package Description: ");
 
     // ~ Check if package Already Exists
-    if(fs.existsSync(`../${packageName}`)) { 
-        console.log("Package Already Exists");
-        return;
-    }
+
+    // if(fs.existsSync(`../${packageName}`)) {
+    //     console.log("Package Already Exists: " + packageName);
+    //     process.exit(1);
+    //     return;
+    // }
 
     // ~ Extract file contents
     const packageJsonFile: PackageJson = readFile('package.json');
@@ -100,26 +120,32 @@ async function updateScripts(): Promise<void> {
     await fs.promises.writeFile(`../${packageName}/package.json`, JSON.stringify(packageJsonFile, null, 2));
     await fs.promises.copyFile('./tsconfig.json', `../${packageName}/tsconfig.json`);
     await fs.promises.copyFile('./.eslintrc.json', `../${packageName}/.eslintrc.json`);
-    
+
 
     // ~ Return Success Message
     console.log("Package Created Successfully");
+
 }
 
 // ! Workflow
 async function workFlow(): Promise<void> {
+
     // ~ Extract Cli Flags
     const cliargs: Array<string> = process.argv.slice(2);
 
     // ~ Incase of update only (--update)
-    if (cliargs.length === 1 && cliargs[0] === '--update') { await updateScripts(); }
-
-    // ~ Incase of just create (no cli flags)
-    else if (cliargs.length === 0) { await createNewModule(); }
-    
-    // ~ any other incorrect flag
-    else { console.log('Sorry Wrong Flag !!'); }
+    if (cliargs.length === 1 && cliargs[0] === '--update') {
+        await updateScripts();
+    } else if (cliargs.length === 0) {
+        // ~ Incase of just create (no cli flags)
+        await createNewModule();
+    } else {
+        // ~ any other incorrect flag
+        console.error('Incorrect args: ', cliargs);
+        process.exit(1);
+    }
 
 }
 
-export default workFlow();
+workFlow()
+    .catch(err => console.error("ERROR: Unable to create module: ", err));
