93811683ee3572f596a10e9b728c0982c810b004
Hadi <hadi.abuhamed@gmail.com>
2021-09-07 20:41:53 +0300

    back under hooks folder

--- packages/polar-bookshelf/apps/repository/js/doc_repo/MUIContextMenu2.tsx
+++ packages/polar-bookshelf/apps/repository/js/doc_repo/MUIContextMenu2.tsx
@@ -8,7 +8,7 @@ import { observer } from "mobx-react-lite"
 import {Devices} from 'polar-shared/src/util/Devices';
 import { makeStyles } from "@material-ui/core";
 import {useHistory} from "react-router-dom";
-import {useContextMenuHook} from "polar-bookshelf/web/js/mui/useContextMenuHook";
+import {useContextMenuHook} from "../../../../web/js/mui/hooks/useContextMenuHook";
 
 export namespace MouseEvents {
     export function fromNativeEvent(event: MouseEvent): IMouseEvent {
