05c5e0c6a3bb93224a9848b922857f4820b3976a
Kevin Burton <burtonator@gmail.com>
2021-09-12 10:01:45 -0600

    Adding vote specific information...

---
+++ packages/polar-answers-api/src/IAnswerExecutorTrace.ts
@@ -0,0 +1,41 @@
+import {IAnswerExecutorRequest} from "./IAnswerExecutorRequest";
+import {IDStr, UserIDStr} from "polar-shared/src/util/Strings";
+import {IOpenAIAnswersResponse} from "./IOpenAIAnswersResponse";
+import {IAnswerDigestRecord} from "./IAnswerDigestRecord";
+
+export interface IAnswerExecutorTrace extends IAnswerExecutorRequest {
+
+    readonly id: IDStr;
+
+    readonly type: 'trace';
+
+    readonly uid: UserIDStr;
+
+    // eslint-disable-next-line camelcase
+    readonly elasticseach_query: any;
+
+    // eslint-disable-next-line camelcase
+    readonly elasticsearch_url: string;
+
+    /**
+     * The records given back from ES.
+     */
+    // eslint-disable-next-line camelcase
+    readonly elasticseach_records: ReadonlyArray<IAnswerDigestRecord>;
+
+    // eslint-disable-next-line camelcase
+    readonly openai_request: IOpenAIAnswersRequest;
+
+    // eslint-disable-next-line camelcase
+    readonly openai_response: IOpenAIAnswersResponse;
+
+}
+
+/**
+ * Allows us to update and specify the vote data.
+ */
+export interface IAnswerExecutorTraceWithFeedback extends IAnswerExecutorTrace {
+    readonly type: 'trace-with-vote';
+    readonly vote: 'up' | 'down';
+    readonly expectation: string;
+}
--- packages/polar-answers/src/AnswerExecutorTracer.ts
+++ packages/polar-answers/src/AnswerExecutorTracer.ts
@@ -1,15 +1,12 @@
 import {IAnswerDigestRecord} from "polar-answers-api/src/IAnswerDigestRecord";
 import {arrayStream} from "polar-shared/src/util/ArrayStreams";
 import {IAnswerDigestRecordPDF} from "polar-answers-api/src/IAnswerDigestRecordPDF";
-import {IDStr} from "polar-shared/src/util/Strings";
 import {OpenAIAnswersClient} from "./OpenAIAnswersClient";
-import {IOpenAIAnswersResponse} from "polar-answers-api/src/IOpenAIAnswersResponse";
 import {AnswerExecutor} from "./AnswerExecutor";
 
 export namespace AnswerExecutorTracer {
 
-    import IOpenAIAnswersRequest = OpenAIAnswersClient.IOpenAIAnswersRequest;
-    import IAnswerExecutorRequestWithUID = AnswerExecutor.IAnswerExecutorRequestWithUID;
+
 
     /**
      * Compute the unique Doc IDs for the records so that we can can store them for auditing.
@@ -37,35 +34,5 @@ export namespace AnswerExecutorTracer {
             .collect();
 
     }
-
-    export interface IAnswerExecutorTrace extends IAnswerExecutorRequestWithUID{
-
-        readonly id: IDStr;
-
-        // eslint-disable-next-line camelcase
-        readonly elasticseach_query: any;
-
-        // eslint-disable-next-line camelcase
-        readonly elasticsearch_url: string;
-
-        /**
-         * The records given back from ES.
-         */
-        // eslint-disable-next-line camelcase
-        readonly elasticseach_records: ReadonlyArray<IAnswerDigestRecord>;
-
-        // eslint-disable-next-line camelcase
-        readonly openai_request: IOpenAIAnswersRequest;
-
-        // eslint-disable-next-line camelcase
-        readonly openai_response: IOpenAIAnswersResponse;
-
-    }
-
-    export interface IAnswerExecutorTraceWithFeedback extends IAnswerExecutorTrace {
-        readonly vote: 'up' | 'down';
-        readonly expectation: string;
-    }
-
 
 }
