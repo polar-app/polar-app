5a3a4711dc8e8b0e5003d7d0d4732c8a81615b97
Kevin Burton <burtonator@gmail.com>
2021-09-16 09:50:26 -0600

    Fixed ClientRect issues... by casting. Not happy but prevents a major refactor.

--- packages/polar-bookshelf/apps/doc/src/annotations/AreaHighlightDrawer.tsx
+++ packages/polar-bookshelf/apps/doc/src/annotations/AreaHighlightDrawer.tsx
@@ -171,13 +171,13 @@ export const usePDFRectangleDrawer = (callback: IUsePDFRectangleDrawerCallback, 
             }
             return false;
         };
-        
+
         const cleanup = (endPosition: IPoint) => {
             if (!pageElement || !rectElem || !start || !pageRect) {
                 return;
             }
 
-            const rect = calculateRectDimensions(start, endPosition, pageRect);
+            const rect = calculateRectDimensions(start, endPosition, pageRect as ClientRect);
             pageElement.removeChild(rectElem);
             if (rect.width > threshold.x || rect.height > threshold.y) {
                 callbackRef.current({
@@ -202,7 +202,7 @@ export const usePDFRectangleDrawer = (callback: IUsePDFRectangleDrawerCallback, 
                 return;
             }
             e.preventDefault();
-            const rect = calculateRectDimensions(start, { x: e.clientX, y: e.clientY }, pageRect);
+            const rect = calculateRectDimensions(start, { x: e.clientX, y: e.clientY }, pageRect as ClientRect);
             updateDOMRect(rectElem, rect);
         };
 
@@ -232,7 +232,7 @@ export const usePDFRectangleDrawer = (callback: IUsePDFRectangleDrawerCallback, 
             e.preventDefault();
             const touch = e.touches[e.touches.length - 1];
             lastPosition = { x: touch.clientX, y: touch.clientY };
-            const rect = calculateRectDimensions(start, lastPosition, pageRect);
+            const rect = calculateRectDimensions(start, lastPosition, pageRect as ClientRect);
             updateDOMRect(rectElem, rect);
         };
 
