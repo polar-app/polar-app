62df37e7ae38ba39d49daf7a310c5d20deda859e
Kevin Burton <burtonator@gmail.com>
2021-09-18 15:38:31 -0600

    This tests the FIRST part but now we have to test the BACKEND part

--- packages/polar-web-extension/src/ContentCaptureContextTestK.ts
+++ packages/polar-web-extension/src/ContentCaptureContextTestK.ts
@@ -1,20 +1,39 @@
 import {ContentCaptureContext} from "./ContentCaptureContext";
 import {ContentCaptureContextEPUB} from "./ContentCaptureContextEPUB";
+import {assertJSON} from "polar-bookshelf/web/js/test/Assertions";
 
-// eslint-disable-next-line no-var
-declare var chrome: any;
 // eslint-disable-next-line no-var
 declare var window: any;
 
-window.chrome = {
-    runtime: {
-        onMessage: {
-            addListener: () => console.log('addListener called'),
-            removeListener: () => console.log('removeListener called')
+const [interactions, chrome] = createChrome();
+
+window.chrome = chrome;
+
+function createChrome() {
+
+    interface IOPSendMessageInteraction {
+        op: 'sendMessage',
+        message: any
+    }
+
+    const interactions: IOPSendMessageInteraction[] = [];
+
+    const chrome = {
+        runtime: {
+            onMessage: {
+                addListener: () => console.log('MOCK chrome: addListener called'),
+                removeListener: () => console.log('MOCK chrome: removeListener called')
+            },
+            sendMessage: (message: any) => {
+                console.log("MOCK chrome: Sending message: ", message);
+                interactions.push({op: 'sendMessage', message});
+            }
         }
     }
-}
 
+    return [interactions, chrome]
+
+}
 
 describe("ContentCaptureContext", function() {
 
@@ -23,9 +42,27 @@ describe("ContentCaptureContext", function() {
         document.body.innerHTML = 'hello world';
         ContentCaptureContextEPUB.handleStartCaptureWithEPUB();
 
+        document.querySelector('button')!.click();
+
         // at this point we just have the react content rendered with a button.
 
         // TODO: click the button or do what the button does directly
+
+        assertJSON(interactions, [
+            {
+                "op": "sendMessage",
+                "message": {
+                    "type": "save-to-polar",
+                    "strategy": "epub",
+                    "value": {
+                        "icon": "http://localhost:9876/favicon.ico",
+                        "url": "http://localhost:9876/context.html",
+                        "text": "hello world",
+                        "content": "<div>hello world</div>"
+                    }
+                }
+            }
+        ]);
 
     });
 
