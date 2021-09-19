f291d90c7413189067dbe4bbc50f717b5eceac2d
Kevin Burton <burtonator@gmail.com>
2021-09-19 13:47:16 -0600

    I think this will enable karma for all modules but not sure yet.

--- packages/polar-create-module/src/CreateModule.ts
+++ packages/polar-create-module/src/CreateModule.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-useless-escape */
 import fs from 'fs';
 import * as readline from 'readline';
 import {Files} from "polar-shared/src/util/Files";
@@ -17,6 +18,7 @@ interface IPackageManifest {
 interface Scripts {
     test?: string;
     mocha?: string;
+    karma?: string;
     eslint?: string;
     eslintfix?: string;
     compile?: string;
@@ -26,6 +28,7 @@ interface Scripts {
 interface ICreateModuleConfig {
     readonly typescript?: 'disabled';
     readonly noTests?: true;
+    readonly noKarma?: true;
 }
 
 async function getUserInput(property: string): Promise<string> {
@@ -95,6 +98,13 @@ async function updateScripts(): Promise<void> {
                 pkg.scripts.test = "echo no tests";
             }
 
+            if (conf.noKarma) {
+                pkg.scripts.test = "echo no karma";
+            } else {
+                pkg.devDependencies['polar-karma'] = `^${pkg.version}`;
+                pkg.devDependencies['polar-webpack'] = `^${pkg.version}`;
+            }
+
             pkg.devDependencies['polar-eslint'] = `^${pkg.version}`;
             pkg.devDependencies['polar-typescript'] = `^${pkg.version}`;
 
@@ -103,10 +113,13 @@ async function updateScripts(): Promise<void> {
             delete pkg.scripts.eslint;
             delete pkg.scripts.eslintfix;
             delete pkg.scripts.test;
+            delete pkg.scripts.karma;
             delete pkg.scripts.compile;
 
             delete pkg.devDependencies['polar-eslint'];
             delete pkg.devDependencies['polar-typescript'];
+            delete pkg.devDependencies['polar-karma'];
+            delete pkg.devDependencies['polar-webpack'];
 
         }
 
@@ -121,9 +134,11 @@ async function updateScripts(): Promise<void> {
     if (conf.typescript !== 'disabled') {
         await fs.promises.writeFile('.eslintrc.json', createJSONDataFile(ESLint.createV0()));
         await fs.promises.writeFile('tsconfig.json', createJSONDataFile(TSConfig.createV0()));
+        await fs.promises.writeFile('karma.conf.js', Karma.createV0());
     } else {
         await Files.deleteAsync('.eslintrc.json');
         await Files.deleteAsync('tsconfig.json');
+        await Files.deleteAsync('karma.conf.js');
     }
     if (fs.existsSync('tslint.yaml')) {
         await fs.promises.rm('tslint.yaml');
@@ -468,3 +483,145 @@ export namespace Package {
     }
 }
 
+export namespace Karma {
+
+    export function createV0() {
+
+        return `
+const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
+const svgToMiniDataURI = require('mini-svg-data-uri');
+const os = require("os");
+const workers = os.cpus().length - 1;
+const isDevServer = process.argv.includes('serve');
+const mode = process.env.NODE_ENV || (isDevServer ? 'development' : 'production');
+const isDev = mode === 'development';
+
+module.exports = (config) => {
+    config.set({
+        client: {
+            // only run tests targeting node/karma or JUST karma but never JUST
+            // node.
+            args: [
+                './{,!(node_modules)/**}/*Test.js',
+                './{,!(node_modules)/**}/*TestK.js',
+                './{,!(node_modules)/**}/*TestNK.js'
+            ],
+            mocha: {
+                timeout : 60000
+            }
+        },
+        // browsers: ['Chrome'],
+        browsers: ['ChromeHeadless'],
+
+        customHeaders: [
+            {
+                match: '.*',
+                name: 'Cross-Origin-Opener-Policy',
+                value: 'same-origin'
+            },
+            {
+                match: '.*',
+                name: 'Cross-Origin-Embedder-Policy',
+                value: 'require-corp',
+            }
+        ],
+
+        // make sure to include webpack as a framework
+        frameworks: ['mocha', 'webpack'],
+
+        plugins: [
+            'karma-chrome-launcher',
+            'karma-webpack',
+            'karma-mocha',
+            'karma-spec-reporter',
+            'karma-junit-reporter'
+        ],
+
+        files: [
+
+            { pattern: 'src/**/*TestK.ts', watched: false },
+
+        ],
+
+        preprocessors: {
+            // add webpack as preprocessor
+            'src/**/*TestK.ts': ['webpack'],
+        },
+        singleRun: true,
+
+        reporters: ['junit', 'spec'],
+
+        webpack: {
+            plugins: [
+                new NodePolyfillPlugin(),
+            ],
+            module: {
+                rules: [
+                    {
+                        test: /\.(jsx|tsx|ts)$/,
+                        exclude: [
+                            /node_modules/,
+                        ],
+                        use: [
+                            {
+                                loader: 'thread-loader',
+                                options: {
+                                    // there should be 1 cpu for the fork-ts-checker-webpack-plugin
+                                    workers,
+                                    // set this to Infinity in watch mode - see https://github.com/webpack-contrib/thread-loader
+                                    workerParallelJobs: 100,
+                                    poolTimeout: 2000,
+                                }
+                            },
+                            {
+                                loader: 'ts-loader',
+                                options: {
+                                    // performance: this improved performance by about 2x.
+                                    // from 20s to about 10s
+                                    transpileOnly: isDev,
+                                    experimentalWatchApi: true,
+
+                                    // IMPORTANT! use happyPackMode mode to speed-up
+                                    // compilation and reduce errors reported to webpack
+                                    happyPackMode: true
+
+                                }
+                            }
+
+                        ]
+
+                    },
+                    {
+                        // make SVGs use data URLs.
+                        test: /\.(svg)(\?v=\d+\.\d+\.\d+)?$/i,
+                        exclude: [],
+                        use: [
+                            {
+                                loader: 'url-loader',
+                                options: {
+                                    limit: 32768,
+                                    generator: (content) => svgToMiniDataURI(content.toString()),
+                                }
+                            },
+                        ],
+                    },
+
+                ]
+
+            },
+            resolve: {
+                fallback: {
+                    fs: false,
+                    net: false,
+                    tls: false,
+                    child_process: false,
+                    electron: false
+                }
+            },
+
+        },
+    });
+}`;
+    }
+
+}
