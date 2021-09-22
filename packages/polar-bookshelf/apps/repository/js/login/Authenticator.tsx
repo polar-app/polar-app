58140c34a77efb09c819c6793f4149e769f196dc
Kevin Burton <burtonator@gmail.com>
2021-09-22 09:10:01 -0600

    Fixed code duplication issues...

--- packages/polar-bookshelf/apps/repository/js/login/Authenticator.tsx
+++ packages/polar-bookshelf/apps/repository/js/login/Authenticator.tsx
@@ -116,33 +116,10 @@ interface IAuthButtonProps {
     readonly startIcon: React.ReactNode;
     readonly onClick: () => void;
     readonly strategy: string;
+    readonly style?: React.CSSProperties;
 }
 
-const AuthButtonNotMobile = (props: IAuthButtonProps) => {
-
-    const classes = useStyles();
-
-    const mode = React.useContext(AuthenticatorModeContext);
-
-    const hint = mode === 'create-account' ? 'Continue' : 'Sign In'
-
-    return (
-        <>
-            <Button variant="contained"
-                    color="primary"
-                    className={classes.button}
-                    onClick={props.onClick}
-                    startIcon={props.startIcon}>
-
-                {hint} with {props.strategy}
-
-            </Button>
-
-        </>
-    );
-}
-
-const AuthButtonMobile = (props: IAuthButtonProps) => {
+const AuthButton = (props: IAuthButtonProps) => {
 
     const classes = useStyles();
 
@@ -157,13 +134,20 @@ const AuthButtonMobile = (props: IAuthButtonProps) => {
                     className={classes.button}
                     onClick={props.onClick}
                     startIcon={props.startIcon}
-                    style={{width: '95vw', margin: '10px', textAlign: 'center'}}>
+                    style={props.style}>
 
                 {hint} with {props.strategy}
 
             </Button>
 
         </>
+    );
+}
+
+const AuthButtonMobile = (props: IAuthButtonProps) => {
+    return (
+        <AuthButtonMobile {...props}
+                          style={{width: '95vw', margin: '10px', textAlign: 'center'}}/>
     );
 }
 
@@ -222,7 +206,7 @@ const GoogleAuthButton = () => {
                 </Alert>
             )}
 
-            <AuthButtonNotMobile onClick={handleTriggerAuth}
+            <AuthButton onClick={handleTriggerAuth}
                         strategy="Google"
                         startIcon={<FAGoogleIcon />}/>
 
@@ -449,7 +433,7 @@ const EmailTokenAuthButtonNotPhone = () => {
 
             {!triggered && (
 
-                <AuthButtonNotMobile onClick={handleClick}
+                <AuthButton onClick={handleClick}
                             strategy="Email"
                             startIcon={<EmailIcon />}
                             />
@@ -756,7 +740,7 @@ const EmailAuthButton = () => {
                 </>
             )}
 
-            <AuthButtonNotMobile onClick={handleClick}
+            <AuthButton onClick={handleClick}
                         strategy="Email"
                         startIcon={<EmailIcon />}/>
 
