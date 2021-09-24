5452bbff670b9586101a309435f6a89ba604c240
Kevin Burton <burtonator@gmail.com>
2021-09-24 09:32:07 -0600

    private beta system...

---
+++ packages/polar-private-beta/src/ComputeNextUserPriority.ts
@@ -0,0 +1,14 @@
+export namespace ComputeNextUserPriority {
+
+    export async function compute() {
+        // ** get the list of all users that haven't yet been invited.
+        // ** sort them by prioritized tags, using a score, then created
+        // ** sort them via metadata (IE what they do)
+
+        // FIXME: I need to prompt for their metadata and what they do... We
+        // might want to prioritize by this strategy.
+
+        //
+    }
+
+}
