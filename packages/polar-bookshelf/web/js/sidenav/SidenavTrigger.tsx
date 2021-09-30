7dfedb0ffa17499ed85d80aa6b577975a96248b0
Hadi <hadi.abuhamed@gmail.com>
2021-09-30 21:18:05 +0300

    fix: sidenav navigation issues

--- packages/polar-bookshelf/web/js/sidenav/SidenavTrigger.tsx
+++ packages/polar-bookshelf/web/js/sidenav/SidenavTrigger.tsx
@@ -8,19 +8,34 @@ import {useSideNavCallbacks, useSideNavStore} from "./SideNavStore";
 export const SidenavTrigger: React.FC = () => {
     const {setOpen} = useSideNavCallbacks();
     const {isOpen} = useSideNavStore(['isOpen']);
-    // const history = useHistory();
+    const history = useHistory();
 
-    const handleToggle = React.useCallback(() => {
-        setOpen(! isOpen);
-        // history.push(`#sidenav${Date.now()}`);
-    }, [setOpen, isOpen]);
+    const handleToggle = (url: string) => {
+        setOpen(!isOpen);
+        history.replace(url);
+    };
+            
+    React.useEffect(()=>{
+        return history.listen((location) => {
+            // if we press the back button
+            if (history.action === 'POP' && history.location.pathname !== '/') {
+                handleToggle('/');
+            
+                //if we try to open the sidenave not from the homepage
+                if (history.location.pathname !== '/' ){
+                    history.push('/');// not working yet!
+                    handleToggle(`#sidenav${Date.now()}`)
+                }
+            }
+        })
+    },[history]);
 
     if (Devices.isDesktop()) {
         return null;
     }
 
     return (
-        <IconButton onClick={handleToggle}>
+        <IconButton onClick={ () => handleToggle(`#sidenav${Date.now()}`)}>
             <MenuIcon/>
         </IconButton>
     );
