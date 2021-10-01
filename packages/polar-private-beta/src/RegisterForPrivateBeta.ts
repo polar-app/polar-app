cb4ea38843637ac8a3b3449348f4127cb95571b2
Kevin Burton <burtonator@gmail.com>
2021-10-01 12:18:50 -0600

    don't handle raw errors here... handle this in a wrapper function otherwise we will keep duplicating this code.

--- packages/polar-private-beta/src/RegisterForPrivateBeta.ts
+++ packages/polar-private-beta/src/RegisterForPrivateBeta.ts
@@ -1,4 +1,3 @@
-import {SentryReporters} from "polar-hooks-functions/impl/reporters/SentryReporter";
 import {PrivateBetaReqCollection} from "polar-firebase/src/firebase/om/PrivateBetaReqCollection";
 import {FirestoreAdmin} from "polar-firebase-admin/src/FirestoreAdmin";
 import {Challenges} from "polar-shared/src/util/Challenges";
@@ -16,31 +15,19 @@ export namespace RegisterForPrivateBeta {
 
     export async function exec(request: IRegisterForPrivateBetaRequest): Promise<IRegisterForPrivateBetaResponse | IRegisterForPrivateBetaError> {
 
-        try {
+        const firestore = FirestoreAdmin.getInstance();
+        const {challenge} = Challenges.create();
 
-            const firestore = FirestoreAdmin.getInstance();
-            const {challenge} = Challenges.create();
+        await PrivateBetaReqCollection.set(firestore, {
+            tags: [request.tag],
+            email: request.email,
+            challenge
+        })
 
-            await PrivateBetaReqCollection.set(firestore, {
-                tags: [request.tag],
-                email: request.email,
-                challenge
-            })
+        // TODO: send them an email thanking them...
+        // TODO: no metadata yet...
 
-            // TODO: send them an email thanking them...
-            // TODO: no metadata yet...
-
-            return {};
-
-        } catch (e) {
-            SentryReporters.reportError("Failed to execute: ", e);
-            return {
-                error: true,
-                code: 'failed',
-                // eslint-disable-next-line @typescript-eslint/no-explicit-any
-                message: (e as any).message || undefined
-            };
-        }
+        return {};
 
     }
 
