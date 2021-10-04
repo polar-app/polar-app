4c78fc90bb64766c4ebec61c01e6d4002950048e
Dzhuneyt <dzhuneyt@dzhuneyt.com>
2021-10-05 00:08:11 +0300

    feat: RPC bridge for CDK deployed AWS Lambdas

--- packages/polar-cdk/lib/ApiGatewayRoutes/RPCSample/RPCSample.lambda.ts
+++ packages/polar-cdk/lib/ApiGatewayRoutes/RPCSample/RPCSample.lambda.ts
@@ -1,5 +1,7 @@
 import {FuncWithAuth, lambdaWrapper} from "../../shared/lambdaWrapper";
 
+// The original implementation. This is runtime-agnostic,
+// meaning that it can be ran inside AWS Lambda OR Firebase Functions
 const originalHandler: FuncWithAuth = (idUser, request) => {
     return {
         success: true,
@@ -8,6 +10,7 @@ const originalHandler: FuncWithAuth = (idUser, request) => {
     }
 };
 
+// Export the Lambda to the Runtime
 export const handler = lambdaWrapper(originalHandler, {
     authRequired: true,
 });
