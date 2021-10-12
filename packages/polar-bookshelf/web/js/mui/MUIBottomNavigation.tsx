ff7ba1b798a0adcf0f7049df4ae36d784ae17db2
Kevin Burton <burtonator@gmail.com>
2021-10-12 12:36:00 -0600

    fix: cleanup of code.  Don't use hard coded colors. Bug with white mode. fix: better registry for URLs fix: use react router history properly.

--- packages/polar-bookshelf/web/js/mui/MUIBottomNavigation.tsx
+++ packages/polar-bookshelf/web/js/mui/MUIBottomNavigation.tsx
@@ -2,48 +2,84 @@ import { BottomNavigationAction } from '@material-ui/core';
 import BottomNavigation from '@material-ui/core/BottomNavigation';
 import Paper from '@material-ui/core/Paper';
 import * as React from 'react';
-
 import { createStyles, makeStyles } from "@material-ui/core/styles";
-
 import AddIcon from '@material-ui/icons/Add';
-// import SearchIcon from '@material-ui/icons/Search';
-// import ViewCarouselIcon from '@material-ui/icons/ViewCarousel';
 import HomeIcon from '@material-ui/icons/Home';
-import { useHistory } from 'react-router-dom';
+import {useHistory, useLocation} from 'react-router-dom';
 import { RoutePathnames } from '../apps/repository/RoutePathnames';
 
 const useStyles = makeStyles((theme) =>
     createStyles({
         root: {
             '& .Mui-selected':{
-                backgroundColor: '#6754D6',
-                color: 'white'
+                backgroundColor: theme.palette.primary.main,
+                color: theme.palette.primary.contrastText
             },
         },
     })
 );
+
+interface IBottomNavLocation {
+    readonly id: string;
+    readonly label: string;
+    readonly href: string;
+    readonly icon: React.ReactNode;
+}
+
+const BOTTOM_NAV_LOCATIONS: ReadonlyArray<IBottomNavLocation> = [
+    {
+        id: 'home',
+        label: 'Home',
+        href: '/',
+        icon: <HomeIcon/>
+    },
+    {
+        id: 'add',
+        label: 'Add',
+        href: RoutePathnames.ADD_MOBILE,
+        icon: <AddIcon/>
+    },
+]
+
 export const MUIBottomNavigation = ()  => {
+
     const classes = useStyles();
     const history = useHistory();
     const [value, setValue] = React.useState('/');
 
-    const changeRoute = (newValue: string) =>{
+    const location = useLocation();
+
+    const changeRoute = React.useCallback((newValue: string) =>{
+
         if(newValue !== value){
-            setValue(newValue);
             history.push(newValue);
         }
-    }
+
+    }, [history, value]);
+
+    React.useEffect(() => {
+        setValue(location.pathname);
+    }, [location])
 
     return (
-        <Paper style={{ position: 'fixed', bottom: 0, left: 0, right: 0, zIndex:3 }} elevation={3}>
+        <Paper style={{
+                   position: 'fixed',
+                   bottom: 0,
+                   left: 0,
+                   right: 0,
+                   zIndex:3
+               }}
+               elevation={3}>
+
             <BottomNavigation value={value}
-                            onChange={(event, newValue) => changeRoute(newValue)}                          
-                            showLabels
-                            className={classes.root}>
-                <BottomNavigationAction label="Home" value='/' icon={<HomeIcon/>} />
-                {/* <BottomNavigationAction label="Search" value='#search' icon={<SearchIcon />} /> */}
-                <BottomNavigationAction label="Add" value={RoutePathnames.ADD_MOBILE} icon={<AddIcon />} />
-                {/* <BottomNavigationAction label="Switch" value='#switch' icon={<ViewCarouselIcon />} /> */}
+                              onChange={(event, newValue) => changeRoute(newValue)}
+                              showLabels
+                              className={classes.root}>
+
+                {BOTTOM_NAV_LOCATIONS.map(current => (
+                    <BottomNavigationAction key={current.id} label={current.label} value={current.href} icon={current.icon} />
+                ))}
+
             </BottomNavigation>
         </Paper>
     );
