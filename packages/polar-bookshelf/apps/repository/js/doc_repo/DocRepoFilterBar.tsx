a03144c36b59a7787aca3ed050c6605b65539e45
Kevin Burton <burtonator@gmail.com>
2021-10-12 15:12:49 -0600

    use proper app bar...

--- packages/polar-bookshelf/apps/repository/js/doc_repo/DocRepoFilterBar.tsx
+++ packages/polar-bookshelf/apps/repository/js/doc_repo/DocRepoFilterBar.tsx
@@ -10,6 +10,8 @@ import {UserAvatar} from '../../../../web/js/ui/cloud_auth/UserAvatar';
 import {useUserInfoContext} from "../../../../web/js/apps/repository/auth_handler/UserInfoProvider";
 import {useHistory} from 'react-router-dom';
 import { RoutePathnames } from '../../../../web/js/apps/repository/RoutePathnames';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
 export interface IProps {
 
     /**
@@ -28,26 +30,20 @@ export const DocRepoFilterBar = deepMemo(function DocRepoFilterBar(props: IProps
 
     const {setFilters} = callbacks;
 
-    const Right = () => {
-
-        if (props.right) {
-            return props.right;
-        } else {
-            return <div/>;
-        }
-
-    };
-
     return (
         <>
             <DeviceRouters.NotDesktop>
-                <div style={{display: 'flex',  width: '100%', alignItems: 'center'}}>
 
+                <>
                     <span>My workspace</span>
 
-                    <UserAvatar onClick={()=>history.push(RoutePathnames.ACCOUNT_MOBILE)} style={{marginLeft: 'auto'}} photoURL={userInfoContext?.userInfo?.photoURL} displayName={userInfoContext?.userInfo?.displayName}/>
+                    <UserAvatar onClick={()=>history.push(RoutePathnames.ACCOUNT_MOBILE)}
+                                style={{marginLeft: 'auto'}}
+                                photoURL={userInfoContext?.userInfo?.photoURL}
+                                displayName={userInfoContext?.userInfo?.displayName}/>
 
-                </div>
+                </>
+
             </DeviceRouters.NotDesktop>
             <DeviceRouters.Desktop>
                 <MUIButtonBar>
--- packages/polar-bookshelf/apps/repository/js/doc_repo/HeaderBar.tsx
+++ packages/polar-bookshelf/apps/repository/js/doc_repo/HeaderBar.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import {createStyles, makeStyles, useTheme} from '@material-ui/core';
+import {AppBar, createStyles, makeStyles, Toolbar, useTheme} from '@material-ui/core';
 import { useHistory } from 'react-router-dom';
 import IconButton from '@material-ui/core/IconButton';
 import ArrowBackIcon from '@material-ui/icons/ArrowBack';
@@ -26,18 +26,29 @@ interface IProps{
  * A header that will showcase a back button with a title
  */
 export const HeaderBar = React.memo(function HeaderBar(props: IProps) {
-    
+
     const classes = useStyles();
     const history = useHistory();
 
     return(
-        <>
-            <div className={classes.root}>
+        // <>
+        //     <div className={classes.root}>
+        //         <IconButton onClick={()=>history.goBack()}>
+        //             <ArrowBackIcon/>
+        //         </IconButton>
+        //         <span className={classes.title}>{props.title}</span>
+        //     </div>
+        // </>
+        <AppBar position="static">
+            <Toolbar>
                 <IconButton onClick={()=>history.goBack()}>
                     <ArrowBackIcon/>
                 </IconButton>
-                <span className={classes.title}>{props.title}</span>   
-            </div>
-        </>
+                <span className={classes.title}>{props.title}</span>
+
+            </Toolbar>
+            {/*<div style={{display: 'flex'}}>*/}
+            {/*</div>*/}
+        </AppBar>
     );
 });
