9a6f4dc1289c9ecad05faee4c9b37657df6208d3
Kevin Burton <burtonator@gmail.com>
2021-10-15 14:48:01 -0600

    improved firestore snapshots...

---
+++ packages/polar-bookshelf/web/js/ui/data_loader/UseFirestoreSnapshot.ts
@@ -0,0 +1,61 @@
+import {useFirestore} from "../../../../apps/repository/js/FirestoreProvider";
+import {CollectionNameStr} from "polar-shared/src/util/Strings";
+import {IQuery} from "polar-firestore-like/src/IQuery";
+import {ISnapshotMetadata} from "polar-firestore-like/src/ISnapshotMetadata";
+import {IWhereClause} from "polar-firestore-like/src/ICollectionReference";
+import {IFirestoreError} from "polar-firestore-like/src/IFirestoreError";
+import React from "react";
+import {IQuerySnapshot} from "polar-firestore-like/src/IQuerySnapshot";
+
+interface IFirestoreSnapshotOpts {
+    readonly offset?: number;
+    readonly limit?: number;
+    readonly where?: ReadonlyArray<IWhereClause>;
+}
+
+export type IFirestoreSnapshotTuple = [IQuerySnapshot<ISnapshotMetadata>| undefined, IFirestoreError | undefined];
+
+/**
+ * Get a snapshot from Firestore but also allow the user to subscribe by types
+ * to it and receive updates.
+ */
+export function useFirestoreSnapshot(collectionName: CollectionNameStr, opts: IFirestoreSnapshotOpts = {}): IFirestoreSnapshotTuple {
+
+    const {firestore} = useFirestore();
+
+    const [snapshot, setSnapshot] = React.useState<IQuerySnapshot<ISnapshotMetadata> | undefined>(undefined);
+    const [error, setError] = React.useState<IFirestoreError | undefined>(undefined);
+
+    const onNext = React.useCallback((snapshot: IQuerySnapshot<ISnapshotMetadata>) => {
+        setSnapshot(snapshot);
+    }, []);
+
+    const onError = React.useCallback((err: IFirestoreError) => {
+        setError(err);
+    }, []);
+
+    React.useEffect(() => {
+
+        let query: IQuery<ISnapshotMetadata> = firestore.collection(collectionName);
+
+        if (opts.offset) {
+            query = query.offset(opts.offset)
+        }
+
+        if (opts.limit) {
+            query = query.limit(opts.limit)
+        }
+
+        if (opts.where) {
+            for(const where of opts.where) {
+                query = query.where(where.fieldPath, where.opStr, where.value);
+            }
+        }
+
+        return query.onSnapshot(onNext, onError);
+
+    }, []);
+
+    return [snapshot, error];
+
+}
