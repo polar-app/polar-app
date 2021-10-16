0a330d14f381ca587e555fda468eed00a2a70537
Kevin Burton <burtonator@gmail.com>
2021-10-16 14:50:15 -0600

    cherrypicking changes for creating users...

--- packages/polar-firebase-admin/src/FirebaseUserCreator.ts
+++
@@ -1,8 +0,0 @@
-import {FirebaseAdmin} from "./FirebaseAdmin";
-
-export namespace FirebaseUserCreator {
-    export const create = async (email: string, password: string) => {
-        const auth = FirebaseAdmin.app().auth();
-        return await auth.createUser({email, password});
-    }
-}
