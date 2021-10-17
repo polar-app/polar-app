8dd074fed75829a5e1d46e2d742776bff35525cc
Kevin Burton <burtonator@gmail.com>
2021-10-16 19:58:34 -0600

    Try another purge...

--- packages/polar-bookshelf/web/js/datastore/sharing/db/doc_annotations/ProfileDocAnnotations.ts
+++
@@ -1,7 +0,0 @@
-// @NotStale
-import {BaseDocAnnotation} from "./BaseDocAnnotation";
-import {ProfileIDStr } from 'polar-shared/src/util/Strings';
-
-export interface ProfileDocAnnotation extends BaseDocAnnotation {
-    readonly profileID: ProfileIDStr;
-}
