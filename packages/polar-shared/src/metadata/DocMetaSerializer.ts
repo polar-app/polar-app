4bd5626f5367c112f58b047eec6afeacc6b041f4
Kevin Burton <burtonator@gmail.com>
2021-10-20 17:51:04 -0600

    ok.. I think DocMetas should work...

--- packages/polar-shared/src/metadata/DocMetaSerializer.ts
+++ packages/polar-shared/src/metadata/DocMetaSerializer.ts
@@ -6,7 +6,6 @@ import {IPageMeta} from "./IPageMeta";
 import {PageInfo} from "./PageInfo";
 import {PageMeta} from "./PageMeta";
 import {SparseDocMetas} from "./SparseDocMetas";
-import {Preconditions} from "../Preconditions";
 
 export namespace DocMetaSerializer {
 
@@ -51,63 +50,5 @@ export namespace DocMetaSerializer {
         }
 
     }
-
-    //
-    // export function deserialize(data: string, fingerprint: string): IDocMeta {
-    //
-    //     Preconditions.assertPresent(data, 'data');
-    //
-    //     if (typeof data !== "string") {
-    //         throw new Error("We can only deserialize strings: " + typeof data);
-    //     }
-    //
-    //     const docMeta: IDocMeta = Object.create(DocMeta.prototype);
-    //
-    //     try {
-    //
-    //         let obj = JSON.parse(data);
-    //
-    //         if (SparseDocMetas.isSparse(obj)) {
-    //             obj = SparseDocMetas.fromSparse(obj);
-    //         }
-    //
-    //         Object.assign(docMeta, obj);
-    //
-    //         if (docMeta.docInfo && !docMeta.docInfo.filename) {
-    //             // log.warn("DocMeta has no filename: " + docMeta.docInfo.fingerprint);
-    //         }
-    //
-    //         return upgrade(docMeta);
-    //
-    //     } catch (e) {
-    //         throw Errors.rethrow(e, "Unable to deserialize doc: " + fingerprint);
-    //     }
-    //
-    // }
-    //
-    // export function upgrade(docMeta: IDocMeta): IDocMeta {
-    //
-    //     // validate the JSON data and set defaults. In the future we should
-    //     // migrate to using something like AJV to provide these defaults and
-    //     // also perform type assertion.
-    //
-    //     docMeta.pageMetas = PageMetas.upgrade(docMeta.pageMetas);
-    //
-    //     if (!docMeta.annotationInfo) {
-    //         // log.debug("No annotation info.. Adding default.");
-    //         docMeta.annotationInfo = AnnotationInfos.create();
-    //     }
-    //
-    //     if (!docMeta.attachments) {
-    //         // log.debug("No attachments. Adding empty map.");
-    //         docMeta.attachments = {};
-    //     }
-    //
-    //     docMeta.docInfo = DocInfos.upgrade(docMeta.docInfo);
-    //
-    //     return docMeta;
-    //
-    // }
-
 
 }
