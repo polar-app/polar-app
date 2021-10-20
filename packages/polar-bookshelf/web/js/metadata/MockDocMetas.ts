67c3f993a61714ae610e076a3c0d6e80ee791ce3
Kevin Burton <burtonator@gmail.com>
2021-10-20 14:25:34 -0600

    wasn't being used to removed this method

--- packages/polar-bookshelf/web/js/metadata/MockDocMetas.ts
+++ packages/polar-bookshelf/web/js/metadata/MockDocMetas.ts
@@ -2,11 +2,7 @@ import {IDocMeta} from "polar-shared/src/metadata/IDocMeta";
 import {Pagemarks} from "./Pagemarks";
 import {PagemarkType} from "polar-shared/src/metadata/PagemarkType";
 import {TextHighlights} from "./TextHighlights";
-import {Datastore} from "../datastore/Datastore";
-import {FilePaths} from "polar-shared/src/util/FilePaths";
-import {FileRef} from "polar-shared/src/datastore/FileRef";
-import {Backend} from "polar-shared/src/datastore/Backend";
-import {DocMetas, MockDoc} from "./DocMetas";
+import {DocMetas} from "./DocMetas";
 
 export class MockDocMetas {
 
@@ -49,31 +45,5 @@ export class MockDocMetas {
         return docMeta;
 
     }
-
-    public static async createMockDocMetaFromPDF(datastore: Datastore): Promise<MockDoc> {
-
-        const docMeta = MockDocMetas.createMockDocMeta();
-
-        const pdfPath = FilePaths.join(__dirname, "..", "..", "..", "docs", "examples", "pdf", "chubby.pdf");
-
-        const fileRef: FileRef = {
-            name: "chubby.pdf"
-        };
-
-        docMeta.docInfo.filename = fileRef.name;
-        docMeta.docInfo.backend = Backend.STASH;
-
-        await datastore.writeFile(Backend.STASH, fileRef, {path: pdfPath});
-
-        await datastore.writeDocMeta(docMeta);
-
-        const result: MockDoc = {
-            docMeta, fileRef
-        };
-
-        return result;
-
-    }
-
 
 }
