849284ad51647190127a5058b4d0c290cce4c6a5
polar-mckaylan <mckaylan@getpolarized.io>
2021-10-25 21:48:41 +0200

    Removed Adaptive Dialog, Created new Story

--- packages/polar-bookshelf/apps/stories/impl/MigrationToBlockAnnotations.tsx
+++ packages/polar-bookshelf/apps/stories/impl/MigrationToBlockAnnotations.tsx
@@ -3,42 +3,65 @@ import * as React from 'react';
 import {Box, Typography} from '@material-ui/core';
 import { LinearProgressWithLabel } from '../../../web/js/ui/dialogs/LinearProgressWithLabel';
 import { LogoAndTextSideBySide } from '../../repository/js/login/Authenticator';
+import Grid from '@material-ui/core/Grid';
 import { AdaptiveDialog } from '../../../web/js/mui/AdaptiveDialog';
 
 export const MigrationToBlockAnnotationsMainContent = () => {
 
     const [progress, setProgress] = React.useState(10);
 
-    return (  
-        <AdaptiveDialog>
+    const StoriesCard = () => (
+        <Grid item style={{flexGrow: 1, display: 'flex'}}>
                 <Box m={2} display='flex' textAlign={'center'} flexGrow={1} alignItems={'center'} justifyContent={'center'} flexDirection={'column'}>
-                    <Box m={2}>
-                        <LogoAndTextSideBySide/>
-                    </Box>
-                    
-                    <Typography variant="body1">
-                        <b>
-                            Just a Minute
-                        </b>
-                    </Typography>
-
-                    <Typography variant="body1">
-                        <Box m={1}>
-                            We're Migrating you to the latest version of Polar
-                        </Box>
-                        <Box m={2}>
-                            <LinearProgressWithLabel value={progress}/>
-                        </Box>
-                    </Typography>
-                        
-                    <Typography variant="caption">
-                        <Box m={2} textAlign={'center'} justifyContent={'center'}>
-                            As part of Polar's Updates, we're migrating the backend which can take up to a 
-                            couple minutes, depending on the size of your repository. This is a one time migration. 
-                            <Link> Learn More</Link>
-                        </Box>
-                    </Typography>
+                <Box m={2}>
+                    <LogoAndTextSideBySide/>
                 </Box>
-        </AdaptiveDialog>
+                
+                <Typography variant="body1">
+                    <b>
+                        Just a Minute
+                    </b>
+                </Typography>
+
+                <Typography variant="body1">
+                    <Box m={1}>
+                        We're Migrating you to the latest version of Polar
+                    </Box>
+                    <Box m={2}>
+                        <LinearProgressWithLabel value={progress}/>
+                    </Box>
+                </Typography>
+                    
+                <Typography variant="caption">
+                    <Box m={2} textAlign={'center'} justifyContent={'center'}>
+                        As part of Polar's Updates, we're migrating the backend which can take up to a 
+                        couple minutes, depending on the size of your repository. This is a one time migration. 
+                        <Link> Learn More</Link>
+                    </Box>
+                </Typography>
+            </Box>
+        </Grid>
+    )
+
+    return (
+            <div style={{
+                    display: 'flex',
+                    width: '100%',
+                    height: '100%'
+                }}>
+                    <Paper style={{
+                        margin: 'auto',
+                        maxWidth: '450px',
+                        minHeight: '450px',
+                        maxHeight: '650px',
+                        width: '100%',
+                        display: 'flex',
+                        flexDirection: 'column'
+                    }}>
+
+                    <StoriesCard/>
+
+                </Paper>
+            </div>
     )
 }
