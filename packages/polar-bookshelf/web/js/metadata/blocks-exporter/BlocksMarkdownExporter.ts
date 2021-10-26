f9d25c630470b5e7b0a33b73a48915c4eda7ba6a
WinterCore <hogobbl@gmail.com>
2021-10-26 12:56:07 +0200

    refactor(notes): Refactor markdown block converter & exporter

--- packages/polar-bookshelf/web/js/metadata/blocks-exporter/BlocksMarkdownExporter.ts
+++ packages/polar-bookshelf/web/js/metadata/blocks-exporter/BlocksMarkdownExporter.ts
@@ -1,11 +1,8 @@
-import {IBlockContentStructure, INamedContent} from "polar-blocks/src/blocks/IBlock";
-import {arrayStream} from "polar-shared/src/util/ArrayStreams";
+import {IBlockContentStructure} from "polar-blocks/src/blocks/IBlock";
 import {MarkdownStr} from "polar-shared/src/util/Strings";
-import {BlockContentStructureConverter} from "../../notes/BlockContentStructureConverter";
+import {BlockContentStructureMarkdownConverter} from "../../notes/BlockContentStructureMarkdownConverter";
 import {IBlockContentStructurePredicates} from "../../notes/store/IBlockContentStructurePredicates";
 import {IBlocksFormatExporter} from "./IBlocksFormatExporter";
-
-const INDENT_SPACE_COUNT = 4;
 
 export class BlocksMarkdownExporter implements IBlocksFormatExporter {
 
@@ -30,16 +27,10 @@ export class BlocksMarkdownExporter implements IBlocksFormatExporter {
         const named = structures.filter(IBlockContentStructurePredicates.isNamed);
         const others = structures.filter(structure => ! IBlockContentStructurePredicates.isNamed(structure));
 
-        const namedMarkdownContent = arrayStream(named)
-            .map(this.namedStructureToMarkdown.bind(this))
-            .filterPresent()
-            .collect();
+        const namedMarkdownContent = named
+            .map(structure => BlockContentStructureMarkdownConverter.toMarkdown([structure]));
 
-        const othersMarkdownContent = arrayStream(others)
-            .map(this.convert.bind(this, 0))
-            .filterPresent()
-            .collect();
-
+        const othersMarkdownStr = BlockContentStructureMarkdownConverter.toMarkdown(others);
 
         let markdownItems: MarkdownStr[] = [];
 
@@ -47,84 +38,11 @@ export class BlocksMarkdownExporter implements IBlocksFormatExporter {
             markdownItems.push(namedMarkdownContent.join('\n\n\n'));
         }
 
-        if (othersMarkdownContent.length > 0) {
-            markdownItems.push(`# Custom Blocks\n${othersMarkdownContent.join('\n')}`);
+        if (othersMarkdownStr.length > 0) {
+            markdownItems.push(`# Custom Blocks\n${othersMarkdownStr}`);
         }
 
         return markdownItems.join('\n\n');
     }
 
-    /**
-     * Generate a markdown representation of a named block content structure @see IBlockContentStructure
-     *
-     * This is the same as @see convert but this one treats named contents (e.g. document, name & date block contents)
-     * as headings when generating the markdown.
-     *
-     * @param level The starting markdown indentation level
-     * @param structure Block content structure object
-     */
-    private namedStructureToMarkdown(structure: IBlockContentStructure<INamedContent>): string {
-        const namedContentMarkdown = BlockContentStructureConverter.convertContentToMarkdown(structure.content);
-        const childrenMarkdown = arrayStream(structure.children)
-            .map(this.convert.bind(this, 0))
-            .filterPresent()
-            .collect()
-            .join('\n');
-
-        return `${namedContentMarkdown}\n${childrenMarkdown}`;
-    }
-
-    /**
-     * Generate the markdown representation of a block content structure @see IBlockContentStructure
-     *
-     * @param level The starting markdown indentation level
-     * @param structure Block content structure object
-     */
-    private convert(level: number, structure: IBlockContentStructure): string | undefined {
-        const markdown = BlockContentStructureConverter.convertContentToMarkdown(structure.content);
-
-        const hasContent = markdown.length > 0;
-
-        const current = `${this.getIndentSpaces(level)}- ${markdown}`;
-
-        // If the current block is empty just skip it and add its children on the same level
-        const newLevel = ! hasContent ? level : level + 1;
-
-        const children = arrayStream(structure.children)
-            .map(this.convert.bind(this, newLevel))
-            .filterPresent()
-            .collect();
-
-        const hasChildren = structure.children.length > 0;
-
-
-        if (! hasChildren) {
-            if (hasContent) {
-                return current;
-            } else {
-                return undefined;
-            }
-        }
-
-        if (! hasContent) {
-            if (hasChildren) {
-                return `${children.join('\n')}`;
-            } else {
-                return undefined;
-            }
-        }
-
-        return `${current}\n${children.join('\n')}`;
-    };
-
-    /**
-     * Generate spaces based on the given indentation level
-     *
-     * This is useful for creating sub bullet points when generating markdown.
-     *
-     * @param level The wanted indentation level
-     */
-    private getIndentSpaces(level: number): string {
-        return ' '.repeat(level * INDENT_SPACE_COUNT);
-    }
 }
--- packages/polar-bookshelf/web/js/notes/BlockContentStructureHTMLConverter.ts
+++ packages/polar-bookshelf/web/js/notes/BlockContentStructureHTMLConverter.ts
@@ -1,4 +1,4 @@
-import {HTMLStr, MarkdownStr} from "polar-shared/src/util/Strings";
+import {HTMLStr} from "polar-shared/src/util/Strings";
 import {IBlockContentStructure} from "polar-blocks/src/blocks/IBlock";
 import {MarkdownContentConverter} from "./MarkdownContentConverter";
 import {IBlockContent} from "polar-blocks/src/blocks/IBlock";
@@ -6,7 +6,7 @@ import {AnnotationContentType} from "polar-blocks/src/blocks/content/IAnnotation
 import {BlockTextContentUtils} from "./NoteUtils";
 import {FlashcardType} from "polar-shared/src/metadata/FlashcardType";
 
-export namespace BlockContentStructureConverter {
+export namespace BlockContentStructureHTMLConverter {
 
     export function convertContentToHTML(content: IBlockContent): HTMLStr {
         switch (content.type) {
@@ -48,56 +48,6 @@ export namespace BlockContentStructureConverter {
         }
     }
 
-    export function convertContentToMarkdown(content: IBlockContent): MarkdownStr {
-
-        switch (content.type) {
-            case 'date':
-            case 'name':
-                const titleMarkdown = BlockTextContentUtils.getTextContentMarkdown(content)
-                return `# ${titleMarkdown}`;
-
-            case 'document':
-                const documentTitle = BlockTextContentUtils.getTextContentMarkdown(content)
-                return `# *Document*: ${documentTitle}`;
-
-            case 'markdown':
-                const markdown = BlockTextContentUtils.getTextContentMarkdown(content)
-                return `${markdown}`;
-
-            case 'image':
-                const src = content.src;
-                return `![](${src})`;
-
-            case AnnotationContentType.AREA_HIGHLIGHT:
-                const { image } = content.value;
-                
-                if (! image) {
-                    return `<b>AREA HIGHLIGHT</b>: Missing image`;
-                }
-
-                // TODO: find a way to convert image.src to a URL without the extra deps
-                return `<b style="color: ${content.value.color}">■</b> <b>AREA HIGHLIGHT</b>:  \n![](https://i.giphy.com/media/2UCt7zbmsLoCXybx6t/giphy.webp) `;
-
-            case AnnotationContentType.TEXT_HIGHLIGHT:
-                const textHighlightMarkdown = BlockTextContentUtils.getTextContentMarkdown(content);
-                return `<b style="color: ${content.value.color}">■</b> <b>TEXT HIGHLIGHT</b>: ${textHighlightMarkdown}`;
-
-            case AnnotationContentType.FLASHCARD:
-                if (content.value.type === FlashcardType.CLOZE) {
-                    const { text } = content.value.fields;
-                    return `**FLASHCARD**: ${text}`;
-                } else {
-                    const { front, back } = content.value.fields;
-                    return `**FLASHCARD**: ${front} ➔ ${back}`;
-                }
-
-            default:
-                const _: never = content;
-                throw new Error('BlockContentStructureConverter.convertContentToMarkdown: Unhandled block content');
-
-        }
-
-    }
 
     export function toHTML(blocks: ReadonlyArray<IBlockContentStructure>): HTMLStr {
         const html = blocks
@@ -106,4 +56,5 @@ export namespace BlockContentStructureConverter {
 
         return blocks.length > 0 ? `<ul>${html}</ul>` : '';
     }
+
 }
