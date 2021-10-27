833d9ba3546a3758d8b4c6c426c1cca37648b381
Kevin Burton <burtonator@gmail.com>
2021-10-26 21:46:29 -0700

    implement chromatic ...

--- packages/polar-ui-framework/.storybook/preview.js
+++ packages/polar-ui-framework/.storybook/preview.js
@@ -1,3 +1,4 @@
+import { themes } from '@storybook/theming';
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -6,4 +7,11 @@ export const parameters = {
       date: /Date$/,
     },
   },
+  darkMode: {
+    // Override the default dark theme
+    dark: { ...themes.dark, appBg: 'black' },
+    // Override the default light theme
+    light: { ...themes.normal, appBg: 'red' }
+  }
 }
+
