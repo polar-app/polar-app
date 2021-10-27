86636dd0a0887a93cd7dcb830d0e9e26015f63a2
Kevin Burton <burtonator@gmail.com>
2021-10-26 22:25:52 -0700

    ...

--- packages/polar-ui-framework/.storybook/main.js
+++ packages/polar-ui-framework/.storybook/main.js
@@ -3,6 +3,8 @@
 // https://storybook.js.org/addons/@storybook/addon-viewport
 // https://storybook.js.org/addons/@storybook/addon-storyshots
 // https://storybook.js.org/addons/@storybook/addon-console
+// https://storybook.js.org/addons/@storybook/addon-actions/
+// https://storybook.js.org/addons/storybook-facelift/
 
 module.exports = {
   "stories": [
