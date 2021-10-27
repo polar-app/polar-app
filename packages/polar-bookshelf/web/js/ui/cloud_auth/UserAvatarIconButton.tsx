e1392ee453695e31fa6e7413d2e7ba06b1b9ce29
Hadi <hadi.abuhamed@gmail.com>
2021-10-27 20:10:13 +0300

    fixed

--- packages/polar-bookshelf/web/js/ui/cloud_auth/UserAvatarIconButton.tsx
+++ packages/polar-bookshelf/web/js/ui/cloud_auth/UserAvatarIconButton.tsx
@@ -6,11 +6,10 @@ interface UserAvatarIconButtonProps extends UserAvatarProps {
 }
 
 export const UserAvatarIconButton = React.memo(function UserAvatarIconButton(props: UserAvatarIconButtonProps) {
-    const newProps = {...props};
-    delete newProps['onClick'];
+
     return (
         <IconButton onClick={props.onClick} className={props.className} style={props.style}>
-            <UserAvatar {...newProps}/>
+            <UserAvatar size={props.size} className={props.className} style={props.style}/>
         </IconButton>
     )
 
