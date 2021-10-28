a6ee50c67d32fc083db154c2075dbcea28aafcfb
Kevin Burton <burtonator@gmail.com>
2021-10-28 13:16:00 -0700

    Fixed unused import...

--- packages/polar-bookshelf/web/js/notes/NoteScreen.tsx
+++ packages/polar-bookshelf/web/js/notes/NoteScreen.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import {Route, RouteComponentProps, Switch} from 'react-router-dom';
+import {Route, Switch} from 'react-router-dom';
 import {createStyles, LinearProgress, makeStyles} from '@material-ui/core';
 import {useBlocksStore} from './store/BlocksStore';
 import {JumpToNoteKeyboardCommand} from './JumpToNoteKeyboardCommand';
