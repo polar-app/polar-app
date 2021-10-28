4dac71cd8a77f16b85be15823fe88825395866e4
Kevin Burton <burtonator@gmail.com>
2021-10-27 23:03:49 -0700

    ...

---
+++ packages/polar-ui-framework/src/Logo/Logo.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {StoryAppRoot} from "../StoryAppRoot/StoryAppRoot";
+
+export interface LogoProps {
+
+}
+
+export const Logo = (props: LogoProps) => {
+    return (
+        <StoryAppRoot>
+            this is a fake logo
+        </StoryAppRoot>
+    );
+}
+
