62bc4bc78156bbbbe6b632ea812fb2ad8063f757
Kevin Burton <burtonator@gmail.com>
2021-10-29 11:56:27 -0700

    We have to disable the TerserPlugin on webpack..

--- packages/polar-web-extension/webpack.config.js
+++ packages/polar-web-extension/webpack.config.js
@@ -156,15 +156,15 @@ module.exports = {
     optimization: {
         // minimize: ! isDev,
         minimize: true,
-        minimizer: [new TerserPlugin({
-            // disable caching to:  node_modules/.cache/terser-webpack-plugin/
-            // because intellij will index this data and lock up my machine
-            // and generally waste space and CPU
-            // cache: ".terser-webpack-plugin",
-            terserOptions: {
-                output: { ascii_only: true },
-            }})
-        ],
+        // minimizer: [new TerserPlugin({
+        //     // disable caching to:  node_modules/.cache/terser-webpack-plugin/
+        //     // because intellij will index this data and lock up my machine
+        //     // and generally waste space and CPU
+        //     // cache: ".terser-webpack-plugin",
+        //     terserOptions: {
+        //         output: { ascii_only: true },
+        //     }})
+        // ],
         // usedExports: true,
         // removeAvailableModules: true,
         // removeEmptyChunks: true,
