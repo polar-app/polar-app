4605d8e64d84f8a42e71e678c34838b72018b8f3
Mohammed Atallah <mohammed@getpolarized.io>
2021-11-03 02:06:16 +0200

    add additional capture tests including twitter meta scheme

--- packages/polar-web-extension/src/capture/ExtensionContentCaptureTestK.ts
+++ packages/polar-web-extension/src/capture/ExtensionContentCaptureTestK.ts
@@ -3,7 +3,7 @@ import { assert } from 'chai';
 
 describe("Extension Capture", function() {
 
-    it("Basic capture", async () => {
+    it("Basic image capture", async () => {
         const imgURL = 'https://discord.com/assets/652f40427e1f5186ad54836074898279.png';
 
         document.head.innerHTML = `<meta property="og:image" content="${imgURL}">`;
@@ -12,5 +12,24 @@ describe("Extension Capture", function() {
 
         assert.equal(capture.image, imgURL);
     });
+    
+    it("Twitter meta scheme image capture", async () => {
+        const imgURL = 'https://twitter.com/';
 
+        document.head.innerHTML = `<meta property="twitter:image" content="${imgURL}">`;
+
+        const capture = ExtensionContentCapture.capture();
+
+        assert.equal(capture.image, imgURL);
+    });
+
+    it("Twitter meta scheme title capture", async () => {
+        const twitterTitle = 'twitter title';
+
+        document.head.innerHTML = `<meta property="twitter:title" content="${twitterTitle}">`;
+
+        const capture = ExtensionContentCapture.capture();
+
+        assert.equal(capture.title, twitterTitle);
+    });
 });
