6b5475c36949a46c18b3fc88e71ab67009b16fb4
Mohammed Atallah <mohammed@getpolarized.io>
2021-11-05 16:24:08 +0200

    refactor common parts for JSDOM async URL capture

--- packages/polar-web-capture/src/capture/ReadabilityCapture.ts
+++ packages/polar-web-capture/src/capture/ReadabilityCapture.ts
@@ -1,27 +1,37 @@
 import {HTMLSanitizer} from "polar-html/src/sanitize/HTMLSanitizer";
-import Readability from "readability";
-import {getMetadata} from "polar-page-metadata-parser/src/parsers/PageParser";
+import Readability, { ParseResult } from "readability";
+import {getMetadata, PageMetadata} from "polar-page-metadata-parser/src/parsers/PageParser";
 import {ExtensionContentCapture} from "./ExtensionContentCapture";
+import { JSDOM } from "jsdom";
 
 export namespace ReadabilityCapture {
 
     import ICapturedEPUB = ExtensionContentCapture.ICapturedEPUB;
 
-    export function capture(): ICapturedEPUB {
-        const url = document.location.href;
-        const doc = <Document> document.cloneNode(true);
+    interface CaptureParse {
+        readonly readable: ParseResult;
+        readonly metadata: PageMetadata;
+    };
 
-        function parseReadability() {
-            const readability = new Readability(doc);
-            return readability.parse();
-        }
+    function parseReadability(doc: Document) {
+        const readability = new Readability(doc);
+        return readability.parse();
+    }
 
-        function parseMetadata() {
-            return getMetadata(doc, url);
-        }
+    function parseMetadata(doc: Document, url: string) {
+        return getMetadata(doc, url);
+    }
 
-        const readable = parseReadability();
-        const metadata = parseMetadata();
+    async function fetchDocument(URL: string): Promise<Document> {
+        const dom = await JSDOM.fromURL(URL);
+
+        return <Document> dom.window.document;
+    }
+
+    function parseCapture(doc: Document, url: string) : CaptureParse {
+        const readable = parseReadability(doc);
+
+        const metadata = parseMetadata(doc, url);
 
         if (! readable) {
             throw new Error("Readability parse failed.");
@@ -31,6 +41,15 @@ export namespace ReadabilityCapture {
             throw new Error("No readable content");
         }
 
+        return {
+            readable,
+            metadata
+        };
+    }
+
+    function extractCapturedEPUB(doc: Document, url: string) {
+        const { readable, metadata } = parseCapture(doc, url);
+
         const sanitized = HTMLSanitizer.sanitizePortableDocument(readable.content);
 
         return {
@@ -39,7 +58,20 @@ export namespace ReadabilityCapture {
             text: readable.textContent,
             content: sanitized
         }
-
     }
 
+    
+    export function capture(): ICapturedEPUB {
+        const url = document.location.href;
+
+        const doc = <Document> document.cloneNode(true);
+
+        return extractCapturedEPUB(doc, url);
+    }
+
+    export async function captureURL(URL: string): Promise<ICapturedEPUB> {
+        const doc = await fetchDocument(URL);
+
+        return extractCapturedEPUB(doc, URL);
+    }
 }
