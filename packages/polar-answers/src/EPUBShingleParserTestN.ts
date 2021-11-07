54c35e9d01abfa51d077892498d447754fce7bf7
Kevin Burton <burtonator@gmail.com>
2021-11-07 13:37:05 -0800

    Fixed some errors in timeouts... I don't think this is the root cause though.

--- packages/polar-answers/src/EPUBShingleParserTestN.ts
+++ packages/polar-answers/src/EPUBShingleParserTestN.ts
@@ -3,11 +3,11 @@ import { EPUBShingleParser } from './EPUBShingleParser';
 
 describe("EPUB Shingle Parser", function() {
 
-    this.timeout(60000);
+    this.timeout(120000);
 
     it("basic", async () => {
         const path = FilePaths.resolve(__dirname, '../../polar-epub/alice.epub');
-        
+
         await EPUBShingleParser.parse(path);
     });
 });
