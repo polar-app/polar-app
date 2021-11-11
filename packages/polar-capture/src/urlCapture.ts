81ecb910bf417eb68fea98eae90d7ee28da6e95f
Mohammed Atallah <mohammed@getpolarized.io>
2021-11-11 18:54:55 +0200

    url capture pdfs/epubs -- wip

--- packages/polar-capture/src/urlCapture.ts
+++ packages/polar-capture/src/urlCapture.ts
@@ -1,12 +1,39 @@
 import { JSDOM } from "jsdom";
 import { ReadabilityCapture } from "polar-web-capture/src/capture/ReadabilityCapture";
-
+import { CapturedContentEPUBGenerator } from "polar-web-capture/src/captured/CapturedContentEPUBGenerator";
+import fetch from 'node-fetch';
 export namespace urlCapture {
-    export async function fetchDocument(url: string) {
-        const dom = await JSDOM.fromURL(url);
+    const PDF_TYPE = "application/pdf";
+
+    export async function fetchUrl(url: string): Promise<void> {
+        const response = await fetch(url);
+
+        if (response.headers.get('content-type') === PDF_TYPE) {
+            for await (const chunk of response.body) {
+                console.dir( chunk.toString() );
+            }
+        } else {
+            generateEPUB(await response.text(), url);
+        }
+    }
+
+    export async function generateEPUB(html: string, url: string): Promise<ArrayBuffer> {
+        const dom = new JSDOM(html);
         
         const doc = <Document> dom.window.document;
 
-        console.log(ReadabilityCapture.extractCapturedEPUB(doc, url));
+        const epubCapture = ReadabilityCapture.extractCapturedEPUB(doc, url);
+
+        return await CapturedContentEPUBGenerator.generate(epubCapture);
+    }
+}
+
+export namespace storeCapture {
+    export function saveCaptureAsPDF(blob: Blob, url: string) {
+        // TODO: save blob in Datastore
+    }
+
+    export function saveCaptureAsEPUB() {
+        // TODO: save generated epub
     }
 }
--- packages/polar-capture/src/urlCaptureTestN.ts
+++ packages/polar-capture/src/urlCaptureTestN.ts
@@ -1,12 +1,10 @@
-// import { assert } from 'chai';
+import { assert } from 'chai';
 import { urlCapture } from './urlCapture';
 
 
 describe("Node URL capture", () => {
 
     it("Can capture a webpage using it URL", async () => {
-        const res = await urlCapture.fetchDocument("https://based.cooking/italian-bread.html");
-
-        console.log(res);
+        await urlCapture.fetchUrl("https://arxiv.org/pdf/1912.06680.pdf");
     });
 });
