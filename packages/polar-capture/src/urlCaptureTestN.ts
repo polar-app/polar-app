b1abdb2455b416707e043255f7fe2e18247326d1
Mohammed Atallah <mohammed@getpolarized.io>
2021-11-12 21:19:44 +0200

    add additional capture test case setup.. awaiting datastore updates

--- packages/polar-capture/src/urlCaptureTestN.ts
+++ packages/polar-capture/src/urlCaptureTestN.ts
@@ -4,7 +4,11 @@ import { urlCapture } from './urlCapture';
 
 describe("Node URL capture", () => {
 
-    it("Can capture a webpage using it URL", async () => {
+    it("Can capture a webpage using it URL and parse it as an epub", async () => {
+        await urlCapture.fetchUrl("https://based.cooking");
+    });
+
+    it("Can capture a PDF using it URL", async () => {
         await urlCapture.fetchUrl("https://arxiv.org/pdf/1912.06680.pdf");
     });
 });
