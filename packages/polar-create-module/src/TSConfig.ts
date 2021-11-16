0f92eabc88d86e693256c454a626c93298d3324d
aeweda <mraeweda@gmail.com>
2021-11-16 19:44:18 +0200

    update modules tsconfig

--- packages/polar-create-module/src/TSConfig.ts
+++ packages/polar-create-module/src/TSConfig.ts
@@ -1,7 +1,22 @@
 export namespace TSConfig {
     export function create() {
         return {
-            "extends": "../../tsconfig.json"
+            "extends": "../../tsconfig.json",
+            "types": [
+              "mocha",
+              "reflect-metadata"
+            ],
+            "include": [
+              "**/*.ts",
+              "**/*.tsx"
+            ],
+            "exclude": [
+              "node_modules/**",
+              "dist",
+              "*.d.ts",
+              "*.js",
+              "cdk.out"
+            ]
         }
     }
 }
