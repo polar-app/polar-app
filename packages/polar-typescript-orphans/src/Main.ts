c2bcc7f1f9b896fa5297c21ba741a4a910282f66
Kevin Burton <burtonator@gmail.com>
2021-11-16 23:31:07 -0500

    don't purge stories.. remove them in a separate step.

--- packages/polar-typescript-orphans/src/Main.ts
+++ packages/polar-typescript-orphans/src/Main.ts
@@ -1,6 +1,6 @@
 import {OrphanFinder} from "./OrphanFinder";
 import {Files} from "polar-shared/src/util/Files";
-import { FilePaths } from "polar-shared/src/util/FilePaths";
+import {FilePaths} from "polar-shared/src/util/FilePaths";
 import * as fs from "fs";
 import {PathStr} from "polar-shared/src/util/Strings";
 import {IModuleReference} from "./IModuleReference";
@@ -132,14 +132,13 @@ async function doAsync() {
     console.log("====================================")
     console.log(createModuleReport());
 
-
     const testsFilter = [
-        "Test.ts$",
-        "TestN.ts$",
-        "TestK.ts$",
-        "TestNK.ts$",
-        "TestKN.ts$",
-        "Story.tsx",
+        "Test\.ts$",
+        "TestN\.ts$",
+        "TestK\.ts$",
+        "TestNK\.ts$",
+        "TestKN\.ts$",
+        // "Story\.tsx$",
     ]
 
     const entriesFilter = [
