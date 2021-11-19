aeb46cb45d100ab0cd54b0f8664cf86d7477d995
Kevin Burton <burtonator@gmail.com>
2021-11-19 14:29:16 -0500

    remove react-hot-keys

--- packages/polar-bookshelf/web/js/mui/complete_listeners/InputEscapeListener.tsx
+++ packages/polar-bookshelf/web/js/mui/complete_listeners/InputEscapeListener.tsx
@@ -1,6 +1,5 @@
 import * as React from "react";
 import {Callback} from "polar-shared/src/util/Functions";
-import {GlobalHotKeys} from "react-hotkeys";
 import {deepMemo} from "../../react/ReactUtils";
 
 const globalKeyMap = {
@@ -19,7 +18,7 @@ interface IProps {
  */
 export const InputEscapeListener = deepMemo(function InputEscapeListener(props: IProps) {
 
-    const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    const handleKeyDown = React.useCallback((event: React.KeyboardEvent | KeyboardEvent) => {
 
         if (event.key === 'Escape') {
             props.onEscape();
@@ -28,17 +27,18 @@ export const InputEscapeListener = deepMemo(function InputEscapeListener(props: 
             return;
         }
 
-    };
+    }, [props]);
 
-    const handlers = {
-        ESCAPE: () => props.onEscape()
-    };
+    React.useEffect(() => {
 
-    return (
-        <div onKeyDown={handleKeyDown}>
-            <GlobalHotKeys allowChanges keyMap={globalKeyMap} handlers={handlers}/>
-            {props.children}
-        </div>
-    );
+        window.addEventListener('keydown', handleKeyDown);
+
+        return () => {
+            window.removeEventListener('keydown', handleKeyDown);
+        }
+
+    }, [handleKeyDown])
+
+    return props.children;
 
 });
