8fa8e9457f72c245df4b148896099a8c23f2578b
Mohammed Atallah <mohammed@getpolarized.io>
2021-11-23 16:13:51 +0200

    add generating metadata

--- packages/polar-capture/src/urlCapture.ts
+++ packages/polar-capture/src/urlCapture.ts
@@ -2,19 +2,44 @@ import { JSDOM } from "jsdom";
 import { ReadabilityCapture } from "polar-web-capture/src/capture/ReadabilityCapture";
 import { CapturedContentEPUBGenerator } from "polar-web-capture/src/captured/CapturedContentEPUBGenerator";
 import fetch from 'node-fetch';
+import { writeFileSync, mkdtempSync} from "fs";
+import { EPUBMetadataUsingNode } from "polar-epub/src/EPUBMetadataUsingNode";
+import { PDFMetadata } from "polar-pdf/src/pdf/PDFMetadata";
 export namespace urlCapture {
+
+    type CaptureType = 'pdf' | 'epub';
+
     const PDF_TYPE = "application/pdf";
+
 
     export async function fetchUrl(url: string): Promise<void> {
         const response = await fetch(url);
 
+        let path;
+        
         if (response.headers.get('content-type') === PDF_TYPE) {
-            for await (const chunk of response.body) {
-                console.dir( chunk.toString() );
-            }
+            const pdf = await response.arrayBuffer();
+
+            path = writeTempFile(pdf, 'pdf');
+            
+            console.log(await PDFMetadata.getMetadata(path));
         } else {
-            generateEPUB(await response.text(), url);
+            const epub = await generateEPUB(await response.text(), url);
+
+            path = writeTempFile(epub, 'epub');
+            
+            console.log(await EPUBMetadataUsingNode.getMetadata(path));
         }
+    }
+
+    function writeTempFile(arrayBuffer: ArrayBuffer, type: CaptureType): string {
+        const file = new Uint8Array(arrayBuffer);
+
+        const relativePath = mkdtempSync('temp-') + `temp.${type}`
+
+        writeFileSync(relativePath, file);
+
+        return `${__dirname}/../${relativePath}`;
     }
 
     export async function generateEPUB(html: string, url: string): Promise<ArrayBuffer> {
