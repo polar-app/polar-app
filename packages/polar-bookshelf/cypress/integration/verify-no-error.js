6a1fd5661db3d0c41e793faad8eb6b82c96d6151
Kevin Burton <burtonator@gmail.com>
2021-11-23 23:20:47 -0500

    Verify that we have no errors...

--- packages/polar-bookshelf/cypress/integration/verify-no-error.js
+++ packages/polar-bookshelf/cypress/integration/verify-no-error.js
@@ -2,7 +2,7 @@ describe('Verify we do not have any errors', () => {
 
     beforeEach(() => {
         // run the following "it" tests with the specified width/height viewport
-        // cy.viewport(Cypress.env('WIDTH'), Cypress.env('HEIGHT'));
+        cy.viewport(Cypress.env('WIDTH'), Cypress.env('HEIGHT'));
 
         cy.clearLocalStorage()
         cy.clearCookies();
