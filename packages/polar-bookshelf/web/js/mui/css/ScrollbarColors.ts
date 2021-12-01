bc5fbc0aa52e992d73a2bdbd1e7af070bdfb46c9
Kevin Burton <burtonator@gmail.com>
2021-12-01 10:29:26 -0700

    Fix for scrollbar regression on desktop

--- packages/polar-bookshelf/web/js/mui/css/ScrollbarColors.ts
+++ packages/polar-bookshelf/web/js/mui/css/ScrollbarColors.ts
@@ -1,8 +1,49 @@
 import {Theme} from "@material-ui/core";
+import {Devices} from "polar-shared/src/util/Devices";
 
 namespace ScrollbarColors {
 
-    export function createCSSForReact(theme: Theme) {
+    function createCSSForReactDesktop(theme: Theme) {
+        const isDark = theme.palette.type === 'dark';
+        const isLight = theme.palette.type === 'light';
+
+        // https://css-tricks.com/almanac/properties/s/scrollbar-color/
+        // https://stackoverflow.com/questions/65940522/how-do-i-switch-to-chromes-dark-scrollbar-like-github-does
+
+        return {
+            '*::-webkit-scrollbar': {
+                width: '12px !important'
+            },
+            '*::-webkit-scrollbar-track': {
+                // '-webkit-box-shadow': 'inset 0 0 5px rgba(255, 255, 255, 0.3'
+                ...(isDark &&
+                    { backgroundColor: '#494949 !important' }),
+
+                ...(isLight &&
+                    { backgroundColor: `${theme.palette.background.paper} !important` }),
+            },
+            '*::-webkit-scrollbar-thumb': {
+                borderRadius: '10px!important',
+                ...(isDark &&
+                    {
+                        backgroundColor: `${theme.palette.grey['600']} !important`,
+                        border: 'solid 2px #494949 !important',
+                    }),
+
+                ...(isLight &&
+                    {
+                        backgroundColor: `${theme.palette.grey['400']} !important`,
+                        border: `solid 2px ${theme.palette.background.paper} !important`,
+                    }),
+            },
+            ':root': {
+                colorScheme: theme.palette.type,
+                scrollbarColor: theme.palette.type
+            }
+        };
+    }
+
+    function createCSSForReactMobile(theme: Theme) {
 
         // https://css-tricks.com/almanac/properties/s/scrollbar-color/
         // https://stackoverflow.com/questions/65940522/how-do-i-switch-to-chromes-dark-scrollbar-like-github-does
@@ -13,9 +54,57 @@ namespace ScrollbarColors {
                 scrollbarColor: theme.palette.type
             }
         };
+
     }
 
-    export function createCSS(theme: Theme) {
+    export function createCSSForReact(theme: Theme) {
+
+        if (Devices.isDesktop()) {
+            return createCSSForReactDesktop(theme);
+        }
+
+        return createCSSForReactMobile(theme);
+
+    }
+
+    function createCSSForDesktop(theme: Theme) {
+        const isDark = theme.palette.type === 'dark';
+        const isLight = theme.palette.type === 'light';
+
+        return {
+            '*::-webkit-scrollbar': {
+                width: '12px!important'
+            },
+            '*::-webkit-scrollbar-track': {
+                // '-webkit-box-shadow': 'inset 0 0 5px rgba(255, 255, 255, 0.3'
+                ...(isDark &&
+                    { 'background-color': '#494949!important' }),
+
+                ...(isLight &&
+                    { 'background-color': `${theme.palette.background.paper}!important` }),
+            },
+            '*::-webkit-scrollbar-thumb': {
+                'border-radius': '10px!important',
+                ...(isDark &&
+                    {
+                        'background-color': `${theme.palette.grey['600']}!important`,
+                        'border': 'solid 2px #494949!important',
+                    }),
+
+                ...(isLight &&
+                    {
+                        'background-color': `${theme.palette.grey['400']}!important`,
+                        'border': `solid 2px ${theme.palette.background.paper}!important`,
+                    }),
+            },
+            ':root': {
+                "color-scheme": theme.palette.type,
+                "scrollbar-color": theme.palette.type
+            }
+        };
+    }
+
+    function createCSSForMobile(theme: Theme) {
 
         return {
             ':root': {
@@ -24,6 +113,17 @@ namespace ScrollbarColors {
             }
         };
     }
+
+    export function createCSS(theme: Theme) {
+
+        if (Devices.isDesktop()) {
+            return createCSSForDesktop(theme);
+        }
+
+        return createCSSForMobile(theme);
+
+    }
+
 }
 
 export default ScrollbarColors;
