80564ad83430c4ea0778b279f3619fd91757c636
Kevin Burton <burtonator@gmail.com>
2021-12-03 11:24:59 -0700

    This test runs but the results are wrong.

--- packages/polar-bookshelf/apps/repository/js/notes_repo/NotesRepoTable2TestK.tsx
+++ packages/polar-bookshelf/apps/repository/js/notes_repo/NotesRepoTable2TestK.tsx
@@ -1,13 +1,38 @@
 import React from 'react'
-import {render, screen, waitFor} from '@testing-library/react'
+import {fireEvent, render, screen, waitFor} from '@testing-library/react'
 import {NotesRepoTable2, TableGridStoreProvider} from "./NotesRepoTable2";
-import {BlocksStoreProvider} from '../../../../web/js/notes/store/BlocksStore';
+import {BlocksStoreProvider, useBlocksStore} from '../../../../web/js/notes/store/BlocksStore';
 import {BlockStoreContextProvider} from "../../../../web/js/notes/store/BlockStoreContextProvider";
 import {assertJSON} from "polar-test/src/test/Assertions";
 import {createRenderSnapshotAndReset} from "../../../../web/js/profiler/ProfiledComponents";
 import {assert} from 'chai';
+import Button from '@material-ui/core/Button';
 
 describe("NotesRepoTable2", function() {
+
+    const BlockStoreMutator = () => {
+
+        const blocksStore = useBlocksStore();
+        const [deleted, setDeleted] = React.useState(false);
+
+        const handleClick = React.useCallback(() => {
+
+            const block = blocksStore.getBlockByName("World War II");
+
+            if (block) {
+                blocksStore.deleteBlocks([block.id]);
+                setDeleted(true)
+            }
+
+        }, []);
+
+        return (
+            <>
+                {deleted && <div>Deleted</div>}
+                <Button onClick={handleClick}>Delete Block</Button>
+            </>
+        );
+    }
 
     const Test = () => {
         return (
@@ -15,6 +40,7 @@ describe("NotesRepoTable2", function() {
                 <BlocksStoreProvider>
                     <TableGridStoreProvider>
                         <NotesRepoTable2/>
+                        <BlockStoreMutator/>
                     </TableGridStoreProvider>
                 </BlocksStoreProvider>
             </BlockStoreContextProvider>
@@ -57,6 +83,29 @@ describe("NotesRepoTable2", function() {
             },
             {
                 "id": "NotesRepoTable2",
+                "phase": "update"
+            },
+            {
+                "id": "NotesRepoTable2",
+                "phase": "update"
+            },
+            {
+                "id": "NotesRepoTable2",
+                "phase": "update"
+            }
+        ]);
+
+        fireEvent.click(screen.getByText('Delete Block'))
+
+        await waitFor(() => screen.getByText("Deleted"))
+
+        assertJSON(createRenderSnapshotAndReset(), [
+            {
+                "id": "NotesRepoTable2",
+                "phase": "update"
+            },
+            {
+                "id": "NotesRepoTableToolbar",
                 "phase": "update"
             },
             {
