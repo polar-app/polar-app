8322bc11a2bada9fd0a2bb4b41232afca99e4330
Mohammed Atallah <mohammed@getpolarized.io>
2021-12-06 18:22:38 +0200

    update tmp bucket and path

--- packages/polar-capture/src/fileUpload.ts
+++ packages/polar-capture/src/fileUpload.ts
@@ -1,24 +1,34 @@
-import { Datastores } from "polar-hooks/functions/impl/datastore/Datastores";
+import { ServiceAccounts } from "polar-firebase-admin/src/FirebaseAdmin";
+import { File, Storage, Bucket} from '@google-cloud/storage';
+import { FirebaseConfig } from 'polar-firebase-admin/src/FirebaseConfig';
 import { Hashcodes } from 'polar-shared/src/util/Hashcodes';
-import { File } from '@google-cloud/storage'
+
 export namespace FileUpload {
+    type CaptureExtension = 'pdf' | 'epub';
 
-    export function init() {
+    async function getBucket(): Promise<Bucket> {
+        const config = FirebaseConfig.create()!;
 
-        const { config, storage} = Datastores.createStorage();
-    
-        const tmpName = "testcapture.pdf";
+        const bucketName = `gs://tmp-${config.project}`;
 
-        const bucketName = `gs://${config.project}.appspot.com`;
+        const storage = new Storage(ServiceAccounts.toStorageOptions(config.serviceAccount));
 
         const bucket = storage.bucket(bucketName);
 
-        const path = `tmpCapture/${tmpName}`;
+        return bucket;
+    }
+
+    export async function init(extension: CaptureExtension) {
+        const bucket = await getBucket();
+
+        const filename = `${Hashcodes.createRandomID()}.${extension}`;
+
+        const path = `captures/${filename}`;
 
         const file = new File(bucket, path);
 
-        const stream = file.createWriteStream();
+        const uploadStream = file.createWriteStream();
 
-        return {path, file, stream};
+        return { file, uploadStream };
     }
 }
