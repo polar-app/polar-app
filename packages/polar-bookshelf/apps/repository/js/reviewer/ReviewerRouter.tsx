df08cf7e5d5157c6f63b81871e3432f79c75d241
Kevin Burton <burtonator@gmail.com>
2021-12-11 09:19:32 -0700

    another big code purge...

--- packages/polar-bookshelf/apps/repository/js/reviewer/ReviewerRouter.tsx
+++ packages/polar-bookshelf/apps/repository/js/reviewer/ReviewerRouter.tsx
@@ -1,11 +1,9 @@
-import {useAnnotationRepoStore} from "../annotation_repo/AnnotationRepoStore";
 import {observer} from "mobx-react-lite";
 import {Route, Switch} from "react-router-dom";
 import {ReactRouters} from "../../../../web/js/react/router/ReactRouters";
-import {BlockReviewerScreen, DocAnnotationReviewerScreen} from "./ReviewerScreen";
+import {BlockReviewerScreen} from "./ReviewerScreen";
 import * as React from "react";
 import {useBlocksAnnotationRepoStore} from "../block_annotation_repo/BlocksAnnotationRepoStore";
-import {useNotesIntegrationEnabled} from "../../../../web/js/notes/NoteUtils";
 
 export const BlocksReviewRouter = observer(() => {
     const store = useBlocksAnnotationRepoStore();
@@ -29,35 +27,7 @@ export const BlocksReviewRouter = observer(() => {
     );
 });
 
-export const DocAnnotationReviewRouter = () => {
-    const store = useAnnotationRepoStore(['view']);
-
-    return (
-
-        <Switch location={ReactRouters.createLocationWithHashOnly()}>
-
-
-            <Route path='#review-flashcards'>
-                <DocAnnotationReviewerScreen mode="flashcard"
-                                             annotations={store.view}/>
-            </Route>
-
-            <Route path='#review-reading'>
-                <DocAnnotationReviewerScreen mode="reading"
-                                             annotations={store.view}/>
-            </Route>
-
-        </Switch>
-
-    );
-};
 
 export const ReviewRouter = () => {
-    const notesIntegrationEnabled = useNotesIntegrationEnabled();
-
-    if (notesIntegrationEnabled) {
-        return <BlocksReviewRouter />
-    }
-
-    return <DocAnnotationReviewRouter />
+    return <BlocksReviewRouter />
 };
