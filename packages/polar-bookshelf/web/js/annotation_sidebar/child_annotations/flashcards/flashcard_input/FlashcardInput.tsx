ee7975bef96e3bd3eb20168bd12975a6bc3ea652
Kevin Burton <burtonator@gmail.com>
2021-12-13 10:23:37 -0700

    moved to dedicated component

--- packages/polar-bookshelf/web/js/annotation_sidebar/child_annotations/flashcards/flashcard_input/FlashcardInput.tsx
+++ packages/polar-bookshelf/web/js/annotation_sidebar/child_annotations/flashcards/flashcard_input/FlashcardInput.tsx
@@ -4,22 +4,7 @@ import {FlashcardInputFieldsType} from './FlashcardInputs';
 import {FlashcardInputForCloze} from './FlashcardInputForCloze';
 import {FlashcardInputForFrontAndBack} from './FlashcardInputForFrontAndBack';
 import {Flashcard} from 'polar-shared/src/metadata/Flashcard';
-
-export const getDefaultFlashcardType = () => {
-    const defaultFlashcardType = window.localStorage.getItem('default-flashcard-type');
-
-    switch (defaultFlashcardType) {
-
-        case FlashcardType.BASIC_FRONT_BACK:
-            return FlashcardType.BASIC_FRONT_BACK;
-
-        case FlashcardType.CLOZE:
-            return FlashcardType.CLOZE;
-
-        default:
-            return FlashcardType.BASIC_FRONT_BACK;
-    }
-};
+import {getDefaultFlashcardType} from "./GetDefaultFlashcardType";
 
 export interface IProps {
 
