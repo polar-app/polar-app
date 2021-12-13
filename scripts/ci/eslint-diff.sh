247fd820ae55f3f4e0f01b03809568c1455c8c4d
aeweda <mraeweda@gmail.com>
2021-12-13 16:26:21 +0200

    add isolated scripts

---
+++ scripts/ci/eslint-diff.sh
@@ -0,0 +1,15 @@
+FORK_POINT=$(git merge-base origin/master $CIRCLE_BRANCH)
+git reset --hard
+git checkout $FORK_POINT
+sed -i 's#"functional/prefer-readonly-type": "off"#"functional/prefer-readonly-type": "error"#g' ./.eslintrc.json
+pnpm run eslint-ci-all >> forkpoint-output.txt || true
+git reset --hard
+git checkout $CIRCLE_BRANCH
+sed -i 's#"functional/prefer-readonly-type": "off"#"functional/prefer-readonly-type": "error"#g' ./.eslintrc.json
+pnpm run eslint-ci-all >> branch-output.txt || true
+sed -i '/Error - /!d' forkpoint-output.txt
+sed -i '/Error - /!d' branch-output.txt
+OUTPUT=$(comm -13 <(sort ./forkpoint-output.txt) <(sort ./branch-output.txt))
+RED='\033[0;31m'
+NC='\033[0m'
+if [ ! -z "$OUTPUT" ]; then echo -e "\n ${RED} Please Fix the Following Errors: \n ${NC} $OUTPUT" && exit 1; fi;
---
+++ scripts/ci/publish-npm-packages.sh
@@ -0,0 +1,5 @@
+npm config set always-auth true
+source packages/polar-bookshelf-secrets/credentials.sh
+rm -f ~/.npmrc && cat .npmrc > ~/.npmrc
+pnpm run publish-all
+(cd packages/polar-webapp-dist && pnpm run dist)
