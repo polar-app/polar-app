e62bc4cc19933c9f75086dd5db69a9871e25733d
Kevin Burton <burtonator@gmail.com>
2021-12-15 10:24:26 -0700

    Updated edge case for persistence writer.

--- packages/polar-bookshelf/web/js/notes/persistence/FirestoreBlocksPersistenceWriter.tsx
+++ packages/polar-bookshelf/web/js/notes/persistence/FirestoreBlocksPersistenceWriter.tsx
@@ -68,26 +68,49 @@ export namespace FirestoreBlocksPersistenceWriter {
                         'mutation',
                     ];
 
+                    // TODO: there's a bug here that we need to fix in the future. If the user updates the
+                    // same block twice (probably due to an offline-operation), then what happens is that
+                    // the last writer wins and overwrites the created and updated fields.
+                    //
+                    // The only way I can think of fixing this would be to:
+                    //
+                    // - Have Firestore handle this no the client which isn't supported yet because it doesn't
+                    //   properly support UPSERTs, ON DUPLUCATE KEY UPDATE semantics, or any type of CAS operations
+                    //   that would support this.
+                    //
+                    // - Implement a trigger on the backend to detect this and then now allow created to be changed.
+                    //
+
                     batch.set(doc, firestoreBlock, {
                         mergeFields: [...mergeFields]
                     });
 
-                    if (Object.keys(firestoreBlock.items).length > 0) {
+                    const itemsKeys = Object.keys(firestoreBlock.items);
 
-                        // TODO: I don't think this is actually ever DONE in practice
-                        // but that we had it setup for tests but that the test data
-                        // is invalid.
+                    if (itemsKeys.length > 0) {
 
-                        batch.set(doc, firestoreBlock, {
-                            mergeFields: ['items']
-                        });
+                        // If the user is adding more items, we will just set
+                        // them into the items array rather than overwriting the
+                        // items array as this is probably what the user intends
+                        // but I think we need to tighten up the mutations here
+                        // because there are too many edge cases and if we reduce
+                        // the operations there will be less to test.
+
+                        for (const itemKey of itemsKeys) {
+                            const itemValue = firestoreBlock.items[itemKey];
+                            batch.update(doc, `items.${itemKey}`, itemValue);
+                        }
 
                     } else {
 
-                        // this is a bit of a hack and what we're doing here is
-                        // creating the 'items' as an empty array when it DOES
-                        // not exist, OR, if it DOES exist, then we update it by
-                        // adding items from the empty union which is a noop.
+                        // HACK: this is a bit of a hack to get Firestore to create an empty items
+                        // object / array rather than overwrite.  It handles the following scenarios:
+                        //
+                        // - When there is no existing record in the DB, it will create an empty items
+                        //   array.
+                        //
+                        // - When a record ALREADY exists in the DB, it doesn't overwrite it and this
+                        //   is a noop.  Whatever 'items' already exist are never modified.
                         batch.update(doc, 'items.0', '');
                         batch.update(doc, 'items.0', firebase.firestore.FieldValue.delete());
 
