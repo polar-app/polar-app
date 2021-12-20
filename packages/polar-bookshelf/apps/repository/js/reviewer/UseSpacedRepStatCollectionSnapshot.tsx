d46f234a2e8e529bd118263e651cc3cfbc7fb8e6
Kevin Burton <burtonator@gmail.com>
2021-12-20 16:12:47 -0700

    migrating this to using snapshots works now...

--- packages/polar-bookshelf/apps/repository/js/reviewer/UseSpacedRepStatCollectionSnapshot.tsx
+++ packages/polar-bookshelf/apps/repository/js/reviewer/UseSpacedRepStatCollectionSnapshot.tsx
@@ -1,30 +1,25 @@
 import {createFirestoreSnapshotForUserCollection} from "../../../../web/js/stores/FirestoreSnapshotStore";
 import {
-    ISpacedRepStatRecord,
     SpacedRepStatCollection,
+    SpacedRepStatRecord,
     StatType
 } from "polar-firebase/src/firebase/om/SpacedRepStatCollection";
-import React from "react";
 import {RepetitionMode} from "polar-spaced-repetition-api/src/scheduler/S2Plus/S2Plus";
 
 export const [SpacedRepStatCollectionSnapshotProvider, useSpacedRepStatCollectionSnapshot]
-    = createFirestoreSnapshotForUserCollection<ISpacedRepStatRecord>(SpacedRepStatCollection.COLLECTION_NAME);
+    = createFirestoreSnapshotForUserCollection<SpacedRepStatRecord>(SpacedRepStatCollection.COLLECTION_NAME);
 
-export function useSpacedRepCollectionSnapshotForModeAndType() {
+export function useSpacedRepCollectionSnapshotForModeAndType(mode: RepetitionMode, type: StatType) {
 
     const snapshot = useSpacedRepStatCollectionSnapshot();
 
-    return React.useCallback((mode: RepetitionMode, type: StatType) => {
-
-        if (snapshot.right) {
-            return snapshot.right.docs.filter(current => {
-                const data = current.data();
-                return data.mode === mode && data.type === type;
-            })
-        } else {
-            throw snapshot.left;
-        }
-
-    }, [snapshot.left, snapshot.right])
+    if (snapshot.right) {
+        return snapshot.right.docs.filter(current => {
+            const data = current.data();
+            return data.mode === mode && data.type === type;
+        })
+    } else {
+        throw snapshot.left;
+    }
 
 }
