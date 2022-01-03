38db2de7c8fc902d59d98d507a30d395b995d32c
Mohammed Atallah <mohammed@getpolarized.io>
2022-01-03 17:21:18 +0200

    add polar-anki-export module

---
+++ packages/polar-anki-export/src/FlashCardTestN.ts
@@ -0,0 +1,24 @@
+const { APKG } = require("anki-apkg");
+
+describe("Flashcard export", async () => {
+    it("generates flashcard", () => {
+        const apkg = new APKG({
+            name: 'VocabularyBuilder',
+            card: {
+                fields: ['word', 'meaning', 'usage'],
+                template: {
+                    question: '{{word}}',
+                    answer: `
+                        <div class="word">{{word}}</div>
+                        <div class="meaning">{{meaning}}</div>
+                        <div class="usage">{{usage}}</div>
+                    `
+                }
+            }
+        })
+    apkg.addCard({
+        content: ['sample word', 'sample meaning', 'sample usage']
+    })
+    apkg.save(__dirname)
+    })
+});
