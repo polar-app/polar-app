d99eb87a187d8a25b02b7e7cf3a3f29a94a1b217
aeweda <mraeweda@gmail.com>
2022-01-04 21:52:36 +0200

    ci: fix script not exiting 1 properly

--- scripts/ci/deploy-to-firebase.sh
+++ scripts/ci/deploy-to-firebase.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-(cd /home/circleci/project/packages/polar-hooks && echo Pushing to target ${FIREBASE_HOSTING_TARGET} && firebase use polar-32b0f && firebase target:clear hosting app.getpolarized.io && firebase target:apply hosting app.getpolarized.io ${FIREBASE_HOSTING_TARGET} && firebase deploy --only hosting,storage,firestore)
+cd /home/circleci/project/packages/polar-hooks 
+echo Pushing to target ${FIREBASE_HOSTING_TARGET} 
+firebase use polar-32b0f
+firebase target:clear hosting app.getpolarized.io 
+firebase target:apply hosting app.getpolarized.io ${FIREBASE_HOSTING_TARGET}
+firebase deploy --only hosting
+firebase deploy --only storage
+firebase deploy --only firestore
 mv /home/circleci/project/packages/polar-hooks/ /home/circleci/
 rm -rf /home/circleci/project
 cd /home/circleci/polar-hooks
