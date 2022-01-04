13a6c6f5bae7e430d029f7ccf1c897fc6685a21d
WinterCore <hogobbl@gmail.com>
2022-01-04 18:04:25 +0200

    feat(docviewer): Add different area highlight creation behaviour for handheld devices

--- packages/polar-bookshelf/apps/doc/src/toolbar/AreaHighlightModeToggle.tsx
+++ packages/polar-bookshelf/apps/doc/src/toolbar/AreaHighlightModeToggle.tsx
@@ -6,6 +6,9 @@ import {useDocViewerCallbacks, useDocViewerStore} from "../DocViewerStore";
 import {useDocViewerContext} from "../renderers/DocRenderer";
 import {StandardToggleButton} from "../../../repository/js/doc_repo/buttons/StandardToggleButton";
 import {InputEscapeListener} from "../../../../web/js/mui/complete_listeners/InputEscapeListener";
+import {Devices} from "polar-shared/src/util/Devices";
+import {useBlockAreaHighlight} from "../../../../web/js/notes/HighlightBlocksHooks";
+import {useDocViewerElementsContext} from "../renderers/DocViewerElementsContext";
 
 const globalKeyMap = keyMapWithGroup({
     group: "Document Viewer",
@@ -26,17 +29,51 @@ const globalKeyMap = keyMapWithGroup({
 
 
 export const AreaHighlightModeToggle: React.FC = () => {
-    const {toggleAreaHighlightMode, setAreaHighlightMode} = useDocViewerCallbacks();
-    const {areaHighlightMode}
-        = useDocViewerStore(["areaHighlightMode"]);
-    const {fileType} = useDocViewerContext();
     const {active} = usePersistentRouteContext();
+    const {toggleAreaHighlightMode, setAreaHighlightMode} = useDocViewerCallbacks();
+    const {areaHighlightMode, page: pageNum, docMeta, docScale}
+        = useDocViewerStore(["areaHighlightMode", "page", "docMeta", "docScale"]);
+    const {fileType} = useDocViewerContext();
+    const {create: createBlockAreaHighlight} = useBlockAreaHighlight();
+    const docViewerElements = useDocViewerElementsContext();
 
     const handlers = React.useMemo(() => ({ TOGGLE: toggleAreaHighlightMode}), [toggleAreaHighlightMode]);
 
     const handleEscape = React.useCallback(() => setAreaHighlightMode(false), [setAreaHighlightMode]);
 
-    if (~["epub"].indexOf(fileType)) {
+    const handleToggleAreaHighlightMode = React.useCallback(() => {
+        toggleAreaHighlightMode();
+
+        if (! Devices.isDesktop()) {
+            if (! docMeta || ! docScale || areaHighlightMode) {
+                return;
+            }
+
+            const pageMeta = docMeta.pageMetas[pageNum];
+
+            if (! pageMeta || ! pageMeta.pageInfo.dimensions) {
+                return;
+            }
+
+            const rect = {
+                left: 30,
+                top: 30,
+                width: 150,
+                height: 150,
+            };
+            const docViewerElement = docViewerElements.getDocViewerElement();
+
+            createBlockAreaHighlight(docMeta.docInfo.fingerprint, {
+                pageNum,
+                fileType,
+                docScale,
+                docViewerElement,
+                rect,
+            }).catch(console.error);
+        }
+    }, [toggleAreaHighlightMode, docMeta, docScale, pageNum, docViewerElements, areaHighlightMode]);
+
+    if (fileType === "epub") {
         return null;
     }
 
@@ -50,10 +87,12 @@ export const AreaHighlightModeToggle: React.FC = () => {
             )}
 
             <StandardToggleButton
-                onClick={toggleAreaHighlightMode}
+                onClick={handleToggleAreaHighlightMode}
                 active={areaHighlightMode}
                 tooltip="Area highlight mode (a)">
+
                 <PhotoSizeSelectLargeIcon/>
+
             </StandardToggleButton>
 
         </>
