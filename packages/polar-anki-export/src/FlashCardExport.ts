57c7cffcdae73549f5ca9b19ee13373d029e3e5f
Mohammed Atallah <mohammed@getpolarized.io>
2022-01-12 01:03:28 +0200

    add basic testing of flashcard types

--- packages/polar-anki-export/src/FlashCardExport.ts
+++ packages/polar-anki-export/src/FlashCardExport.ts
@@ -1,5 +1,7 @@
 
 import { APKG } from 'polar-anki-apkg/src/index';
+import { readFileSync } from 'fs';
+import { join } from 'path';
 export namespace FlashCardExport {
     export async function init() {
 
@@ -9,19 +11,37 @@ export namespace FlashCardExport {
 
         function addBasicFlashcard () {
             apkg.addCard(models.BASIC_FRONT_BACK, {
-                content: ["front example", "back example"]
-            })
+                content: ["front example", `<div>back example</div><img src="ankimg.png">`]
+            });
+
+            apkg.addMedia("ankimg.png", readFileSync(join(__dirname, "ankimg.png")));
         }
 
         function addClozeFlashcard() {
             apkg.addCard(models.CLOZE, {
-                content: ["front cloze"]
+                content: ["text {{c1::test}}"]
+            });
+        }
+
+        function addFrontBackAndReverse(){
+            apkg.addCard(models.BASIC_FRONT_BACK_AND_REVERSE, {
+                content: ["front and reverse", "back and reverse"]
+            });
+        }
+        
+        function addFrontBackOrReverse(){
+            apkg.addCard(models.BASIC_FRONT_BACK_OR_REVERSE, {
+                content: ["Front optional reverse", "back or reverse", "add reverse"]
             });
         }
 
         addBasicFlashcard();
 
         addClozeFlashcard();
+
+        addFrontBackAndReverse();
+
+        addFrontBackOrReverse();
 
         await apkg.save(__dirname);
     };
--- packages/polar-anki-export/src/FlashCardTestN.ts
+++ packages/polar-anki-export/src/FlashCardTestN.ts
@@ -1,6 +1,3 @@
-import { Template } from 'polar-anki-apkg/src/DeckConfig';
-import { DeckConfig } from 'polar-anki-apkg/src/DeckConfig';
-import { APKG } from 'polar-anki-apkg/src/index'
 import { FlashCardExport } from './FlashCardExport';
 
 describe("Flashcard export", () => {
