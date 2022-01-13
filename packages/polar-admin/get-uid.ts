5e1b5d1fba54fbe0345130a4ba1fcc0e421ee5d0
Kevin Burton <burtonator@gmail.com>
2022-01-13 10:49:32 -0700

    feat: command line to get a uid by user for debugging firestore data..

---
+++ packages/polar-admin/get-uid.ts
@@ -0,0 +1,29 @@
+import {FirebaseAdmin} from "polar-firebase-admin/src/FirebaseAdmin";
+import {Lazy} from "polar-shared/src/util/Lazy";
+
+const firebaseProvider = Lazy.create(() => FirebaseAdmin.app());
+
+async function exec() {
+
+    const firebase = firebaseProvider();
+
+    const auth = firebase.auth();
+
+    // gets the first command line argument
+    const email = process.argv.slice(2)[0];
+
+    if (! email) {
+        throw new Error("This command requires an email as argument to generate a valid token");
+    }
+
+    const user = await auth.getUserByEmail(email);
+
+    if (! user) {
+        throw new Error("No user for email: " + email);
+    }
+
+    console.log("uid: " + user.uid);
+
+}
+
+exec().catch(err => console.error(err));
