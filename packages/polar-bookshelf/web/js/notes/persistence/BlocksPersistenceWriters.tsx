66ff7f3433ce44621ae3b506657262e595c7dd55
Kevin Burton <burtonator@gmail.com>
2022-01-14 17:39:36 -0700

    implement a write debouncer.  Seems like this is working fine...

--- packages/polar-bookshelf/web/js/notes/persistence/BlocksPersistenceWriters.tsx
+++ packages/polar-bookshelf/web/js/notes/persistence/BlocksPersistenceWriters.tsx
@@ -6,8 +6,71 @@ import {useRepoDocMetaManager} from '../../../../apps/repository/js/persistence_
 import {Testing} from "polar-shared/src/util/Testing";
 import {FirestoreBlocksPersistenceWriter} from "./FirestoreBlocksPersistenceWriter";
 import {IBlocksStoreMutation} from '../store/IBlocksStoreMutation';
+import {Debouncers} from "polar-shared/src/util/Debouncers";
 
 const IS_NODE = typeof window === 'undefined';
+
+interface IBuffer<T> {
+
+    /**
+     * Push records to the end of the buffer.
+     */
+    readonly push: (data: ReadonlyArray<T>) => void;
+
+    /**
+     * True when we need to flush the buffer.
+     */
+    readonly flushable: () => boolean;
+
+    /**
+     * Create a snapshot of the underlying data and reset the internal buffer.
+     */
+    readonly snapshotAndReset: () => ReadonlyArray<T>;
+}
+
+function useBuffer<T>(capacity: number): IBuffer<T> {
+
+    const buff = React.useMemo<T[]>(() =>[], []);
+
+    const push = React.useCallback((data: ReadonlyArray<T>) => {
+        buff.push(...data);
+    }, [buff])
+
+    const flushable = React.useCallback(() => {
+        return buff.length >= capacity;
+    }, [buff, capacity]);
+
+    const snapshotAndReset = React.useCallback(() => {
+        return buff.splice(0, buff.length);
+    }, [buff])
+
+    return React.useMemo(() => {
+        return {
+            push, flushable, snapshotAndReset
+        }
+    }, [flushable, push, snapshotAndReset])
+
+}
+
+/**
+ * Fancy debouncer which also executes on unmount.
+ *
+ * Note that the debouncer must be idempotent.
+ */
+function useDebouncer(callback: () => void, interval: number = 500) {
+
+    React.useEffect(() => {
+
+        return () => {
+            // the debouncer must be called when we unmount.
+            callback();
+        }
+
+    }, [callback])
+
+    return React.useMemo(() => Debouncers.create(callback, {interval}), [callback, interval]);
+
+}
 
 export function useFirestoreBlocksPersistenceWriter(): BlocksPersistenceWriter {
 
@@ -15,17 +78,45 @@ export function useFirestoreBlocksPersistenceWriter(): BlocksPersistenceWriter {
     const {uid} = useBlocksStoreContext();
     const repoDocMetaManager = useRepoDocMetaManager();
 
-    return React.useCallback((mutations: ReadonlyArray<IBlocksStoreMutation>) => {
+    const mutationBuffer = useBuffer<IBlocksStoreMutation>(50);
+
+    const doCommit = React.useCallback(() => {
+
+        const snapshot = mutationBuffer.snapshotAndReset()
+
+        // IMPORTANT NOTE: with this mechanism, we might write to a block N
+        // times but the operations are ordered so the last writer wins, so, I
+        // think this should be safe.
+
+        console.log("Going to commit N snapshot mutations: " + snapshot.length);
 
         // TODO use a dialog handler for this for when we get an error.
         FirestoreBlocksPersistenceWriter.doExec(
             uid,
             firestore,
             repoDocMetaManager.repoDocInfoIndex,
-            mutations
-        ).catch(err => console.log("Unable to commit mutations: ", err, mutations));
+            snapshot
+        ).catch(err => console.log("Unable to commit mutations: ", err, snapshot));
 
-    }, [firestore, repoDocMetaManager.repoDocInfoIndex, uid]);
+    }, [firestore, mutationBuffer, repoDocMetaManager.repoDocInfoIndex, uid])
+
+    const debouncer = useDebouncer(doCommit, 5000);
+
+    return React.useCallback((mutations: ReadonlyArray<IBlocksStoreMutation>) => {
+
+        mutationBuffer.push(mutations);
+
+        // the debouncer needs to be run on every single commit to schedule
+        // commits in the future so that if we have less data than the
+        // mutationBuffer it will get flushed eventually.
+        debouncer();
+
+        // we have to flush if we've exceeded capacity.
+        if (mutationBuffer.flushable()) {
+            doCommit();
+        }
+
+    }, [debouncer, doCommit, mutationBuffer]);
 
 }
 
