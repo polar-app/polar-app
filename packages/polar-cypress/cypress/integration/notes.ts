1a8bc0b33196c3487994a2c67ee9c7f2dd491f3f
Kevin Burton <burtonator@gmail.com>
2022-01-27 14:02:14 -0700

    moved login code to E2E framework...

--- packages/polar-cypress/cypress/integration/notes.ts
+++ packages/polar-cypress/cypress/integration/notes.ts
@@ -9,37 +9,16 @@ describe('Notes', function () {
     });
 
     it('Can open a single note', () => {
+
         cy.visit('http://localhost:8050');
-        login();
-        goToNotes();
+
+        E2E.Auth.doLogin('testing@getpolarized.io', '123 456');
+
+        E2E.Nav.goToNotes();
+
         openSingleNote();
+
     })
-
-    function login() {
-        cy.get('input[type=email]').type('testing@getpolarized.io')
-        cy.get('button[type=button]')
-            .contains("SIGN IN WITH EMAIL", {matchCase: false})
-            .click();
-        cy.get("input[placeholder='Enter your Code Here']", {timeout: 10000}).type('123 456')
-
-        cy.get('button[type=button]')
-            .contains("VERIFY CODE", {matchCase: false, timeout: 15000})
-            .click();
-
-        // this will wait for the app to login now
-        cy.get('#add-content-dropdown', {timeout: 15000});
-    }
-
-    function goToNotes() {
-        // Click on "Notes" in the sidebar
-        cy.get('#sidenav div[title=Notes]').click();
-
-        // Wait for notes listing data table to have at least one note
-        cy.get('.NotesRepoTable2 .MuiTableBody-root')
-            .children({})
-            .its('length')
-            .should('be.gte', 1);
-    }
 
     function openSingleNote() {
         // Try to open a single note
@@ -61,4 +40,5 @@ describe('Notes', function () {
             assert.isAtMost(timeToOpenSingleNote, 2000);
         });
     }
+
 });
--- packages/polar-cypress/cypress/lib/E2E.ts
+++ packages/polar-cypress/cypress/lib/E2E.ts
@@ -15,5 +15,40 @@ export namespace E2E {
 
     }
 
+    export namespace Auth {
+
+        export function doLogin(email: string, code: string) {
+
+            cy.get('input[type=email]').type(email)
+            cy.get('button[type=button]')
+                .contains("SIGN IN WITH EMAIL", {matchCase: false})
+                .click();
+            cy.get("input[placeholder='Enter your Code Here']", {timeout: 10000}).type(code)
+
+            cy.get('button[type=button]')
+                .contains("VERIFY CODE", {matchCase: false, timeout: 15000})
+                .click();
+
+            // this will wait for the app to login now
+            cy.get('#add-content-dropdown', {timeout: 15000});
+
+        }
+
+    }
+
+    export namespace Nav {
+
+        export function goToNotes() {
+            // Click on "Notes" in the sidebar
+            cy.get('#sidenav div[title=Notes]').click();
+
+            // Wait for notes listing data table to have at least one note
+            cy.get('.NotesRepoTable2 .MuiTableBody-root')
+                .children({})
+                .its('length')
+                .should('be.gte', 1);
+        }
+
+    }
 
 }
