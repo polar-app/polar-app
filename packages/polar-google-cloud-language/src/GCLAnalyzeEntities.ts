4a5a1d3b3a0dba32d2aaaea13deea853303bed33
Mohammed Atallah <mohammed@getpolarized.io>
2022-02-02 17:38:27 +0200

    add an explicit return type

--- packages/polar-google-cloud-language/src/GCLAnalyzeEntities.ts
+++ packages/polar-google-cloud-language/src/GCLAnalyzeEntities.ts
@@ -1,10 +1,12 @@
 import language from '@google-cloud/language'
 import {GCLCredentials} from "./GCLCredentials";
 import {TextStr} from "polar-shared/src/util/Strings";
+import { google } from '@google-cloud/language/build/protos/protos';
 
 export namespace GCLAnalyzeEntities {
+    export type IAnalyizeEntitiesResponse = google.cloud.language.v1.IAnalyzeEntitySentimentResponse;
 
-    export async function analyzeEntities(content: TextStr) {
+    export async function analyzeEntities(content: TextStr): Promise<IAnalyizeEntitiesResponse> {
 
         GCLCredentials.init();
 
@@ -19,7 +21,6 @@ export namespace GCLAnalyzeEntities {
         });
 
         return response;
-
     }
 
 }
