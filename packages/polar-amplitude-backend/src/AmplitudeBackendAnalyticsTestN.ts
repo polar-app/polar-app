4872ba564adaf229d6ff7518d4d4061b7eec938b
Kevin Burton <burtonator@gmail.com>
2022-02-16 13:45:16 -0700

    Test to just always run an AmplitudeBackendAnalytics event.

---
+++ packages/polar-amplitude-backend/src/AmplitudeBackendAnalyticsTestN.ts
@@ -0,0 +1,10 @@
+import {AmplitudeBackendAnalytics} from "./AmplitudeBackendAnalytics";
+
+describe("AmplitudeBackendAnalytics", function() {
+
+
+    it("basic", () => {
+        AmplitudeBackendAnalytics.event2("AmplitudeBackendAnalyticsTestN");
+    });
+
+})
