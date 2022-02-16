626092420639320039b694860366abdc53d76611
Kevin Burton <burtonator@gmail.com>
2022-02-16 10:25:05 -0700

    chore: migrate to a premium upgrade system that supports stories but we need a storybook upgrade first.

---
+++ packages/polar-bookshelf/apps/repository/js/ui/usePremiumFeatureCallback.tsx
@@ -0,0 +1,42 @@
+import {useAccountUpgrader} from "../../../../web/js/ui/account_upgrade/AccountUpgrader";
+import {useDialogManager} from "../../../../web/js/mui/dialogs/MUIDialogControllers";
+import {useHistory} from "react-router-dom";
+import * as React from "react";
+import {Plans} from "polar-accounts/src/Plans";
+import {Analytics} from "../../../../web/js/analytics/Analytics";
+import {Billing} from "polar-accounts/src/Billing";
+import V2PlanFree = Billing.V2PlanFree;
+
+export function usePremiumFeatureCallback(delegate: () => void) {
+
+    const accountUpgrade = useAccountUpgrader();
+    const dialogManager = useDialogManager();
+    const history = useHistory();
+
+    return React.useCallback(() => {
+
+        const currentPlan = Plans.toV2(accountUpgrade?.plan);
+
+        if (currentPlan === V2PlanFree) {
+
+            Analytics.event2('account-upgrade-required', {
+                reason: 'anki_export'
+            });
+
+            dialogManager.confirm({
+                title: 'Account Upgraded Required',
+                acceptText: "Upgrade Plan",
+                type: 'primary',
+                subtitle: 'This feature is only available in the Plus and Pro plan',
+                onAccept: () => history.push('/plans')
+            });
+
+            return;
+
+        }
+
+        delegate();
+
+    }, [accountUpgrade, dialogManager, history, delegate])
+
+}
