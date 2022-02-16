e7c3686bd11908f7814207d43ceb8be07dddeade
WinterCore <hogobbl@gmail.com>
2022-02-16 20:56:19 +0200

    feat: Wrap the new AI cloze deletions into a feature toggle

--- packages/polar-bookshelf/web/js/notes/blocks/BlockAnnotationContent/BlockFlashcardAnnotationContent.tsx
+++ packages/polar-bookshelf/web/js/notes/blocks/BlockAnnotationContent/BlockFlashcardAnnotationContent.tsx
@@ -15,8 +15,9 @@ import {useBlocksTreeStore} from "../../BlocksTree";
 import {AnnotationContentType} from "polar-blocks/src/blocks/content/IAnnotationContent";
 import {BlockTextContentUtils} from "../../BlockTextContentUtils";
 import {AutoClozeDeletionIcon} from "../../../icons/AutoClozeDeletionIcon";
-import {useAIFlashcardVerifiedAction} from "../../../../../apps/repository/js/ui/AIFlashcardVerifiedAction";
 import {useAutoClozeDeletionBlock} from "../../../annotation_sidebar/AutoClozeDeletionHook";
+import {AccountVerifiedAction} from "../../../../../apps/repository/js/ui/AccountVerifiedAction";
+import {FeatureEnabled} from "../../../features/FeaturesRegistry";
 
 interface IProps extends BlockEditorGenericProps {
     readonly annotation: FlashcardAnnotationContent;
@@ -78,8 +79,8 @@ interface IFrontBackFlashcardProps extends BlockEditorGenericProps {
 const FrontBackFlashcard: React.FC<IFrontBackFlashcardProps> = (props) => {
     const { flashcard, id, onKeyDown, innerRef: frontRef, created } = props;
     const { fields: { front, back } } = flashcard;
-    const backRef = React.useRef<HTMLDivElement>(null);
     const { getBlock } = useAnnotationBlockManager();
+    const backRef = React.useRef<HTMLDivElement>(null);
     const blocksTreeStore = useBlocksTreeStore();
 
     const handleChange = React.useCallback((field: keyof IBlockFrontBackFlashcard['fields']) => (markdown: MarkdownStr) => {
@@ -127,11 +128,11 @@ const ClozeFlashcard: React.FC<IClozeFlashcardProps> = (props) => {
     const { fields: { text } } = flashcard;
     const { getBlock } = useAnnotationBlockManager();
     const blocksTreeStore = useBlocksTreeStore();
-    const verifiedAction = useAIFlashcardVerifiedAction();
     const [aiClozeDeletionState, aiClozeDeletionHandler] = useAutoClozeDeletionBlock();
 
     const handleChange = React.useCallback((markdown: MarkdownStr) => {
         const block = getBlock(id, AnnotationContentType.FLASHCARD);
+
         if (block && BlockPredicates.isClozeFlashcardBlock(block)) {
             const content = BlockTextContentUtils.updateFlashcardContentMarkdown(block.content, 'text', markdown);
             blocksTreeStore.setBlockContent(id, content);
@@ -143,11 +144,11 @@ const ClozeFlashcard: React.FC<IClozeFlashcardProps> = (props) => {
             return;
         }
 
-        verifiedAction(() => {
-            aiClozeDeletionHandler(id)
-                .catch(e => console.error("Could not handle verified action: ", e));
-        });
-    }, [verifiedAction, aiClozeDeletionHandler, aiClozeDeletionState, id]);
+        aiClozeDeletionHandler(id)
+            .catch(e => console.error("Could not handle verified action: ", e));
+    }, [aiClozeDeletionHandler, aiClozeDeletionState, id]);
+
+    const premiumFeatureCallback = AccountVerifiedAction.usePremiumFeatureCallback(handleAutoCloze);
 
     return (
         <div className="flashcard-wrapper-item">
@@ -157,14 +158,20 @@ const ClozeFlashcard: React.FC<IClozeFlashcardProps> = (props) => {
                 onChange={handleChange}
             />
             <Box display="flex" mt="0.75rem" style={{ marginTop: 5 }}>
-                <Box mr="0.5rem">
-                    <IconButton disabled={aiClozeDeletionState === 'waiting'} size="small" onClick={handleAutoCloze}>
-                        {aiClozeDeletionState === 'idle'
-                            ? <AutoClozeDeletionIcon />
-                            : <CircularProgress size="1.7rem" color="secondary" /> 
-                        }
-                    </IconButton>
-                </Box>
+                <FeatureEnabled feature="ai-cloze-deletions">
+                    <Box mr="0.5rem">
+                        <IconButton disabled={aiClozeDeletionState === 'waiting'}
+                                    size="small"
+                                    onClick={premiumFeatureCallback}>
+
+                            {aiClozeDeletionState === 'idle'
+                                ? <AutoClozeDeletionIcon />
+                                : <CircularProgress size="1.7rem" color="secondary" /> 
+                            }
+
+                        </IconButton>
+                    </Box>
+                </FeatureEnabled>
                 <DocAnnotationMoment created={created} />
             </Box>
         </div>
