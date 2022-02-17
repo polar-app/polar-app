0563a5d13f13802f4af30fa3bec21dc29331a9ff
Kevin Burton <burtonator@gmail.com>
2022-02-17 10:45:35 -0700

    working on loading so that we can time the event and now long it takes to load.

---
+++ packages/polar-bookshelf/web/js/apps/repository/AppLoadingProgress.tsx
@@ -0,0 +1,30 @@
+import LinearProgress from '@material-ui/core/LinearProgress';
+import React from 'react';
+import {PolarLogoImage} from "../../../../apps/repository/js/nav/PolarLogoImage";
+
+/**
+ * Should be used while the app is loading as the main progress loading component.
+ */
+export const AppLoadingProgress = React.memo(function AppLoadingProgress() {
+
+    return (
+        <div style={{display: 'flex', height: '100%', flexGrow: 1, flexDirection: 'column'}}>
+
+            <LinearProgress/>
+
+            <div style={{
+                     flexGrow: 1,
+                     display: 'flex',
+                     // textAlign: 'center',
+                     justifyContent: 'center',
+                     alignItems: 'center'
+                 }}>
+
+                <PolarLogoImage height={250} width={250}/>
+
+            </div>
+
+        </div>
+    );
+
+});
