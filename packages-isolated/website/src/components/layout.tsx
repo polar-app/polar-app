fb8725311afc6c9b4dc443dd45280d8a0648ca70
Dzhuneyt <dzhuneyt@dzhuneyt.com>
2022-02-19 01:19:49 +0200

    feat: Push users to download the mobile app when using the mobile browser but not through the mobile app

--- packages-isolated/website/src/components/layout.tsx
+++ packages-isolated/website/src/components/layout.tsx
@@ -1,34 +1,39 @@
 import * as React from "react"
 import Nav from "./nav";
-import { Box, CssBaseline } from "@material-ui/core";
-import { Helmet } from "react-helmet";
+import {Box, CssBaseline} from "@material-ui/core";
+import {Helmet} from "react-helmet";
 import Footer from "./footer";
+import {PropsWithChildren} from "react";
 
-const Layout = ({ children }) => {
-  return (
-    <React.Fragment>
-      <Helmet>
-        <CssBaseline />
-      </Helmet>
-      <Nav />
-      {children}
-      <Box style={{ paddingBottom: "0px", width: "100%", bottom: 0 }}>
-        <Footer />
-      </Box>
-    </React.Fragment>
-  );
+interface LayoutProps extends PropsWithChildren<unknown> {
+    noHeader: boolean,
+}
+
+const Layout = (props: LayoutProps) => {
+    return (
+        <React.Fragment>
+            <Helmet>
+                <CssBaseline/>
+            </Helmet>
+            {props.noHeader || <Nav/>}
+            {props.children}
+            <Box style={{paddingBottom: "0px", width: "100%", bottom: 0}}>
+                <Footer/>
+            </Box>
+        </React.Fragment>
+    );
 };
 
-export const LayoutDocs = ({ children }) => {
-  return (
-    <React.Fragment>
-      <Helmet>
-        <CssBaseline />
-      </Helmet>
-      <Nav />
-      {children}
-    </React.Fragment>
-  );
+export const LayoutDocs = ({children}) => {
+    return (
+        <React.Fragment>
+            <Helmet>
+                <CssBaseline/>
+            </Helmet>
+            <Nav/>
+            {children}
+        </React.Fragment>
+    );
 };
 
 export default Layout;
