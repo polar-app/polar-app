2b3136c608f54d37a7f365607d9e8cd92b7f595e
Kevin Burton <burtonator@gmail.com>
2022-02-18 12:57:02 -0700

    cleanup

--- packages/polar-bookshelf/apps/repository/js/notes_repo/NotesRepoScreen2.tsx
+++ packages/polar-bookshelf/apps/repository/js/notes_repo/NotesRepoScreen2.tsx
@@ -2,15 +2,12 @@ import React from 'react';
 import {NotesRepoTable2, TableGridStoreProvider} from './NotesRepoTable2';
 import {DeviceRouters} from '../../../../web/js/ui/DeviceRouter';
 import {MobileDailyNotesFab} from "./MobileDailyNotesFab";
-import {NotesToolbar} from "../../../../web/js/notes/NotesToolbar";
 
 export const NotesRepoScreen2 = React.memo(function NotesRepoScreen2() {
 
     return (
         <TableGridStoreProvider>
             <>
-
-                <NotesToolbar/>
 
                 <NotesRepoTable2/>
 
