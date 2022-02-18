43862f27f8befa772c2468f7b3905e0f28724d37
Kevin Burton <burtonator@gmail.com>
2022-02-18 12:34:35 -0700

    rework this so that we can make the repository load better.

--- packages/polar-bookshelf/apps/repository/js/notes_repo/NotesRepoScreen2.tsx
+++ packages/polar-bookshelf/apps/repository/js/notes_repo/NotesRepoScreen2.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import {NotesRepoTable2, TableGridStoreProvider} from './NotesRepoTable2';
 import {DeviceRouters} from '../../../../web/js/ui/DeviceRouter';
 import {MobileDailyNotesFab} from "./MobileDailyNotesFab";
+import {NotesToolbar} from "../../../../web/js/notes/NotesToolbar";
 
 export const NotesRepoScreen2 = React.memo(function NotesRepoScreen2() {
 
     return (
         <TableGridStoreProvider>
             <>
+
+                <NotesToolbar/>
+
                 <NotesRepoTable2/>
 
                 <DeviceRouters.NotDesktop>
