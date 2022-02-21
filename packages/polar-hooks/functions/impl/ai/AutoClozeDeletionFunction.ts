6dd43cf1b765fe9b51bfdcd178316ac93fe32523
Kevin Burton <burtonator@gmail.com>
2022-02-21 14:13:10 -0700

    fix: using the correct replacement algorithm.

--- packages/polar-hooks/functions/impl/ai/AutoClozeDeletionFunction.ts
+++ packages/polar-hooks/functions/impl/ai/AutoClozeDeletionFunction.ts
@@ -1,7 +1,9 @@
-import { AutoClozeDeletions } from "polar-backend-api/src/api/AutoClozeDeletion";
+import {AutoClozeDeletions} from "polar-backend-api/src/api/AutoClozeDeletion";
 import {ExpressFunctions} from "../util/ExpressFunctions";
 import {IDUser} from "../util/IDUsers";
-import { GCLAnalyzeEntities } from "polar-google-cloud-language/src/GCLAnalyzeEntities";
+import {GCLAnalyzeEntities} from "polar-google-cloud-language/src/GCLAnalyzeEntities";
+import {arrayStream} from "polar-shared/src/util/ArrayStreams";
+import {GCL} from "polar-backend-api/src/api/GCL";
 
 
 export namespace AutoClozeDeletion {
@@ -9,33 +11,48 @@ export namespace AutoClozeDeletion {
     import AutoClozeDeletionError = AutoClozeDeletions.AutoClozeDeletionError;
     import AutoClozeDeletionRequest = AutoClozeDeletions.AutoClozeDeletionRequest;
     import AutoClozeDeletionResponse = AutoClozeDeletions.AutoClozeDeletionResponse;
+    import IEntityMention = GCL.IEntityMention;
 
-    function clozeDeletionTagGenerator(seed: number = 1): (deletion: string) => string {
-        return (deletion: string): string => {
-            const tag = `{{c${seed}::${deletion}}}`
-            seed++;
-            return tag;
-        }
+    export function _createClozeWithinText(text: string,
+                                           start: number,
+                                           end: number,
+                                           idx: number) {
+
+        const clozedText = text.substring(start, end);
+
+        const replacement = `{{c${idx}::${clozedText}}}`
+
+        return text.substring(0, start) + replacement + text.substring(end, text.length);
+
     }
-    function generateClozeDeletions(text: string,
-                                    entities: ReadonlyArray<GCLAnalyzeEntities.IEntity> ): string {
 
-        let mutText = text;
+    export function generateClozeDeletions(text: string,
+                                           entities: ReadonlyArray<GCLAnalyzeEntities.IEntity> ): string {
 
-        const createClozeDeletionTag = clozeDeletionTagGenerator();
+        let result = text;
 
-        for (const entity of entities) {
-            if (entity.name) {
+        // sort the entities by offset desc...
 
-                // Regex pattern matches entities that does NOT have the Anki cloze deletion tag pattern
-                // e.g.: `{{c1::example}}`
-                const pattern = new RegExp(`(?!{{c\#::)${entity.name}(?!}})`);
-                
-                mutText = mutText.replace(pattern, createClozeDeletionTag(entity.name));
-            }
+        function toBeginOffset(mention: IEntityMention) {
+            return mention.text?.beginOffset || 0;
         }
 
-        return mutText;
+        const mentions = arrayStream(entities)
+            .map(current => current.mentions)
+            .flatMap(current => current!)
+            .sort((a, b) => toBeginOffset(b) - toBeginOffset(a))
+            .collect()
+
+        let idx = mentions.length;
+
+        for (const mention of mentions) {
+            const start = mention.text?.beginOffset || 0;
+            const end = start + (mention.text?.content || '').length;
+            result = _createClozeWithinText(result, start, end, idx--)
+        }
+
+        return result;
+
     }
 
     export async function analyzeText(text: string): Promise<AutoClozeDeletionResponse | AutoClozeDeletionError> {
@@ -56,7 +73,7 @@ export namespace AutoClozeDeletion {
 
     export async function exec(idUser: IDUser,
                                request: AutoClozeDeletionRequest): Promise<AutoClozeDeletionResponse | AutoClozeDeletionError> {
-        
+
         return await analyzeText(request.text);
     }
 
