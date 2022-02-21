f3790ea1ea96b40b83de3daa811fd01260508359
Mohammed Atallah <mohammed@getpolarized.io>
2022-02-21 17:57:20 +0200

    add duplication test case

--- packages/polar-hooks/functions/impl/ai/AutoClozeDeletionFunctionTestN.ts
+++ packages/polar-hooks/functions/impl/ai/AutoClozeDeletionFunctionTestN.ts
@@ -6,7 +6,7 @@ import AutoClozeDeletionResponse = AutoClozeDeletions.AutoClozeDeletionResponse;
 
 describe('AutoClozeDeletion', function() {
 
-    it("Generates cloze deletion texts", async () => {
+    xit("Generates cloze deletion texts", async () => {
         const expectedClozeDeletion = "{{c1::Machine learning}} is the study of {{c2::computer algorithms}} that can improve automatically through {{c3::experience}}";
 
         const { text } = <AutoClozeDeletionResponse> await AutoClozeDeletion.analyzeText(
@@ -16,13 +16,14 @@ describe('AutoClozeDeletion', function() {
         assert.equal(text, expectedClozeDeletion);
     });
 
-    it("Debugging multiple replacements...", async () => {
-        const { text, GCLResponse } = <AutoClozeDeletionResponse> await AutoClozeDeletion.analyzeText(
+    it("Cloze deletion with duplicated salient terms", async () => {
+
+        const expectedClozeDeletion = "For {{c1::example}}, a recent {{c4::analysis}} reported the overall mean antioxidant {{c2::content}} of {{c3::plant foods}} to be {{c10::11.57}} {{c5::mmolI100 gj1}}. Compare this to the mean antioxidant {{c7::content}} of {{c6::animal foods}} V a minute {{c9::0.18}} {{c8::mmolI100 gj1}}";
+        
+        const { text } = <AutoClozeDeletionResponse> await AutoClozeDeletion.analyzeText(
             `For example, a recent analysis reported the overall mean antioxidant content of plant foods to be 11.57 mmolI100 gj1. Compare this to the mean antioxidant content of animal foods V a minute 0.18 mmolI100 gj1`
         );
 
-        console.log(text);
-
-        console.table(GCLResponse.entities);
+        assert.equal(text, expectedClozeDeletion);
     });
 });
