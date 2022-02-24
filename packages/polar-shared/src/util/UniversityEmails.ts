30a2108cdb9747932bf357a46c5b1b557202348b
Dzhuneyt <dzhuneyt@dzhuneyt.com>
2022-02-24 19:30:28 +0200

    feat: Allow emails matching the "-uni@getpolarized.io" suffix pattern to take advantage of the referral program

--- packages/polar-shared/src/util/UniversityEmails.ts
+++ packages/polar-shared/src/util/UniversityEmails.ts
@@ -8,16 +8,23 @@ export namespace UniversityEmails {
     /**
      * Get the university by the domain of the given email.
      */
-    export function getUniversityByEmailDomain(email: EmailStr): University |  undefined {
+    export function getUniversityByEmailDomain(email: EmailStr): University | undefined {
 
         const domain = Emails.toDomain(email);
 
-        if (! domain) {
+        if (!domain) {
             return undefined;
         }
 
         return Universities.getByDomain(domain);
+    }
 
+    export function isUniversityEmail(email: EmailStr): boolean {
+        if (email.endsWith('-uni@getpolarized.io')) {
+            return true;
+        }
+        const university = getUniversityByEmailDomain(email);
+        return !!university;
     }
 
 }
