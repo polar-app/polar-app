8530c6f24ddd2e57e72f2c99d78ae2249fbf1cc9
Mohammed Atallah <mohammed@getpolarized.io>
2022-03-02 17:46:27 +0200

    refactor to login with custom token instead of password

--- packages/polar-firebase-browser/src/firebase/FirebaseBrowserTesting.ts
+++ packages/polar-firebase-browser/src/firebase/FirebaseBrowserTesting.ts
@@ -1,15 +1,18 @@
 import {FirebaseBrowser} from "./FirebaseBrowser";
 import {FirebaseTestingUsers} from "polar-firebase-test/src/firebase/FirebaseTestingUsers";
+import fetchTokenRequest from "./fetchTokenRequest";
 
 export namespace FirebaseBrowserTesting {
 
-    async function authWithUser(user: string, pass: string) {
+    async function authWithUser(user: string){
 
         const app = FirebaseBrowser.init();
 
         const auth = app.auth();
 
-        const userCredential = await auth.signInWithEmailAndPassword(user, pass);
+        const token = await fetchTokenRequest(user);
+
+        const userCredential = await auth.signInWithCustomToken(token);
 
         if (! userCredential.user) {
             throw new Error("No user");
@@ -19,15 +22,15 @@ export namespace FirebaseBrowserTesting {
     }
 
     export async function authWithUser0() {
-        return await authWithUser(FirebaseTestingUsers.FIREBASE_USER, FirebaseTestingUsers.FIREBASE_PASS);
+        return await authWithUser(FirebaseTestingUsers.FIREBASE_USER);
     }
 
     export async function authWithUser1() {
-        return await authWithUser(FirebaseTestingUsers.FIREBASE_USER1, FirebaseTestingUsers.FIREBASE_PASS1);
+        return await authWithUser(FirebaseTestingUsers.FIREBASE_USER1);
     }
 
     export async function authWithUser2() {
-        return await authWithUser(FirebaseTestingUsers.FIREBASE_USER2, FirebaseTestingUsers.FIREBASE_PASS2);
+        return await authWithUser(FirebaseTestingUsers.FIREBASE_USER2);
     }
 
 }
