4265ac24e3b18be015d3031ef9b07bb0700385df
Mohammed Atallah <mohammed@getpolarized.io>
2022-03-03 16:18:44 +0200

    add DEFAULT_CHALLENGE constant elaborate on failure error message

--- packages/polar-firebase-browser/src/firebase/fetchTokenRequest.ts
+++ packages/polar-firebase-browser/src/firebase/fetchTokenRequest.ts
@@ -4,13 +4,16 @@ import {
 } from "polar-backend-api/src/api/VerifyTokenAuth";
 import { Fetches } from "polar-shared/src/util/Fetch";
 
+
+const DEFAULT_CHALLENGE = "123456";
+
 export default async function fetchTokenRequest(email: string): Promise<string> {
 
     const url = "https://us-central1-polar-32b0f.cloudfunctions.net/VerifyTokenAuth/";
 
     const requestBody: IVerifyTokenAuthRequest = {
         email,
-        challenge: "123456"
+        challenge: DEFAULT_CHALLENGE
     }
 
     const response = await Fetches.fetch(url, {
@@ -24,7 +27,7 @@ export default async function fetchTokenRequest(email: string): Promise<string> 
     const responseBody = <IVerifyTokenAuthResponse>await response.json();
 
     if (response.status !== 200 || responseBody.code !== 'ok') {
-        throw new Error("Failed to verify fetch token");
+        throw new Error("Failed to verify fixed challenge, no custom token recieved.");
     }
 
     return responseBody.customToken;
