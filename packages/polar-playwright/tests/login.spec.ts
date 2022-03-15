4c74e198888cd4ddeb413e05f5c3ce5b90b20f6a
Mohammed Atallah <mohammed@getpolarized.io>
2022-03-15 11:53:16 +0200

    add new browser context to login

--- packages/polar-playwright/tests/login.spec.ts
+++ packages/polar-playwright/tests/login.spec.ts
@@ -1,9 +1,13 @@
 import {test} from '@playwright/test';
 import {E2E} from "../lib/E2E"
 
-test('Login/Logout succeeds', async ({page, browserName}) => {
+test('Login/Logout succeeds', async ({browser, browserName}) => {
     // Default timeout of 30 seconds is not enough for this set of steps
     test.slow();
+
+    const context = await browser.newContext();
+
+    const page = await context.newPage();
 
     await page.goto(await E2E.Sessions.appURL());
 
